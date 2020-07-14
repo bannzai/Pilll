a8fe2a347ed094377d886579da75d00d15bd689f
bannzai <kingkong999yhirose@gmail.com>
2020-07-15 02:15:37 +0900

    :art: Remove floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -133,11 +133,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
