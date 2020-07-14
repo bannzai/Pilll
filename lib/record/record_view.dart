f33b819440107bb58c4bbbbd40f433e4c537b1e8
bannzai <kingkonog999yhirose@gmail.com>
2020-07-14 23:33:11 +0900

    :recycle: fix container style

--- lib/record/record_view.dart
+++ lib/record/record_view.dart
@@ -3,25 +3,16 @@ import 'package:flutter/material.dart';
 class RecordView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Grid List';
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: GridView.count(
-          crossAxisCount: 7,
-          children: List.generate(28, (index) {
-            return Center(
-              child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-            );
-          }),
-        ),
-      ),
+    return GridView.count(
+      crossAxisCount: 7,
+      children: List.generate(28, (index) {
+        return Center(
+          child: Text(
+            'Item $index',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        );
+      }),
     );
   }
 }
