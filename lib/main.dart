6a7ce614359b2918ce73a86acf0a7bcbc265f4e4
bannzai <kingkong999yhirose@gmail.com>
2020-07-24 23:04:29 +0900

    :recycle: switching initial screen with shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:Pilll/color.dart';
+import 'package:Pilll/initial_setting/initial_setting_1.dart';
 import 'package:Pilll/record/pill_sheet.dart';
 import 'package:Pilll/record/record_taken_information.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
@@ -47,63 +49,80 @@ class _MyHomePageState extends State<MyHomePage>
     setState(() {});
   }
 
+  Future<bool> getIsDidEndInitialSettingKey() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getBool("isDidEndInitialSettingKey");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        backgroundColor: PilllColors.background,
-        appBar: AppBar(
-          title: Text('Pilll'),
-          backgroundColor: PilllColors.primary,
-        ),
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            border:
-                Border(top: BorderSide(width: 1, color: PilllColors.border)),
-          ),
-          child: Ink(
-            color: PilllColors.bottomBar,
-            child: SafeArea(
-              child: TabBar(
+    return FutureBuilder(
+        future: getIsDidEndInitialSettingKey(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (!snapshot.hasData) {
+            return InitialSetting1();
+          }
+          if (!snapshot.data) {
+            return InitialSetting1();
+          }
+          return DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              backgroundColor: PilllColors.background,
+              appBar: AppBar(
+                title: Text('Pilll'),
+                backgroundColor: PilllColors.primary,
+              ),
+              bottomNavigationBar: Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                      top: BorderSide(width: 1, color: PilllColors.border)),
+                ),
+                child: Ink(
+                  color: PilllColors.bottomBar,
+                  child: SafeArea(
+                    child: TabBar(
+                      controller: _tabController,
+                      labelColor: PilllColors.primary,
+                      indicatorColor: Colors.transparent,
+                      unselectedLabelColor: PilllColors.plainText,
+                      tabs: <Tab>[
+                        Tab(
+                            text: "ピル",
+                            icon: SvgPicture.asset("images/tab_icon_pill.svg",
+                                color: _tabController.index == 0
+                                    ? PilllColors.primary
+                                    : PilllColors.plainText)),
+                        Tab(
+                            text: "2020/07",
+                            icon: SvgPicture.asset(
+                                "images/tab_icon_calendar.svg",
+                                color: _tabController.index == 1
+                                    ? PilllColors.primary
+                                    : PilllColors.plainText)),
+                        Tab(
+                            text: "設定",
+                            icon: SvgPicture.asset(
+                                "images/tab_icon_setting.svg",
+                                color: _tabController.index == 2
+                                    ? PilllColors.primary
+                                    : PilllColors.plainText)),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              body: TabBarView(
                 controller: _tabController,
-                labelColor: PilllColors.primary,
-                indicatorColor: Colors.transparent,
-                unselectedLabelColor: PilllColors.plainText,
-                tabs: <Tab>[
-                  Tab(
-                      text: "ピル",
-                      icon: SvgPicture.asset("images/tab_icon_pill.svg",
-                          color: _tabController.index == 0
-                              ? PilllColors.primary
-                              : PilllColors.plainText)),
-                  Tab(
-                      text: "2020/07",
-                      icon: SvgPicture.asset("images/tab_icon_calendar.svg",
-                          color: _tabController.index == 1
-                              ? PilllColors.primary
-                              : PilllColors.plainText)),
-                  Tab(
-                      text: "設定",
-                      icon: SvgPicture.asset("images/tab_icon_setting.svg",
-                          color: _tabController.index == 2
-                              ? PilllColors.primary
-                              : PilllColors.plainText)),
+                children: <Widget>[
+                  InitialSetting1(),
+                  _recordView(),
+                  _recordView(),
                 ],
               ),
             ),
-          ),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: <Widget>[
-            _recordView(),
-            _recordView(),
-            _recordView(),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 
   Center _recordView() {
