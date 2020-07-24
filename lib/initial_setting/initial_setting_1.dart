b549ce7e4a47bcf49e2e3bceb15d5e7d1210912c
bannzai <kingkonog999yhirose@gmail.com>
2020-07-25 00:22:31 +0900

    :art: fix vertical layouts

--- lib/initial_setting/initial_setting_1.dart
+++ lib/initial_setting/initial_setting_1.dart
@@ -20,43 +20,152 @@ class InitialSetting1 extends StatelessWidget {
           child: Column(
             children: <Widget>[
               Text("飲んでいるピルのタイプはどれ？", style: TextStyles.title),
-              Container(
-                width: 295,
-                height: 143,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1, color: PilllColors.plainText),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.check, color: PilllColors.disable, size: 13),
-                        SizedBox(width: 8),
-                        Text("21錠タイプ", style: TextStyles.subTitle),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        SvgPicture.asset("images/pillsheet_21.svg"),
-                        Column(
-                          children: <Widget>[
-                            Text("・トリキュラー21", style: TextStyles.list),
-                            Text("・トリキュラー21", style: TextStyles.list),
-                            Text("・トリキュラー21", style: TextStyles.list),
-                            Text("・トリキュラー21", style: TextStyles.list),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+              _pillSheet21(),
+              _pillSheet28_4(),
+              _pillSheet28_7()
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Container _pillSheet28_7() {
+    return Container(
+      width: 295,
+      height: 143,
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: PilllColors.plainText),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(width: 16),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.check, color: PilllColors.disable, size: 13),
+                  SizedBox(width: 8),
+                  Text("28錠タイプ(7錠偽薬)", style: TextStyles.subTitle),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SvgPicture.asset("images/pillsheet_28_7.svg"),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("・トリキュラー28", style: TextStyles.list),
+                      Text("・マーベロン28", style: TextStyles.list),
+                      Text("・アンジュ28", style: TextStyles.list),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _pillSheet28_4() {
+    return Container(
+      width: 295,
+      height: 143,
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: PilllColors.plainText),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(width: 16),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.check, color: PilllColors.disable, size: 13),
+                  SizedBox(width: 8),
+                  Text("28錠タイプ(4錠偽薬)", style: TextStyles.subTitle),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SvgPicture.asset("images/pillsheet_28_4.svg"),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("・ヤーズなど", style: TextStyles.list),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _pillSheet21() {
+    return Container(
+      width: 295,
+      height: 143,
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: PilllColors.plainText),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(width: 16),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.check, color: PilllColors.disable, size: 13),
+                  SizedBox(width: 8),
+                  Text("21錠タイプ", style: TextStyles.subTitle),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SvgPicture.asset("images/pillsheet_21.svg"),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("・トリキュラー21", style: TextStyles.list),
+                      Text("・トリキュラー21", style: TextStyles.list),
+                      Text("・トリキュラー21", style: TextStyles.list),
+                      Text("・トリキュラー21", style: TextStyles.list),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
