6a73618d325879ea5856de5b1e8ccc30d3a44d23
bannzai <kingkonog999yhirose@gmail.com>
2020-07-25 04:55:21 +0900

    :sparkels: add widget for PillSheet

---
+++ lib/initial_setting/pill_sheet.dart
@@ -0,0 +1,61 @@
+import 'package:Pilll/color.dart';
+import 'package:Pilll/model/pill_sheet_type.dart';
+import 'package:Pilll/text_style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PillSheet extends StatelessWidget {
+  PillSheetType pillSheetType;
+  PillSheet({
+    Key key,
+    this.pillSheetType,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 295,
+      height: 143,
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: PilllColors.plainText),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(width: 16),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.check, color: PilllColors.disable, size: 13),
+                  SizedBox(width: 8),
+                  Text(this.pillSheetType.name, style: TextStyles.subTitle),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  this.pillSheetType.image,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: this
+                        .pillSheetType
+                        .examples
+                        .map((e) => Text("$e", style: TextStyles.list))
+                        .toList(),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
