aaf69ee44a373b63744b60f708732e77f66b8a9d
bannzai <kingkong999yhirose@gmail.com>
2020-07-24 23:03:26 +0900

    :memo: add text style dart

---
+++ lib/text_style.dart
@@ -0,0 +1,23 @@
+import 'package:Pilll/color.dart';
+import 'package:Pilll/font.dart';
+import 'package:flutter/widgets.dart';
+
+class TextStyles {
+  static final TextStyle title = TextStyle(
+      fontFamily: PilllFontFamily.japanese,
+      fontWeight: FontWeight.w600,
+      fontSize: 18,
+      color: PilllColors.blackText);
+  static final TextStyle subTitle = TextStyle(
+    fontFamily: PilllFontFamily.japanese,
+    fontWeight: FontWeight.normal,
+    fontSize: 17,
+    color: PilllColors.plainText,
+  );
+  static final TextStyle list = TextStyle(
+    fontFamily: PilllFontFamily.japanese,
+    fontWeight: FontWeight.normal,
+    fontSize: 12,
+    color: PilllColors.blackText,
+  );
+}
