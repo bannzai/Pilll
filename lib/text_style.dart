3ec371de3ddbdffd08ea40fa9e24acd509dc7d15
bannzai <kingkong999yhirose@gmail.com>
2020-07-27 00:01:19 +0900

    :recycle: fix text styles

--- lib/text_style.dart
+++ lib/text_style.dart
@@ -4,12 +4,31 @@ import 'package:flutter/widgets.dart';
 
 class TextStyles {
   static final TextStyle title = TextStyle(
-      fontFamily: PilllFontFamily.japanese,
-      fontWeight: FontWeight.w600,
-      fontSize: 18,
-      color: PilllColors.blackText);
+    fontFamily: PilllFontFamily.japanese,
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    color: PilllColors.blackText,
+  );
   static final TextStyle subTitle = TextStyle(
     fontFamily: PilllFontFamily.japanese,
+    fontWeight: FontWeight.normal,
+    fontSize: 17,
+    color: PilllColors.plainText,
+  );
+  static final TextStyle question = TextStyle(
+    fontFamily: PilllFontFamily.japanese,
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    color: PilllColors.blackText,
+  );
+  static final TextStyle assisting = TextStyle(
+    fontFamily: PilllFontFamily.japanese,
+    fontWeight: FontWeight.w300,
+    fontSize: 14,
+    color: PilllColors.plainText,
+  );
+  static final TextStyle input = TextStyle(
+    fontFamily: PilllFontFamily.number,
     fontWeight: FontWeight.normal,
     fontSize: 17,
     color: PilllColors.plainText,
