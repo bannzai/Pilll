cd086ac1f9d9dd8944249759aff29b9d5b5ee9b2
bannzai <kingkong999yhirose@gmail.com>
2020-07-29 19:15:27 +0900

    :sparakles: add font type

--- lib/theme/font.dart
+++ lib/theme/font.dart
@@ -1,3 +1,4 @@
+import 'package:Pilll/theme/color.dart';
 import 'package:flutter/material.dart';
 
 class FontFamily {
@@ -8,9 +9,48 @@ class FontFamily {
 }
 
 class FontSize {
-  static final int xHuge = 40;
-  static final int huge = 32;
-  static final int large = 17;
-  static final int normal = 14;
-  static final int small = 12;
+  static final double xHuge = 40;
+  static final double huge = 32;
+  static final double large = 17;
+  static final double sLarge = 16;
+  static final double normal = 14;
+  static final double small = 12;
+}
+
+class FontType {
+  static final TextStyle largeNumber = TextStyle(
+    fontFamily: FontFamily.number,
+    fontWeight: FontWeight.w300,
+    fontSize: FontSize.huge,
+  );
+  static final TextStyle title = TextStyle(
+    fontFamily: FontFamily.japanese,
+    fontWeight: FontWeight.w300,
+    fontSize: FontSize.large,
+  );
+  static final TextStyle done = TextStyle(
+    fontFamily: FontFamily.japanese,
+    fontWeight: FontWeight.w600,
+    fontSize: FontSize.sLarge,
+  );
+  static final TextStyle assisting = TextStyle(
+    fontFamily: FontFamily.japanese,
+    fontWeight: FontWeight.w300,
+    fontSize: FontSize.normal,
+  );
+  static final TextStyle assistingBold = TextStyle(
+    fontFamily: FontFamily.japanese,
+    fontWeight: FontWeight.w600,
+    fontSize: FontSize.normal,
+  );
+  static final TextStyle inputNumber = TextStyle(
+    fontFamily: FontFamily.number,
+    fontWeight: FontWeight.w300,
+    fontSize: FontSize.large,
+  );
+  static final TextStyle description = TextStyle(
+    fontFamily: FontFamily.japanese,
+    fontWeight: FontWeight.w300,
+    fontSize: FontSize.small,
+  );
 }
