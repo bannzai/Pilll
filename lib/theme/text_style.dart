dd8b2cbe634a3eded03878a60714be3a3465c3d6
bannzai <kingkong999yhirose@gmail.com>
2020-07-29 19:49:56 +0900

    :art: add light gray

--- lib/theme/text_style.dart
+++ lib/theme/text_style.dart
@@ -4,15 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class TextColor {
+  static Color get standard => black;
   static final Color black = Colors.black;
   static final Color white = Colors.white;
   static final Color gray = HexColor.fromHex("7E7E7E");
+  static final Color lightGray = HexColor.fromHex("CDCFD1");
 }
 
 class TextColorStyle {
+  static TextStyle get standard => black;
   static final TextStyle black = TextStyle(color: TextColor.black);
   static final TextStyle white = TextStyle(color: TextColor.white);
   static final TextStyle gray = TextStyle(color: TextColor.gray);
+  static final TextStyle lightGray = TextStyle(color: TextColor.lightGray);
 }
 
 class TextStyles {
