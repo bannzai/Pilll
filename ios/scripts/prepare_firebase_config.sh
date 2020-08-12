4b2ed8423e09dec468c8e8da80d5b6c6366ca2d5
bannzai <kingkonog999yhirose@gmail.com>
2020-08-13 00:27:00 +0900

    :memo: add scripts of replacing GoogleService-Info.plist

---
+++ ios/scripts/prepare_firebase_config.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+if [[ $CONFIGURATION == *"Development"* ]]; then
+    cp $PRODUCT_NAME/Firebase/GoogleService-Info-Development.plist $PRODUCT_NAME/GoogleService-Info.plist
+if [[ $CONFIGURATION == *"Production"* ]]; then
+    cp $PRODUCT_NAME/Firebase/GoogleService-Info-Production.plist $PRODUCT_NAME/GoogleService-Info.plist
+elif [[ $CONFIGURATION == "Release" ]]; then
+    echo "Might be CI archive"
+else
+    echo "configuration didn't match to Development, Production"
+    echo $CONFIGURATION
+    exit 1
+fi
+
