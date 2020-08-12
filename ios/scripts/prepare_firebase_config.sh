f07acaa601309ecd8e70b5a5cf59004e1f318c31
bannzai <kingkonog999yhirose@gmail.com>
2020-08-13 00:31:59 +0900

    :memo: add configurations type for Debug and Release prefixes

--- ios/scripts/prepare_firebase_config.sh
+++ ios/scripts/prepare_firebase_config.sh
@@ -2,7 +2,11 @@
 
 if [[ $CONFIGURATION == *"Development" ]]; then
     cp $PRODUCT_NAME/Firebase/GoogleService-Info-Development.plist $PRODUCT_NAME/GoogleService-Info.plist
+elif [[ $CONFIGURATION == "Debug"* ]]; then
+    cp $PRODUCT_NAME/Firebase/GoogleService-Info-Development.plist $PRODUCT_NAME/GoogleService-Info.plist
 elif [[ $CONFIGURATION == *"Production" ]]; then
+    cp $PRODUCT_NAME/Firebase/GoogleService-Info-Production.plist $PRODUCT_NAME/GoogleService-Info.plist
+elif [[ $CONFIGURATION == "Release"* ]]; then
     cp $PRODUCT_NAME/Firebase/GoogleService-Info-Production.plist $PRODUCT_NAME/GoogleService-Info.plist
 else
     echo "configuration didn't match to Development, Production"
