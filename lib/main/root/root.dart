9a2ac2ade14e83b320bf6e4ba59b4c4a79f8e24e
bannzai <kingkong999yhirose@gmail.com>
2020-08-26 16:24:07 +0900

    :sparkles: fix using scaffold

--- lib/main/root/root.dart
+++ lib/main/root/root.dart
@@ -1,6 +1,9 @@
 import 'package:Pilll/main/application/router.dart';
+import 'package:Pilll/theme/color.dart';
+import 'package:Pilll/theme/text_color.dart';
 import 'package:Pilll/util/shared_preference/keys.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Root extends StatefulWidget {
@@ -25,7 +28,13 @@ class RootState extends State<Root> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Implement
-    return Container();
+    return Scaffold(
+      backgroundColor: PilllColors.background,
+      appBar: AppBar(
+        title: Text('Pilll'),
+        backgroundColor: PilllColors.primary,
+      ),
+      body: Container(),
+    );
   }
 }
