761dd189e203a877fa5c7f0ba224d790bd1f9667
bannzai <kingkong999yhirose@gmail.com>
2020-08-26 16:24:32 +0900

    :recycle: fix test compile error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -17,7 +17,7 @@ void main() {
   });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(App());
 
     // Verify that our counter starts at 0.
     // expect(find.text('0'), findsOneWidget);
