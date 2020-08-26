4c5f8ecabe45dce2b389054236c78692d213b4ee
bannzai <kingkong999yhirose@gmail.com>
2020-08-26 14:13:00 +0900

    :memo: remove title

--- lib/main.dart
+++ lib/main.dart
@@ -53,15 +53,14 @@ class MyApp extends StatelessWidget {
             textTheme: ButtonTextTheme.primary,
           ),
         ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
+        home: MyHomePage(),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
