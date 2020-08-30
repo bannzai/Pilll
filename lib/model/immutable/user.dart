fc4cceff6cde755c37fe956dc4d6ac496d118a58
bannzai <kingkonog999yhirose@gmail.com>
2020-08-31 06:31:12 +0900

    :recycle: remove unused import

--- lib/model/immutable/user.dart
+++ lib/model/immutable/user.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class UserNotFound implements Exception {
   toString() {
