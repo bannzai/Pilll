bb4df7611b4cf2dbd821b04f8c80fd72f4182262
bannzai <kingkong999yhirose@gmail.com>
2020-09-07 10:59:27 +0900

    :recycle: Replace to use AuthUser

--- lib/initial_setting/initial_setting_1.dart
+++ lib/initial_setting/initial_setting_1.dart
@@ -1,6 +1,6 @@
 import 'package:Pilll/initial_setting/initial_setting_2.dart';
 import 'package:Pilll/main/components/pill_sheet_type_select_page.dart';
-import 'package:Pilll/initial_setting/initial_setting.dart';
+import 'package:Pilll/model/auth_user.dart';
 import 'package:Pilll/model/setting.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,7 +15,7 @@ class InitialSetting1 extends StatefulWidget {
 class _InitialSetting1State extends State<InitialSetting1> {
   @override
   Widget build(BuildContext context) {
-    Setting model = Provider.of<Setting>(context, listen: false);
+    Setting model = Provider.of<AuthUser>(context).user.setting;
     return PillSheetTypeSelectPage(
       callback: (type) {
         setState(() {
