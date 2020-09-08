0e15ed2639ad8be0225eb7a69cb084fd9c149955
bannzai <kingkonog999yhirose@gmail.com>
2020-09-08 15:31:16 +0900

    :art: WIP implement calendar

---
+++ lib/main/calendar/calendar.dart
@@ -0,0 +1,58 @@
+import 'package:Pilll/main/record/weekday_badge.dart';
+import 'package:Pilll/model/weekday.dart';
+import 'package:flutter/material.dart';
+
+abstract class CalendarConstants {
+  static final int weekdayCount = 7;
+}
+
+class Calendar extends StatelessWidget {
+  final DateTime firstDayOfMonth;
+
+  const Calendar({Key key, this.firstDayOfMonth}) : super(key: key);
+
+  int _lastDay() {
+    return DateTime(firstDayOfMonth.year, firstDayOfMonth.month + 1, 0).day;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: List.generate(
+              CalendarConstants.weekdayCount,
+              (index) => Expanded(
+                    child: WeekdayBadge(
+                      weekday: Weekday.values[index],
+                    ),
+                  )),
+        ),
+        Divider(
+          height: 1,
+        ),
+        ...List.generate((_lastDay() / 7).round(), (line) {
+          return Row(
+            children: List.generate(CalendarConstants.weekdayCount, (weekday) {
+              return Expanded(
+                child: _element(Weekday.values[weekday],
+                    line * CalendarConstants.weekdayCount + weekday + 1),
+              );
+            }),
+          );
+        }),
+      ],
+    );
+  }
+
+  Widget _element(Weekday weekday, int day) {
+    return Text(
+      "$day",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: WeekdayFunctions.weekdayColor(weekday),
+      ),
+    );
+  }
+}
