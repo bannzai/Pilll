1af80139c2bb858a7520115c444112dfa514731d
bannzai <kingkong999yhirose@gmail.com>
2020-09-16 10:19:20 +0900

    :sparkles: fix dateRangeOfLine
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

--- test/calendar/calculator_test.dart
+++ test/calendar/calculator_test.dart
@@ -1,4 +1,5 @@
 import 'package:Pilll/main/calendar/calculator.dart';
+import 'package:Pilll/main/calendar/date_range.dart';
 import 'package:Pilll/model/weekday.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -15,7 +16,6 @@ void main() {
           DateTime.parse("2020-08-30"));
       expect(calculator.dateTimeForPreviousMonthTile(Weekday.Monday),
           DateTime.parse("2020-08-31"));
-      ;
     });
     test("#lastDay", () {
       expect(calculator.lastDay(), DateTime.parse("2020-09-30").day);
@@ -44,6 +44,43 @@ void main() {
       expect(calculator.numberOfLine(26), 4);
       expect(calculator.numberOfLine(27), 5);
       expect(calculator.numberOfLine(30), 5);
+    });
+    test("#dateRangeOfLine -> begin", () {
+      expect(
+        calculator.dateRangeOfLine(1),
+        DateRange(
+          DateTime.parse("2020-09-01"),
+          DateTime.parse("2020-09-05"),
+        ),
+      );
+      expect(
+        calculator.dateRangeOfLine(2),
+        DateRange(
+          DateTime.parse("2020-09-06"),
+          DateTime.parse("2020-09-12"),
+        ),
+      );
+      expect(
+        calculator.dateRangeOfLine(3),
+        DateRange(
+          DateTime.parse("2020-09-13"),
+          DateTime.parse("2020-09-19"),
+        ),
+      );
+      expect(
+        calculator.dateRangeOfLine(4),
+        DateRange(
+          DateTime.parse("2020-09-20"),
+          DateTime.parse("2020-09-26"),
+        ),
+      );
+      expect(
+        calculator.dateRangeOfLine(5),
+        DateRange(
+          DateTime.parse("2020-09-27"),
+          DateTime.parse("2020-09-30"),
+        ),
+      );
     });
   });
 }
