118d5e0af23bf4f7ff2fcf704c635acfc0a02e60
bannzai <kingkong999yhirose@gmail.com>
2020-09-18 08:12:10 +0900

    :sparkles: add widget tests

--- test/calendar/widget/calendar_test.dart
+++ test/calendar/widget/calendar_test.dart
@@ -11,17 +11,44 @@ void main() {
     WidgetsBinding.instance.renderView.configuration =
         new TestViewConfiguration(size: const Size(375.0, 667.0));
   });
-  testWidgets('Calendar tests', (WidgetTester tester) async {
-    var now = DateTime(2020, 09, 14);
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Calendar(calculator: Calculator(now), bandModels: [
-          CalendarNextPillSheetBandModel(
-              DateTime(2020, 09, 15), DateTime(2020, 09, 18)),
-        ]),
-      ),
-    );
-
-    expect(find.text("新しいシート開始 ▶︎"), findsOneWidget);
+  group("Appearance Next Sheet Label", () {
+    testWidgets('when showing 新しいシート開始 ▶︎', (WidgetTester tester) async {
+      var now = DateTime(2020, 09, 14);
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Calendar(calculator: Calculator(now), bandModels: [
+            CalendarNextPillSheetBandModel(
+                DateTime(2020, 09, 15), DateTime(2020, 09, 18)),
+          ]),
+        ),
+      );
+      expect(find.text("新しいシート開始 ▶︎"), findsOneWidget);
+    });
+    testWidgets('when showing new sheet label to next month',
+        (WidgetTester tester) async {
+      var now = DateTime(2020, 09, 14);
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Calendar(calculator: Calculator(now), bandModels: [
+            CalendarNextPillSheetBandModel(
+                DateTime(2020, 10, 15), DateTime(2020, 10, 18)),
+          ]),
+        ),
+      );
+      expect(find.text("新しいシート開始 ▶︎"), isNot(findsOneWidget));
+    });
+    testWidgets('when showing new sheet label to before month',
+        (WidgetTester tester) async {
+      var now = DateTime(2020, 09, 14);
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Calendar(calculator: Calculator(now), bandModels: [
+            CalendarNextPillSheetBandModel(
+                DateTime(2020, 08, 15), DateTime(2020, 08, 18)),
+          ]),
+        ),
+      );
+      expect(find.text("新しいシート開始 ▶︎"), isNot(findsOneWidget));
+    });
   });
 }
