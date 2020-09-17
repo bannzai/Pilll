4493c444f48e4b10e8f448261e8cf53c928ed732
bannzai <kingkong999yhirose@gmail.com>
2020-09-17 18:50:34 +0900

    :recycle: remove unnecessary user credential

--- lib/model/auth_user.dart
+++ lib/model/auth_user.dart
@@ -1,15 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:Pilll/model/user.dart' as pilllUser;
 
 class AuthUser extends ChangeNotifier {
-  UserCredential _userCredential;
-  UserCredential get userCredential => _userCredential;
-  set userCredential(UserCredential value) {
-    _userCredential = value;
-    notifyListeners();
-  }
-
   pilllUser.User _user;
   pilllUser.User get user => _user;
   set user(pilllUser.User value) {
