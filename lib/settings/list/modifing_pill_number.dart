916b2db5d4849dac5a1d97ff490318a462bf93da
bannzai <kingkonog999yhirose@gmail.com>
2020-09-19 16:05:31 +0900

    :sparkles: add page of modifing pill number

---
+++ lib/settings/list/modifing_pill_number.dart
@@ -0,0 +1,48 @@
+import 'package:Pilll/main/record/pill_sheet.dart';
+import 'package:Pilll/theme/color.dart';
+import 'package:Pilll/theme/font.dart';
+import 'package:Pilll/theme/text_color.dart';
+import 'package:Pilll/util/formatter/date_time_formatter.dart';
+import 'package:flutter/material.dart';
+
+class ModifingPillNumber extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PilllColors.background,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Text(
+          "番号変更",
+          style: TextStyle(color: TextColor.black),
+        ),
+        backgroundColor: PilllColors.background,
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 20),
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width - 32),
+              child: Text("今日${_today()}に飲む・飲んだピル番号をタップ",
+                  style: FontType.sBigTitle.merge(TextColorStyle.main)),
+            ),
+            SizedBox(height: 56),
+            Center(
+              child: PillSheet(
+                isHideWeekdayLine: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _today() => DateTimeFormatter.slashYearAndMonthAndDay(DateTime.now());
+}
