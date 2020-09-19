a95b34214bd5ad8d89aec08866c8d84b2608afe3
bannzai <kingkonog999yhirose@gmail.com>
2020-09-19 21:45:36 +0900

    :recycle: remove unnecessary import

--- lib/main/components/pill/pill_mark.dart
+++ lib/main/components/pill/pill_mark.dart
@@ -1,10 +1,7 @@
-import 'package:Pilll/main/components/pill/pill_mark_model.dart';
-import 'package:Pilll/main/components/pill/pill_sheet_model.dart';
 import 'package:Pilll/theme/color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:provider/provider.dart';
 
 enum PillMarkType {
   normal,
