b32e4156df7a896262c39839ff2a71459b9f328d
bannzai <kingkonog999yhirose@gmail.com>
2020-09-19 16:50:17 +0900

    :recycle: Refactor to stateless

--- lib/main/components/pill/pill_mark.dart
+++ lib/main/components/pill/pill_mark.dart
@@ -1,45 +1,76 @@
 import 'package:Pilll/main/components/pill/pill_mark_model.dart';
 import 'package:Pilll/main/components/pill/pill_sheet_model.dart';
+import 'package:Pilll/theme/color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
-enum PillMarkState {
-  none,
+enum PillMarkType {
+  normal,
   notTaken,
   todo,
   done,
 }
 
+extension PillMarkTypeFunctions on PillMarkType {
+  SvgPicture image() {
+    switch (this) {
+      case PillMarkType.normal:
+        return null;
+      case PillMarkType.notTaken:
+        return null;
+      case PillMarkType.todo:
+        return null;
+      case PillMarkType.done:
+        return SvgPicture.asset("images/check_mark.svg");
+      default:
+        assert(false);
+        return null;
+    }
+  }
+
+  @override
+  Color color() {
+    switch (this) {
+      case PillMarkType.normal:
+        return PilllColors.potti;
+      case PillMarkType.notTaken:
+        return PilllColors.blank;
+      case PillMarkType.todo:
+        return PilllColors.potti;
+      case PillMarkType.done:
+        return PilllColors.lightGray;
+      default:
+        assert(false);
+        return null;
+    }
+  }
+}
+
 class PillMark extends StatelessWidget {
-  final int number;
-  int get index => number - 1;
+  final PillMarkType type;
+  final VoidCallback tapped;
   const PillMark({
     Key key,
-    @required this.number,
+    @required this.type,
+    this.tapped,
   }) : super(key: key);
-
-  PillMarkModel model(BuildContext context) {
-    return Provider.of<PillSheetModel>(context).marks[index];
-  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: Container(
-        width: 20,
-        height: 20,
-        child: Center(
-          child: model(context).image(),
+        child: Container(
+          width: 20,
+          height: 20,
+          child: Center(
+            child: type.image(),
+          ),
+          decoration: BoxDecoration(
+            color: type.color(),
+            shape: BoxShape.circle,
+          ),
         ),
-        decoration: BoxDecoration(
-          color: model(context).color(),
-          shape: BoxShape.circle,
-        ),
-      ),
-      onTap: () {
-        Provider.of<PillSheetModel>(context, listen: false).number = number;
-      },
-    );
+        onTap: tapped);
   }
 }
