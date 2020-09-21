62ba62efb4ad8a79351581f8ceb35138fe6cfc33
bannzai <kingkonog999yhirose@gmail.com>
2020-09-21 14:59:44 +0900

    :recycle: Repalce to freezed package

--- lib/model/setting.dart
+++ lib/model/setting.dart
@@ -1,80 +1,37 @@
 import 'package:Pilll/model/pill_sheet_type.dart';
 import 'package:Pilll/model/user.dart';
-import 'package:Pilll/util/shared_preference/keys.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
 
-class SettingFirestoreFieldyKeys {
-  static final String beginingMenstruationFromAfterFakePeriod =
-      "beginingMenstruationFromAfterFakePeriod";
-  static final String durationMenstruation = "durationMenstruation";
-  static final String reminderTime = "reminderTime";
-  static final String reminderTimeHour = "hour";
-  static final String reminderTimeMinute = "minute";
-  static final String isOnReminder = "isOnReminder";
-  static final String pillSheetTypeRawPath = "pillSheetTypeRawPath";
+part 'setting.freezed.dart';
+part 'setting.g.dart';
+
+@freezed
+abstract class ReminderTime with _$ReminderTime {
+  factory ReminderTime({
+    @required int hour,
+    @required int minute,
+  }) = _Reminder;
+
+  factory ReminderTime.fromJson(Map<String, dynamic> json) =>
+      _$ReminderTimeFromJson(json);
+  Map<String, dynamic> toJson() => _$_$_ReminderToJson(this);
 }
 
-class Setting extends ChangeNotifier {
-  PillSheetType pillSheetType;
-  int fromMenstruation;
-  int durationMenstruation;
-  int reminderHour;
-  int reminderMinute;
-  bool isOnReminder;
+@freezed
+abstract class Setting with _$Setting {
+  factory Setting({
+    @nullable @required PillSheetType pillSheetType,
+    @nullable @required int fromMenstruation,
+    @nullable @required int durationMenstruation,
+    @nullable @required ReminderTime reminderTime,
+    @Default(false) bool isOnReminder,
+  }) = _Setting;
 
-  factory Setting(Map<String, dynamic> firestoreRowData) {
-    assert(firestoreRowData != null);
-    String pillSheetTypeRawPath =
-        firestoreRowData[SettingFirestoreFieldyKeys.pillSheetTypeRawPath];
-    return Setting._(
-      pillSheetType: pillSheetTypeRawPath == null
-          ? null
-          : PillSheetTypeFunctions.fromName(pillSheetTypeRawPath),
-      fromMenstruation: firestoreRowData[
-          SettingFirestoreFieldyKeys.beginingMenstruationFromAfterFakePeriod],
-      durationMenstruation:
-          firestoreRowData[SettingFirestoreFieldyKeys.durationMenstruation],
-      reminderHour: firestoreRowData[SettingFirestoreFieldyKeys.reminderTime]
-          [SettingFirestoreFieldyKeys.reminderTimeHour],
-      reminderMinute: firestoreRowData[SettingFirestoreFieldyKeys.reminderTime]
-          [SettingFirestoreFieldyKeys.reminderTimeMinute],
-      isOnReminder:
-          firestoreRowData[SettingFirestoreFieldyKeys.isOnReminder] ?? false,
-    );
-  }
-  Setting._({
-    @required this.pillSheetType,
-    @required this.fromMenstruation,
-    @required this.durationMenstruation,
-    @required this.reminderHour,
-    @required this.reminderMinute,
-    @required this.isOnReminder,
-  }) : assert(isOnReminder != null);
-
-  Map<String, dynamic> get firestoreRowData {
-    var rowData = Map<String, dynamic>();
-    if (fromMenstruation != null)
-      rowData[SettingFirestoreFieldyKeys
-          .beginingMenstruationFromAfterFakePeriod] = fromMenstruation;
-    if (durationMenstruation != null)
-      rowData[SettingFirestoreFieldyKeys.durationMenstruation] =
-          durationMenstruation;
-    if (reminderHour != null || reminderMinute != null)
-      rowData[SettingFirestoreFieldyKeys.reminderTime] = {};
-    if (reminderHour != null)
-      rowData[SettingFirestoreFieldyKeys.reminderTime]
-          [SettingFirestoreFieldyKeys.reminderTimeHour] = reminderHour;
-    if (reminderMinute != null)
-      rowData[SettingFirestoreFieldyKeys.reminderTime]
-          [SettingFirestoreFieldyKeys.reminderTimeMinute] = reminderMinute;
-    rowData[SettingFirestoreFieldyKeys.isOnReminder] = isOnReminder ?? false;
-    if (pillSheetType != null)
-      rowData[SettingFirestoreFieldyKeys.pillSheetTypeRawPath] =
-          pillSheetType.name;
-    return rowData;
-  }
+  factory Setting.fromJson(Map<String, dynamic> json) =>
+      _$SettingFromJson(json);
+  Map<String, dynamic> toJson() => _$_$_SettingToJson(this);
 
   Future<void> save() {
     return User.fetch().then((value) {
@@ -83,22 +40,16 @@ class Setting extends ChangeNotifier {
           .doc(value.documentID)
           .set(
         {
-          UserFirestoreFieldKeys.settings: firestoreRowData,
+          UserFirestoreFieldKeys.settings: toJson(),
         },
         SetOptions(merge: true),
       );
     });
   }
 
-  Future<Setting> notifyWith(void update(Setting setting)) {
-    update(this);
-    notifyListeners();
-    return Future.value(this);
-  }
-
   DateTime reminderDateTime() {
     var t = DateTime.now().toLocal();
-    return DateTime(t.year, t.month, t.day, reminderHour, reminderMinute,
-        t.second, t.millisecond, t.microsecond);
+    return DateTime(t.year, t.month, t.day, reminderTime.hour,
+        reminderTime.minute, t.second, t.millisecond, t.microsecond);
   }
 }
