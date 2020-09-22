4f25277ff3b9e80f608c05b7062b0b1f635e39bd
bannzai <kingkong999yhirose@gmail.com>
2020-09-22 23:56:30 +0900

    :recycle: fix build PillSheetModel

--- lib/model/initial_setting.dart
+++ lib/model/initial_setting.dart
@@ -23,60 +23,33 @@ class InitialSettingModel extends ChangeNotifier {
   // User.Settings & User/{id}/PillSheet
   PillSheetType pillSheetType;
 
-  Map<String, dynamic> settingFirestoreRowData() {
-    var settings = Map<String, dynamic>();
-    if (fromMenstruation != null)
-      settings[SettingFirestoreFieldyKeys
-          .beginingMenstruationFromAfterFakePeriod] = fromMenstruation;
-    if (durationMenstruation != null)
-      settings[SettingFirestoreFieldyKeys.durationMenstruation] =
-          durationMenstruation;
-    if (reminderHour != null || reminderMinute != null)
-      settings[SettingFirestoreFieldyKeys.reminderTime] = {};
-    if (reminderHour != null)
-      settings[SettingFirestoreFieldyKeys.reminderTime]
-          [SettingFirestoreFieldyKeys.reminderTimeHour] = reminderHour;
-    if (reminderMinute != null)
-      settings[SettingFirestoreFieldyKeys.reminderTime]
-          [SettingFirestoreFieldyKeys.reminderTimeMinute] = reminderMinute;
-    settings[SettingFirestoreFieldyKeys.isOnReminder] = isOnReminder ?? false;
-    if (pillSheetType != null)
-      settings[SettingFirestoreFieldyKeys.pillSheetTypeRawPath] =
-          pillSheetType.name;
-    return settings;
-  }
-
-  Map<String, dynamic> userPillSheetRowData() {
-    var rowData = Map<String, dynamic>();
-    if (pillSheetType != null) {
-      rowData[PillSheetFirestoreFieldKeys.pillSheetTypeInfo] = {
-        PillSheetFirestoreFieldKeys.pillSheetTypeInfoRef:
-            pillSheetType.documentReference,
-        PillSheetFirestoreFieldKeys.pillSheetTypeInfoPillCount:
-            pillSheetType.totalCount,
-        PillSheetFirestoreFieldKeys.pillSheetTypeInfoDosingPeriod:
-            pillSheetType.dosingPeriod,
-      };
-      rowData[PillSheetFirestoreFieldKeys.creator] = {
-        PillSheetFirestoreFieldKeys.creatorReference:
-            user.User.user().documentReference(),
-      };
-      rowData[PillSheetFirestoreFieldKeys.beginingDate] = Timestamp.fromDate(
-          today().subtract(Duration(days: todayPillNumber - 1)));
-      // NOTE: when user selected taken pill number is 1, treat as user not yet take pill in current pillsheet.
-      rowData[PillSheetFirestoreFieldKeys.lastTakenDate] =
-          todayPillNumber == 1 ? null : Timestamp.fromDate(today());
-    }
-    return rowData;
-  }
-
   Setting buildSetting() => Setting(
         fromMenstruation: fromMenstruation,
         durationMenstruation: durationMenstruation,
         pillSheetTypeRawPath: pillSheetType.rawPath,
         reminderTime: ReminderTime(hour: reminderHour, minute: reminderMinute),
       );
-  PillSheetModel buildPillSheet() => PillSheetModel(userPillSheetRowData());
+  PillSheetModel buildPillSheet() => PillSheetModel(
+        beginingDate: _beginingDate(),
+        lastTakenDate: _lastTakenDate(),
+        typeInfo: _typeInfo(),
+      );
+
+  DateTime _beginingDate() {
+    return today().subtract(Duration(days: todayPillNumber - 1));
+  }
+
+  DateTime _lastTakenDate() {
+    return todayPillNumber == 1 ? null : today();
+  }
+
+  PillSheetTypeInfo _typeInfo() {
+    return PillSheetTypeInfo(
+      pillSheetTypeReferencePath: pillSheetType.rawPath,
+      dosingPeriod: pillSheetType.dosingPeriod,
+      totalCount: pillSheetType.totalCount,
+    );
+  }
 
   Future<InitialSettingModel> notifyWith(
       void update(InitialSettingModel model)) {
