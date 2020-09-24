a5c96ceea13d75d8ec157da188f76831f5afe438
bannzai <kingkonog999yhirose@gmail.com>
2020-09-24 16:19:21 +0900

    :sparkles: add inject today method

--- lib/util/today.dart
+++ lib/util/today.dart
@@ -1,3 +1,14 @@
+DateTime Function() _fakeToday;
+void injectToday(DateTime Function() closure) {
+  if (!bool.fromEnvironment('testing_mode', defaultValue: false)) {
+    assert(false, "inject today should use testing mode");
+  }
+  _fakeToday = closure;
+}
+
 DateTime today() {
+  if (bool.fromEnvironment('testing_mode', defaultValue: false)) {
+    return _fakeToday();
+  }
   return DateTime.now();
 }
