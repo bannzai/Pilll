6648c065ad2f3500d11499baff3286a5e9c41d75
bannzai <kingkong999yhirose@gmail.com>
2020-10-01 23:58:19 +0900

    :sparekls: add page for calendar list

---
+++ lib/main/calendar/calendar_list_page.dart
@@ -0,0 +1,62 @@
+import 'package:Pilll/main/calendar/calculator.dart';
+import 'package:Pilll/main/calendar/calendar.dart';
+import 'package:Pilll/theme/color.dart';
+import 'package:Pilll/theme/font.dart';
+import 'package:Pilll/theme/text_color.dart';
+import 'package:Pilll/util/formatter/date_time_formatter.dart';
+import 'package:Pilll/util/today.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CalendarListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        backgroundColor: PilllColors.background,
+      ),
+      backgroundColor: PilllColors.background,
+      body: ListView(
+        children: _components(context),
+      ),
+    );
+  }
+
+  List<Widget> _components(BuildContext context) {
+    var date = today();
+    var previousMonthDate = DateTime(date.year, date.month - 1, 1);
+    return [date, previousMonthDate].map((d) {
+      return Column(
+        children: <Widget>[
+          _header(context, d),
+          _calendar(context, d),
+        ],
+      );
+    }).toList();
+  }
+
+  Widget _header(BuildContext context, DateTime date) {
+    return ConstrainedBox(
+      constraints: BoxConstraints.expand(height: 64),
+      child: Row(
+        children: [
+          SizedBox(width: 16),
+          Text(
+            DateTimeFormatter.yearAndMonth(date),
+            textAlign: TextAlign.left,
+            style: FontType.cardHeader.merge(TextColorStyle.noshime),
+          ),
+          SizedBox(width: 16),
+        ],
+      ),
+    );
+  }
+
+  Widget _calendar(BuildContext context, DateTime date) {
+    return Calendar(calculator: Calculator(date), bandModels: []);
+  }
+}
