0a29d693cbeedfc86ade3f824338421275db7897
bannzai <kingkonog999yhirose@gmail.com>
2020-10-01 12:54:08 +0900

    :recycle: remove group

--- test/pill_sheet/pill_sheet_model_test.dart
+++ test/pill_sheet/pill_sheet_model_test.dart
@@ -4,7 +4,8 @@ import 'package:Pilll/util/today.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group("today: 2020-09-19, begin: 2020-09-14, end: 2020-09-18", () {
+  test("#todayPillNumber today: 2020-09-19, begin: 2020-09-14, end: 2020-09-18",
+      () {
     var sheetType = PillSheetType.pillsheet_21;
     var model = PillSheetModel(
       beginingDate: DateTime.parse("2020-09-14"),
@@ -16,58 +17,51 @@ void main() {
       ),
       todayBuilder: () => DateTime.parse("2020-09-19"),
     );
-    test("#todayPillNumber", () {
-      expect(model.todayPillNumber, 6);
-    });
+    expect(model.todayPillNumber, 6);
   });
-  group("boundary value tests", () {
-    group("today: 2020-09-28, begin: 2020-09-01, end: 2020-09-28", () {
-      var sheetType = PillSheetType.pillsheet_21;
-      var model = PillSheetModel(
-        beginingDate: DateTime.parse("2020-09-01"),
-        lastTakenDate: DateTime.parse("2020-09-28"),
-        typeInfo: PillSheetTypeInfo(
-          dosingPeriod: sheetType.dosingPeriod,
-          totalCount: sheetType.totalCount,
-          pillSheetTypeReferencePath: sheetType.rawPath,
-        ),
-        todayBuilder: () => DateTime.parse("2020-09-28"),
-      );
-      test("#todayPillNumber", () {
-        expect(model.todayPillNumber, 28);
-      });
-    });
-    group("today: 2020-09-29, begin: 2020-09-01, end: 2020-09-28", () {
-      var sheetType = PillSheetType.pillsheet_21;
-      var model = PillSheetModel(
-        beginingDate: DateTime.parse("2020-09-01"),
-        lastTakenDate: DateTime.parse("2020-09-28"),
-        typeInfo: PillSheetTypeInfo(
-          dosingPeriod: sheetType.dosingPeriod,
-          totalCount: sheetType.totalCount,
-          pillSheetTypeReferencePath: sheetType.rawPath,
-        ),
-        todayBuilder: () => DateTime.parse("2020-09-29"),
-      );
-      test("#todayPillNumber", () {
-        expect(model.todayPillNumber, 1);
-      });
-    });
-    group("today: 2020-10-27, begin: 2020-09-01, end: 2020-09-28", () {
-      var sheetType = PillSheetType.pillsheet_21;
-      var model = PillSheetModel(
-        beginingDate: DateTime.parse("2020-09-01"),
-        lastTakenDate: DateTime.parse("2020-09-28"),
-        typeInfo: PillSheetTypeInfo(
-          dosingPeriod: sheetType.dosingPeriod,
-          totalCount: sheetType.totalCount,
-          pillSheetTypeReferencePath: sheetType.rawPath,
-        ),
-        todayBuilder: () => DateTime.parse("2020-10-27"),
-      );
-      test("#todayPillNumber", () {
-        expect(model.todayPillNumber, 1);
-      });
-    });
+  test("#todayPillNumber today: 2020-09-28, begin: 2020-09-01, end: 2020-09-28",
+      () {
+    var sheetType = PillSheetType.pillsheet_21;
+    var model = PillSheetModel(
+      beginingDate: DateTime.parse("2020-09-01"),
+      lastTakenDate: DateTime.parse("2020-09-28"),
+      typeInfo: PillSheetTypeInfo(
+        dosingPeriod: sheetType.dosingPeriod,
+        totalCount: sheetType.totalCount,
+        pillSheetTypeReferencePath: sheetType.rawPath,
+      ),
+      todayBuilder: () => DateTime.parse("2020-09-28"),
+    );
+    expect(model.todayPillNumber, 28);
+  });
+  test("#todayPillNumber today: 2020-09-29, begin: 2020-09-01, end: 2020-09-28",
+      () {
+    var sheetType = PillSheetType.pillsheet_21;
+    var model = PillSheetModel(
+      beginingDate: DateTime.parse("2020-09-01"),
+      lastTakenDate: DateTime.parse("2020-09-28"),
+      typeInfo: PillSheetTypeInfo(
+        dosingPeriod: sheetType.dosingPeriod,
+        totalCount: sheetType.totalCount,
+        pillSheetTypeReferencePath: sheetType.rawPath,
+      ),
+      todayBuilder: () => DateTime.parse("2020-09-29"),
+    );
+    expect(model.todayPillNumber, 1);
+  });
+  test("#todayPillNumber today: 2020-10-27, begin: 2020-09-01, end: 2020-09-28",
+      () {
+    var sheetType = PillSheetType.pillsheet_21;
+    var model = PillSheetModel(
+      beginingDate: DateTime.parse("2020-09-01"),
+      lastTakenDate: DateTime.parse("2020-09-28"),
+      typeInfo: PillSheetTypeInfo(
+        dosingPeriod: sheetType.dosingPeriod,
+        totalCount: sheetType.totalCount,
+        pillSheetTypeReferencePath: sheetType.rawPath,
+      ),
+      todayBuilder: () => DateTime.parse("2020-10-27"),
+    );
+    expect(model.todayPillNumber, 1);
   });
 }
