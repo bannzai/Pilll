694b7a2b6c7b204b7f2056123bbb6cbf7bbb1a9f
bannzai <kingkong999yhirose@gmail.com>
2020-10-01 12:03:55 +0900

    :recycle: remove injection of today

--- lib/util/today.dart
+++ lib/util/today.dart
@@ -1,15 +1,5 @@
 import 'package:Pilll/util/environment.dart';
 
-DateTime Function() _fakeToday;
-void injectToday(DateTime Function() closure) {
-  if (!Environment.isProduction) _fakeToday = closure;
-}
-
 DateTime today() {
-  if (Environment.isProduction) {
-    return DateTime.now();
-  }
-  var date = _fakeToday();
-  injectToday(null);
-  return date ?? DateTime.now();
+  return DateTime.now();
 }
