2cbe6984b4a36167663a1e04712507207f4b4b2d
bannzai <kingkonog999yhirose@gmail.com>
2020-10-01 16:12:54 +0900

    :sparkels: fix resetTodayTakenPillNumber

--- test/pill_sheet/pill_sheet_model_test.dart
+++ test/pill_sheet/pill_sheet_model_test.dart
@@ -62,4 +62,55 @@ void main() {
       expect(model.todayPillNumber, 1);
     });
   });
+
+  group("#resetTodayTakenPillNumber", () {
+    test("pass before pill number for today", () {
+      var sheetType = PillSheetType.pillsheet_21;
+      var model = PillSheetModel(
+        beginingDate: DateTime.parse("2020-09-14"),
+        lastTakenDate: DateTime.parse("2020-09-28"),
+        typeInfo: PillSheetTypeInfo(
+          dosingPeriod: sheetType.dosingPeriod,
+          totalCount: sheetType.totalCount,
+          pillSheetTypeReferencePath: sheetType.rawPath,
+        ),
+        todayBuilder: () => DateTime.parse("2020-09-28"),
+      );
+      expect(model.todayPillNumber, 15);
+      model.resetTodayTakenPillNumber(14);
+      expect(model.beginingDate, DateTime.parse("2020-09-13"));
+    });
+    test("pass after pill number for today", () {
+      var sheetType = PillSheetType.pillsheet_21;
+      var model = PillSheetModel(
+        beginingDate: DateTime.parse("2020-09-14"),
+        lastTakenDate: DateTime.parse("2020-09-28"),
+        typeInfo: PillSheetTypeInfo(
+          dosingPeriod: sheetType.dosingPeriod,
+          totalCount: sheetType.totalCount,
+          pillSheetTypeReferencePath: sheetType.rawPath,
+        ),
+        todayBuilder: () => DateTime.parse("2020-09-28"),
+      );
+      expect(model.todayPillNumber, 15);
+      model.resetTodayTakenPillNumber(16);
+      expect(model.beginingDate, DateTime.parse("2020-09-15"));
+    });
+    test("pass same pill number as today", () {
+      var sheetType = PillSheetType.pillsheet_21;
+      var model = PillSheetModel(
+        beginingDate: DateTime.parse("2020-09-14"),
+        lastTakenDate: DateTime.parse("2020-09-28"),
+        typeInfo: PillSheetTypeInfo(
+          dosingPeriod: sheetType.dosingPeriod,
+          totalCount: sheetType.totalCount,
+          pillSheetTypeReferencePath: sheetType.rawPath,
+        ),
+        todayBuilder: () => DateTime.parse("2020-09-28"),
+      );
+      expect(model.todayPillNumber, 15);
+      model.resetTodayTakenPillNumber(15);
+      expect(model.beginingDate, DateTime.parse("2020-09-14"));
+    });
+  });
 }
