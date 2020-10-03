ac4706bdfeb0f2dd80357ced124ce7be2ff5c87e
bannzai <kingkonog999yhirose@gmail.com>
2020-10-03 12:38:06 +0900

    :sparkles: add widget tests

--- test/calendar/widget/calendar_test.dart
+++ test/calendar/widget/calendar_test.dart
@@ -1,6 +1,7 @@
 import 'package:Pilll/main/calendar/calculator.dart';
 import 'package:Pilll/main/calendar/calendar.dart';
 import 'package:Pilll/main/calendar/calendar_band_model.dart';
+import 'package:Pilll/main/calendar/date_range.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,16 +13,20 @@ void main() {
   group("Appearance Next Sheet Label", () {
     testWidgets('when showing 新しいシート開始 ▶︎', (WidgetTester tester) async {
       var now = DateTime(2020, 09, 14);
+      var model = CalendarNextPillSheetBandModel(
+          DateTime(2020, 09, 15), DateTime(2020, 09, 18));
       await tester.pumpWidget(
         MaterialApp(
-          home: Calendar(calculator: Calculator(now), bandModels: [
-            CalendarNextPillSheetBandModel(
-                DateTime(2020, 09, 15), DateTime(2020, 09, 18)),
-          ]),
+          home: Calendar(calculator: Calculator(now), bandModels: [model]),
         ),
       );
       expect(find.text("新しいシート開始 ▶︎"), findsOneWidget);
-      expect(find.byType(CalendarBand), findsWidgets);
+      expect(find.byType(CalendarBand), findsOneWidget);
+      expect(
+          find.byWidgetPredicate((widget) => (widget is CalendarBand &&
+              DateRange.isSameDay(widget.model.begin, model.begin) &&
+              DateRange.isSameDay(widget.model.end, model.end))),
+          findsOneWidget);
     });
     testWidgets('when showing new sheet label to next month',
         (WidgetTester tester) async {
