2cda5534d7df9e63d428acc5c24009fc9cdf4bd3
bannzai <kingkonog999yhirose@gmail.com>
2020-10-04 10:39:22 +0900

    :recycle: rename function

--- lib/util/formatter/date_time_formatter.dart
+++ lib/util/formatter/date_time_formatter.dart
@@ -12,7 +12,7 @@ class DateTimeFormatter {
     return DateFormat(DateFormat.YEAR_ABBR_MONTH, "ja_JP").format(dateTime);
   }
 
-  static String monthAndYearAndWeekday(DateTime dateTime) {
+  static String monthAndWeekday(DateTime dateTime) {
     return DateFormat(DateFormat.NUM_MONTH_WEEKDAY_DAY, "ja_JP")
         .format(dateTime);
   }
