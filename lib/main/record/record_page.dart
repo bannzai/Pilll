3fb6df7b655a4b779f1c314ba75636a60975cafb
bannzai <kingkonog999yhirose@gmail.com>
2020-10-04 10:19:09 +0900

    :sparkles: add record page

---
+++ lib/main/record/record_page.dart
@@ -0,0 +1,54 @@
+import 'package:Pilll/main/components/pill/pill_mark.dart';
+import 'package:Pilll/main/components/pill/pill_sheet.dart';
+import 'package:Pilll/main/record/record_taken_information.dart';
+import 'package:Pilll/style/button.dart';
+import 'package:Pilll/theme/color.dart';
+import 'package:flutter/material.dart';
+
+class RecordPage extends StatefulWidget {
+  @override
+  _RecordPageState createState() => _RecordPageState();
+}
+
+class _RecordPageState extends State<RecordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PilllColors.background,
+      appBar: null,
+      extendBodyBehindAppBar: true,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 60),
+            // TODO: Should not use DateTime.now for beginingTakenDate and lastTakenDate
+            RecordTakenInformation(
+              today: DateTime.now(),
+              beginingTakenDate: DateTime.now(),
+              lastTakenDate: DateTime.now(),
+            ),
+            SizedBox(height: 24),
+            PillSheet(
+              isHideWeekdayLine: false,
+              pillMarkTypeBuilder: (number) {
+                return PillMarkType.normal;
+              },
+              markSelected: (number) {},
+            ),
+            SizedBox(height: 24),
+            Container(
+              height: 44,
+              width: 180,
+              child: PrimaryButton(
+                text: "飲んだ",
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(height: 8),
+          ],
+        ),
+      ),
+    );
+  }
+}
