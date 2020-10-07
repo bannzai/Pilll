9d9e36ff27bdcae95bf9d6201283f20c566115f8
bannzai <kingkong999yhirose@gmail.com>
2020-10-07 23:58:10 +0900

    :recycle: update widget with provider:selector when pill sheet is created

--- lib/main/record/record_page.dart
+++ lib/main/record/record_page.dart
@@ -13,6 +13,7 @@ import 'package:Pilll/theme/text_color.dart';
 import 'package:Pilll/util/today.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 
 class RecordPage extends StatefulWidget {
   @override
@@ -26,44 +27,56 @@ class _RecordPageState extends State<RecordPage> {
       backgroundColor: PilllColors.background,
       appBar: null,
       extendBodyBehindAppBar: true,
-      body: FutureBuilder(
-        future: pillSheetRepository
-            .fetchLast(AppState.shared.user.documentID)
-            .then((model) => AppState.shared.updated(model,
-                (state, pillSheet) => state.currentPillSheet = pillSheet)),
+      body: Selector<AppState, PillSheetModel>(
+        selector: (context, state) => state.currentPillSheet,
+        shouldRebuild: (prev, next) => !identical(prev, next),
         builder:
-            (BuildContext context, AsyncSnapshot<PillSheetModel> snapshot) {
-          if (snapshot.connectionState != ConnectionState.done)
-            return Indicator();
-          var pillSheet = snapshot.data;
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                RecordTakenInformation(
-                  today: DateTime.now(),
-                  pillSheetModel: AppState.shared.currentPillSheet,
-                ),
-                if (pillSheet == null) _empty(),
-                if (pillSheet != null) ...[
-                  _pillSheet(),
-                  SizedBox(height: 24),
-                  Container(
-                    height: 44,
-                    width: 180,
-                    child: PrimaryButton(
-                      text: "飲んだ",
-                      onPressed: () {},
+            (BuildContext context, PillSheetModel pillSheet, Widget child) {
+          return FutureBuilder(
+            future: _fetchPillSheet(),
+            builder:
+                (BuildContext context, AsyncSnapshot<PillSheetModel> snapshot) {
+              if (snapshot.connectionState != ConnectionState.done)
+                return Indicator();
+              var pillSheet = snapshot.data;
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    RecordTakenInformation(
+                      today: DateTime.now(),
+                      pillSheetModel: AppState.shared.currentPillSheet,
                     ),
-                  ),
-                ],
-                SizedBox(height: 8),
-              ],
-            ),
+                    if (pillSheet == null) _empty(),
+                    if (pillSheet != null) ...[
+                      _pillSheet(),
+                      SizedBox(height: 24),
+                      Container(
+                        height: 44,
+                        width: 180,
+                        child: PrimaryButton(
+                          text: "飲んだ",
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                    SizedBox(height: 8),
+                  ],
+                ),
+              );
+            },
           );
         },
       ),
     );
+  }
+
+  Future<PillSheetModel> _fetchPillSheet() {
+    return AppState.shared.currentPillSheet == null
+        ? pillSheetRepository.fetchLast(AppState.shared.user.documentID).then(
+            (model) => AppState.shared.updated(model,
+                (state, pillSheet) => state.currentPillSheet = pillSheet))
+        : Future.value(AppState.shared.currentPillSheet);
   }
 
   PillSheet _pillSheet() {
