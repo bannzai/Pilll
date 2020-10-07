54463c984ead7d2a7bac277c7a8daa826381de83
bannzai <kingkong999yhirose@gmail.com>
2020-10-07 20:51:05 +0900

    :recycle: Remove unnecessary constants

--- lib/model/pill_sheet.dart
+++ lib/model/pill_sheet.dart
@@ -6,15 +6,6 @@ import 'package:json_annotation/json_annotation.dart';
 import 'package:meta/meta.dart';
 part 'pill_sheet.g.dart';
 
-abstract class PillSheetFirestoreFieldKeys {
-  static final String beginingDate = "beginingDate";
-  static final String pillSheetTypeInfo = "pillSheetTypeInfo";
-  static final String pillSheetTypeInfoRef = "reference";
-  static final String pillSheetTypeInfoPillCount = "pillCount";
-  static final String pillSheetTypeInfoDosingPeriod = "dosingPeriod";
-  static final String lastTakenDate = "lastTakenDate";
-}
-
 @JsonSerializable(nullable: false)
 class PillSheetTypeInfo {
   final String pillSheetTypeReferencePath;
