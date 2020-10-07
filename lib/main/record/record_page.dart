530398be65e427099be0d2201dd85bb635e319dd
bannzai <kingkonog999yhirose@gmail.com>
2020-10-07 21:04:59 +0900

    :recycle: Wrap with future builder

--- lib/main/record/record_page.dart
+++ lib/main/record/record_page.dart
@@ -2,6 +2,8 @@ import 'package:Pilll/main/components/pill/pill_mark.dart';
 import 'package:Pilll/main/components/pill/pill_sheet.dart';
 import 'package:Pilll/main/record/record_taken_information.dart';
 import 'package:Pilll/model/app_state.dart';
+import 'package:Pilll/model/pill_sheet.dart';
+import 'package:Pilll/repository/pill_sheet.dart';
 import 'package:Pilll/style/button.dart';
 import 'package:Pilll/theme/color.dart';
 import 'package:Pilll/theme/font.dart';
@@ -21,30 +23,35 @@ class _RecordPageState extends State<RecordPage> {
       backgroundColor: PilllColors.background,
       appBar: null,
       extendBodyBehindAppBar: true,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            RecordTakenInformation(
-              today: DateTime.now(),
-              pillSheetModel: AppState.shared.currentPillSheet,
-            ),
-            if (AppState.shared.currentPillSheet == null) _empty(),
-            if (AppState.shared.currentPillSheet != null) ...[
-              _pillSheet(),
-              SizedBox(height: 24),
-              Container(
-                height: 44,
-                width: 180,
-                child: PrimaryButton(
-                  text: "飲んだ",
-                  onPressed: () {},
+      body: FutureBuilder(
+        future: pillSheetRepository.fetchLast(AppState.shared.user.documentID),
+        builder: (BuildContext context, AsyncSnapshot<PillSheetModel> model) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RecordTakenInformation(
+                  today: DateTime.now(),
+                  pillSheetModel: AppState.shared.currentPillSheet,
                 ),
-              ),
-            ],
-            SizedBox(height: 8),
-          ],
-        ),
+                if (AppState.shared.currentPillSheet == null) _empty(),
+                if (AppState.shared.currentPillSheet != null) ...[
+                  _pillSheet(),
+                  SizedBox(height: 24),
+                  Container(
+                    height: 44,
+                    width: 180,
+                    child: PrimaryButton(
+                      text: "飲んだ",
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+                SizedBox(height: 8),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
