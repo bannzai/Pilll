39b07243d3d67dcc4d5b2ecc79214f4ed3f9e036
bannzai <kingkonog999yhirose@gmail.com>
2020-10-07 19:46:00 +0900

    :recycle: remove unnecessary import

--- lib/model/app_state.dart
+++ lib/model/app_state.dart
@@ -1,7 +1,6 @@
 import 'package:Pilll/model/initial_setting.dart';
 import 'package:Pilll/model/user.dart';
 import 'package:Pilll/repository/user.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
