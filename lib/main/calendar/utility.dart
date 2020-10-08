fd7dd1c3b7797df3205756c41a63407676fc0df9
bannzai <kingkonog999yhirose@gmail.com>
2020-10-08 11:31:28 +0900

    :recycle: fix test case

--- lib/main/calendar/utility.dart
+++ lib/main/calendar/utility.dart
@@ -11,10 +11,10 @@ DateRange menstruationDateRange(
 ) {
   var offset = page * pillSheet.pillSheetType.totalCount;
   var begin = pillSheet.beginingDate.add(Duration(
-      days: pillSheet.pillSheetType.dosingPeriod +
-          setting.fromMenstruation +
+      days: (pillSheet.pillSheetType.dosingPeriod - 1) +
+          (setting.fromMenstruation + 1) +
           offset));
-  var end = begin.add(Duration(days: setting.durationMenstruation));
+  var end = begin.add(Duration(days: (setting.durationMenstruation - 1)));
   return DateRange(begin, end);
 }
 
