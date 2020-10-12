6f85407dccb4791ecdd09e510878ed0906b397e0
bannzai <kingkonog999yhirose@gmail.com>
2020-10-12 23:32:56 +0900

    :recycle: using future provider

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -4,15 +4,15 @@ import 'package:riverpod/riverpod.dart';
 
 final authProvider = Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
 
-final signInProvider = StreamProvider<UserCredential>(
-  (ref) => ref.watch(authProvider).signInAnonymously().asStream(),
+final signInProvider = FutureProvider<UserCredential>(
+  (ref) => ref.watch(authProvider).signInAnonymously(),
 );
 
 final databaseProvider = Provider<DatabaseConnection>((ref) {
   final userCredential = ref.watch(signInProvider);
 
   if (userCredential.data?.value?.user?.uid != null) {
-    return DatabaseConnection(userID: userCredential.data.value.user.uid);
+    return DatabaseConnection(userID: userCredential.data?.value?.user?.uid);
   }
   return null;
 });
