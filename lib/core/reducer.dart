7585e6b5234ff69e612c29aab49bdc197baa8458
bannzai <kingkong999yhirose@gmail.com>
2020-10-12 21:57:53 +0900

    :recycle: remove redux like interface

--- lib/core/reducer.dart
+++
@@ -1,4 +0,0 @@
-import 'package:Pilll/core/action.dart';
-import 'package:Pilll/core/state.dart';
-
-typedef Reducer = State Function<State extends ReduxState>(State, Action);
--- lib/core/store.dart
+++
@@ -1,5 +0,0 @@
-class Store {
-  static final Store shared = Store._internal();
-  factory Store() => shared;
-  Store._internal();
-}
