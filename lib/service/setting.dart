cc308990138d4862298f3a2580d4b5c3898e2040
bannzai <kingkong999yhirose@gmail.com>
2020-10-12 23:40:55 +0900

    :recycle: fix setting reference and remove global variables

--- lib/service/setting.dart
+++ lib/service/setting.dart
@@ -1,6 +1,8 @@
+import 'package:Pilll/database/database.dart';
 import 'package:Pilll/model/app_state.dart';
 import 'package:Pilll/model/setting.dart';
 import 'package:Pilll/model/user.dart';
+import 'package:Pilll/provider/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:riverpod/all.dart';
 
@@ -12,16 +14,15 @@ class SettingService extends SettingServiceInterface {
   final Reader reader;
   SettingService(this.reader);
 
+  DatabaseConnection get _database => reader(databaseProvider);
+
   Future<Setting> save(Setting setting) {
     if (AppState.shared.user.documentID == null) {
       throw UserNotFound();
     }
-    return FirebaseFirestore.instance
-        .collection(User.path)
-        .doc(AppState.shared.user.documentID)
+    return _database
+        .userReference()
         .update({UserFirestoreFieldKeys.settings: setting.toJson()}).then(
             (_) => setting);
   }
 }
-
-final SettingServiceInterface settingRepository = SettingService();
