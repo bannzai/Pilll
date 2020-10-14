64abb7cc0a0207ade1152aa94edb116ba2c5d905
bannzai <kingkong999yhirose@gmail.com>
2020-10-14 16:03:22 +0900

    :sparkles: WIP

--- lib/initial_setting/initial_setting_1.dart
+++ lib/initial_setting/initial_setting_1.dart
@@ -2,18 +2,12 @@ import 'package:Pilll/initial_setting/initial_setting_2.dart';
 import 'package:Pilll/main/components/pill_sheet_type_select_page.dart';
 import 'package:Pilll/model/app_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class InitialSetting1 extends StatefulWidget {
-  const InitialSetting1({Key key}) : super(key: key);
-
-  @override
-  _InitialSetting1State createState() => _InitialSetting1State();
-}
-
-class _InitialSetting1State extends State<InitialSetting1> {
+class InitialSetting1 extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    var state = AppState.watch(context);
+    final state = userProvider();
     return PillSheetTypeSelectPage(
       title: "1/4",
       callback: (type) {
