595c7d1007e8ae74c5ec3a33219dd8a68cc8a690
bannzai <kingkong999yhirose@gmail.com>
2020-10-14 19:10:25 +0900

    :recycle: Replace with riverpod

--- lib/initial_setting/initial_setting_1.dart
+++ lib/initial_setting/initial_setting_1.dart
@@ -1,26 +1,25 @@
 import 'package:Pilll/initial_setting/initial_setting_2.dart';
 import 'package:Pilll/main/components/pill_sheet_type_select_page.dart';
-import 'package:Pilll/model/app_state.dart';
-import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:Pilll/store/initial_setting.dart';
+import 'package:flutter/material.dart';
 
 class InitialSetting1 extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final state = userProvider();
+    final store = useProvider(initialSettingStoreProvider);
+    final state = useProvider(initialSettingStoreProvider.state);
     return PillSheetTypeSelectPage(
       title: "1/4",
       callback: (type) {
-        setState(() {
-          state.initialSetting.pillSheetType = type;
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (BuildContext context) {
-            return InitialSetting2();
-          }));
-        });
+        store.modify((model) => model.copyWith(pillSheetType: type));
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (BuildContext context) {
+          return InitialSetting2();
+        }));
       },
-      selectedPillSheetType:
-          AppState.watch(context).initialSetting.pillSheetType,
+      selectedPillSheetType: state.entity.pillSheetType,
     );
   }
 }
