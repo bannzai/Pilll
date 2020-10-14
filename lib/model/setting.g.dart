54b6a8d45590eebf1099fd7b3113393902a495be
bannzai <kingkong999yhirose@gmail.com>
2020-10-14 14:48:47 +0900

    :memo: add setting model

--- lib/model/setting.g.dart
+++ lib/model/setting.g.dart
@@ -6,35 +6,36 @@ part of 'setting.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-ReminderTime _$ReminderTimeFromJson(Map<String, dynamic> json) {
-  return ReminderTime(
+_$_ReminderTime _$_$_ReminderTimeFromJson(Map<String, dynamic> json) {
+  return _$_ReminderTime(
     hour: json['hour'] as int,
     minute: json['minute'] as int,
   );
 }
 
-Map<String, dynamic> _$ReminderTimeToJson(ReminderTime instance) =>
+Map<String, dynamic> _$_$_ReminderTimeToJson(_$_ReminderTime instance) =>
     <String, dynamic>{
       'hour': instance.hour,
       'minute': instance.minute,
     };
 
-Setting _$SettingFromJson(Map<String, dynamic> json) {
-  return Setting(
+_$_Setting _$_$_SettingFromJson(Map<String, dynamic> json) {
+  return _$_Setting(
     pillSheetTypeRawPath: json['pillSheetTypeRawPath'] as String,
     fromMenstruation: json['fromMenstruation'] as int,
     durationMenstruation: json['durationMenstruation'] as int,
     reminderTime: json['reminderTime'] == null
         ? null
-        : ReminderTime.fromJson(json['reminderTime'] as Map<String, dynamic>),
-    isOnReminder: json['isOnReminder'] as bool ?? false,
+        : DateTime.parse(json['reminderTime'] as String),
+    isOnReminder: json['isOnReminder'],
   );
 }
 
-Map<String, dynamic> _$SettingToJson(Setting instance) => <String, dynamic>{
+Map<String, dynamic> _$_$_SettingToJson(_$_Setting instance) =>
+    <String, dynamic>{
       'pillSheetTypeRawPath': instance.pillSheetTypeRawPath,
       'fromMenstruation': instance.fromMenstruation,
       'durationMenstruation': instance.durationMenstruation,
-      'reminderTime': instance.reminderTime?.toJson(),
+      'reminderTime': instance.reminderTime?.toIso8601String(),
       'isOnReminder': instance.isOnReminder,
     };
