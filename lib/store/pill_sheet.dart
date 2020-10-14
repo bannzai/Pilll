1347184cb31a260f45fe06556d2be27717d81c7b
bannzai <kingkonog999yhirose@gmail.com>
2020-10-14 11:21:15 +0900

    :spakrles: defined pill sheet store provider

---
+++ lib/store/pill_sheet.dart
@@ -0,0 +1,18 @@
+import 'package:Pilll/model/pill_sheet.dart';
+import 'package:Pilll/provider/service.dart';
+import 'package:Pilll/service/pill_sheet.dart';
+import 'package:Pilll/state/pill_sheet.dart';
+import 'package:riverpod/riverpod.dart';
+
+final pillSheetStoreProvider =
+    StateNotifierProvider((ref) => PillSheetStateStore(ref.read));
+
+class PillSheetStateStore extends StateNotifier<PillSheetState> {
+  final Reader _read;
+  PillSheetStateStore(this._read) : super(PillSheetState()) {
+    Future(() async {
+      state = PillSheetState(
+          entity: await _read(fetchLastPillSheetProvider.future));
+    });
+  }
+}
