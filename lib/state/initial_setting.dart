88106891efb779a6af0a232e856ee0230ceeea5e
bannzai <kingkonog999yhirose@gmail.com>
2020-10-14 15:38:00 +0900

    :sparkles: add initial setting state

---
+++ lib/state/initial_setting.dart
@@ -0,0 +1,11 @@
+import 'package:Pilll/model/initial_setting.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'initial_setting.freezed.dart';
+
+@freezed
+abstract class InitialSettingState implements _$InitialSettingState {
+  InitialSettingState._();
+  factory InitialSettingState({@Default(null) InitialSettingModel entity}) =
+      _InitialSettingState;
+}
