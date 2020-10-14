a10fb8dc385d3b6140e678203df458e7a57d9cb9
bannzai <kingkong999yhirose@gmail.com>
2020-10-14 12:18:36 +0900

    :sparkles: defined setting state

---
+++ lib/state/setting.dart
@@ -0,0 +1,10 @@
+import 'package:Pilll/model/setting.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'setting.freezed.dart';
+
+@freezed
+abstract class SettingState implements _$SettingState {
+  SettingState._();
+  factory SettingState({@Default(null) Setting entity}) = _SettingState;
+}
