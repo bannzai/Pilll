e7569328bfc06c089386847d97aa7224ae8a5638
bannzai <kingkong999yhirose@gmail.com>
2020-10-14 12:29:36 +0900

    :sparkles: WIP Prepare setting store

---
+++ lib/store/setting.dart
@@ -0,0 +1,22 @@
+import 'package:Pilll/model/setting.dart';
+import 'package:Pilll/service/setting.dart';
+import 'package:Pilll/state/setting.dart';
+import 'package:riverpod/riverpod.dart';
+
+final settingStoreProvider =
+    StateNotifierProvider((ref) => SettingStateStore(ref.read));
+
+class SettingStateStore extends StateNotifier<SettingState> {
+  final Reader _read;
+  SettingServiceInterface get _service => _read(settingServiceProvider);
+  SettingStateStore(this._read) : super(SettingState()) {
+    _reset();
+  }
+
+  void _reset() {
+    Future(() async {
+      state =
+          SettingState(entity: await _read(fetchLastSettingProvider.future));
+    });
+  }
+}
