9d8c36cedcd46d2a89cf501dc6a9c5a5a6a63b62
bannzai <kingkong999yhirose@gmail.com>
2020-10-15 09:29:17 +0900

    :sparkels: add mock helper

---
+++ test/helper/mock.dart
@@ -0,0 +1,8 @@
+import 'package:Pilll/service/pill_sheet.dart';
+import 'package:Pilll/service/today.dart';
+import 'package:mockito/mockito.dart';
+
+class MockPillSheetRepository extends Mock
+    implements PillSheetServiceInterface {}
+
+class MockTodayRepository extends Mock implements TodayServiceInterface {}
