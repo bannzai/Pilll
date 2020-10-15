c638fdf269951c641b8b8c6c91dab645938dc348
bannzai <kingkonog999yhirose@gmail.com>
2020-10-15 09:45:28 +0900

    :recycle: using mock today repository

--- test/pill_sheet/pill_sheet_model_test.dart
+++ test/pill_sheet/pill_sheet_model_test.dart
@@ -1,10 +1,18 @@
 import 'package:Pilll/model/pill_sheet.dart';
 import 'package:Pilll/model/pill_sheet_type.dart';
+import 'package:Pilll/service/today.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../helper/mock.dart';
 
 void main() {
   group("#todayPillNumber", () {
     test("today: 2020-09-19, begin: 2020-09-14, end: 2020-09-18", () {
+      var mockTodayRepository = MockTodayRepository();
+      todayRepository = mockTodayRepository;
+      when(todayRepository.today()).thenReturn(DateTime.parse("2020-09-19"));
+
       var sheetType = PillSheetType.pillsheet_21;
       var model = PillSheetModel(
         beginingDate: DateTime.parse("2020-09-14"),
@@ -14,11 +22,14 @@ void main() {
           totalCount: sheetType.totalCount,
           pillSheetTypeReferencePath: sheetType.rawPath,
         ),
-        todayBuilder: () => DateTime.parse("2020-09-19"),
       );
       expect(model.todayPillNumber, 6);
     });
     test("today: 2020-09-28, begin: 2020-09-01, end: 2020-09-28", () {
+      var mockTodayRepository = MockTodayRepository();
+      todayRepository = mockTodayRepository;
+      when(todayRepository.today()).thenReturn(DateTime.parse("2020-09-28"));
+
       var sheetType = PillSheetType.pillsheet_21;
       var model = PillSheetModel(
         beginingDate: DateTime.parse("2020-09-01"),
@@ -28,11 +39,14 @@ void main() {
           totalCount: sheetType.totalCount,
           pillSheetTypeReferencePath: sheetType.rawPath,
         ),
-        todayBuilder: () => DateTime.parse("2020-09-28"),
       );
       expect(model.todayPillNumber, 28);
     });
     test("today: 2020-09-29, begin: 2020-09-01, end: 2020-09-28", () {
+      var mockTodayRepository = MockTodayRepository();
+      todayRepository = mockTodayRepository;
+      when(todayRepository.today()).thenReturn(DateTime.parse("2020-09-29"));
+
       var sheetType = PillSheetType.pillsheet_21;
       var model = PillSheetModel(
         beginingDate: DateTime.parse("2020-09-01"),
@@ -42,11 +56,14 @@ void main() {
           totalCount: sheetType.totalCount,
           pillSheetTypeReferencePath: sheetType.rawPath,
         ),
-        todayBuilder: () => DateTime.parse("2020-09-29"),
       );
       expect(model.todayPillNumber, 1);
     });
     test("today: 2020-10-27, begin: 2020-09-01, end: 2020-09-28", () {
+      var mockTodayRepository = MockTodayRepository();
+      todayRepository = mockTodayRepository;
+      when(todayRepository.today()).thenReturn(DateTime.parse("2020-10-27"));
+
       var sheetType = PillSheetType.pillsheet_21;
       var model = PillSheetModel(
         beginingDate: DateTime.parse("2020-09-01"),
@@ -56,7 +73,6 @@ void main() {
           totalCount: sheetType.totalCount,
           pillSheetTypeReferencePath: sheetType.rawPath,
         ),
-        todayBuilder: () => DateTime.parse("2020-10-27"),
       );
       expect(model.todayPillNumber, 1);
     });
@@ -64,6 +80,10 @@ void main() {
 
   group("#resetTodayTakenPillNumber", () {
     test("pass before pill number for today", () {
+      var mockTodayRepository = MockTodayRepository();
+      todayRepository = mockTodayRepository;
+      when(todayRepository.today()).thenReturn(DateTime.parse("2020-09-28"));
+
       var sheetType = PillSheetType.pillsheet_21;
       var model = PillSheetModel(
         beginingDate: DateTime.parse("2020-09-14"),
@@ -73,13 +93,16 @@ void main() {
           totalCount: sheetType.totalCount,
           pillSheetTypeReferencePath: sheetType.rawPath,
         ),
-        todayBuilder: () => DateTime.parse("2020-09-28"),
       );
       expect(model.todayPillNumber, 15);
       model.resetTodayTakenPillNumber(14);
       expect(model.beginingDate, DateTime.parse("2020-09-13"));
     });
     test("pass after pill number for today", () {
+      var mockTodayRepository = MockTodayRepository();
+      todayRepository = mockTodayRepository;
+      when(todayRepository.today()).thenReturn(DateTime.parse("2020-09-28"));
+
       var sheetType = PillSheetType.pillsheet_21;
       var model = PillSheetModel(
         beginingDate: DateTime.parse("2020-09-14"),
@@ -89,13 +112,16 @@ void main() {
           totalCount: sheetType.totalCount,
           pillSheetTypeReferencePath: sheetType.rawPath,
         ),
-        todayBuilder: () => DateTime.parse("2020-09-28"),
       );
       expect(model.todayPillNumber, 15);
       model.resetTodayTakenPillNumber(16);
       expect(model.beginingDate, DateTime.parse("2020-09-15"));
     });
     test("pass same pill number as today", () {
+      var mockTodayRepository = MockTodayRepository();
+      todayRepository = mockTodayRepository;
+      when(todayRepository.today()).thenReturn(DateTime.parse("2020-09-28"));
+
       var sheetType = PillSheetType.pillsheet_21;
       var model = PillSheetModel(
         beginingDate: DateTime.parse("2020-09-14"),
@@ -105,7 +131,6 @@ void main() {
           totalCount: sheetType.totalCount,
           pillSheetTypeReferencePath: sheetType.rawPath,
         ),
-        todayBuilder: () => DateTime.parse("2020-09-28"),
       );
       expect(model.todayPillNumber, 15);
       model.resetTodayTakenPillNumber(15);
