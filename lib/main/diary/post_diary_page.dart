084b863fb746fa051e5a386fdb73b4b8ae8c3cfd
bannzai <kingkonog999yhirose@gmail.com>
2020-10-21 22:18:17 +0900

    :art: WIP add diary post page

---
+++ lib/main/diary/post_diary_page.dart
@@ -0,0 +1,28 @@
+import 'package:Pilll/theme/color.dart';
+import 'package:Pilll/util/formatter/date_time_formatter.dart';
+import 'package:flutter/material.dart';
+
+class PostDiaries extends StatelessWidget {
+  final DateTime date;
+  const PostDiaries({Key key, this.date}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PilllColors.background,
+      appBar: AppBar(
+        backgroundColor: PilllColors.background,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(DateTimeFormatter.yearAndMonthAndDay(date)),
+          ],
+        ),
+      ),
+    );
+  }
+}
