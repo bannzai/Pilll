53055b59db8d3b422d2e1e525641116dafa6299c
bannzai <kingkonog999yhirose@gmail.com>
2020-10-22 10:25:34 +0900

    :sparkles: layouting phshical conditions

--- lib/main/diary/post_diary_page.dart
+++ lib/main/diary/post_diary_page.dart
@@ -1,6 +1,7 @@
 import 'package:Pilll/theme/color.dart';
 import 'package:Pilll/util/formatter/date_time_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class PostDiaryPage extends StatelessWidget {
   final DateTime date;
@@ -11,8 +12,12 @@ class PostDiaryPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: PilllColors.background,
       appBar: AppBar(
-        backgroundColor: PilllColors.background,
         elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.close, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        backgroundColor: PilllColors.background,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -20,8 +25,41 @@ class PostDiaryPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(DateTimeFormatter.yearAndMonthAndDay(date)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("体調"),
+                Spacer(),
+                _physicalConditions(),
+                Spacer(),
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _physicalConditions() {
+    return Container(
+      height: 48,
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: PilllColors.divider,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          IconButton(
+              icon: SvgPicture.asset("images/laugh.svg"), onPressed: null),
+          Container(
+              height: 48,
+              child: VerticalDivider(width: 1, color: PilllColors.divider)),
+          IconButton(
+              icon: SvgPicture.asset("images/angry.svg"), onPressed: null),
+        ],
       ),
     );
   }
