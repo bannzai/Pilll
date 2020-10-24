686c37fd6f7bd7c99d03c299f6635658fc796fa0
bannzai <kingkonog999yhirose@gmail.com>
2020-10-24 19:02:15 +0900

    :recycle: move data source to model

--- lib/model/physical_condition.dart
+++ lib/model/physical_condition.dart
@@ -5,6 +5,21 @@ part 'physical_condition.freezed.dart';
 
 @freezed
 abstract class PhysicalCondition with _$PhysicalCondition {
+  static final List<String> all = [
+    "頭痛",
+    "腹痛",
+    "吐き気",
+    "貧血",
+    "下痢",
+    "便秘",
+    "ほてり",
+    "眠気",
+    "腰痛",
+    "動悸",
+    "不正出血",
+    "食欲不振",
+  ];
+
   @JsonSerializable(nullable: false, explicitToJson: true)
   factory PhysicalCondition({
     @required String name,
