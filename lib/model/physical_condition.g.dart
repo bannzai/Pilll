536695abf0641044e6239fa9be9b4bd99488cc96
bannzai <kingkonog999yhirose@gmail.com>
2020-10-24 18:54:55 +0900

    :sparkles: add physical condition model

---
+++ lib/model/physical_condition.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'physical_condition.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_PhysicalCondition _$_$_PhysicalConditionFromJson(Map<String, dynamic> json) {
+  return _$_PhysicalCondition(
+    name: json['name'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_PhysicalConditionToJson(
+        _$_PhysicalCondition instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+    };
