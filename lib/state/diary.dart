0e15a27e0302c09d2d70cdda41a204cf64fd3687
bannzai <kingkong999yhirose@gmail.com>
2020-10-24 23:17:22 +0900

    :sparkles: add method to state and service

--- lib/state/diary.dart
+++ lib/state/diary.dart
@@ -1,3 +1,4 @@
+import 'package:Pilll/main/utility/utility.dart';
 import 'package:Pilll/model/diary.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -8,9 +9,40 @@ abstract class DiariesState implements _$DiariesState {
   DiariesState._();
   factory DiariesState({@Default([]) List<Diary> entities}) = _DiariesState;
 
+  int _sort(Diary a, Diary b) => a.date.compareTo(b.date);
+
   List<Diary> diariesForMonth(DateTime dateTimeOfMonth) {
-    return entities.where((element) =>
-        element.date.year == dateTimeOfMonth.year &&
-        element.date.month == dateTimeOfMonth.month);
+    entities
+        .where((element) =>
+            element.date.year == dateTimeOfMonth.year &&
+            element.date.month == dateTimeOfMonth.month)
+        .toList()
+        .sort(_sort);
+    return entities;
+  }
+
+  List<Diary> merged(List<Diary> diaries) {
+    if (entities.isEmpty) {
+      diaries.sort(_sort);
+      return diaries;
+    }
+    final elementNotFound = -1;
+    List<Diary> result = entities;
+    result.sort(_sort);
+    diaries.forEach((diary) {
+      final indexForInsert = entities
+          .lastIndexWhere((element) => diary.date.isBefore(element.date));
+      if (indexForInsert != elementNotFound) {
+        result.insert(indexForInsert, diary);
+        return;
+      }
+      final indexForReplace =
+          entities.indexWhere((element) => isSameDay(diary.date, element.date));
+      if (indexForReplace != elementNotFound) {
+        result[indexForReplace] = diary;
+      }
+      result.insert(0, diary);
+    });
+    return result;
   }
 }
--- lib/store/diary.dart
+++ lib/store/diary.dart
@@ -1,1 +1,48 @@
+import 'dart:async';
 
+import 'package:Pilll/main/utility/utility.dart';
+import 'package:Pilll/model/diary.dart';
+import 'package:Pilll/service/diary.dart';
+import 'package:Pilll/state/diary.dart';
+import 'package:riverpod/all.dart';
+
+final diariesStoreProvider = StateNotifierProvider(
+    (ref) => DiariesStateStore(ref.watch(diariesServiceProvider)));
+
+class DiariesStateStore extends StateNotifier<DiariesState> {
+  final DiariesServiceInterface _service;
+  DiariesStateStore(this._service) : super(DiariesState()) {
+    _subscribe();
+  }
+
+  StreamSubscription canceller;
+  void _subscribe() {
+    canceller?.cancel();
+    canceller = _service.subscribe().listen((entities) {
+      assert(entities != null, "Diary could not null on subscribe");
+      if (entities == null) return;
+      state = state.copyWith(entities: state.merged(entities));
+    });
+  }
+
+  void fetchListForMonth(DateTime dateTimeOfMonth) {
+    _service.fetchListForMonth(dateTimeOfMonth).then(
+        (entities) => state = state.copyWith(entities: state.merged(entities)));
+  }
+
+  void register(Diary diary) {
+    if (state.entities
+        .where((element) => isSameDay(diary.date, element.date))
+        .isNotEmpty) throw DiaryAleradyExists(diary);
+    _service.register(diary);
+  }
+}
+
+class DiaryAleradyExists implements Exception {
+  final Diary _diary;
+
+  DiaryAleradyExists(this._diary);
+  toString() {
+    return "diary already exists for date ${_diary.date}";
+  }
+}
