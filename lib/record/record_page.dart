cbf251c4d98d1be3cd703def76a77b73406b8166
bannzai <kingkong999yhirose@gmail.com>
2020-11-03 19:56:45 +0900

    :recycle: Replace to components/organisms/pill

--- lib/record/record_page.dart
+++ lib/record/record_page.dart
@@ -1,5 +1,5 @@
 import 'package:Pilll/components/indicator.dart';
-import 'package:Pilll/components/pill/pill_sheet.dart';
+import 'package:Pilll/components/organisms/pill/pill_sheet.dart';
 import 'package:Pilll/record/record_taken_information.dart';
 import 'package:Pilll/entity/pill_mark_type.dart';
 import 'package:Pilll/entity/pill_sheet.dart';
