94e2e2e8369e1ba97ab6629e9aa621d8551f55da
bannzai <kingkonog999yhirose@gmail.com>
2020-11-04 21:33:50 +0900

    :sparkles: add service about push notification

---
+++ lib/service/push_notification.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+FirebaseMessaging firebaseMessaging;
+
+Future<void> initNotification() async {
+  firebaseMessaging = FirebaseMessaging()
+    ..requestNotificationPermissions()
+    ..onIosSettingsRegistered.listen((IosNotificationSettings settings) {})
+    ..configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('onMessage: $message');
+      },
+      onBackgroundMessage:
+          Platform.isAndroid ? myBackgroundMessageHandler : null,
+      onLaunch: (Map<String, dynamic> message) async {
+        print('onLaunch: $message');
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('onResume: $message');
+      },
+    );
+}
+
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  if (message.containsKey('data')) {
+    // データメッセージをハンドリング
+    final data = message['data'];
+    print("data: $data");
+  }
+
+  if (message.containsKey('notification')) {
+    // 通知メッセージをハンドリング
+    final notification = message['notification'];
+    print("notification: $notification");
+  }
+  print('onBackground: $message');
+}
