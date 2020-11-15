0394cffd830880206be5f1884ef69af6f4d4ccc1
bannzai <kingkonog999yhirose@gmail.com>
2020-11-15 18:27:23 +0900

    :memo: add reference path about user private

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -16,6 +16,7 @@ abstract class _CollectionPath {
   static final String users = "users";
   static String pillSheets(String userID) => "$users/$userID/pill_sheets";
   static String diaries(String userID) => "$users/$userID/diaries";
+  static String userPrivates(String userID) => "$users/$userID/privates";
 }
 
 class DatabaseConnection {
@@ -43,6 +44,10 @@ class DatabaseConnection {
   DocumentReference diaryReference(Diary diary) => FirebaseFirestore.instance
       .collection(_CollectionPath.diaries(_userID))
       .doc(diary.id);
+
+  DocumentReference userPrivateReference() => FirebaseFirestore.instance
+      .collection(_CollectionPath.userPrivates(_userID))
+      .doc(_userID);
 
   Future<T> transaction<T>(TransactionHandler<T> transactionHandler) {
     return FirebaseFirestore.instance.runTransaction(transactionHandler);
