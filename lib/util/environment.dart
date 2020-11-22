8cc2e6425a8b849eea0b9ed089bcf3d913b805cf
bannzai <kingkonog999yhirose@gmail.com>
2020-11-23 00:19:28 +0900

    :recycle: add flavor type for environment

--- lib/util/environment.dart
+++ lib/util/environment.dart
@@ -1,5 +1,11 @@
+enum Flavor {
+  DEVELOP,
+  PRODUCTION,
+}
+
 abstract class Environment {
-  static bool get isProduction => bool.fromEnvironment('dart.vm.product');
-  static bool get isDevelopment => !isProduction;
+  static bool get isProduction => flavor == Flavor.DEVELOP;
+  static bool get isDevelopment => flavor == Flavor.PRODUCTION;
   static bool isTest = false;
+  static Flavor flavor;
 }
