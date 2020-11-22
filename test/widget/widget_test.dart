fce49b09be715b6fdf76fcf30fe6854882f83b45
bannzai <kingkong999yhirose@gmail.com>
2020-11-23 00:19:19 +0900

    :recycle: rename entrypoint

--- test/widget/widget_test.dart
+++ test/widget/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Pilll/main.dart';
+import 'package:Pilll/entrypoint.dart';
 
 void main() {
   setUp(() {
