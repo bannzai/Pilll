a952beffb562fc42fa053784d6f3577ccad3eda0
bannzai <kingkong999yhirose@gmail.com>
2020-11-23 23:54:36 +0900

    :recycle: rename to InitialSettingService

--- lib/service/initial_setting.dart
+++ lib/service/initial_setting.dart
@@ -3,20 +3,20 @@ import 'package:Pilll/service/pill_sheet.dart';
 import 'package:Pilll/service/setting.dart';
 import 'package:riverpod/all.dart';
 
-abstract class InitialSettingInterface {
+abstract class InitialSettingServiceInterface {
   Future<void> register(InitialSettingModel initialSetting) async {
     throw new UnimplementedError("Should call subclass");
   }
 }
 
-final initialSettingServiceProvider = Provider((ref) => InitialSettingPage(
+final initialSettingServiceProvider = Provider((ref) => InitialSettingService(
     ref.watch(settingServiceProvider), ref.watch(pillSheetServiceProvider)));
 
-class InitialSettingPage extends InitialSettingInterface {
+class InitialSettingService extends InitialSettingServiceInterface {
   final SettingServiceInterface settingService;
   final PillSheetServiceInterface pillSheetService;
 
-  InitialSettingPage(this.settingService, this.pillSheetService);
+  InitialSettingService(this.settingService, this.pillSheetService);
 
   Future<void> register(InitialSettingModel initialSetting) async {
     var setting = initialSetting.buildSetting();
