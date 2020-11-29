1f7c938cb04f5bf3be8099f76612cd282b29e654
bannzai <kingkonog999yhirose@gmail.com>
2020-11-29 21:26:55 +0900

    :memo: using future builder

--- lib/domain/root/root.dart
+++ lib/domain/root/root.dart
@@ -10,47 +10,61 @@ import 'package:Pilll/util/shared_preference/keys.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/all.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Root extends HookWidget {
   @override
   Widget build(BuildContext context) {
     listenNotificationEvents();
-    return useProvider(authStateChangesProvider).when(data: (authInfo) {
-      print("when success: ${authInfo.uid}");
-      final userService = UserService(DatabaseConnection(authInfo.uid));
-      userService.prepare().then((_) {
-        return FirebaseMessaging()
-            .getToken()
-            .then(
-              (token) => userService.registerRemoteNotificationToken(token),
-            )
-            .then(
-              (_) => userService.fetch(),
-            )
-            .then((user) {
-          _transition(context, user);
+    return FutureBuilder(
+        future: auth(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            print("loading ... ");
+            return Scaffold(
+              backgroundColor: PilllColors.background,
+              body: Indicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            print("err: ${snapshot.error}");
+            return Scaffold(
+              backgroundColor: PilllColors.background,
+              body: Indicator(),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              final userService =
+                  UserService(DatabaseConnection(snapshot.data.uid));
+              userService.prepare().then((_) {
+                return FirebaseMessaging()
+                    .getToken()
+                    .then(
+                      (token) =>
+                          userService.registerRemoteNotificationToken(token),
+                    )
+                    .then(
+                      (_) => userService.fetch(),
+                    )
+                    .then((user) {
+                  _transition(context, user);
+                });
+              });
+            }
+
+            return Scaffold(
+              backgroundColor: PilllColors.background,
+              body: Indicator(),
+            );
+          }
+          print(
+              "unknown state :${snapshot.connectionState}, data: ${snapshot.data}, error: ${snapshot.error}");
+          return Scaffold(
+            backgroundColor: PilllColors.background,
+            body: Indicator(),
+          );
         });
-      });
-      return Scaffold(
-        backgroundColor: PilllColors.background,
-        body: Indicator(),
-      );
-    }, loading: () {
-      print("loading ... ");
-      auth();
-      return Scaffold(
-        backgroundColor: PilllColors.background,
-        body: Indicator(),
-      );
-    }, error: (err, stack) {
-      print("err: $err");
-      return Scaffold(
-        backgroundColor: PilllColors.background,
-        body: Indicator(),
-      );
-    });
   }
 
   void _transition(BuildContext context, User user) async {
