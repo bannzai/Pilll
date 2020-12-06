eaf0e846c99b497d2dc4367de709d83e34621904
bannzai <kingkong999yhirose@gmail.com>
2020-12-07 08:58:01 +0900

    :art: fix selected color

--- lib/components/atoms/color.dart
+++ lib/components/atoms/color.dart
@@ -35,7 +35,8 @@ class PilllColors {
   static final Color bottomBar = HexColor.fromHex("FAFAFA");
   static final Color border = HexColor.fromHex("DADADA");
   static final Color background = HexColor.fromHex("F4F7F9");
-  static final Color selected = HexColor.fromHex("FCF1F1");
+  static final Color selected = HexColor.fromHex("4E6287");
+  static final Color selected2 = HexColor.fromHex("FCF1F1");
 
   static final Color blank = Colors.white;
   static final Color potti = HexColor.fromHex("7C8E9C");
