22d6e712489487b4156e3e839923e5e715514c85
bannzai <kingkonog999yhirose@gmail.com>
2020-12-17 00:05:22 +0900

    :recycle: fix settings connect to emulator

--- lib/util/environment.dart
+++ lib/util/environment.dart
@@ -8,6 +8,7 @@ abstract class Environment {
   static bool get isProduction => flavor == Flavor.PRODUCTION;
   static bool get isDevelopment =>
       flavor == Flavor.DEVELOP || flavor == Flavor.LOCAL;
+  static bool get isLocal => flavor == Flavor.LOCAL;
   static bool isTest = false;
   static Flavor flavor;
 }
