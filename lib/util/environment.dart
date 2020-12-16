c9176012a7ff60dd4909fcaa244f2d5044630256
bannzai <kingkong999yhirose@gmail.com>
2020-12-16 23:54:09 +0900

    :memo: add local flavor

--- lib/util/environment.dart
+++ lib/util/environment.dart
@@ -1,11 +1,13 @@
 enum Flavor {
   DEVELOP,
   PRODUCTION,
+  LOCAL, // Local is special flavor for development with firebase:emulators
 }
 
 abstract class Environment {
   static bool get isProduction => flavor == Flavor.PRODUCTION;
-  static bool get isDevelopment => flavor == Flavor.DEVELOP;
+  static bool get isDevelopment =>
+      flavor == Flavor.DEVELOP || flavor == Flavor.LOCAL;
   static bool isTest = false;
   static Flavor flavor;
 }
