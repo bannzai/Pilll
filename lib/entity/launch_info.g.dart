8f19319fe08caabbd0f5f56133f3989f2268649a
bannzai <kingkong999yhirose@gmail.com>
2020-12-21 17:16:41 +0900

    :spakrles: add property for package info

--- lib/entity/launch_info.g.dart
+++ lib/entity/launch_info.g.dart
@@ -9,12 +9,16 @@ part of 'launch_info.dart';
 _$_LaunchInfo _$_$_LaunchInfoFromJson(Map<String, dynamic> json) {
   return _$_LaunchInfo(
     latestOS: json['latestOS'] as String,
+    appName: json['appName'] as String,
     appVersion: json['appVersion'] as String,
+    buildNumber: json['buildNumber'] as String,
   );
 }
 
 Map<String, dynamic> _$_$_LaunchInfoToJson(_$_LaunchInfo instance) =>
     <String, dynamic>{
       'latestOS': instance.latestOS,
+      'appName': instance.appName,
       'appVersion': instance.appVersion,
+      'buildNumber': instance.buildNumber,
     };
