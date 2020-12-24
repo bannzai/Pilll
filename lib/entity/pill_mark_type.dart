13cdf69721631fd3900dfea95b46f6670cc130b1
bannzai <kingkonog999yhirose@gmail.com>
2020-12-25 00:47:49 +0900

    :sparkles: using dot border view

--- lib/entity/pill_mark_type.dart
+++ lib/entity/pill_mark_type.dart
@@ -1,4 +1,5 @@
 import 'package:Pilll/components/atoms/color.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -21,47 +22,51 @@ extension PillMarkTypeFunctions on PillMarkType {
   }
 
   static Widget create(bool isDone, PillMarkType type) {
-    return Container(
-      width: 20,
-      height: 20,
-      child: Center(
-        child: () {
-          switch (type) {
-            case PillMarkType.normal:
-              return null;
-            case PillMarkType.rest:
-              return isDone ? _checkImage() : null;
-            case PillMarkType.fake:
-              return isDone ? _checkImage() : null;
-            case PillMarkType.selected:
-              return null;
-            case PillMarkType.done:
-              return isDone ? _checkImage() : null;
-            default:
-              assert(false);
-              return null;
-          }
-        }(),
-      ),
-      decoration: BoxDecoration(
-        color: () {
-          switch (type) {
-            case PillMarkType.normal:
-              return PilllColors.potti;
-            case PillMarkType.rest:
-              return PilllColors.blank;
-            case PillMarkType.fake:
-              return PilllColors.blank;
-            case PillMarkType.selected:
-              return PilllColors.enable;
-            case PillMarkType.done:
-              return PilllColors.lightGray;
-            default:
-              assert(false);
-              return null;
-          }
-        }(),
-        shape: BoxShape.circle,
+    return DottedBorder(
+      color: type == PillMarkType.rest ? PilllColors.gray : Colors.transparent,
+      strokeWidth: type == PillMarkType.rest ? 1 : 0,
+      child: Container(
+        width: 20,
+        height: 20,
+        child: Center(
+          child: () {
+            switch (type) {
+              case PillMarkType.normal:
+                return null;
+              case PillMarkType.rest:
+                return isDone ? _checkImage() : null;
+              case PillMarkType.fake:
+                return isDone ? _checkImage() : null;
+              case PillMarkType.selected:
+                return null;
+              case PillMarkType.done:
+                return isDone ? _checkImage() : null;
+              default:
+                assert(false);
+                return null;
+            }
+          }(),
+        ),
+        decoration: BoxDecoration(
+          color: () {
+            switch (type) {
+              case PillMarkType.normal:
+                return PilllColors.potti;
+              case PillMarkType.rest:
+                return PilllColors.blank;
+              case PillMarkType.fake:
+                return PilllColors.blank;
+              case PillMarkType.selected:
+                return PilllColors.enable;
+              case PillMarkType.done:
+                return PilllColors.lightGray;
+              default:
+                assert(false);
+                return null;
+            }
+          }(),
+          shape: BoxShape.circle,
+        ),
       ),
     );
   }
