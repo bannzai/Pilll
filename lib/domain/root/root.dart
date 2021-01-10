dcd152f1db4280e2a311a4875a244cdadc735846
bannzai <kingkong999yhirose@gmail.com>
2021-01-11 00:18:20 +0900

    :sparkles: to stateful widget

--- lib/domain/root/root.dart
+++ lib/domain/root/root.dart
@@ -1,23 +1,26 @@
 import 'package:Pilll/auth/auth.dart';
 import 'package:Pilll/database/database.dart';
+import 'package:Pilll/domain/home/home_page.dart';
+import 'package:Pilll/domain/initial_setting/initial_setting_1_page.dart';
 import 'package:Pilll/entity/user_error.dart';
-import 'package:Pilll/router/router.dart';
 import 'package:Pilll/components/molecules/indicator.dart';
-import 'package:Pilll/entity/user.dart';
 import 'package:Pilll/service/user.dart';
 import 'package:Pilll/components/atoms/color.dart';
 import 'package:Pilll/util/shared_preference/keys.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/all.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 GlobalKey<RootState> rootKey = GlobalKey();
 
 class Root extends StatefulWidget {
+  Root({Key key}) : super(key: key);
+
   @override
   RootState createState() => RootState();
 }
+
+enum ScreenType { home, initialSetting }
 
 class RootState extends State<Root> {
   UserDisplayedError error;
@@ -27,75 +30,86 @@ class RootState extends State<Root> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (error != null) {
-      return ErrorWidget(error);
-    }
-    return Consumer(builder: (context, watch, child) {
-      return watch(authStateChangesProvider).when(data: (authInfo) {
-        print("when success: ${authInfo.uid}");
-        final userService = UserService(DatabaseConnection(authInfo.uid));
-        userService.prepare().then((_) {
-          return FirebaseMessaging()
-              .getToken()
-              .then(
-                (token) => userService.registerRemoteNotificationToken(token),
-              )
-              .then(
-                (_) => userService.fetch(),
-              )
-              .then((user) {
-            userService.saveLaunchInfo();
-            _transition(context, user, userService);
-          });
-        });
-        return Scaffold(
-          backgroundColor: PilllColors.background,
-          body: Indicator(),
-        );
-      }, loading: () {
-        print("loading ... ");
-        auth();
-        return Scaffold(
-          backgroundColor: PilllColors.background,
-          body: Indicator(),
-        );
-      }, error: (err, stack) {
-        print("err: $err");
-        return Scaffold(
-          backgroundColor: PilllColors.background,
-          body: Indicator(),
-        );
-      });
+  ScreenType screenType;
+  showHome() {
+    setState(() {
+      screenType = ScreenType.home;
     });
   }
 
-  void _transition(
-      BuildContext context, User user, UserServiceInterface service) async {
-    if (!user.migratedFlutter) {
-      service
-          .deleteSettings()
-          .then((_) => service.setFlutterMigrationFlag())
-          .then(
-              (_) => Navigator.popAndPushNamed(context, Routes.initialSetting));
-      return;
+  showInitialSetting() {
+    setState(() {
+      screenType = ScreenType.initialSetting;
+    });
+  }
+
+  @override
+  void initState() {
+    auth().then((authInfo) {
+      final userService = UserService(DatabaseConnection(authInfo.uid));
+      return userService.prepare().then((_) async {
+        final token = await FirebaseMessaging().getToken();
+        await userService.registerRemoteNotificationToken(token);
+        userService.saveLaunchInfo();
+        final user = await userService.fetch();
+        if (!user.migratedFlutter) {
+          await userService.deleteSettings();
+          await userService.setFlutterMigrationFlag();
+          return ScreenType.initialSetting;
+        }
+        if (user.setting == null) {
+          return ScreenType.initialSetting;
+        }
+        final storage = await SharedPreferences.getInstance();
+        if (!storage.getKeys().contains(StringKey.firebaseAnonymousUserID)) {
+          storage.setString(
+              StringKey.firebaseAnonymousUserID, user.anonymouseUserID);
+        }
+        bool didEndInitialSetting =
+            storage.getBool(BoolKey.didEndInitialSetting);
+        if (didEndInitialSetting == null) {
+          return ScreenType.initialSetting;
+        }
+        if (!didEndInitialSetting) {
+          return ScreenType.initialSetting;
+        }
+        return ScreenType.home;
+      });
+    }).then((screenType) {
+      setState(() {
+        this.screenType = screenType;
+      });
+    }).catchError((error) {
+      onError(UserDisplayedError(
+          displayedMessage: "通信環境が不安定のようです。時間をおいて再度お試しください"));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (screenType == null) {
+      return _indicator();
     }
-    final storage = await SharedPreferences.getInstance();
-    if (!storage.getKeys().contains(StringKey.firebaseAnonymousUserID)) {
-      storage.setString(
-          StringKey.firebaseAnonymousUserID, user.anonymouseUserID);
+    if (error != null) {
+      return ErrorWidget(error);
     }
-    bool didEndInitialSetting = storage.getBool(BoolKey.didEndInitialSetting);
-    if (didEndInitialSetting == null) {
-      Navigator.popAndPushNamed(context, Routes.initialSetting);
-      return;
+    switch (screenType) {
+      case ScreenType.home:
+        return HomePage();
+      case ScreenType.initialSetting:
+        return InitialSetting1Page();
+      default:
+        onError(UserDisplayedError(
+            displayedMessage: "通信環境が不安定のようです。時間をおいて再度お試しください"));
+        return _indicator();
     }
-    if (!didEndInitialSetting) {
-      Navigator.popAndPushNamed(context, Routes.initialSetting);
-      return;
-    }
-    Navigator.popAndPushNamed(context, Routes.main);
-    // Navigator.popAndPushNamed(context, Routes.initialSetting);
+  }
+
+  Widget _indicator() {
+    return Scaffold(
+      backgroundColor: PilllColors.background,
+      body: Indicator(),
+    );
   }
 }
