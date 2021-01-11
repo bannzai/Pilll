2c34d3e2c9d484afe3f9bdd30b5a444ab4d7d804
bannzai <kingkonog999yhirose@gmail.com>
2021-01-11 15:31:15 +0900

    :sparkles: add error alert

---
+++ lib/error/error_alert.dart
@@ -0,0 +1,44 @@
+import 'package:Pilll/components/atoms/buttons.dart';
+import 'package:Pilll/components/atoms/font.dart';
+import 'package:Pilll/components/atoms/text_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ErrorAlert extends StatelessWidget {
+  final String title;
+  final String errorMessage;
+
+  const ErrorAlert({Key key, this.title, @required this.errorMessage})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        title ?? "エラーが発生しました",
+        style: FontType.subTitle.merge(TextColorStyle.black),
+      ),
+      content: Text(errorMessage,
+          style: FontType.assisting.merge(TextColorStyle.black)),
+      actions: <Widget>[
+        SecondaryButton(
+          text: "閉じる",
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
+
+void showErrorAlert(BuildContext context,
+    {String title, @required String message}) {
+  showDialog(
+      context: context,
+      builder: (_) {
+        return ErrorAlert(
+          title: title,
+          errorMessage: message,
+        );
+      });
+}
