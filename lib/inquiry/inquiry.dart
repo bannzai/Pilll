0fb0144524169d3205d059a328fd5d7d242c7e3c
bannzai <kingkong999yhirose@gmail.com>
2021-01-13 22:36:38 +0900

    :memo: fix inquiry url

--- lib/inquiry/inquiry.dart
+++ lib/inquiry/inquiry.dart
@@ -1,11 +1,42 @@
+import 'package:Pilll/auth/auth.dart';
+import 'package:Pilll/database/database.dart';
+import 'package:Pilll/entity/pill_sheet.dart';
+import 'package:Pilll/entity/setting.dart';
+import 'package:Pilll/service/pill_sheet.dart';
+import 'package:Pilll/service/setting.dart';
+import 'package:Pilll/state/pill_sheet.dart';
+import 'package:Pilll/state/setting.dart';
+import 'package:Pilll/util/environment.dart';
+import 'package:hooks_riverpod/all.dart';
 import 'package:package_info/package_info.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 inquiry() {
-  PackageInfo.fromPlatform().then((value) {
-    var version = value.version;
+  PackageInfo.fromPlatform().then((value) => debugInfo()).then((info) {
     launch(
-        "https://docs.google.com/forms/d/e/1FAIpQLSdLX5lLdOSr2B2mwzCptH1kjsJUYy8cKFSYguxl9yvep5b7ig/viewform?usp=pp_url&entry.2066946565=$version",
+        "https://docs.google.com/forms/d/e/1FAIpQLSddEpE641jIKEL9cxgiKaRytmBtsP7PXnDdXonEyE-n62JMWQ/viewform?usp=pp_url&entry.2066946565=$info",
         forceSafariVC: true);
   });
+}
+
+Future<String> debugInfo() async {
+  String userID = (await auth()).uid;
+  DatabaseConnection databaseConnection = DatabaseConnection(userID);
+  PillSheetModel pillSheet =
+      await PillSheetService(databaseConnection).fetchLast();
+  Setting setting = await SettingService(databaseConnection).fetch();
+  final package = await PackageInfo.fromPlatform();
+  final appName = package.appName;
+  final packageName = package.packageName;
+  final contents = [
+    "DEBUG INFO",
+    "appName: $appName",
+    "packageName: $packageName",
+    "env: ${Environment.isProduction ? "production" : "development"}",
+    "user id: $userID",
+    "pillSheet.entity.id: ${pillSheet?.id}",
+    "pillSheetState.entity: ${pillSheet == null ? "null" : pillSheet.toJson()}",
+    "settingState.entity: ${setting == null ? "null" : setting.toJson()}",
+  ];
+  return contents.join(", ");
 }
