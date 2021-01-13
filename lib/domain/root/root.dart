d496651a621a882143a574dcd68e0ba3db9959b0
bannzai <kingkong999yhirose@gmail.com>
2021-01-14 00:08:45 +0900

    :recycle: add early return of isEmpty

--- lib/domain/root/root.dart
+++ lib/domain/root/root.dart
@@ -58,6 +58,9 @@ class RootState extends State<Root> {
   showIndicator() {
     _indicatorTypes.add(IndicatorType.shown);
     Future.delayed(Duration(milliseconds: 200)).then((value) {
+      if (_indicatorTypes.isEmpty) {
+        return;
+      }
       if (_indicatorTypes.last == IndicatorType.hidden) {
         return;
       }
@@ -71,6 +74,9 @@ class RootState extends State<Root> {
   }
 
   hideIndicator() {
+    if (_indicatorTypes.isEmpty) {
+      return;
+    }
     if (_indicatorTypes.last == IndicatorType.shown) {
       return;
     }
