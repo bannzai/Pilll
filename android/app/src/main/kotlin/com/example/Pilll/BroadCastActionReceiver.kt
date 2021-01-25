fa1cd18a337210596765bf181d6fe85c7dc9ee8b
bannzai <kingkong999yhirose@gmail.com>
2021-01-25 14:01:23 +0900

    :sparkles: add cancellation code

--- android/app/src/main/kotlin/com/example/Pilll/BroadCastActionReceiver.kt
+++ android/app/src/main/kotlin/com/example/Pilll/BroadCastActionReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationManagerCompat
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.dart.DartExecutor
 import io.flutter.plugin.common.MethodChannel
@@ -12,12 +13,11 @@ import io.flutter.plugin.common.MethodChannel
 public class BroadCastActionReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.d("android message: ", "onReceive")
-        callRecordPill(context)
-    }
-    private fun methodChannel(context: Context?): MethodChannel? {
-        if (context == null) {
-            return null;
+        if (context != null) {
+            callRecordPill(context)
         }
+    }
+    private fun methodChannel(context: Context): MethodChannel {
         val flutterEngine = FlutterEngine(context)
         flutterEngine
                 .dartExecutor
@@ -26,7 +26,10 @@ public class BroadCastActionReceiver: BroadcastReceiver() {
                 )
         return MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "method.channel.MizukiOhashi.Pilll")
     }
-    private fun callRecordPill(context: Context?) {
-        methodChannel(context)?.invokeMethod("recordPill", "")
+    private fun callRecordPill(context: Context) {
+        methodChannel(context).invokeMethod("recordPill", "")
+        with(NotificationManagerCompat.from(context)) {
+            cancel(PilllFirebaseMessagingService.pillReminderID)
+        }
     }
 }
