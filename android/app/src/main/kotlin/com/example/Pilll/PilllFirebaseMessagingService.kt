701f78035c87d5d710bbccd80c58ac4810e1b044
bannzai <kingkong999yhirose@gmail.com>
2021-01-25 12:21:58 +0900

    :tada: call flutter method

--- android/app/src/main/kotlin/com/example/Pilll/PilllFirebaseMessagingService.kt
+++ android/app/src/main/kotlin/com/example/Pilll/PilllFirebaseMessagingService.kt
@@ -1,30 +1,29 @@
 package com.mizuki.Ohashi.Pilll
 
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import com.mizuki.Ohashi.Pilll.ActionReceiver
 import com.mizuki.Ohashi.Pilll.R
 
 public class PilllFirebaseMessagingService: FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         Log.d("android message: ", "onMessageReceived")
-        val notification = remoteMessage.notification
-        if (notification != null) {
-            send(notification, remoteMessage.data)
-        }
+        send(remoteMessage.data)
     }
 
-    fun send(notification: RemoteMessage.Notification, data: Map<String, String>) {
+    fun send( data: Map<String, String>) {
+        Log.d("android message: ", "send")
+
         // Create an explicit intent for an Activity in your app
-        val snoozeIntent = Intent(this, ActionReceiver::class.java).apply {
+        val snoozeIntent = Intent().apply {
             action = "PILL_REMINDER"
-            putExtra("TESTPUTEXTRA", "TESTPUTEXTRAVALUE")
         }
         val snoozePendingIntent: PendingIntent =
                 PendingIntent.getBroadcast(this, 0, snoozeIntent, 0)
@@ -33,13 +32,12 @@ public class PilllFirebaseMessagingService: FirebaseMessagingService() {
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("My notification")
                 .setContentText(data.toString())
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .addAction(R.drawable.common_google_signin_btn_icon_dark, "ORE NO ACTION",
                         snoozePendingIntent)
-                .setAutoCancel(true)
         with(NotificationManagerCompat.from(this)) {
             // notificationId is a unique int for each notification that you must define
-            notify(1, builder.build())
+            notify(0, builder.build())
         }
     }
 }
