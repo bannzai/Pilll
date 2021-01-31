84061ddafec874b1e92edab7947c1ce334ec1c2b
bannzai <kingkong999yhirose@gmail.com>
2021-01-31 12:03:46 +0900

    :art: add app card

---
+++ lib/components/molecules/app_card.dart
@@ -0,0 +1,25 @@
+import 'package:Pilll/components/atoms/color.dart';
+import 'package:flutter/material.dart';
+
+class AppCard extends StatelessWidget {
+  final Widget child;
+
+  const AppCard({Key key, this.child}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: PilllColors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: PilllColors.shadow,
+            blurRadius: 6.0,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
