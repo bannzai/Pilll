24fd594b8b4fcbc75d9a56322adf9e3776d5f12b
bannzai <kingkong999yhirose@gmail.com>
2021-02-01 14:43:20 +0900

    :shell: flutter pub run build_runner build --delete-conflicting-outputs

--- lib/entity/setting.freezed.dart
+++ lib/entity/setting.freezed.dart
@@ -191,7 +191,7 @@ class _$SettingTearOff {
       @required int durationMenstruation,
       @required List<ReminderTime> reminderTimes,
       @required @JsonSerializable(explicitToJson: true) bool isOnReminder,
-      bool isOnNotifyInNotTakenDuration = true}) {
+      bool isOnNotifyInNotTakenDuration = false}) {
     return _Setting(
       pillSheetTypeRawPath: pillSheetTypeRawPath,
       fromMenstruation: fromMenstruation,
@@ -342,7 +342,7 @@ class _$_Setting extends _Setting with DiagnosticableTreeMixin {
       @required this.durationMenstruation,
       @required this.reminderTimes,
       @required @JsonSerializable(explicitToJson: true) this.isOnReminder,
-      this.isOnNotifyInNotTakenDuration = true})
+      this.isOnNotifyInNotTakenDuration = false})
       : assert(pillSheetTypeRawPath != null),
         assert(fromMenstruation != null),
         assert(durationMenstruation != null),
@@ -365,7 +365,7 @@ class _$_Setting extends _Setting with DiagnosticableTreeMixin {
   @override
   @JsonSerializable(explicitToJson: true)
   final bool isOnReminder;
-  @JsonKey(defaultValue: true)
+  @JsonKey(defaultValue: false)
   @override
   final bool isOnNotifyInNotTakenDuration;
 
