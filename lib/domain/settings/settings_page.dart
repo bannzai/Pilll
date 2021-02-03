a2de9fbdd53174c5c198974d524343e2d114e85a
bannzai <kingkong999yhirose@gmail.com>
2021-02-04 00:22:39 +0900

    :recycle: fix condition

--- lib/domain/settings/settings_page.dart
+++ lib/domain/settings/settings_page.dart
@@ -303,23 +303,24 @@ class SettingsPage extends HookWidget {
         ];
       case SettingSection.other:
         return [
-          SettingListTitleRowModel(
-              title: "大型アップデート前の情報",
-              onTap: () {
-                analytics
-                    .logEvent(name: "did_select_migrate_132", parameters: {});
-                SharedPreferences.getInstance().then((storage) {
-                  final salvagedOldStartTakenDate =
-                      storage.getString(StringKey.salvagedOldStartTakenDate);
-                  final salvagedOldLastTakenDate =
-                      storage.getString(StringKey.salvagedOldLastTakenDate);
-                  Navigator.of(context)
-                      .push(InformationForBeforeMigrate132Route.route(
-                    salvagedOldStartTakenDate: salvagedOldStartTakenDate,
-                    salvagedOldLastTakenDate: salvagedOldLastTakenDate,
-                  ));
-                });
-              }),
+          if (settingState.userIsUpdatedFrom132)
+            SettingListTitleRowModel(
+                title: "大型アップデート前の情報",
+                onTap: () {
+                  analytics
+                      .logEvent(name: "did_select_migrate_132", parameters: {});
+                  SharedPreferences.getInstance().then((storage) {
+                    final salvagedOldStartTakenDate =
+                        storage.getString(StringKey.salvagedOldStartTakenDate);
+                    final salvagedOldLastTakenDate =
+                        storage.getString(StringKey.salvagedOldLastTakenDate);
+                    Navigator.of(context)
+                        .push(InformationForBeforeMigrate132Route.route(
+                      salvagedOldStartTakenDate: salvagedOldStartTakenDate,
+                      salvagedOldLastTakenDate: salvagedOldLastTakenDate,
+                    ));
+                  });
+                }),
           SettingListTitleRowModel(
               title: "利用規約",
               onTap: () {
