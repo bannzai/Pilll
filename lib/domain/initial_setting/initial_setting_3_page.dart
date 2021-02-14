8b472b294ca4228b7b146860557692512f837b79
bannzai <kingkonog999yhirose@gmail.com>
2021-02-14 14:10:59 +0900

    :sparkles: passing pill sheet total count from outside and adjustment index

--- lib/domain/initial_setting/initial_setting_3_page.dart
+++ lib/domain/initial_setting/initial_setting_3_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/all.dart';
+import 'package:Pilll/entity/pill_sheet_type.dart';
 
 class InitialSetting3Page extends HookWidget {
   @override
@@ -17,6 +18,7 @@ class InitialSetting3Page extends HookWidget {
       done: () {
         Navigator.of(context).push(InitialSetting4PageRoute.route());
       },
+      pillSheetTotalCount: state.entity.pillSheetType.totalCount,
       model: SettingMenstruationPageModel(
         pillSheetType: state.entity.pillSheetType,
         selectedFromMenstruation: state.entity.fromMenstruation,
