9a1693e7921a49cc136d0ce9d8ed94403860845d
bannzai <kingkonog999yhirose@gmail.com>
2021-02-14 20:12:36 +0900

    Revert ":shell: flutter pub run build_runner build --delete-conflicting-outputs"
    This reverts commit 43597aa9f1ce521c17c62d9e1da7369e4f97a347.

---
+++ lib/entity/user.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_UserPrivate _$_$_UserPrivateFromJson(Map<String, dynamic> json) {
+  return _$_UserPrivate(
+    fcmToken: json['fcmToken'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_UserPrivateToJson(_$_UserPrivate instance) =>
+    <String, dynamic>{
+      'fcmToken': instance.fcmToken,
+    };
+
+_$_User _$_$_UserFromJson(Map<String, dynamic> json) {
+  return _$_User(
+    anonymouseUserID: json['anonymouseUserID'] as String,
+    setting: json['settings'] == null
+        ? null
+        : Setting.fromJson(json['settings'] as Map<String, dynamic>),
+    migratedFlutter: json['migratedFlutter'] as bool ?? false,
+  );
+}
+
+Map<String, dynamic> _$_$_UserToJson(_$_User instance) => <String, dynamic>{
+      'anonymouseUserID': instance.anonymouseUserID,
+      'settings': instance.setting,
+      'migratedFlutter': instance.migratedFlutter,
+    };
