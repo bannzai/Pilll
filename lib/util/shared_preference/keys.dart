90a2ceeda78dff947c8c2ac0ab7dc5e6b98a5a61
bannzai <kingkong999yhirose@gmail.com>
2021-02-14 19:40:22 +0900

    :memo: add keys

--- lib/util/shared_preference/keys.dart
+++ lib/util/shared_preference/keys.dart
@@ -6,6 +6,12 @@ extension StringKey on String {
   static final String firebaseAnonymousUserID = "firebaseAnonymousUserID";
   static final String salvagedOldStartTakenDate = "salvagedOldStartTakenDate";
   static final String salvagedOldLastTakenDate = "salvagedOldLastTakenDate";
+  static final String beginingVersionKey = "beginingVersion";
+}
+
+extension ReleaseNoteKey on String {
+  static final String renewal = "release_notes_shown_renewal";
+  static final String version2_2_0 = "release_notes_shown_2.2.0";
 }
 
 extension IntKey on String {
