9be85d0e2e93d45d8df13f1446fead56b640fcee
bannzai <kingkonog999yhirose@gmail.com>
2021-02-23 13:24:32 +0900

    :memo: to more short url

--- lib/inquiry/inquiry.dart
+++ lib/inquiry/inquiry.dart
@@ -1,15 +1,15 @@
 import 'package:Pilll/auth/auth.dart';
 import 'package:Pilll/database/database.dart';
 import 'package:Pilll/entity/pill_sheet.dart';
-import 'package:Pilll/entity/setting.dart';
 import 'package:Pilll/service/pill_sheet.dart';
-import 'package:Pilll/service/setting.dart';
 import 'package:Pilll/util/environment.dart';
 import 'package:package_info/package_info.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 inquiry() {
-  PackageInfo.fromPlatform().then((value) => debugInfo(", ")).then((info) {
+  PackageInfo.fromPlatform().then((value) => debugInfo(",")).then((info) {
+    print(
+        "length:${Uri.encodeFull("https://form.run/@pilll-reminder-app-1613749422?_field_14=$info").length}");
     launch(
         Uri.encodeFull(
             "https://form.run/@pilll-reminder-app-1613749422?_field_14=$info"),
@@ -22,21 +22,14 @@ Future<String> debugInfo(String separator) async {
   DatabaseConnection databaseConnection = DatabaseConnection(userID);
   PillSheetModel pillSheet =
       await PillSheetService(databaseConnection).fetchLast();
-  Setting setting = await SettingService(databaseConnection).fetch();
   final package = await PackageInfo.fromPlatform();
-  final appName = package.appName;
   final buildNumber = package.buildNumber;
-  final packageName = package.packageName;
   final contents = [
-    "DEBUG INFO",
-    "appName: $appName",
-    "packageName: $packageName",
-    "buildNumber: $buildNumber",
-    "env: ${Environment.isProduction ? "production" : "development"}",
-    "user id: $userID",
-    "pillSheet.entity.id: ${pillSheet?.id}",
-    "pillSheetState.entity: ${pillSheet == null ? "null" : pillSheet.toJson()}",
-    "settingState.entity: ${setting == null ? "null" : setting.toJson()}",
+    "buildNumber:$buildNumber",
+    "env:${Environment.isProduction ? "prod" : "dev"}",
+    "userID:$userID",
+    "pillSheetID:${pillSheet?.id}",
+    "pillSheetStateJSON:${pillSheet == null ? "null" : pillSheet.toJson()}",
   ];
   return contents.join(separator);
 }
