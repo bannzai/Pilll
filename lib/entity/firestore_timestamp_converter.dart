56641ddb53511422c1fa15b3128bb5ea78bb634f
bannzai <kingkong999yhirose@gmail.com>
2021-03-12 11:02:10 +0900

    :recycle: fix timestamp converter

--- lib/entity/firestore_timestamp_converter.dart
+++ lib/entity/firestore_timestamp_converter.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class TimestampConverter {
-  static Timestamp? dateTimeToTimestamp(DateTime? dateTime) =>
-      dateTime == null ? null : Timestamp.fromDate(dateTime);
-  static DateTime? timestampToDateTime(Timestamp? timestamp) =>
-      timestamp == null ? null : timestamp.toDate();
+  static Timestamp dateTimeToTimestamp(DateTime dateTime) =>
+      Timestamp.fromDate(dateTime);
+  static DateTime timestampToDateTime(Timestamp timestamp) =>
+      timestamp.toDate();
 }
