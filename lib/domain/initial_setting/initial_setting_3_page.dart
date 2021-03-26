eb7044af5710c439f8be20d8d398033e36d58106
bannzai <kingkonog999yhirose@gmail.com>
2021-03-26 15:37:20 +0900

    :recycle: Remove unnecessary import

--- lib/domain/initial_setting/initial_setting_3_page.dart
+++ lib/domain/initial_setting/initial_setting_3_page.dart
@@ -5,7 +5,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:pilll/entity/pill_sheet_type.dart';
 
 class InitialSetting3Page extends HookWidget {
   @override
