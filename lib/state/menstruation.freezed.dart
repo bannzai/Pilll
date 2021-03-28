8f0340f92e112f7637ef65d544743fe8bfdb0e93
bannzai <kingkonog999yhirose@gmail.com>
2021-03-28 16:17:23 +0900

    :shell: flutter pub run build_runner build --delete-conflicting-outputs

---
+++ lib/state/menstruation.freezed.dart
@@ -0,0 +1,171 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'menstruation.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$MenstruationStateTearOff {
+  const _$MenstruationStateTearOff();
+
+  _MenstruationState call(
+      {required int displayMonthOffset,
+      List<Menstruation> entities = const []}) {
+    return _MenstruationState(
+      displayMonthOffset: displayMonthOffset,
+      entities: entities,
+    );
+  }
+}
+
+/// @nodoc
+const $MenstruationState = _$MenstruationStateTearOff();
+
+/// @nodoc
+mixin _$MenstruationState {
+  int get displayMonthOffset => throw _privateConstructorUsedError;
+  List<Menstruation> get entities => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $MenstruationStateCopyWith<MenstruationState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MenstruationStateCopyWith<$Res> {
+  factory $MenstruationStateCopyWith(
+          MenstruationState value, $Res Function(MenstruationState) then) =
+      _$MenstruationStateCopyWithImpl<$Res>;
+  $Res call({int displayMonthOffset, List<Menstruation> entities});
+}
+
+/// @nodoc
+class _$MenstruationStateCopyWithImpl<$Res>
+    implements $MenstruationStateCopyWith<$Res> {
+  _$MenstruationStateCopyWithImpl(this._value, this._then);
+
+  final MenstruationState _value;
+  // ignore: unused_field
+  final $Res Function(MenstruationState) _then;
+
+  @override
+  $Res call({
+    Object? displayMonthOffset = freezed,
+    Object? entities = freezed,
+  }) {
+    return _then(_value.copyWith(
+      displayMonthOffset: displayMonthOffset == freezed
+          ? _value.displayMonthOffset
+          : displayMonthOffset // ignore: cast_nullable_to_non_nullable
+              as int,
+      entities: entities == freezed
+          ? _value.entities
+          : entities // ignore: cast_nullable_to_non_nullable
+              as List<Menstruation>,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$MenstruationStateCopyWith<$Res>
+    implements $MenstruationStateCopyWith<$Res> {
+  factory _$MenstruationStateCopyWith(
+          _MenstruationState value, $Res Function(_MenstruationState) then) =
+      __$MenstruationStateCopyWithImpl<$Res>;
+  @override
+  $Res call({int displayMonthOffset, List<Menstruation> entities});
+}
+
+/// @nodoc
+class __$MenstruationStateCopyWithImpl<$Res>
+    extends _$MenstruationStateCopyWithImpl<$Res>
+    implements _$MenstruationStateCopyWith<$Res> {
+  __$MenstruationStateCopyWithImpl(
+      _MenstruationState _value, $Res Function(_MenstruationState) _then)
+      : super(_value, (v) => _then(v as _MenstruationState));
+
+  @override
+  _MenstruationState get _value => super._value as _MenstruationState;
+
+  @override
+  $Res call({
+    Object? displayMonthOffset = freezed,
+    Object? entities = freezed,
+  }) {
+    return _then(_MenstruationState(
+      displayMonthOffset: displayMonthOffset == freezed
+          ? _value.displayMonthOffset
+          : displayMonthOffset // ignore: cast_nullable_to_non_nullable
+              as int,
+      entities: entities == freezed
+          ? _value.entities
+          : entities // ignore: cast_nullable_to_non_nullable
+              as List<Menstruation>,
+    ));
+  }
+}
+
+/// @nodoc
+class _$_MenstruationState extends _MenstruationState {
+  _$_MenstruationState(
+      {required this.displayMonthOffset, this.entities = const []})
+      : super._();
+
+  @override
+  final int displayMonthOffset;
+  @JsonKey(defaultValue: const [])
+  @override
+  final List<Menstruation> entities;
+
+  @override
+  String toString() {
+    return 'MenstruationState(displayMonthOffset: $displayMonthOffset, entities: $entities)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _MenstruationState &&
+            (identical(other.displayMonthOffset, displayMonthOffset) ||
+                const DeepCollectionEquality()
+                    .equals(other.displayMonthOffset, displayMonthOffset)) &&
+            (identical(other.entities, entities) ||
+                const DeepCollectionEquality()
+                    .equals(other.entities, entities)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(displayMonthOffset) ^
+      const DeepCollectionEquality().hash(entities);
+
+  @JsonKey(ignore: true)
+  @override
+  _$MenstruationStateCopyWith<_MenstruationState> get copyWith =>
+      __$MenstruationStateCopyWithImpl<_MenstruationState>(this, _$identity);
+}
+
+abstract class _MenstruationState extends MenstruationState {
+  factory _MenstruationState(
+      {required int displayMonthOffset,
+      List<Menstruation> entities}) = _$_MenstruationState;
+  _MenstruationState._() : super._();
+
+  @override
+  int get displayMonthOffset => throw _privateConstructorUsedError;
+  @override
+  List<Menstruation> get entities => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$MenstruationStateCopyWith<_MenstruationState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
