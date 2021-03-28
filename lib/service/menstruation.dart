42b9ca9f88bf8b5f3624991b9f5fd40c05dea587
bannzai <kingkonog999yhirose@gmail.com>
2021-03-28 18:00:09 +0900

    :sparkles: add menstruation service

---
+++ lib/service/menstruation.dart
@@ -0,0 +1,22 @@
+import 'package:pilll/database/database.dart';
+import 'package:pilll/entity/menstruation.dart';
+import 'package:riverpod/riverpod.dart';
+
+final menstruationServiceProvider = Provider<MenstruationService>(
+    (ref) => MenstruationService(ref.watch(databaseProvider)));
+
+class MenstruationService {
+  final DatabaseConnection _database;
+
+  MenstruationService(this._database);
+  Future<List<Menstruation>> fetchAll() {
+    return _database
+        .menstruationsReference()
+        .where(MenstruationFirestoreKey.deletedAt, isNull: false)
+        .orderBy(MenstruationFirestoreKey.beginDate)
+        .get()
+        .then((event) => event.docs
+            .map((doc) => Menstruation.fromJson(doc.data()!))
+            .toList());
+  }
+}
