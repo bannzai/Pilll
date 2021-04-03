15edff73a2880065b624b78f3a6c43992c95a4e2
bannzai <kingkonog999yhirose@gmail.com>
2021-04-04 01:05:56 +0900

    :sparkles: using draggable scroll sheet

--- lib/domain/menstruation/menstruation_edit_page.dart
+++ lib/domain/menstruation/menstruation_edit_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/domain/calendar/calendar.dart';
 import 'package:pilll/domain/calendar/calendar_date_header.dart';
 import 'package:pilll/domain/calendar/monthly_calendar_state.dart';
@@ -13,21 +14,35 @@ class MenstruationEditPage extends HookWidget {
     // final state = useProvider(menstruationEditProvider(entity).state);
     final currentMonth = today();
     final nextMonth = DateTime(today().year, today().month + 1, today().day);
-    return ListView(
-      children: [
-        CalendarDateHeader(date: currentMonth),
-        Calendar(
-          calendarState: MonthlyCalendarState(currentMonth),
-          bandModels: [],
-          horizontalPadding: 0,
-        ),
-        CalendarDateHeader(date: nextMonth),
-        Calendar(
-          calendarState: MonthlyCalendarState(nextMonth),
-          bandModels: [],
-          horizontalPadding: 0,
-        ),
-      ],
+    return DraggableScrollableSheet(
+      initialChildSize: 0.7,
+      builder: (context, scrollController) {
+        return Container(
+          decoration: BoxDecoration(
+              color: PilllColors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: const Radius.circular(20.0),
+                topRight: const Radius.circular(20.0),
+              )),
+          child: ListView(
+            controller: scrollController,
+            children: [
+              CalendarDateHeader(date: currentMonth),
+              Calendar(
+                calendarState: MonthlyCalendarState(currentMonth),
+                bandModels: [],
+                horizontalPadding: 0,
+              ),
+              CalendarDateHeader(date: nextMonth),
+              Calendar(
+                calendarState: MonthlyCalendarState(nextMonth),
+                bandModels: [],
+                horizontalPadding: 0,
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
