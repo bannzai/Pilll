9b0a7b4d1fb47945bfb4657a7b174c0f859f78ab
bannzai <kingkong999yhirose@gmail.com>
2021-04-04 17:37:49 +0900

    :recycle: rename more explicity to date for month

--- lib/domain/calendar/monthly_calendar_state.dart
+++ lib/domain/calendar/monthly_calendar_state.dart
@@ -2,44 +2,49 @@ import 'package:pilll/domain/calendar/date_range.dart';
 import 'package:pilll/domain/calendar/weekly_calendar_state.dart';
 import 'package:pilll/entity/weekday.dart';
 
+DateTime _firstDayOfMonth(DateTime date) {
+  return DateTime(date.year, date.month, 1);
+}
+
 abstract class MonthlyCalendarState {
-  DateTime get date;
+  DateTime get dateForMonth;
   DateRange dateRangeOfLine(int line);
   int lineCount();
   WeeklyCalendarState weeklyCalendarState(int line);
 }
 
 class CalendarTabState extends MonthlyCalendarState {
-  final DateTime date;
-  CalendarTabState(this.date);
+  final DateTime dateForMonth;
+  CalendarTabState(this.dateForMonth);
 
   DateRange dateRangeOfLine(int line) {
     if (line == 1) {
       return DateRange(
-        DateTime(date.year, date.month, 1 - _previousMonthDayCount()),
-        DateTime(date.year, date.month,
+        DateTime(dateForMonth.year, dateForMonth.month,
+            1 - _previousMonthDayCount()),
+        DateTime(dateForMonth.year, dateForMonth.month,
             Weekday.values.length - _previousMonthDayCount()),
       );
     }
     if (line == lineCount()) {
       return DateRange(
-        DateTime(date.year, date.month,
+        DateTime(dateForMonth.year, dateForMonth.month,
             Weekday.values.length * (line - 1) + 1 - _previousMonthDayCount()),
-        DateTime(date.year, date.month, _lastDay()),
+        DateTime(dateForMonth.year, dateForMonth.month, _lastDay()),
       );
     }
     var beginDay =
         Weekday.values.length * (line - 1) - _previousMonthDayCount() + 1;
     var endDay = Weekday.values.length * line - _previousMonthDayCount();
     return DateRange(
-      DateTime(date.year, date.month, beginDay),
-      DateTime(date.year, date.month, endDay),
+      DateTime(dateForMonth.year, dateForMonth.month, beginDay),
+      DateTime(dateForMonth.year, dateForMonth.month, endDay),
     );
   }
 
-  int _lastDay() => DateTime(date.year, date.month + 1, 0).day;
+  int _lastDay() => DateTime(dateForMonth.year, dateForMonth.month + 1, 0).day;
   int _weekdayOffset() =>
-      WeekdayFunctions.weekdayFromDate(_firstDayOfMonth(date)).index;
+      WeekdayFunctions.weekdayFromDate(_firstDayOfMonth(dateForMonth)).index;
   int _previousMonthDayCount() => _weekdayOffset();
   int _tileCount() => _previousMonthDayCount() + _lastDay();
   int lineCount() => (_tileCount() / Weekday.values.length).ceil();
@@ -47,11 +52,7 @@ class CalendarTabState extends MonthlyCalendarState {
   WeeklyCalendarState weeklyCalendarState(int line) {
     return MultilineWeeklyCalendarState(
       dateRangeOfLine(line),
-      date,
+      dateForMonth,
     );
   }
-}
-
-DateTime _firstDayOfMonth(DateTime date) {
-  return DateTime(date.year, date.month, 1);
 }
