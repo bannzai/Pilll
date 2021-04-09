e920853c3c57baf8c74c8fac302d0669aea3cef1
bannzai <kingkong999yhirose@gmail.com>
2021-04-09 14:42:54 +0900

    :recycle: Remove unnecessary early return

--- lib/store/menstruation_edit.dart
+++ lib/store/menstruation_edit.dart
@@ -37,9 +37,6 @@ class MenstruationEditStore extends StateNotifier<MenstruationEditState> {
   }
 
   tappedDate(DateTime date) {
-    if (date.isAfter(today())) {
-      return;
-    }
     final menstruation = state.menstruation;
     if (menstruation == null) {
       state = state.copyWith(
