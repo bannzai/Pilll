f580b2bc73aa87f4defb00d942cd733de6fad914
bannzai <kingkonog999yhirose@gmail.com>
2021-04-11 20:15:40 +0900

    :sparkles: add ontap to band view

--- lib/domain/calendar/calendar_band.dart
+++ lib/domain/calendar/calendar_band.dart
@@ -15,29 +15,35 @@ class CalendarBand extends StatelessWidget {
     required this.model,
     required this.isLineBreaked,
     required this.width,
+    this.onTap,
   }) : super(key: key);
 
   final CalendarBandModel model;
   final bool isLineBreaked;
   final double width;
+  final Function(CalendarBandModel)? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: CalendarBandConst.height,
-      child: Stack(
-        children: [
-          CustomPaint(
-            painter: DiagonalStripedLine(
-                color: model.color, isNecessaryBorder: model.isNecessaryBorder),
-            size: Size(width, CalendarBandConst.height),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10),
-            child: Text(isLineBreaked ? "" : model.label,
-                style: FontType.sSmallTitle.merge(TextColorStyle.white)),
-          ),
-        ],
+    return GestureDetector(
+      onTap: () => onTap != null ? onTap!(model) : null,
+      child: Container(
+        height: CalendarBandConst.height,
+        child: Stack(
+          children: [
+            CustomPaint(
+              painter: DiagonalStripedLine(
+                  color: model.color,
+                  isNecessaryBorder: model.isNecessaryBorder),
+              size: Size(width, CalendarBandConst.height),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(isLineBreaked ? "" : model.label,
+                  style: FontType.sSmallTitle.merge(TextColorStyle.white)),
+            ),
+          ],
+        ),
       ),
     );
   }
