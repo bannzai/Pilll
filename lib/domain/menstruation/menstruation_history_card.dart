ca8599b1e105b035c827d2db0070bb558a386946
bannzai <kingkong999yhirose@gmail.com>
2021-04-22 14:27:26 +0900

    :art: Fix card UI

--- lib/domain/menstruation/menstruation_history_card.dart
+++ lib/domain/menstruation/menstruation_history_card.dart
@@ -1,6 +1,7 @@
 import 'package:pilll/components/atoms/buttons.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
+import 'package:pilll/components/molecules/app_card.dart';
 import 'package:pilll/domain/menstruation/menstruation_history_row.dart';
 import 'package:pilll/entity/menstruation.dart';
 import 'package:flutter/material.dart';
@@ -21,23 +22,49 @@ class MenstruationHistoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("生理記録", style: FontType.sBigTitle.merge(TextColorStyle.main)),
-        Column(
-          children:
-              _rows().map((e) => MenstruationHistoryRow(state: e)).toList(),
+    return AppCard(
+      child: Padding(
+        padding:
+            const EdgeInsets.only(top: 16, left: 16, bottom: 16, right: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("生理記録", style: FontType.sBigTitle.merge(TextColorStyle.main)),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              children:
+                  _rows().map((e) => MenstruationHistoryRow(state: e)).toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SecondaryButton(text: "もっと見る", onPressed: () {}),
+              ],
+            ),
+          ],
         ),
-        SecondaryButton(text: "もっと見る", onPressed: () {}),
-      ],
+      ),
     );
+  }
+
+  String? _prefix(int i) {
+    if (i == 0) {
+      return "前回";
+    }
+    if (i == 1) {
+      return "前々回";
+    }
+    return null;
   }
 
   List<MenstruationHistoryRowState> _rows() {
     final menstruations = state.menstruations;
-    menstruations.sort((a, b) => a.beginDate.compareTo(b.beginDate));
+    menstruations.sort((a, b) => b.beginDate.compareTo(a.beginDate));
     return menstruations
-        .map((element) => MenstruationHistoryRowState(element))
+        .asMap()
+        .map((index, element) => MapEntry(
+            index, MenstruationHistoryRowState(element, _prefix(index))))
+        .values
         .toList()
         .reversed
         .fold<List<MenstruationHistoryRowState>>([], (value, element) {
