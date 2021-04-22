a662dfd7e27a470e5909dfac8898ff7870639a02
bannzai <kingkong999yhirose@gmail.com>
2021-04-22 14:27:08 +0900

    :art: fix row UI

--- lib/domain/menstruation/menstruation_history_row.dart
+++ lib/domain/menstruation/menstruation_history_row.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/domain/calendar/date_range.dart';
 import 'package:pilll/entity/menstruation.dart';
@@ -7,15 +6,37 @@ import 'package:flutter/material.dart';
 
 class MenstruationHistoryRowState {
   final Menstruation menstruation;
+  final String? prefix;
   int? menstruationDuration;
 
-  MenstruationHistoryRowState(this.menstruation);
+  MenstruationHistoryRowState(this.menstruation, this.prefix);
 
   static int diff(
       MenstruationHistoryRowState lhs, MenstruationHistoryRowState rhs) {
     final range =
         DateRange(lhs.menstruation.beginDate, rhs.menstruation.beginDate);
-    return range.days - 1;
+    return range.days.abs() - 1;
+  }
+
+  String get dateRange {
+    return "(${DateTimeFormatter.monthAndDay(menstruation.beginDate)} - ${DateTimeFormatter.monthAndDay(menstruation.endDate)})";
+  }
+
+  String get duration {
+    final menstruationDuration = this.menstruationDuration;
+    if (menstruationDuration == null) {
+      return "-";
+    }
+    return "$menstruationDuration周期";
+  }
+
+  double get width {
+    final double widthForDay = 10;
+    final menstruationDuration = this.menstruationDuration;
+    if (menstruationDuration == null) {
+      return widthForDay * 28;
+    }
+    return widthForDay * menstruationDuration;
   }
 }
 
@@ -25,37 +46,56 @@ class MenstruationHistoryRow extends StatelessWidget {
   const MenstruationHistoryRow({Key? key, required this.state})
       : super(key: key);
 
-  String get _menstruationDurationString =>
-      state.menstruationDuration == null ? "-" : "$state.menstruationDuration";
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           children: [
-            Text(DateTimeFormatter.monthAndDay(state.menstruation.beginDate)),
-            Text(" - "),
-            Text(DateTimeFormatter.monthAndDay(state.menstruation.endDate)),
+            if (state.prefix != null) Text(state.prefix ?? ""),
+            Text(state.dateRange),
           ],
         ),
-        Row(
-          children: List.generate(
-            min(9, state.menstruation.dateRange.days),
-            (index) {
-              return _circle();
-            },
+        Container(
+          width: state.width,
+          decoration: BoxDecoration(
+            color: PilllColors.tinBackground,
+            borderRadius: BorderRadius.circular(26),
+          ),
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Row(
+              children: [
+                ...List.generate(
+                  state.menstruation.dateRange.days,
+                  (index) {
+                    return [_circle(), SizedBox(width: 4)];
+                  },
+                ).expand((element) => element).toList(),
+                Spacer(),
+                Text(
+                  state.duration,
+                  textAlign: TextAlign.end,
+                ),
+                SizedBox(width: 10),
+              ],
+            ),
           ),
         ),
-        Text(_menstruationDurationString),
       ],
     );
   }
 
   Widget _circle() {
     return Container(
+      decoration: BoxDecoration(
+        color: PilllColors.menstruation,
+        borderRadius: BorderRadius.circular(6),
+      ),
       width: 12,
       height: 12,
-      color: PilllColors.menstruation,
     );
   }
 }
