d036dec65e3dc09b5c6919926eba97a34f08e3f8
bannzai <kingkong999yhirose@gmail.com>
2021-04-24 09:53:58 +0900

    :recycle: Remove header and more

--- lib/domain/calendar/calendar_card.dart
+++ lib/domain/calendar/calendar_card.dart
@@ -9,9 +9,6 @@ import 'package:pilll/domain/calendar/calendar_list_page.dart';
 import 'package:pilll/entity/menstruation.dart';
 import 'package:pilll/entity/pill_sheet.dart';
 import 'package:pilll/entity/setting.dart';
-import 'package:pilll/components/atoms/buttons.dart';
-import 'package:pilll/components/atoms/font.dart';
-import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/util/datetime/day.dart';
 import 'package:pilll/util/formatter/date_time_formatter.dart';
 import 'package:flutter/cupertino.dart';
@@ -81,7 +78,6 @@ class CalendarCard extends StatelessWidget {
     return AppCard(
       child: Column(
         children: <Widget>[
-          _header(context),
           Calendar(
             calendarState: CalendarTabState(state.date),
             bandModels: state.bands(),
@@ -91,47 +87,6 @@ class CalendarCard extends StatelessWidget {
             },
             horizontalPadding: 16,
           ),
-          _more(context),
-        ],
-      ),
-    );
-  }
-
-  Widget _header(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints.expand(height: 64),
-      child: Row(
-        children: [
-          SizedBox(width: 16),
-          Text(
-            state.dateTitle,
-            textAlign: TextAlign.left,
-            style: FontType.cardHeader.merge(TextColorStyle.noshime),
-          ),
-          Spacer(),
-          SizedBox(width: 16),
-        ],
-      ),
-    );
-  }
-
-  Widget _more(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints.expand(height: 60),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          SecondaryButton(
-            text: "もっと見る",
-            onPressed: () {
-              Navigator.of(context).push(
-                () {
-                  return CalendarListPageRoute.route(
-                      state.calendarListModels());
-                }(),
-              );
-            },
-          )
         ],
       ),
     );
