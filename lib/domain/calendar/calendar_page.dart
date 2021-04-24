e24a290f994cf4145d18614b4a09da15e48cb56a
bannzai <kingkong999yhirose@gmail.com>
2021-04-24 14:22:26 +0900

    :sparkels: using index operator from store

--- lib/domain/calendar/calendar_page.dart
+++ lib/domain/calendar/calendar_page.dart
@@ -11,8 +11,8 @@ import 'package:pilll/store/calendar_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:pilll/util/datetime/day.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 abstract class CalendarPageConstants {
   static final double halfCircleHeight = 300;
@@ -22,14 +22,29 @@ class CalendarPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final state = useProvider(calendarPageStateProvider.state);
+    final store = useProvider(calendarPageStateProvider);
     final settingEntity = state.setting;
     if (state.shouldShowIndicator) {
       return ScaffoldIndicator();
     }
+    final ItemPositionsListener itemPositionsListener =
+        ItemPositionsListener.create();
+    itemPositionsListener.itemPositions.addListener(() {
+      final index = itemPositionsListener.itemPositions.value.last.index;
+      store.updateCurrentCalendarIndex(index);
+    });
+    final ItemScrollController itemScrollController = ItemScrollController();
     return Scaffold(
       backgroundColor: PilllColors.background,
       appBar: AppBar(
-        leading: AppBarTextActionButton(onPressed: () {}, text: "今日"),
+        leading: AppBarTextActionButton(
+            onPressed: () {
+              store.updateCurrentCalendarIndex(state.todayCalendarIndex);
+              itemScrollController.scrollTo(
+                  index: state.todayCalendarIndex,
+                  duration: Duration(milliseconds: 300));
+            },
+            text: "今日"),
         actions: [
           IconButton(
             icon: SvgPicture.asset("images/help.svg"),
@@ -44,7 +59,7 @@ class CalendarPage extends HookWidget {
           ),
         ],
         title: Text(
-          "2020年8月",
+          state.displayMonth,
           style: TextColorStyle.main.merge(FontType.subTitle),
         ),
         elevation: 0,
@@ -54,16 +69,30 @@ class CalendarPage extends HookWidget {
       body: SafeArea(
         child: ListView(
           children: <Widget>[
-            Container(
+            SizedBox(
               width: MediaQuery.of(context).size.width,
               height: 444,
-              child: CalendarCard(
-                state: CalendarCardState(
-                  date: today(),
-                  latestPillSheet: state.latestPillSheet,
-                  setting: settingEntity,
-                  menstruations: state.menstruations,
-                ),
+              child: ScrollablePositionedList.builder(
+                itemScrollController: itemScrollController,
+                initialScrollIndex: state.currentCalendarIndex,
+                scrollDirection: Axis.horizontal,
+                physics: PageScrollPhysics(),
+                itemCount: state.calendarDataSource.length,
+                itemPositionsListener: itemPositionsListener,
+                itemBuilder: (context, index) {
+                  final date = state.calendarDataSource[index];
+                  return SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: CalendarCard(
+                      state: CalendarCardState(
+                        date: date,
+                        latestPillSheet: state.latestPillSheet,
+                        setting: settingEntity,
+                        menstruations: state.menstruations,
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
           ],
