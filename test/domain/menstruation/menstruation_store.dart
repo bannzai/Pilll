a6b15d72f7bc96b6554d227543c3e3c5307369a4
bannzai <kingkong999yhirose@gmail.com>
2021-04-30 10:46:36 +0900

    :sparkels: add test case

--- test/domain/menstruation/menstruation_store.dart
+++ test/domain/menstruation/menstruation_store.dart
@@ -89,5 +89,64 @@ void main() {
                 countdownString: "2日目"));
       },
     );
+    test(
+      "if latestMenstruation is exists but out of duration and latest pill sheet is null",
+      () async {
+        final originalTodayRepository = todayRepository;
+        final mockTodayRepository = MockTodayService();
+        todayRepository = mockTodayRepository;
+        final today = DateTime(2021, 04, 29);
+        when(mockTodayRepository.today()).thenReturn(today);
+        addTearDown(() {
+          todayRepository = originalTodayRepository;
+        });
+
+        final menstruationService = MockMnestruationService();
+        when(menstruationService.fetchAll()).thenAnswer(
+          (realInvocation) => Future.value([
+            Menstruation(
+              beginDate: DateTime(2021, 03, 28),
+              endDate: DateTime(2021, 03, 30),
+              createdAt: DateTime(2021, 03, 28),
+            ),
+          ]),
+        );
+        when(menstruationService.subscribeAll()).thenAnswer(
+          (realInvocation) => Stream.value([
+            Menstruation(
+              beginDate: DateTime(2021, 03, 28),
+              endDate: DateTime(2021, 03, 30),
+              createdAt: DateTime(2021, 03, 28),
+            ),
+          ]),
+        );
+        final pillSheetService = MockPillSheetService();
+        when(pillSheetService.fetchLast())
+            .thenAnswer((realInvocation) => Future.value(null));
+        when(pillSheetService.subscribeForLatestPillSheet())
+            .thenAnswer((realInvocation) => Stream.empty());
+        final diaryService = MockDiaryService();
+        when(diaryService.fetchListAround90Days(today))
+            .thenAnswer((realInvocation) => Future.value([]));
+        when(diaryService.subscribe())
+            .thenAnswer((realInvocation) => Stream.empty());
+        final settingService = MockSettingService();
+        when(settingService.fetch())
+            .thenAnswer((realInvocation) => Future.value(null));
+        when(settingService.subscribe())
+            .thenAnswer((realInvocation) => Stream.empty());
+
+        final store = MenstruationStore(
+          menstruationService: menstruationService,
+          diaryService: diaryService,
+          settingService: settingService,
+          pillSheetService: pillSheetService,
+        );
+        await waitForResetStoreState();
+        final actual = store.cardState();
+
+        expect(actual, null);
+      },
+    );
   });
 }
