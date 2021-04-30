850d748be5748ec1db6d392ffc2fa07b216da93b
bannzai <kingkong999yhirose@gmail.com>
2021-04-30 09:21:40 +0900

    :memo: add mock service

--- test/helper/mock.dart
+++ test/helper/mock.dart
@@ -1,4 +1,6 @@
 import 'package:pilll/analytics.dart';
+import 'package:pilll/service/diary.dart';
+import 'package:pilll/service/menstruation.dart';
 import 'package:pilll/service/pill_sheet.dart';
 import 'package:pilll/service/setting.dart';
 import 'package:pilll/service/day.dart';
@@ -13,3 +15,7 @@ class MockSettingService extends Mock implements SettingService {}
 class MockPillSheetService extends Mock implements PillSheetService {}
 
 class MockAnalytics extends Mock implements AbstractAnalytics {}
+
+class MockDiaryService extends Mock implements DiaryService {}
+
+class MockMnestruationService extends Mock implements MenstruationService {}
