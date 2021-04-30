6d79bcb4b0e31f8cf6fb2dc080a5bfdd6bb10e55
bannzai <kingkong999yhirose@gmail.com>
2021-04-30 10:55:22 +0900

    :sparkels: add schedueld test case

--- test/domain/menstruation/menstruation_store.dart
+++ test/domain/menstruation/menstruation_store.dart
@@ -93,7 +93,7 @@ void main() {
       },
     );
     test(
-      "if latestMenstruation is exists but out of duration and latest pill sheet is null",
+      "if latestMenstruation is out of duration and latest pill sheet is null",
       () async {
         final originalTodayRepository = todayRepository;
         final mockTodayRepository = MockTodayService();
@@ -158,7 +158,7 @@ void main() {
     );
 
     test(
-      "if latestMenstruation is exists but out of duration and setting is null",
+      "if latestMenstruation is out of duration and setting is null",
       () async {
         final originalTodayRepository = todayRepository;
         final mockTodayRepository = MockTodayService();
@@ -220,6 +220,70 @@ void main() {
         final actual = store.cardState();
 
         expect(actual, null);
+      },
+    );
+
+    test(
+      "if latest pillsheet.beginingDate < today, when return schedueld card state",
+      () async {
+        final originalTodayRepository = todayRepository;
+        final mockTodayRepository = MockTodayService();
+        todayRepository = mockTodayRepository;
+        final today = DateTime(2021, 04, 29);
+        when(mockTodayRepository.today()).thenReturn(today);
+        addTearDown(() {
+          todayRepository = originalTodayRepository;
+        });
+
+        final menstruationService = MockMnestruationService();
+        when(menstruationService.fetchAll()).thenAnswer(
+          (realInvocation) => Future.value([]),
+        );
+        when(menstruationService.subscribeAll()).thenAnswer(
+          (realInvocation) => Stream.value([]),
+        );
+        final pillSheetService = MockPillSheetService();
+        when(pillSheetService.fetchLast()).thenAnswer(
+          (realInvocation) => Future.value(
+            PillSheetModel(
+              typeInfo: PillSheetType.pillsheet_21.typeInfo,
+              beginingDate: DateTime(2021, 04, 22),
+            ),
+          ),
+        );
+        when(pillSheetService.subscribeForLatestPillSheet())
+            .thenAnswer((realInvocation) => Stream.empty());
+        final diaryService = MockDiaryService();
+        when(diaryService.fetchListAround90Days(today))
+            .thenAnswer((realInvocation) => Future.value([]));
+        when(diaryService.subscribe())
+            .thenAnswer((realInvocation) => Stream.empty());
+        final settingService = MockSettingService();
+        when(settingService.fetch()).thenAnswer((realInvocation) =>
+            Future.value(Setting(
+                pillSheetTypeRawPath: PillSheetType.pillsheet_21.rawPath,
+                pillNumberForFromMenstruation: 22,
+                durationMenstruation: 3,
+                reminderTimes: [],
+                isOnReminder: true)));
+        when(settingService.subscribe())
+            .thenAnswer((realInvocation) => Stream.empty());
+
+        final store = MenstruationStore(
+          menstruationService: menstruationService,
+          diaryService: diaryService,
+          settingService: settingService,
+          pillSheetService: pillSheetService,
+        );
+        await waitForResetStoreState();
+        final actual = store.cardState();
+
+        expect(
+            actual,
+            MenstruationCardState(
+                title: "生理予定日",
+                scheduleDate: DateTime(2021, 05, 13),
+                countdownString: "あと14日"));
       },
     );
   });
