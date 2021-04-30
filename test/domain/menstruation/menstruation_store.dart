a486dc4eed6c75561ae4e54d174d6a244a1b80bd
bannzai <kingkong999yhirose@gmail.com>
2021-04-30 09:40:27 +0900

    :sparkles: add first test case

---
+++ test/domain/menstruation/menstruation_store.dart
@@ -0,0 +1,91 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pilll/domain/menstruation/menstruation_card_state.dart';
+import 'package:pilll/entity/menstruation.dart';
+import 'package:pilll/service/day.dart';
+import 'package:pilll/store/menstruation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../helper/mock.dart';
+
+void main() {
+  setUp(() {
+    SharedPreferences.setMockInitialValues({});
+  });
+
+  group("#cardState", () {
+    test(
+      "if latestMenstruation is into today, when return card state of begining about menstruation ",
+      () {
+        final originalTodayRepository = todayRepository;
+        final mockTodayRepository = MockTodayService();
+        todayRepository = mockTodayRepository;
+        final today = DateTime(2021, 04, 29);
+        when(mockTodayRepository.today()).thenReturn(today);
+        addTearDown(() {
+          todayRepository = originalTodayRepository;
+        });
+
+        final menstruationService = MockMnestruationService();
+        when(menstruationService.fetchAll()).thenAnswer(
+          (realInvocation) => Future.value([
+            Menstruation(
+              beginDate: DateTime(2021, 03, 28),
+              endDate: DateTime(2021, 03, 30),
+              createdAt: DateTime(2021, 03, 28),
+            ),
+            Menstruation(
+              beginDate: DateTime(2021, 04, 28),
+              endDate: DateTime(2021, 04, 30),
+              createdAt: DateTime(2021, 04, 28),
+            )
+          ]),
+        );
+        when(menstruationService.subscribeAll()).thenAnswer(
+          (realInvocation) => Stream.value([
+            Menstruation(
+              beginDate: DateTime(2021, 03, 28),
+              endDate: DateTime(2021, 03, 30),
+              createdAt: DateTime(2021, 03, 28),
+            ),
+            Menstruation(
+              beginDate: DateTime(2021, 04, 28),
+              endDate: DateTime(2021, 04, 30),
+              createdAt: DateTime(2021, 04, 28),
+            )
+          ]),
+        );
+        final pillSheetService = MockPillSheetService();
+        when(pillSheetService.fetchLast())
+            .thenAnswer((realInvocation) => Future.value(null));
+        when(pillSheetService.subscribeForLatestPillSheet())
+            .thenAnswer((realInvocation) => Stream.empty());
+        final diaryService = MockDiaryService();
+        when(diaryService.fetchListAround90Days(today))
+            .thenAnswer((realInvocation) => Future.value([]));
+        when(diaryService.subscribe())
+            .thenAnswer((realInvocation) => Stream.empty());
+        final settingService = MockSettingService();
+        when(settingService.fetch())
+            .thenAnswer((realInvocation) => Future.value(null));
+        when(settingService.subscribe())
+            .thenAnswer((realInvocation) => Stream.empty());
+
+        final store = MenstruationStore(
+          menstruationService: menstruationService,
+          diaryService: diaryService,
+          settingService: settingService,
+          pillSheetService: pillSheetService,
+        );
+        final actual = store.cardState();
+
+        expect(
+            actual,
+            MenstruationCardState(
+                title: "生理開始日",
+                scheduleDate: DateTime(2021, 04, 28),
+                countdownString: "2日目"));
+      },
+    );
+  });
+}
