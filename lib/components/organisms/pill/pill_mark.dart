675aec47615acd610171f9dd8db7d8ad5b161080
bannzai <kingkong999yhirose@gmail.com>
2021-05-05 17:26:15 +0900

    :recycle: Move pill mark type function to widget

--- lib/components/organisms/pill/pill_mark.dart
+++ lib/components/organisms/pill/pill_mark.dart
@@ -4,6 +4,8 @@ import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/util/environment.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter_svg/svg.dart';
 
 abstract class PillMarkConst {
   static final double edge = 20;
@@ -54,7 +56,7 @@ class _PillMarkState extends State<PillMark> with TickerProviderStateMixin {
     return Stack(
       clipBehavior: Clip.none,
       children: [
-        PillMarkTypeFunctions.create(widget.isDone, widget.pillSheetType),
+        _mark(widget.isDone, widget.pillSheetType),
         if (widget.hasRippleAnimation)
           // NOTE: pill mark size is 20px. Ripple view final size is 80px.
           // Positined ripple animation equal to (80px - 20px) / 2(to center) = 28;
@@ -73,6 +75,66 @@ class _PillMarkState extends State<PillMark> with TickerProviderStateMixin {
             ),
           ),
       ],
+    );
+  }
+
+  SvgPicture _checkImage() {
+    return SvgPicture.asset(
+      "images/checkmark.svg",
+      color: PilllColors.potti,
+      width: 11,
+      height: 8.5,
+    );
+  }
+
+  Widget _mark(bool isDone, PillMarkType type) {
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      radius: Radius.circular(10),
+      padding: EdgeInsets.zero,
+      color: type == PillMarkType.rest ? PilllColors.gray : Colors.transparent,
+      strokeWidth: type == PillMarkType.rest ? 1 : 0,
+      child: Container(
+        width: 20,
+        height: 20,
+        child: Center(
+          child: () {
+            switch (type) {
+              case PillMarkType.normal:
+                return null;
+              case PillMarkType.rest:
+                return isDone ? _checkImage() : null;
+              case PillMarkType.fake:
+                return isDone ? _checkImage() : null;
+              case PillMarkType.selected:
+                return null;
+              case PillMarkType.done:
+                return isDone ? _checkImage() : null;
+              default:
+                throw ArgumentError.notNull("");
+            }
+          }(),
+        ),
+        decoration: BoxDecoration(
+          color: () {
+            switch (type) {
+              case PillMarkType.normal:
+                return PilllColors.potti;
+              case PillMarkType.rest:
+                return PilllColors.blank;
+              case PillMarkType.fake:
+                return PilllColors.blank;
+              case PillMarkType.selected:
+                return PilllColors.enable;
+              case PillMarkType.done:
+                return PilllColors.lightGray;
+              default:
+                throw ArgumentError.notNull("");
+            }
+          }(),
+          shape: BoxShape.circle,
+        ),
+      ),
     );
   }
 }
--- lib/entity/pill_mark_type.dart
+++ lib/entity/pill_mark_type.dart
@@ -1,74 +1,7 @@
-import 'package:pilll/components/atoms/color.dart';
-import 'package:dotted_border/dotted_border.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-
 enum PillMarkType {
   normal,
   fake,
   rest,
   selected,
   done,
-}
-
-extension PillMarkTypeFunctions on PillMarkType {
-  static SvgPicture _checkImage() {
-    return SvgPicture.asset(
-      "images/checkmark.svg",
-      color: PilllColors.potti,
-      width: 11,
-      height: 8.5,
-    );
-  }
-
-  static Widget create(bool isDone, PillMarkType type) {
-    return DottedBorder(
-      borderType: BorderType.RRect,
-      radius: Radius.circular(10),
-      padding: EdgeInsets.zero,
-      color: type == PillMarkType.rest ? PilllColors.gray : Colors.transparent,
-      strokeWidth: type == PillMarkType.rest ? 1 : 0,
-      child: Container(
-        width: 20,
-        height: 20,
-        child: Center(
-          child: () {
-            switch (type) {
-              case PillMarkType.normal:
-                return null;
-              case PillMarkType.rest:
-                return isDone ? _checkImage() : null;
-              case PillMarkType.fake:
-                return isDone ? _checkImage() : null;
-              case PillMarkType.selected:
-                return null;
-              case PillMarkType.done:
-                return isDone ? _checkImage() : null;
-              default:
-                throw ArgumentError.notNull("");
-            }
-          }(),
-        ),
-        decoration: BoxDecoration(
-          color: () {
-            switch (type) {
-              case PillMarkType.normal:
-                return PilllColors.potti;
-              case PillMarkType.rest:
-                return PilllColors.blank;
-              case PillMarkType.fake:
-                return PilllColors.blank;
-              case PillMarkType.selected:
-                return PilllColors.enable;
-              case PillMarkType.done:
-                return PilllColors.lightGray;
-              default:
-                throw ArgumentError.notNull("");
-            }
-          }(),
-          shape: BoxShape.circle,
-        ),
-      ),
-    );
-  }
 }
