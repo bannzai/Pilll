7ae2dfc5bb1979615458f3b143b532a1e7ae6389
bannzai <kingkong999yhirose@gmail.com>
2021-05-06 22:07:31 +0900

    :recycle: Remove unused import

--- lib/service/user.dart
+++ lib/service/user.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:pilll/database/database.dart';
 import 'package:pilll/entity/package.dart';
 import 'package:pilll/entity/user.dart';
