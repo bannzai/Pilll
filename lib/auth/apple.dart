c0c5fadd64e33b27102c4649e4c22d389fa54220
bannzai <kingkong999yhirose@gmail.com>
2021-05-07 08:07:50 +0900

    :sparkles: using util.dart functions

--- lib/auth/apple.dart
+++ lib/auth/apple.dart
@@ -2,21 +2,21 @@ import 'dart:convert';
 
 import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:nonce/nonce.dart';
+import 'package:pilll/auth/util.dart';
 import 'package:pilll/util/environment.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 Future<UserCredential?> siwa(User user) async {
   try {
-    final rawNonce = Nonce.generate();
-    final state = Nonce.generate();
+    final rawNonce = generateNonce();
+    final state = generateNonce();
     final appleCredential = await SignInWithApple.getAppleIDCredential(
       scopes: [],
       webAuthenticationOptions: WebAuthenticationOptions(
         clientId: Environment.siwaServiceIdentifier,
         redirectUri: Uri.parse(Environment.androidSiwaRedirectURL),
       ),
-      nonce: sha256.convert(utf8.encode(rawNonce)).toString(),
+      nonce: sha256ofString(rawNonce).toString(),
       state: state,
     );
     print("appleCredential: $appleCredential");
--- lib/auth/util.dart
+++ lib/auth/util.dart
@@ -11,7 +11,6 @@ String generateNonce([int length = 32]) {
       .join();
 }
 
-/// Returns the sha256 hash of [input] in hex notation.
 String sha256ofString(String input) {
   final bytes = utf8.encode(input);
   final digest = sha256.convert(bytes);
