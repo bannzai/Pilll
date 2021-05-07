0375a578afa62823a5564287e4592278a3568312
bannzai <kingkong999yhirose@gmail.com>
2021-05-08 08:08:18 +0900

    :sparkles: Prepare signing method

--- lib/auth/apple.dart
+++ lib/auth/apple.dart
@@ -33,3 +33,34 @@ Future<UserCredential?> linkWithApple(User user) async {
     rethrow;
   }
 }
+
+Future<UserCredential?> signInWithApple(User user) async {
+  try {
+    final rawNonce = generateNonce();
+    final state = generateNonce();
+    final appleCredential = await SignInWithApple.getAppleIDCredential(
+      scopes: [AppleIDAuthorizationScopes.email],
+      webAuthenticationOptions: WebAuthenticationOptions(
+        clientId: Environment.siwaServiceIdentifier,
+        redirectUri: Uri.parse(Environment.androidSiwaRedirectURL),
+      ),
+      nonce: sha256ofString(rawNonce).toString(),
+      state: state,
+    );
+    print("appleCredential: $appleCredential");
+    if (state != appleCredential.state) {
+      throw AssertionError('state not matched!');
+    }
+    final credential = OAuthProvider('apple.com').credential(
+      idToken: appleCredential.identityToken,
+      accessToken: appleCredential.authorizationCode,
+      rawNonce: rawNonce,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  } on SignInWithAppleAuthorizationException catch (e) {
+    if (e.code == AuthorizationErrorCode.canceled) {
+      return Future.value(null);
+    }
+    rethrow;
+  }
+}
--- lib/auth/google.dart
+++ lib/auth/google.dart
@@ -22,3 +22,25 @@ Future<UserCredential?> linkWithGoogle(User user) async {
 
   return await user.linkWithCredential(credential);
 }
+
+Future<UserCredential?> signInWithGoogle(User user) async {
+  // NOTE: workaround https://github.com/flutter/flutter/issues/44564#issuecomment-655884103
+  final googleUser = await GoogleSignIn(
+    scopes: [
+      'email',
+    ],
+    hostedDomain: "",
+  ).signIn();
+
+  // NOTE: aborted
+  if (googleUser == null) {
+    return Future.value(null);
+  }
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
