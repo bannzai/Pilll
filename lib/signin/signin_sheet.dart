0c9cb62c0de7b72ac00e51c817213e595c6e3926
bannzai <kingkong999yhirose@gmail.com>
2021-05-08 09:24:54 +0900

    :sparkels: add signin sheet

---
+++ lib/signin/signin_sheet.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter/material.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+
+class SigninSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.5,
+      maxChildSize: 0.5,
+      builder: (context, scrollController) {
+        return Column(
+          children: [
+            SvgPicture.asset("assets/draggable_bar.svg", height: 6),
+            Text("アカウント登録"),
+            Text("アカウント登録すると\nデータの引き継ぎが可能になります"),
+            SignInWithAppleButton(onPressed: () {}),
+            OutlinedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.white),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                ),
+              ),
+              onPressed: () async {},
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SvgPicture.asset("assets/google_icon.svg", height: 35),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Google アカウントで登録',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
