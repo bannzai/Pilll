939050f46f934c7e6e7b0079935a7ab1de1601f3
bannzai <kingkong999yhirose@gmail.com>
2021-05-11 07:11:01 +0900

    :sparkles: add signin state

---
+++ lib/signin/signing_sheet_state.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'signin_sheet_state.freezed.dart';
+
+@freezed
+abstract class SigninSheetState implements _$SigninSheetState {
+  SigninSheetState._();
+  factory SigninSheetState({
+    required bool isLogin,
+  }) = _SigninSheetState;
+}
