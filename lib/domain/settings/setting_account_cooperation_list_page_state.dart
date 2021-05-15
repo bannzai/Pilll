466a0425242b6c0addfea812c879660e3d8e7ca8
bannzai <kingkong999yhirose@gmail.com>
2021-05-15 11:24:59 +0900

    :sparekls: add state of cooperation list

---
+++ lib/domain/settings/setting_account_cooperation_list_page_state.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pilll/auth/apple.dart' as apple;
+import 'package:pilll/auth/google.dart' as google;
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'setting_account_cooperation_list_page_state.freezed.dart';
+
+@freezed
+abstract class SettingAccountCooperationListState
+    implements _$SettingAccountCooperationListState {
+  SettingAccountCooperationListState._();
+  factory SettingAccountCooperationListState(User? user) =
+      _SettingAccountCooperationListState;
+
+  bool get isLinkedApple {
+    final user = this.user;
+    if (user == null) {
+      return false;
+    }
+    return apple.isLinkedAppleFor(user);
+  }
+
+  bool get isLinkedGoogle {
+    final user = this.user;
+    if (user == null) {
+      return false;
+    }
+    return google.isLinkedGoogleFor(user);
+  }
+}
