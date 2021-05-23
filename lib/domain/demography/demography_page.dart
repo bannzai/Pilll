edf5e0260518641d39387449daca31ea79045173
bannzai <kingkong999yhirose@gmail.com>
2021-05-23 16:37:48 +0900

    :recycle: scratch demography page and remove depend on userService

--- lib/domain/demography/demography_page.dart
+++ lib/domain/demography/demography_page.dart
@@ -1,53 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/buttons.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/domain/demography/demography_completed_dialog.dart';
-import 'package:pilll/entity/demographic.dart';
-import 'package:pilll/service/user.dart';
-import 'package:pilll/util/datetime/day.dart';
+import 'package:pilll/domain/demography/demography_page_state.dart';
+import 'package:pilll/domain/demography/demography_page_store.dart';
 import 'package:pilll/util/shared_preference/keys.dart';
 import 'package:pilll/util/toolbar/picker_toolbar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class DemographyPage extends StatefulWidget {
-  final UserService userService;
+String _unknown = "該当なし";
+
+class DemographyPage extends HookWidget {
   final VoidCallback done;
 
-  const DemographyPage(
-      {Key? key, required this.userService, required this.done})
-      : super(key: key);
-  @override
-  _DemographyPageState createState() => _DemographyPageState();
-}
-
-String _unknown = "該当なし";
-final _purposeDataSource = [
-  "婦人病の治療",
-  "生理・PMSの症状緩和",
-  "生理不順のため",
-  "避妊のため",
-  "美容のため",
-  "ホルモン療法",
-  _unknown,
-];
-
-class _DemographyPageState extends State<DemographyPage> {
-  String? _purpose1;
-  String _purpose2 = _unknown;
-  String? _prescription;
-  String? _birthYear;
-  String? _job;
+  DemographyPage(this.done);
   @override
   Widget build(BuildContext context) {
-    final purpose1 = _purpose1;
-    final prescription = _prescription;
-    final birthYear = _birthYear;
-    final job = _job;
-    final demographic = _demographic();
+    final store = useProvider(demographyPageStoreProvider);
+    final state = useProvider(demographyPageStoreProvider.state);
+    final purpose1 = state.purpose1;
+    final prescription = state.prescription;
+    final birthYear = state.birthYear;
+    final job = state.job;
+    final demographic = state.demographic();
     return Scaffold(
       appBar: null,
       body: SafeArea(
@@ -65,55 +46,63 @@ class _DemographyPageState extends State<DemographyPage> {
                     ),
                     SizedBox(height: 36),
                     _layout(
+                      context,
                       "ピルを服用している1番の目的/理由",
                       GestureDetector(
                         child: Text(purpose1 == null ? "選択してください" : purpose1,
                             style:
                                 FontType.assisting.merge(TextColorStyle.black)),
-                        onTap: () => _showPurposePicker1(),
+                        onTap: () => _showPurposePicker1(context, store, state),
                       ),
                     ),
                     if (purpose1 != _unknown) ...[
                       SizedBox(height: 30),
                       _layout(
+                        context,
                         "ピルを服用しているその他の目的/理由",
                         GestureDetector(
-                          child: Text(_purpose2,
+                          child: Text(state.purpose2,
                               style: FontType.assisting
                                   .merge(TextColorStyle.black)),
-                          onTap: () => _showPurposePicker2(),
+                          onTap: () =>
+                              _showPurposePicker2(context, store, state),
                         ),
                       ),
                     ],
                     SizedBox(height: 30),
                     _layout(
+                      context,
                       "ピルの処方はどのように行っていますか？",
                       GestureDetector(
                         child: Text(
                             prescription == null ? "選択してください" : prescription,
                             style:
                                 FontType.assisting.merge(TextColorStyle.black)),
-                        onTap: () => _showPrescriptionPicker(),
+                        onTap: () =>
+                            _showPrescriptionPicker(context, store, state),
                       ),
                     ),
                     SizedBox(height: 30),
                     _layout(
+                      context,
                       "生まれ年を教えて下さい",
                       GestureDetector(
                         child: Text(birthYear == null ? "選択してください" : birthYear,
                             style:
                                 FontType.assisting.merge(TextColorStyle.black)),
-                        onTap: () => _showBirthYearPicker(),
+                        onTap: () =>
+                            _showBirthYearPicker(context, store, state),
                       ),
                     ),
                     SizedBox(height: 30),
                     _layout(
+                      context,
                       "職業",
                       GestureDetector(
                         child: Text(job == null ? "選択してください" : job,
                             style:
                                 FontType.assisting.merge(TextColorStyle.black)),
-                        onTap: () => _showJobPicker(),
+                        onTap: () => _showJobPicker(context, store, state),
                       ),
                     ),
                     SizedBox(height: 50),
@@ -133,12 +122,9 @@ class _DemographyPageState extends State<DemographyPage> {
                             await SharedPreferences.getInstance();
                         sharedPreferences.setBool(
                             BoolKey.isAlreadyDoneDemography, true);
-                        await this
-                            .widget
-                            .userService
-                            .postDemographic(demographic);
+                        await store.register(demographic);
                         Navigator.of(context).pop();
-                        widget.done();
+                        this.done();
                       },
                 text: "完了",
               ),
@@ -149,39 +135,20 @@ class _DemographyPageState extends State<DemographyPage> {
     );
   }
 
-  Demographic? _demographic() {
-    final purpose1 = _purpose1;
-    final purpose2 = _purpose2;
-    final prescription = _prescription;
-    final birthYear = _birthYear;
-    final job = _job;
-    if (purpose1 == null ||
-        prescription == null ||
-        birthYear == null ||
-        job == null) {
-      return null;
-    }
-    return Demographic(
-        purpose1: purpose1,
-        purpose2: purpose2,
-        prescription: prescription,
-        birthYear: birthYear,
-        job: job);
-  }
+  double _columnWidht(BuildContext context) =>
+      MediaQuery.of(context).size.width - 39 * 2;
 
-  double _columnWidht() => MediaQuery.of(context).size.width - 39 * 2;
-
-  Widget _layout(String title, Widget form) {
+  Widget _layout(BuildContext context, String title, Widget form) {
     return Center(
       child: Container(
-        width: _columnWidht(),
+        width: _columnWidht(context),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(title, style: FontType.assisting.merge(TextColorStyle.black)),
             SizedBox(height: 10),
             Container(
-              width: _columnWidht(),
+              width: _columnWidht(context),
               padding: EdgeInsets.only(top: 16, bottom: 16, left: 16),
               decoration: BoxDecoration(
                   border: Border.all(color: PilllColors.border),
@@ -194,11 +161,15 @@ class _DemographyPageState extends State<DemographyPage> {
     );
   }
 
-  _showPurposePicker1() {
+  _showPurposePicker1(
+    BuildContext context,
+    DemographyPageStore store,
+    DemographyPageState state,
+  ) {
     analytics.logEvent(name: "show_purpose_picker_1");
-    final dataSource = _purposeDataSource;
-    String? selected = _purpose1;
-    final purpose = _purpose1;
+    final dataSource = DemographyPageDataSource.purposes;
+    String? selected = state.purpose1;
+    final purpose = state.purpose1;
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
@@ -208,15 +179,16 @@ class _DemographyPageState extends State<DemographyPage> {
           children: <Widget>[
             PickerToolbar(
               done: (() {
-                analytics.logEvent(
-                    name: "done_purpose_picker_1",
-                    parameters: {"purpose": _purpose1});
-                if (selected != null) {
-                  analytics.setUserProperties("purpose_1", selected);
+                final _selected = selected;
+                if (_selected != null) {
+                  analytics
+                      .logEvent(name: "done_purpose_picker_1", parameters: {
+                    "before": state.purpose1,
+                    "after": _selected,
+                  });
+                  analytics.setUserProperties("purpose_1", _selected);
+                  store.setPurpose1(_selected);
                 }
-                setState(() {
-                  _purpose1 = selected;
-                });
                 Navigator.pop(context);
               }),
               cancel: (() {
@@ -250,12 +222,18 @@ class _DemographyPageState extends State<DemographyPage> {
     );
   }
 
-  _showPurposePicker2() {
+  _showPurposePicker2(
+    BuildContext context,
+    DemographyPageStore store,
+    DemographyPageState state,
+  ) {
     analytics.logEvent(name: "show_purpose_picker_2");
-    final dataSource =
-        _purposeDataSource.where((element) => element != _purpose1).toList();
-    String selected = _purpose2;
-    final purpose = _purpose2;
+    final dataSource = DemographyPageDataSource.purposes
+        .where((element) => element != state.purpose1)
+        .toList();
+
+    String selected = state.purpose2;
+    final purpose = state.purpose2;
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
@@ -265,13 +243,12 @@ class _DemographyPageState extends State<DemographyPage> {
           children: <Widget>[
             PickerToolbar(
               done: (() {
-                analytics.logEvent(
-                    name: "done_purpose_picker_2",
-                    parameters: {"purpose": _purpose2});
-                analytics.setUserProperties("purpose_2", selected);
-                setState(() {
-                  _purpose2 = selected;
+                analytics.logEvent(name: "done_purpose_picker_2", parameters: {
+                  "before": purpose,
+                  "after": selected,
                 });
+                analytics.setUserProperties("purpose_2", selected);
+                store.setPurpose2(selected);
                 Navigator.pop(context);
               }),
               cancel: (() {
@@ -304,7 +281,11 @@ class _DemographyPageState extends State<DemographyPage> {
     );
   }
 
-  _showPrescriptionPicker() {
+  _showPrescriptionPicker(
+    BuildContext context,
+    DemographyPageStore store,
+    DemographyPageState state,
+  ) {
     analytics.logEvent(name: "show_prescription_picker");
     final dataSource = [
       "病院",
@@ -313,8 +294,8 @@ class _DemographyPageState extends State<DemographyPage> {
       "海外在住で薬局購入",
       _unknown,
     ];
-    String? selected = _prescription;
-    final prescription = _prescription;
+    String? selected = state.prescription;
+    final prescription = state.prescription;
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
@@ -324,15 +305,16 @@ class _DemographyPageState extends State<DemographyPage> {
           children: <Widget>[
             PickerToolbar(
               done: (() {
-                analytics.logEvent(
-                    name: "done_prescription_picker",
-                    parameters: {"presecription": _prescription});
-                if (selected != null) {
-                  analytics.setUserProperties("prescription", selected);
+                final _selected = selected;
+                if (_selected != null) {
+                  analytics
+                      .logEvent(name: "done_prescription_picker", parameters: {
+                    "before": state.prescription,
+                    "after": _selected,
+                  });
+                  analytics.setUserProperties("prescription", _selected);
+                  store.setPrescription(_selected);
                 }
-                setState(() {
-                  _prescription = selected;
-                });
                 Navigator.pop(context);
               }),
               cancel: (() {
@@ -367,17 +349,15 @@ class _DemographyPageState extends State<DemographyPage> {
     );
   }
 
-  _showBirthYearPicker() {
+  _showBirthYearPicker(
+    BuildContext context,
+    DemographyPageStore store,
+    DemographyPageState state,
+  ) {
     analytics.logEvent(name: "show_birth_year_picker");
-    final offset = 1950;
-    final dataSource = [
-      ...List.generate(today().year - offset, (index) => offset + index)
-          .reversed
-          .map((e) => e.toString()),
-      _unknown,
-    ];
-    String? selected = _birthYear;
-    final birthYear = _birthYear;
+    final dataSource = DemographyPageDataSource.birthYears;
+    String? selected = state.birthYear;
+    final birthYear = state.birthYear;
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
@@ -387,15 +367,16 @@ class _DemographyPageState extends State<DemographyPage> {
           children: <Widget>[
             PickerToolbar(
               done: (() {
-                analytics.logEvent(
-                    name: "done_birth_year_picker",
-                    parameters: {"birt_year": _birthYear});
-                if (selected != null) {
-                  analytics.setUserProperties("birth_year", selected);
+                final _selected = selected;
+                if (_selected != null) {
+                  analytics
+                      .logEvent(name: "done_birth_year_picker", parameters: {
+                    "before": birthYear,
+                    "after": _selected,
+                  });
+                  analytics.setUserProperties("birth_year", _selected);
+                  store.setBirthYear(_selected);
                 }
-                setState(() {
-                  _birthYear = selected;
-                });
                 Navigator.pop(context);
               }),
               cancel: (() {
@@ -430,34 +411,15 @@ class _DemographyPageState extends State<DemographyPage> {
     );
   }
 
-  _showJobPicker() {
+  _showJobPicker(
+    BuildContext context,
+    DemographyPageStore store,
+    DemographyPageState state,
+  ) {
     analytics.logEvent(name: "show_job_picker");
-    final dataSource = [
-      "建築業",
-      "製造業",
-      "情報通信業",
-      "運送業・郵便業",
-      "電気・ガス・熱供給・水道業",
-      "卸売・小売業",
-      "金融業・保険業",
-      "不動産業・物品賃貸業",
-      "学術研究",
-      "技術サービス(測量など)",
-      "専門サービス(法律・税理士など)",
-      "教育・学習支援業",
-      "宿泊業・飲食サービス業",
-      "生活関連サービス業・娯楽業",
-      "その他サービス業全般",
-      "医療・介護・福祉",
-      "公務",
-      "農業・林業",
-      "鉱業・採石業・砂利採取業",
-      "主婦",
-      "学生",
-      _unknown,
-    ];
-    String? selected = _job;
-    final job = _job;
+    final dataSource = DemographyPageDataSource.jobs;
+    String? selected = state.job;
+    final job = state.job;
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
@@ -467,14 +429,15 @@ class _DemographyPageState extends State<DemographyPage> {
           children: <Widget>[
             PickerToolbar(
               done: (() {
-                analytics.logEvent(
-                    name: "done_job_picker", parameters: {"job": _job});
-                if (selected != null) {
+                final _selected = selected;
+                if (_selected != null) {
+                  analytics.logEvent(name: "done_job_picker", parameters: {
+                    "before": state.job,
+                    "after": _selected,
+                  });
                   analytics.setUserProperties("job", selected);
+                  store.setJob(_selected);
                 }
-                setState(() {
-                  _job = selected;
-                });
                 Navigator.pop(context);
               }),
               cancel: (() {
@@ -509,18 +472,17 @@ class _DemographyPageState extends State<DemographyPage> {
 }
 
 extension _DemographyPageRoute on DemographyPage {
-  static Route<dynamic> route(UserService userService, VoidCallback done) {
+  static Route<dynamic> route(VoidCallback done) {
     return MaterialPageRoute(
       settings: RouteSettings(name: "DemographyPage"),
       builder: (_) => DemographyPage(
-        userService: userService,
-        done: done,
+        done,
       ),
     );
   }
 }
 
-showDemographyPageIfNeeded(BuildContext context, UserService userService) async {
+showDemographyPageIfNeeded(BuildContext context) async {
   final sharedPreference = await SharedPreferences.getInstance();
   final isAlreadyShowDemography =
       sharedPreference.getBool(BoolKey.isAlreadyShowDemography);
@@ -530,7 +492,7 @@ showDemographyPageIfNeeded(BuildContext context, UserService userService) async 
   }
   sharedPreference.setBool(BoolKey.isAlreadyShowDemography, true);
 
-  Navigator.of(context).push(_DemographyPageRoute.route(userService, () {
+  Navigator.of(context).push(_DemographyPageRoute.route(() {
     showDemographyCompletedDialog(context);
   }));
 }
