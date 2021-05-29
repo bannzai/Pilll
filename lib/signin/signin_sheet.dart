b1713a0e81607183d8aaba240542adf08db0c0eb
bannzai <kingkong999yhirose@gmail.com>
2021-05-29 09:56:39 +0900

    :recycle: Remove unnecessary routes

--- lib/signin/signin_sheet.dart
+++ lib/signin/signin_sheet.dart
@@ -8,7 +8,6 @@ import 'package:pilll/auth/google.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
-import 'package:pilll/components/page/hud.dart';
 import 'package:pilll/domain/root/root.dart';
 import 'package:pilll/entity/link_account_type.dart';
 import 'package:pilll/entity/user_error.dart';
