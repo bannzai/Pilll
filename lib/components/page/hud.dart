5802ba3d963fe1d9a02a3723606bd112b6c813ef
bannzai <kingkong999yhirose@gmail.com>
2021-05-29 09:43:18 +0900

    :sparkles: add hud widget

---
+++ lib/components/page/hud.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:pilll/components/molecules/indicator.dart';
+
+class HUD extends StatefulWidget {
+  final Widget child;
+
+  const HUD({Key? key, required this.child}) : super(key: key);
+  @override
+  _HUDState createState() => _HUDState();
+
+  static _HUDState of(BuildContext context) {
+    final state = context.findAncestorStateOfType<_HUDState>();
+    if (state == null) {
+      throw AssertionError('''
+      Not found HUD from this context: $context
+      The context should contains HUD widget into current widget tree
+      ''');
+    }
+    return state;
+  }
+}
+
+class _HUDState extends State<HUD> {
+  bool _shown = false;
+  bool _barrierEnabled = false;
+
+  show() {
+    setState(() {
+      _shown = true;
+      _barrierEnabled = true;
+    });
+  }
+
+  hide() {
+    setState(() {
+      _shown = false;
+      _barrierEnabled = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        widget.child,
+        IgnorePointer(
+          ignoring: !_shown,
+          child: Stack(
+            children: [
+              if (_barrierEnabled)
+                Visibility(
+                  visible: _shown,
+                  child: ModalBarrier(
+                    color: Colors.transparent,
+                  ),
+                ),
+              Indicator(),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
