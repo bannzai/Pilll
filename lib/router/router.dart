5881ecbd0419beba53e0f92780322126f79901a5
bannzai <kingkong999yhirose@gmail.com>
2021-06-14 10:59:39 +0900

    :memo: remove current routes

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,27 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:pilll/analytics.dart';
-import 'package:pilll/domain/initial_setting/initial_setting_1_page.dart';
-import 'package:pilll/domain/home/home_page.dart';
 import 'package:pilll/domain/root/root.dart';
 import 'package:pilll/service/push_notification.dart';
 import 'package:pilll/util/shared_preference/keys.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppRouter {
-  static Map<String, WidgetBuilder> routes() {
-    return {
-      Routes.root: (BuildContext context) => ProviderScope(
-            child: Root(key: rootKey),
-          ),
-      Routes.initialSetting: (BuildContext context) =>
-          ProviderScope(child: InitialSetting1Page()),
-      Routes.main: (BuildContext context) =>
-          ProviderScope(child: HomePage(key: homeKey)),
-    };
-  }
-
 // NOTE: This method call after user end all initialSetting
 // OR user signed with 3rd party provider
 // So, Don't forget when this function is edited. Both test necessary .
@@ -32,7 +17,7 @@ class AppRouter {
       requestNotificationPermissions().then((value) {
         listenNotificationEvents();
         Navigator.popUntil(context, (router) => router.isFirst);
-        Navigator.pushReplacementNamed(context, Routes.main);
+        rootKey.currentState?.showHome();
       });
     });
   }
@@ -43,10 +28,4 @@ class AppRouter {
         parameters: {"uid": FirebaseAuth.instance.currentUser?.uid});
     return endInitialSetting(context);
   }
-}
-
-class Routes {
-  static String root = '/';
-  static String initialSetting = '/initial-setting';
-  static String main = '/main';
 }
