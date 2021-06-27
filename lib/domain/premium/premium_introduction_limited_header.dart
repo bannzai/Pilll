9bef31a2f1f85eeb2b09e0f8f05d2f386cef25eb
bannzai <kingkong999yhirose@gmail.com>
2021-06-27 23:17:19 +0900

    :recycle: refactor extract component

---
+++ lib/domain/premium/premium_introduction_limited_header.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+
+class PremiumIntroductionLimitedHeader extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        image: DecorationImage(
+          image: AssetImage("images/premium_background.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      padding: EdgeInsets.only(left: 40, right: 40, bottom: 40),
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "今だけ！リリース記念価格",
+            textAlign: TextAlign.center,
+            style: TextColorStyle.main.merge(
+              TextStyle(
+                fontWeight: FontWeight.w700,
+                fontFamily: FontFamily.japanese,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          SizedBox(height: 4),
+          Text(
+            "47:58:00",
+            style: TextStyle(
+              color: TextColor.main,
+              fontFamily: FontFamily.japanese,
+              fontWeight: FontWeight.w700,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            "通常 月額プラン",
+            textAlign: TextAlign.center,
+            style: TextColorStyle.black.merge(
+              TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 12,
+                fontFamily: FontFamily.japanese,
+              ),
+            ),
+          ),
+          SizedBox(height: 4),
+          Stack(
+            children: [
+              Text(
+                "¥480",
+                textAlign: TextAlign.center,
+                style: TextColorStyle.main.merge(
+                  TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 28,
+                    fontFamily: FontFamily.japanese,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 30,
+                child: SvgPicture.asset("images/strikethrough.svg"),
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          SvgPicture.asset("images/arrow_down.svg"),
+          SizedBox(height: 28),
+        ],
+      ),
+    );
+  }
+}
