13c9acb8a6767767d2f39c26bed3df73fff6cbcf
bannzai <kingkong999yhirose@gmail.com>
2021-06-30 21:51:36 +0900

    :recycle: refactor with inherited widget

--- lib/error/universal_error_page.dart
+++ lib/error/universal_error_page.dart
@@ -5,7 +5,22 @@ import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/inquiry/inquiry.dart';
 import 'package:flutter/material.dart';
 
-class UniversalErrorPage extends StatelessWidget {
+class _InheritedWidget extends InheritedWidget {
+  _InheritedWidget({
+    Key? key,
+    required Widget child,
+    required this.state,
+  }) : super(key: key, child: child);
+
+  final _UniversalErrorPageState state;
+
+  @override
+  bool updateShouldNotify(covariant _InheritedWidget oldWidget) {
+    return false;
+  }
+}
+
+class UniversalErrorPage extends StatefulWidget {
   final Object? error;
   final Widget? child;
   final VoidCallback? reload;
@@ -18,60 +33,83 @@ class UniversalErrorPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _UniversalErrorPageState createState() => _UniversalErrorPageState();
+}
+
+class _UniversalErrorPageState extends State<UniversalErrorPage> {
+  Object? _error;
+  showError(Object error) {
+    setState(() {
+      _error = error;
+    });
+  }
+
+  Object? get error => _error ?? widget.error;
+
+  @override
   Widget build(BuildContext context) {
-    final child = this.child;
+    final child = this.widget.child;
     final error = this.error;
-    if (error == null && child != null) {
-      return child;
-    }
-    return Scaffold(
-      backgroundColor: PilllColors.background,
-      body: Center(
-        child: Container(
-          width: 300,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                "images/universal_error.png",
-                width: 200,
-                height: 190,
-              ),
-              SizedBox(height: 25),
-              Text(error.toString(),
-                  style: FontType.assisting.merge(TextColorStyle.main)),
-              SizedBox(height: 25),
-              TextButton.icon(
-                icon: const Icon(
-                  Icons.refresh,
-                  size: 20,
+    return _InheritedWidget(
+      state: this,
+      child: Stack(
+        children: [
+          if (child != null) child,
+          if (error != null)
+            Scaffold(
+              backgroundColor: PilllColors.background,
+              body: Center(
+                child: Container(
+                  width: 300,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "images/universal_error.png",
+                        width: 200,
+                        height: 190,
+                      ),
+                      SizedBox(height: 25),
+                      Text(error.toString(),
+                          style: FontType.assisting.merge(TextColorStyle.main)),
+                      SizedBox(height: 25),
+                      TextButton.icon(
+                        icon: const Icon(
+                          Icons.refresh,
+                          size: 20,
+                        ),
+                        label: Text("画面を再読み込み",
+                            style:
+                                FontType.assisting.merge(TextColorStyle.black)),
+                        onPressed: () {
+                          analytics.logEvent(name: "reload_button_pressed");
+                          final reload = this.widget.reload;
+                          if (reload != null) {
+                            reload();
+                          }
+                        },
+                      ),
+                      TextButton.icon(
+                        icon: const Icon(
+                          Icons.mail,
+                          size: 20,
+                        ),
+                        label: Text("解決しない場合はこちら",
+                            style:
+                                FontType.assisting.merge(TextColorStyle.black)),
+                        onPressed: () {
+                          analytics.logEvent(
+                              name: "problem_unresolved_button_pressed");
+                          inquiry();
+                        },
+                      )
+                    ],
+                  ),
                 ),
-                label: Text("画面を再読み込み",
-                    style: FontType.assisting.merge(TextColorStyle.black)),
-                onPressed: () {
-                  analytics.logEvent(name: "reload_button_pressed");
-                  final reload = this.reload;
-                  if (reload != null) {
-                    reload();
-                  }
-                },
               ),
-              TextButton.icon(
-                icon: const Icon(
-                  Icons.mail,
-                  size: 20,
-                ),
-                label: Text("解決しない場合はこちら",
-                    style: FontType.assisting.merge(TextColorStyle.black)),
-                onPressed: () {
-                  analytics.logEvent(name: "problem_unresolved_button_pressed");
-                  inquiry();
-                },
-              )
-            ],
-          ),
-        ),
+            ),
+        ],
       ),
     );
   }
