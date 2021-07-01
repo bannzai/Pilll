9c7a90c222d3fed7a07422d04565a8d978e6f301
bannzai <kingkong999yhirose@gmail.com>
2021-07-01 09:08:04 +0900

    :recycle: Extract to footer component

--- lib/domain/premium_introduction/premium_introduction_page.dart
+++ lib/domain/premium_introduction/premium_introduction_page.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/components/atoms/color.dart';
-import 'package:pilll/components/atoms/font.dart';
-import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/components/molecules/indicator.dart';
 import 'package:pilll/components/page/hud.dart';
+import 'package:pilll/domain/premium_introduction/components/premium_introduction_footer.dart';
 import 'package:pilll/domain/premium_introduction/components/premium_introduction_limited_header.dart';
 import 'package:pilll/domain/premium_introduction/components/purchase_buttons.dart';
-import 'package:pilll/domain/premium_introduction/premium_introduction_state.dart';
 import 'package:pilll/domain/premium_introduction/premium_introduction_store.dart';
-import 'package:pilll/entity/user_error.dart';
-import 'package:pilll/error/error_alert.dart';
 import 'package:pilll/error/universal_error_page.dart';
-import 'package:pilll/util/platform/platform.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class PremiumIntroductionPage extends HookWidget {
   @override
@@ -55,9 +48,10 @@ class PremiumIntroductionPage extends HookWidget {
                       child: Column(
                         children: [
                           PremiumIntroductionLimitedHeader(),
-                          if (offerings != null)
+                          if (offerings != null) ...[
                             PurchaseButtons(offers: offerings),
-                          _footer(context, store, state),
+                          ],
+                          PremiumIntroductionFotter(),
                         ],
                       ),
                     ),
@@ -67,125 +61,6 @@ class PremiumIntroductionPage extends HookWidget {
             ),
           ),
         ),
-      ),
-    );
-  }
-
-  Widget _footer(
-    BuildContext context,
-    PremiumIntroductionStore store,
-    PremiumIntroductionState state,
-  ) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24),
-      width: MediaQuery.of(context).size.width,
-      color: PilllColors.white,
-      child: Column(
-        children: [
-          Text(
-            '''
-今後も便利な機能を続々追加予定です！
-お楽しみに✨
-            ''',
-            textAlign: TextAlign.center,
-            style: TextColorStyle.black.merge(
-              TextStyle(
-                fontWeight: FontWeight.w400,
-                fontFamily: FontFamily.japanese,
-                fontSize: 14,
-              ),
-            ),
-          ),
-          SizedBox(height: 32),
-          Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16),
-            child: RichText(
-              textAlign: TextAlign.start,
-              text: TextSpan(
-                style: TextColorStyle.gray.merge(
-                  TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 10,
-                      fontFamily: FontFamily.japanese),
-                ),
-                children: [
-                  TextSpan(text: "・プレミアム契約期間は開始日から起算して1ヶ月または1年ごとの自動更新となります\n"),
-                  TextSpan(text: "・"),
-                  TextSpan(
-                    text: "プライバシーポリシー",
-                    style: TextStyle(decoration: TextDecoration.underline),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        launch("https://bannzai.github.io/Pilll/PrivacyPolicy",
-                            forceSafariVC: true);
-                      },
-                  ),
-                  TextSpan(
-                    text: " / ",
-                  ),
-                  TextSpan(
-                    text: "利用規約",
-                    style: TextStyle(decoration: TextDecoration.underline),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        launch("https://bannzai.github.io/Pilll/Terms",
-                            forceSafariVC: true);
-                      },
-                  ),
-                  TextSpan(
-                    text: "をご確認のうえ登録してください\n",
-                  ),
-                  TextSpan(
-                    text: "・プレミアム契約期間の終了日の24時間以上前に解約しない限り契約期間が自動更新されます\n",
-                  ),
-                  TextSpan(
-                    text:
-                        "・購入後、自動更新の解約は$storeNameアプリのアカウント設定で行えます。(アプリ内から自動更新の解約は行なえません)",
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 24),
-          GestureDetector(
-            onTap: () async {
-              try {
-                store.showHUD();
-                final shouldShowSnackbar = await store.restore();
-                if (shouldShowSnackbar) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      duration: Duration(
-                        seconds: 2,
-                      ),
-                      content: Text("購入情報を復元しました"),
-                    ),
-                  );
-                }
-              } catch (error) {
-                if (error is UserDisplayedError) {
-                  showErrorAlertWithError(context, error);
-                } else {
-                  store.handleException(error);
-                }
-              } finally {
-                store.hideHUD();
-              }
-            },
-            child: Text(
-              '以前購入した方はこちら',
-              style: TextStyle(
-                decoration: TextDecoration.underline,
-              ).merge(TextColorStyle.main).merge(
-                    TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 12,
-                      fontFamily: FontFamily.japanese,
-                    ),
-                  ),
-            ),
-          ),
-        ],
       ),
     );
   }
--- lib/domain/premium_introduction/premium_introduction_store.dart
+++ lib/domain/premium_introduction/premium_introduction_store.dart
@@ -1,14 +1,9 @@
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
-import 'package:pilll/analytics.dart';
 import 'package:pilll/domain/premium_introduction/premium_introduction_state.dart';
-import 'package:pilll/domain/premium_introduction/util/map_to_error.dart';
-import 'package:pilll/entity/user_error.dart';
 import 'package:pilll/error_log.dart';
-import 'package:pilll/purchases.dart';
 import 'package:pilll/service/auth.dart';
 import 'package:pilll/service/user.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
@@ -74,52 +69,6 @@ class PremiumIntroductionStore extends StateNotifier<PremiumIntroductionState> {
     _userStreamCanceller?.cancel();
     _userStreamCanceller = null;
     super.dispose();
-  }
-
-  /// Return true indicates end of regularllly pattern.
-  /// Return false indicates not regulally pattern.
-  /// Return value is used to display the completion snackbar
-  Future<bool> restore() async {
-    try {
-      final purchaserInfo = await Purchases.restoreTransactions();
-      final entitlements = purchaserInfo.entitlements.all[premiumEntitlements];
-      analytics.logEvent(name: "proceed_restore_purchase_info", parameters: {
-        "entitlements": entitlements?.identifier,
-        "isActivated": entitlements?.isActive,
-      });
-      if (entitlements != null && entitlements.isActive) {
-        analytics.logEvent(name: "done_restore_purchase_info", parameters: {
-          "entitlements": entitlements.identifier,
-        });
-        await callUpdatePurchaseInfo(purchaserInfo);
-        state = state.copyWith(isCompletedRestore: true);
-        return Future.value(true);
-      }
-      analytics.logEvent(name: "undone_restore_purchase_info", parameters: {
-        "entitlements": entitlements?.identifier,
-        "isActivated": entitlements?.isActive,
-      });
-      throw UserDisplayedError("以前の購入情報が見つかりません。アカウントをお確かめの上再度お試しください");
-    } on PlatformException catch (exception, stack) {
-      analytics.logEvent(name: "catched_restore_exception", parameters: {
-        "code": exception.code,
-        "details": exception.details.toString(),
-        "message": exception.message
-      });
-      final newException = mapToDisplayedException(exception);
-      if (newException == null) {
-        return Future.value(false);
-      }
-      errorLogger.recordError(exception, stack);
-      throw newException;
-    } catch (exception, stack) {
-      analytics
-          .logEvent(name: "catched_restore_anonymous_exception", parameters: {
-        "exception_type": exception.runtimeType.toString(),
-      });
-      errorLogger.recordError(exception, stack);
-      rethrow;
-    }
   }
 
   String annualPriceString(Package package) {
