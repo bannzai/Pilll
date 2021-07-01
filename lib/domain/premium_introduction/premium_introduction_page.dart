b9b1534416035d1d094e67097fc2047a198df014
bannzai <kingkong999yhirose@gmail.com>
2021-07-01 09:23:24 +0900

    :recycle: remove unnecessary import

--- lib/domain/premium_introduction/premium_introduction_page.dart
+++ lib/domain/premium_introduction/premium_introduction_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/molecules/indicator.dart';
