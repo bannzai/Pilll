a0a8e9969d9eca50021e0d0a71e115386ab7e1ba
bannzai <kingkong999yhirose@gmail.com>
2021-07-01 16:42:55 +0900

    Prepared more domain special provider

--- lib/util/datetime/timer.dart
+++ lib/util/datetime/timer.dart
@@ -4,15 +4,25 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/util/datetime/day.dart';
 import 'package:riverpod/riverpod.dart';
 
-final timerStoreProvider = StateNotifierProvider.autoDispose(
+final timerStoreProvider = StateNotifierProvider(
   (ref) => TimerStateStore(),
 );
+final timerStateProvider = Provider.autoDispose(
+  (ref) => ref.watch(timerStoreProvider.state),
+);
+final isOverTrialDeadlineProvider =
+    Provider.family.autoDispose((ref, DateTime trialDeadlineDate) {
+  final now = ref.watch(timerStateProvider);
+  return now.isAfter(trialDeadlineDate);
+});
 
 class TimerStateStore extends StateNotifier<DateTime> {
   Timer? _timer;
-  TimerStateStore() : super(now());
+  TimerStateStore() : super(now()) {
+    _fire(state);
+  }
 
-  void fire(DateTime start) {
+  void _fire(DateTime start) {
     _timer?.cancel();
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       state = now();
