14792b14edd9f8a2cbc797950fc6679853c019c2
bannzai <kingkong999yhirose@gmail.com>
2021-07-01 17:15:45 +0900

    :recycle: extract body

---
+++ lib/domain/premium_introduction/premium_introduction_body.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:pilll/components/atoms/buttons.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+import 'package:pilll/domain/premium_introduction/components/premium_introduction_footer.dart';
+import 'package:pilll/domain/premium_introduction/components/premium_introduction_header.dart';
+import 'package:pilll/domain/premium_introduction/components/premium_introduction_limited.dart';
+import 'package:pilll/domain/premium_introduction/components/purchase_buttons.dart';
+import 'package:pilll/util/platform/platform.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+
+class PremiumIntroductionBody extends StatelessWidget {
+  const PremiumIntroductionBody({
+    Key? key,
+    required this.isBlessMode,
+    required this.trialDeadlineDate,
+    required this.offerings,
+  }) : super(key: key);
+
+  final bool isBlessMode;
+  final DateTime? trialDeadlineDate;
+  final Offerings? offerings;
+
+  @override
+  Widget build(BuildContext context) {
+    final offerings = this.offerings;
+    final trialDeadlineDate = this.trialDeadlineDate;
+    return SafeArea(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                image: isBlessMode
+                    ? DecorationImage(
+                        image: AssetImage("images/premium_background.png"),
+                        fit: BoxFit.cover,
+                      )
+                    : null,
+              ),
+              padding: EdgeInsets.only(left: 40, right: 40, bottom: 40),
+              width: MediaQuery.of(context).size.width,
+            ),
+            SingleChildScrollView(
+              padding: EdgeInsets.only(bottom: 100),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  PremiumIntroductionHeader(shouldShowDismiss: true),
+                  if (trialDeadlineDate != null)
+                    PremiumIntroductionLimited(
+                      trialDeadlineDate: trialDeadlineDate,
+                    ),
+                  if (offerings != null) ...[
+                    SizedBox(height: 32),
+                    PurchaseButtons(
+                      offerings: offerings,
+                      trialDeadlineDate: trialDeadlineDate,
+                    ),
+                  ],
+                  SizedBox(height: 24),
+                  Text(
+                    "$storeNameからいつでも簡単に解約出来ます",
+                    textAlign: TextAlign.center,
+                    style: TextColorStyle.black.merge(
+                      TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontFamily: FontFamily.japanese,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  SecondaryButton(
+                      onPressed: () {
+                        print("");
+                      },
+                      text: "プレミアム機能を見る"),
+                  SizedBox(height: 24),
+                  PremiumIntroductionFotter(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/domain/premium_introduction/premium_introduction_page.dart
+++ lib/domain/premium_introduction/premium_introduction_page.dart
@@ -2,20 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:pilll/components/atoms/buttons.dart';
 import 'package:pilll/components/atoms/color.dart';
-import 'package:pilll/components/atoms/font.dart';
-import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/components/molecules/indicator.dart';
 import 'package:pilll/components/page/hud.dart';
-import 'package:pilll/domain/premium_introduction/components/premium_introduction_footer.dart';
-import 'package:pilll/domain/premium_introduction/components/premium_introduction_header.dart';
-import 'package:pilll/domain/premium_introduction/components/premium_introduction_limited.dart';
-import 'package:pilll/domain/premium_introduction/components/purchase_buttons.dart';
+import 'package:pilll/domain/premium_introduction/premium_introduction_body.dart';
 import 'package:pilll/domain/premium_introduction/premium_introduction_store.dart';
 import 'package:pilll/error/universal_error_page.dart';
 import 'package:pilll/util/datetime/timer.dart';
-import 'package:pilll/util/platform/platform.dart';
 
 class PremiumIntroductionPage extends HookWidget {
   @override
@@ -44,69 +37,10 @@ class PremiumIntroductionPage extends HookWidget {
         child: Scaffold(
           body: Container(
             color: PilllColors.white,
-            child: SafeArea(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.transparent,
-                        image: isBlessMode
-                            ? DecorationImage(
-                                image:
-                                    AssetImage("images/premium_background.png"),
-                                fit: BoxFit.cover,
-                              )
-                            : null,
-                      ),
-                      padding: EdgeInsets.only(left: 40, right: 40, bottom: 40),
-                      width: MediaQuery.of(context).size.width,
-                    ),
-                    SingleChildScrollView(
-                      padding: EdgeInsets.only(bottom: 100),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          PremiumIntroductionHeader(shouldShowDismiss: true),
-                          if (trialDeadlineDate != null)
-                            PremiumIntroductionLimited(
-                              trialDeadlineDate: trialDeadlineDate,
-                            ),
-                          if (offerings != null) ...[
-                            SizedBox(height: 32),
-                            PurchaseButtons(
-                              offerings: offerings,
-                              trialDeadlineDate: trialDeadlineDate,
-                            ),
-                          ],
-                          SizedBox(height: 24),
-                          Text(
-                            "$storeNameからいつでも簡単に解約出来ます",
-                            textAlign: TextAlign.center,
-                            style: TextColorStyle.black.merge(
-                              TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontFamily: FontFamily.japanese,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 24),
-                          SecondaryButton(
-                              onPressed: () {
-                                print("");
-                              },
-                              text: "プレミアム機能を見る"),
-                          SizedBox(height: 24),
-                          PremiumIntroductionFotter(),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            child: PremiumIntroductionBody(
+                isBlessMode: isBlessMode,
+                trialDeadlineDate: trialDeadlineDate,
+                offerings: offerings),
           ),
         ),
       ),
