6f387d66f482b82f1d87e6d01a5bdd866f089dd1
bannzai <kingkong999yhirose@gmail.com>
2021-07-03 22:37:56 +0900

    :memo: add property of premiumTrialGuide

--- lib/domain/record/components/notification_bar_state.dart
+++ lib/domain/record/components/notification_bar_state.dart
@@ -53,6 +53,13 @@ abstract class NotificationBarState implements _$NotificationBarState {
     return null;
   }
 
+  String? get premiumTrialGuide {
+    if (isPremium || isTrial || trialDeadlineDate != null) {
+      return null;
+    }
+    return "プレミアム機能 お試し体験プレゼント中\n詳しくみる";
+  }
+
   String? get premiumTrialLimit {
     if (!isTrial) {
       return null;
