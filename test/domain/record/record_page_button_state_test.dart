7b6bd1bccd5da60d27d9bd78ac9388b679525e67
bannzai <kingkong999yhirose@gmail.com>
2021-07-04 19:26:08 +0900

    :recycle: remove unnecessary code

--- test/domain/record/record_page_button_state_test.dart
+++ test/domain/record/record_page_button_state_test.dart
@@ -2,53 +2,22 @@ import 'package:pilll/analytics.dart';
 import 'package:pilll/domain/record/components/button/cancel_button.dart';
 import 'package:pilll/domain/record/components/button/record_page_button.dart';
 import 'package:pilll/domain/record/components/button/taken_button.dart';
-import 'package:pilll/domain/record/components/notification_bar/notification_bar_store.dart';
-import 'package:pilll/domain/record/record_page.dart';
 import 'package:pilll/domain/record/record_page_store.dart';
 import 'package:pilll/entity/pill_sheet.dart';
 import 'package:pilll/entity/pill_sheet_type.dart';
-import 'package:pilll/entity/setting.dart';
-import 'package:pilll/entity/user.dart';
-import 'package:pilll/service/day.dart';
 import 'package:pilll/util/datetime/day.dart';
 import 'package:pilll/util/environment.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mockito/mockito.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:pilll/util/shared_preference/keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../helper/mock.mocks.dart';
 
-class _FakeUser extends Fake implements User {
-  _FakeUser({
-    this.fakeIsPremium = false,
-    this.fakeIsTrial = false,
-    this.fakeTrialDeadlineDate,
-  });
-  final bool fakeIsPremium;
-  final bool fakeIsTrial;
-  final DateTime? fakeTrialDeadlineDate;
-  @override
-  bool get isPremium => fakeIsPremium;
-  @override
-  bool get isTrial => fakeIsTrial;
-  @override
-  DateTime? get trialDeadlineDate => fakeTrialDeadlineDate;
-}
-
 void main() {
-  Setting _anySetting() => Setting(
-        pillNumberForFromMenstruation: 22,
-        durationMenstruation: 2,
-        isOnReminder: false,
-        pillSheetTypeRawPath: PillSheetType.pillsheet_21.rawPath,
-        reminderTimes: [],
-      );
-
   setUp(() {
     TestWidgetsFlutterBinding.ensureInitialized();
     SharedPreferences.setMockInitialValues(
