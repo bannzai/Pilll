b9f5405b5354b4f241fb678daf5ba8079096bf81
bannzai <kingkong999yhirose@gmail.com>
2021-07-04 18:04:42 +0900

    :recycle: remove general fakes

--- test/helper/mock.dart
+++ test/helper/mock.dart
@@ -1,12 +1,10 @@
 import 'package:pilll/analytics.dart';
-import 'package:pilll/entity/user.dart';
 import 'package:pilll/service/auth.dart';
 import 'package:pilll/service/diary.dart';
 import 'package:pilll/service/menstruation.dart';
 import 'package:pilll/service/pill_sheet.dart';
 import 'package:pilll/service/setting.dart';
 import 'package:pilll/service/day.dart';
-import 'package:mockito/mockito.dart';
 import 'package:pilll/service/user.dart';
 import 'package:mockito/annotations.dart';
 
@@ -20,7 +18,4 @@ import 'package:mockito/annotations.dart';
   AuthService,
   UserService
 ])
-class FakeUserForNotPremium extends Fake implements User {
-  @override
-  bool get isPremium => false;
-}
+abstract class KeepGeneratedMocks {}
