e4b8924572bc939ab0ef1570935f6372de136e3a
bannzai <kingkong999yhirose@gmail.com>
2021-07-07 20:10:48 +0900

    Add event handler

--- lib/domain/settings/components/rows/quick_record.dart
+++ lib/domain/settings/components/rows/quick_record.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/molecules/premium_badge.dart';
+import 'package:pilll/domain/premium_introduction/premium_introduction_sheet.dart';
 
 class QuickRecordRow extends StatelessWidget {
   final bool isTrial;
@@ -12,20 +13,28 @@ class QuickRecordRow extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Row(
-        children: [
-          Text("クイックレコード", style: FontType.listRow),
-          SizedBox(width: 7),
-          PremiumBadge(),
-        ],
-      ),
-      subtitle: Text("通知画面で今日飲むピルが分かり、そのまま服用記録できます。"),
+    return GestureDetector(
       onTap: () {
-        analytics.logEvent(
-          name: "did_select_quick_record_row",
-        );
+        if (isTrial) {
+          return;
+        }
+        showPremiumIntroductionSheet(context);
       },
+      child: ListTile(
+        title: Row(
+          children: [
+            Text("クイックレコード", style: FontType.listRow),
+            SizedBox(width: 7),
+            PremiumBadge(),
+          ],
+        ),
+        subtitle: Text("通知画面で今日飲むピルが分かり、そのまま服用記録できます。"),
+        onTap: () {
+          analytics.logEvent(
+            name: "did_select_quick_record_row",
+          );
+        },
+      ),
     );
   }
 }
