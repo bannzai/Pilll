47ca3df717adfb90421ef860b6fbeb23bc78c10e
bannzai <kingkong999yhirose@gmail.com>
2021-07-07 19:28:45 +0900

    :sparkels: add link method

--- lib/domain/record/components/notification_bar/premium_trial_limit.dart
+++ lib/domain/record/components/notification_bar/premium_trial_limit.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
+import 'package:pilll/util/links.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PremiumTrialLimitNotificationBar extends StatelessWidget {
   const PremiumTrialLimitNotificationBar({
@@ -12,9 +14,14 @@ class PremiumTrialLimitNotificationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(premiumTrialLimit,
-          style: FontType.assistingBold.merge(TextColorStyle.white)),
+    return GestureDetector(
+      onTap: () async {
+        await launch(preimumLink);
+      },
+      child: Center(
+        child: Text(premiumTrialLimit,
+            style: FontType.assistingBold.merge(TextColorStyle.white)),
+      ),
     );
   }
 }
--- lib/util/links.dart
+++ lib/util/links.dart
@@ -1,1 +1,1 @@
-final preimumLink = "https://pilll.anotion.so/6b4ac5d845f24f7ca864f6b501690694";
+final preimumLink = "https://pilll.anotion.so/fffebcb749b14d08863ecbc41943fb90";
