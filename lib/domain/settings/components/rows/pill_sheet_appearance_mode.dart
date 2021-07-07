2974a1632e3c852e6fd647710e4177407e6a5018
bannzai <kingkong999yhirose@gmail.com>
2021-07-07 20:23:53 +0900

    :art: to toggle UI

--- lib/domain/settings/components/rows/pill_sheet_appearance_mode.dart
+++ lib/domain/settings/components/rows/pill_sheet_appearance_mode.dart
@@ -6,7 +6,6 @@ import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/domain/settings/setting_page_store.dart';
 import 'package:pilll/entity/setting.dart';
-import 'package:pilll/util/toolbar/picker_toolbar.dart';
 
 class PillSheetAppearanceModeRow extends HookWidget {
   final Setting setting;
@@ -20,60 +19,17 @@ class PillSheetAppearanceModeRow extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final store = useProvider(settingStoreProvider);
-    return ListTile(
-      title: Text("表示モード", style: FontType.listRow),
-      subtitle: Text(setting.pillSheetAppearanceMode.itemName),
-      onTap: () {
+    return SwitchListTile(
+      title: Text("日付表示モード", style: FontType.listRow),
+      value: setting.pillSheetAppearanceMode == PillSheetAppearanceMode.date,
+      onChanged: (value) {
         analytics.logEvent(
-          name: "did_select_setting_pill_sheet_appearance",
+          name: "did_select_pill_sheet_appearance",
         );
-        var selected = setting.pillSheetAppearanceMode;
-        showModalBottomSheet(
-          context: context,
-          builder: (BuildContext context) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                PickerToolbar(
-                  done: (() {
-                    analytics.logEvent(
-                        name: "done_setting_pill_sheet_appearance",
-                        parameters: {
-                          "before": setting.pillSheetAppearanceMode.itemName,
-                          "after": selected.itemName,
-                        });
-                    store.modifyPillSheetAppearanceMode(selected);
-                    Navigator.pop(context);
-                  }),
-                  cancel: (() {
-                    Navigator.pop(context);
-                  }),
-                ),
-                Container(
-                  height: 200,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: CupertinoPicker(
-                      itemExtent: 40,
-                      children: PillSheetAppearanceMode.values
-                          .map((v) => Text(v.itemName))
-                          .toList(),
-                      onSelectedItemChanged: (index) {
-                        selected = PillSheetAppearanceMode.values[index];
-                      },
-                      scrollController: FixedExtentScrollController(
-                        initialItem: setting.pillSheetAppearanceMode.index,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
-          },
-        );
+        final pillSheetAppearanceMode = value
+            ? PillSheetAppearanceMode.date
+            : PillSheetAppearanceMode.number;
+        store.modifyPillSheetAppearanceMode(pillSheetAppearanceMode);
       },
     );
   }
