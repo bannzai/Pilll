0958ba3d42389203a4bae2e5b6da055333983e21
bannzai <kingkong999yhirose@gmail.com>
2021-07-10 10:47:03 +0900

    :art: fix release note design

--- lib/domain/modal/release_note.dart
+++ lib/domain/modal/release_note.dart
@@ -12,70 +12,73 @@ class ReleaseNote extends StatelessWidget {
   const ReleaseNote({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.transparency,
-      child: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: PilllColors.white,
-            borderRadius: BorderRadius.circular(4),
-          ),
-          width: 304,
-          height: 302,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: IconButton(
-                      icon: Icon(Icons.close, color: Colors.black),
-                      onPressed: () => Navigator.of(context).pop(),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      padding: EdgeInsets.only(top: 40),
-                      child: Text(
-                        "通知から服用記録ができるようになりました！",
-                        style: FontType.subTitle.merge(TextColorStyle.black),
-                        textAlign: TextAlign.center,
+    return Padding(
+      padding: const EdgeInsets.only(left: 35, right: 35),
+      child: Material(
+        type: MaterialType.transparency,
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: PilllColors.white,
+              borderRadius: BorderRadius.circular(4),
+            ),
+            constraints: BoxConstraints(maxWidth: 320),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        icon: Icon(Icons.close, color: Colors.black),
+                        onPressed: () => Navigator.of(context).pop(),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 25, left: 20, right: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '''
-Apple Watchからも服用記録ができます。
-その他にも新機能が続々登場！
-                      ''',
-                      style: FontType.assisting.merge(TextColorStyle.main),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        padding: EdgeInsets.only(top: 40, left: 40, right: 40),
+                        child: Text(
+                          "通知から服用記録ができるようになりました！",
+                          style: FontType.subTitle.merge(TextColorStyle.black),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: 230,
-                child: SecondaryButton(
-                    onPressed: () async {
-                      analytics.logEvent(name: "pressed_show_release_note");
-                      Navigator.of(context).pop();
-                      await openReleaseNote();
-                    },
-                    text: "詳細を見る"),
-              ),
-              Spacer(),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 25, left: 25, right: 25),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '''
+Apple Watchからも服用記録ができます。
+その他にも新機能が続々登場！
+                        ''',
+                        style: FontType.assisting.merge(TextColorStyle.main),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 230,
+                  child: SecondaryButton(
+                      onPressed: () async {
+                        analytics.logEvent(name: "pressed_show_release_note");
+                        Navigator.of(context).pop();
+                        await openReleaseNote();
+                      },
+                      text: "詳細を見る"),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
@@ -86,10 +89,10 @@ Apple Watchからも服用記録ができます。
 showReleaseNotePreDialog(BuildContext context) async {
   final key = ReleaseNoteKey.version3_0_0;
   final storage = await SharedPreferences.getInstance();
-  if (storage.getBool(key) ?? false) {
-    return;
-  }
-  storage.setBool(key, true);
+//  if (storage.getBool(key) ?? false) {
+//    return;
+//  }
+  // storage.setBool(key, true);
 
   showDialog(
       context: context,
