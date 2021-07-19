f862931e0fcad198f62cdc1e5de52f82e62dd716
bannzai <kingkong999yhirose@gmail.com>
2021-07-19 10:38:51 +0900

    :recycle: Rename to weekly calendar state

--- lib/domain/calendar/monthly_calendar_state.dart
+++ lib/domain/calendar/monthly_calendar_state.dart
@@ -1,5 +1,5 @@
 import 'package:pilll/components/organisms/calendar/monthly/calendar_state.dart';
-import 'package:pilll/domain/calendar/weekly/calendar_tab_state.dart';
+import 'package:pilll/domain/calendar/weekly_calendar_state.dart';
 import 'package:pilll/components/organisms/calendar/weekly/weekly_calendar_state.dart';
 import 'package:pilll/entity/menstruation.dart';
 
