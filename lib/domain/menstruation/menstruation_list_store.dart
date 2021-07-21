a30489a46d82b264db26a6d02c89618f5456773a
bannzai <kingkong999yhirose@gmail.com>
2021-07-21 20:32:39 +0900

    :recycle: subscribe isPremium and isTrial state

--- lib/domain/menstruation/menstruation_list_store.dart
+++ lib/domain/menstruation/menstruation_list_store.dart
@@ -1,19 +1,25 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/domain/menstruation/menstruation_history_row.dart';
 import 'package:pilll/domain/menstruation/menstruation_list_state.dart';
 import 'package:pilll/service/menstruation.dart';
+import 'package:pilll/service/user.dart';
 import 'package:pilll/store/menstruation.dart';
 
-final menstruationListStoreProvider = StateNotifierProvider((ref) =>
-    MenstruationListStore(
-        menstruationService: ref.watch(menstruationServiceProvider)));
+final menstruationListStoreProvider = StateNotifierProvider(
+  (ref) => MenstruationListStore(
+      menstruationService: ref.watch(menstruationServiceProvider),
+      userService: ref.watch(userServiceProvider)),
+);
 
 class MenstruationListStore extends StateNotifier<MenstruationListState> {
   final MenstruationService menstruationService;
+  final UserService userService;
   MenstruationListStore({
     required this.menstruationService,
+    required this.userService,
   }) : super(MenstruationListState()) {
     _reset();
   }
@@ -21,8 +27,11 @@ class MenstruationListStore extends StateNotifier<MenstruationListState> {
   void _reset() {
     Future(() async {
       final menstruations = await menstruationService.fetchAll();
+      final user = await userService.fetch();
       state = state.copyWith(
         isNotYetLoaded: false,
+        isPremium: user.isPremium,
+        isTrial: user.isTrial,
         allRows: MenstruationHistoryRowState.rows(
             dropLatestMenstruationIfNeeded(menstruations)),
       );
@@ -31,6 +40,7 @@ class MenstruationListStore extends StateNotifier<MenstruationListState> {
   }
 
   StreamSubscription? _menstruationCanceller;
+  StreamSubscription? _userStreamCanceller;
   void _subscribe() {
     _menstruationCanceller?.cancel();
     _menstruationCanceller =
@@ -39,11 +49,16 @@ class MenstruationListStore extends StateNotifier<MenstruationListState> {
           allRows: MenstruationHistoryRowState.rows(
               dropLatestMenstruationIfNeeded(entities)));
     });
+    _userStreamCanceller?.cancel();
+    _userStreamCanceller = userService.subscribe().listen((user) {
+      state = state.copyWith(isPremium: user.isPremium, isTrial: user.isTrial);
+    });
   }
 
   @override
   void dispose() {
     _menstruationCanceller?.cancel();
+    _userStreamCanceller?.cancel();
     super.dispose();
   }
 }
