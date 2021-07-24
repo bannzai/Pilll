f8a72f4ccc5023590fbd13ef1d394e333fd0237f
bannzai <kingkong999yhirose@gmail.com>
2021-07-24 10:47:52 +0900

    :memo: fix UI

--- lib/domain/premium_introduction/components/premium_user_info.dart
+++ lib/domain/premium_introduction/components/premium_user_info.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
 
 class PremiumuserInfoRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "既にプレミアムユーザーです",
-      style: TextStyle(
-        fontFamily: FontFamily.japanese,
-        fontWeight: FontWeight.w700,
-        fontSize: 16,
-        color: TextColor.main,
-      ),
+    return Column(
+      children: [
+        Text(
+          "あなたは\vプレミアムメンバーです",
+          style: TextStyle(
+            fontFamily: FontFamily.japanese,
+            fontWeight: FontWeight.w700,
+            fontSize: 16,
+            color: TextColor.main,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 32),
+        SvgPicture.asset("images/jewel.svg"),
+        SizedBox(height: 24),
+        Text(
+          "ご利用ありがとうございます。\nお陰様でPilllの運営を継続できています。",
+          style: TextStyle(
+            fontFamily: FontFamily.japanese,
+            fontWeight: FontWeight.w400,
+            fontSize: 14,
+            color: TextColor.main,
+          ),
+          textAlign: TextAlign.center,
+        )
+      ],
     );
   }
 }
