fab61017549a3032fe143cdb27dd26c4690bbc08
bannzai <kingkong999yhirose@gmail.com>
2021-07-24 10:07:57 +0900

    :sparkels: add events for shown trial modal

--- lib/domain/settings/components/rows/premium_introduction.dart
+++ lib/domain/settings/components/rows/premium_introduction.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/domain/premium_introduction/premium_introduction_sheet.dart';
+import 'package:pilll/domain/premium_trial/premium_trial_complete_modal.dart';
+import 'package:pilll/domain/premium_trial/premium_trial_modal.dart';
 
 class PremiumIntroductionRow extends StatelessWidget {
   final bool isPremium;
+  final bool isTrial;
+  final DateTime? trialDeadlineDate;
 
   const PremiumIntroductionRow({
     Key? key,
     required this.isPremium,
+    required this.isTrial,
+    required this.trialDeadlineDate,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return ListTile(
       onTap: () {
-        showPremiumIntroductionSheet(context);
+        if (!isTrial && trialDeadlineDate == null) {
+          showPremiumTrialModal(context, () {
+            showPremiumTrialCompleteModalPreDialog(context);
+          });
+        } else {
+          showPremiumIntroductionSheet(context);
+        }
       },
       title: Text("Pilllプレミアム",
           style: FontType.assisting.merge(TextColorStyle.black)),
--- lib/domain/settings/setting_page.dart
+++ lib/domain/settings/setting_page.dart
@@ -65,7 +65,11 @@ class SettingPage extends HookWidget {
                           text: "機種変更やスマホ紛失時など、データの引き継ぎ・復元には、アカウント登録が必要です。"),
                       AccountLinkRow(),
                       _separator(),
-                      PremiumIntroductionRow(isPremium: state.isPremium),
+                      PremiumIntroductionRow(
+                        isPremium: state.isPremium,
+                        isTrial: state.isTrial,
+                        trialDeadlineDate: state.trialDeadlineDate,
+                      ),
                       _separator(),
                     ],
                   );
