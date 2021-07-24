5a82152f2837bb06bf173b356609573d16157cf4
bannzai <kingkong999yhirose@gmail.com>
2021-07-24 15:46:27 +0900

    :recycle: fix handling envets

--- lib/domain/settings/setting_account_list/components/delete_user_button.dart
+++ lib/domain/settings/setting_account_list/components/delete_user_button.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pilll/components/atoms/buttons.dart';
 import 'package:pilll/components/page/discard_dialog.dart';
+import 'package:pilll/error/error_alert.dart';
+import 'package:pilll/router/router.dart';
 
 class DeleteUserButton extends StatelessWidget {
   @override
@@ -18,69 +20,14 @@ class DeleteUserButton extends StatelessWidget {
             done: () async {
               try {
                 await FirebaseAuth.instance.currentUser?.delete();
-                showDialog(
-                    context: context,
-                    builder: (_) {
-                      return _CompletedDialog(onClose: () {
-                        Navigator.of(context).pop();
-                      });
-                    });
-              } catch (error) {}
+                await AppRouter.routeToInitialSetting(context);
+              } catch (error) {
+                showErrorAlert(context, message: error.toString());
+              }
             },
           );
         },
         text: "退会する",
-      ),
-    );
-  }
-}
-
-class _CompletedDialog extends StatelessWidget {
-  final VoidCallback onClose;
-
-  const _CompletedDialog({Key? key, required this.onClose}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      contentPadding: EdgeInsets.fromLTRB(24, 48, 24, 24),
-      content: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              "Pilllプレミアム登録完了",
-              style: TextStyle(
-                color: TextColor.main,
-                fontFamily: FontFamily.japanese,
-                fontWeight: FontWeight.w700,
-                fontSize: 16,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 24),
-            SvgPicture.asset("images/jewel.svg"),
-            SizedBox(height: 24),
-            Text(
-              "ご登録ありがとうございます。\nすべての機能が使えるようになりました！",
-              style: TextStyle(
-                color: TextColor.main,
-                fontFamily: FontFamily.japanese,
-                fontWeight: FontWeight.w400,
-                fontSize: 14,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 24),
-            PrimaryButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                onClose();
-              },
-              text: "OK",
-            ),
-          ],
-        ),
       ),
     );
   }
