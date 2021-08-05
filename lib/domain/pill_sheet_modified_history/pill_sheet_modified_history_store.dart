8d9cc8e031cc2c27994c86b8e8e5694e0cc21898
bannzai <kingkong999yhirose@gmail.com>
2021-08-05 09:08:46 +0900

    :sparkels: defined store

--- lib/domain/pill_sheet_modified_history/pill_sheet_modified_history_store.dart
+++ lib/domain/pill_sheet_modified_history/pill_sheet_modified_history_store.dart
@@ -1,21 +1,25 @@
 import 'dart:async';
 
 import 'package:pilll/domain/pill_sheet_modified_history/pill_sheet_modified_history_state.dart';
-import 'package:pilll/service/user.dart';
+import 'package:pilll/entity/pill_sheet_modified_history.dart';
+import 'package:pilll/service/pill_sheet_modified_history.dart';
 import 'package:riverpod/riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-final pillSheetModifiedHistoryStoreProvider = StateNotifierProvider(
-  (ref) => PillSheetModifiedHistoryStateStore(
-    ref.watch(userServiceProvider),
+final pillSheetModifiedHistoryStoreProvider =
+    StateNotifierProvider.autoDispose.family(
+  (ref, DateTime dateTimeOfMonth) => PillSheetModifiedHistoryStateStore(
+    dateTimeOfMonth,
+    ref.watch(pillSheetModifiedHistoryServiceProvider),
   ),
 );
 
 class PillSheetModifiedHistoryStateStore
     extends StateNotifier<PillSheetModifiedHistoryState> {
-  final UserService _userService;
+  final DateTime _dateTimeOfMonth;
+  final PillSheetModifiedHistoryService _pillSheetModifiedHistoryService;
   PillSheetModifiedHistoryStateStore(
-    this._userService,
+    this._dateTimeOfMonth,
+    this._pillSheetModifiedHistoryService,
   ) : super(PillSheetModifiedHistoryState()) {
     reset();
   }
@@ -23,45 +27,43 @@ class PillSheetModifiedHistoryStateStore
   void reset() {
     state = state.copyWith(isLoading: true);
     Future(() async {
-      final storage = await SharedPreferences.getInstance();
-      final user = await _userService.fetch();
-      this.state = PillSheetModifiedHistoryState(
-        isPremium: user.isPremium,
-        isTrial: user.isTrial,
-        beginTrialDate: user.beginTrialDate,
-        isLoading: false,
-        isFirstLoadEnded: true,
-      );
+      final pillSheetModifiedHistories = await _pillSheetModifiedHistoryService
+          .fetchListForMonth(_dateTimeOfMonth);
+      final elements = _mapToElements(pillSheetModifiedHistories);
+      state = state.copyWith(
+          elements: elements, isFirstLoadEnded: true, isLoading: false);
       _subscribe();
     });
   }
 
-  StreamSubscription? _userSubscribeCanceller;
+  StreamSubscription? _pillSheetModifiedHistoryCanceller;
   void _subscribe() {
-    _userSubscribeCanceller?.cancel();
-    _userSubscribeCanceller = _userService.subscribe().listen((event) {
-      state = state.copyWith(
-          isPremium: event.isPremium,
-          isTrial: event.isTrial,
-          beginTrialDate: event.beginTrialDate);
+    _pillSheetModifiedHistoryCanceller?.cancel();
+    _pillSheetModifiedHistoryCanceller = _pillSheetModifiedHistoryService
+        .subscribeForMonth(_dateTimeOfMonth)
+        .listen((event) {
+      final elements = _mapToElements(event);
+      state = state.copyWith(elements: elements);
     });
+  }
+
+  List<PillSheetModifiedHistoryElementState> _mapToElements(
+      List<PillSheetModifiedHistory> histories) {
+    return histories
+        .where((history) => history.enumActionType != null)
+        .map(
+          (history) => PillSheetModifiedHistoryElementState(
+            actionType: history.enumActionType!,
+            createdAt: history.createdAt,
+            value: history.value,
+          ),
+        )
+        .toList();
   }
 
   @override
   void dispose() {
-    _userSubscribeCanceller?.cancel();
+    _pillSheetModifiedHistoryCanceller?.cancel();
     super.dispose();
-  }
-
-  handleException(Object exception) {
-    state = state.copyWith(exception: exception);
-  }
-
-  showHUD() {
-    state = state.copyWith(isLoading: true);
-  }
-
-  hideHUD() {
-    state = state.copyWith(isLoading: false);
   }
 }
