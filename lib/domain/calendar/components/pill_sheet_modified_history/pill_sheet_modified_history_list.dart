fbd5ec8bc1145c99052aff4ed7a9da22e9970d73
bannzai <kingkong999yhirose@gmail.com>
2021-08-08 13:46:27 +0900

    Revert ":recycle: Rename with domain prefix"
    This reverts commit 71dd5351534a8c5599fd318088d5cb1fdd9f756e.

--- lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list.dart
+++ lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_monthly_header.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/monthly_header.dart';
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_taken_pill_action.dart';
 import 'package:pilll/util/datetime/day.dart';
 
