ff6bc5b67d8d47f6818f5b30fef3591f5deb8a07
bannzai <kingkong999yhirose@gmail.com>
2021-08-08 14:04:08 +0900

    :sparkles: Add PillSheetModifiedHistoryAutomaticallyRecordedLastTakenDateAction

---
+++ lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_automatically_recorded_last_taken_date_action.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pilll/components/atoms/color.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_taken_pill_action.dart';
+
+class PillSheetModifiedHistoryAutomaticallyRecordedLastTakenDateAction
+    extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 4, bottom: 4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            LimitedBox(
+              maxWidth:
+                  CalendarPillSheetModifiedHistoryTakenPillActionElementWidth
+                      .leading,
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      style: TextStyle(color: TextColor.main),
+                      children: [
+                        TextSpan(
+                          text: "22",
+                          style: TextStyle(
+                            fontFamily: FontFamily.number,
+                            fontSize: 23,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "(木)",
+                          style: TextStyle(
+                            fontFamily: FontFamily.japanese,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Container(
+                    height: 26,
+                    child: VerticalDivider(
+                      color: PilllColors.divider,
+                      width: 0.5,
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Text(
+                    "22番",
+                    style: TextStyle(
+                      color: TextColor.main,
+                      fontFamily: FontFamily.japanese,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Container(
+              constraints: BoxConstraints(
+                maxWidth:
+                    CalendarPillSheetModifiedHistoryTakenPillActionElementWidth
+                        .takenTime,
+              ),
+              padding: EdgeInsets.only(left: 8),
+              child: Text(
+                "  -  ",
+                style: TextStyle(
+                  color: TextColor.main,
+                  fontSize: 15,
+                  fontFamily: FontFamily.number,
+                  fontWeight: FontWeight.w400,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Spacer(flex: 2),
+            Container(
+              constraints: BoxConstraints(
+                maxWidth:
+                    CalendarPillSheetModifiedHistoryTakenPillActionElementWidth
+                        .takenMark,
+              ),
+              padding: EdgeInsets.only(left: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [SvgPicture.asset("images/o.svg")],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
