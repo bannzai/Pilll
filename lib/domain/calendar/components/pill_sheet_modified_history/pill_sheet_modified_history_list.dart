9904a61d5109976af1aa15510e05cacbec3a2ea8
bannzai <kingkong999yhirose@gmail.com>
2021-08-08 23:29:34 +0900

    :sparkles: add internal model

--- lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list.dart
+++ lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_automatically_recorded_last_taken_date_action.dart';
-import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_changed_pill_number_action.dart';
-import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_deleted_pill_sheet_action.dart';
-import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_ended_pill_sheet_action.dart';
-import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_monthly_header.dart';
-import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_revert_taken_pill_action.dart';
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_taken_pill_action.dart';
 import 'package:pilll/entity/pill_sheet_modified_history.dart';
-import 'package:pilll/util/datetime/day.dart';
+import 'package:pilll/util/datetime/date_compare.dart';
+
+class CalendarPillSheetModifiedHistoryListModel {
+  final DateTime dateTimeOfMonth;
+  final List<PillSheetModifiedHistory> pillSheetModifiedHistories;
+  CalendarPillSheetModifiedHistoryListModel({
+    required this.dateTimeOfMonth,
+    required this.pillSheetModifiedHistories,
+  });
+}
 
 class CalendarPillSheetModifiedHistoryList extends StatelessWidget {
   final List<PillSheetModifiedHistory> pillSheetModifiedHistories;
@@ -15,6 +18,30 @@ class CalendarPillSheetModifiedHistoryList extends StatelessWidget {
   const CalendarPillSheetModifiedHistoryList(
       {Key? key, required this.pillSheetModifiedHistories})
       : super(key: key);
+
+  List<CalendarPillSheetModifiedHistoryListModel> get models {
+    final List<CalendarPillSheetModifiedHistoryListModel> models = [];
+    pillSheetModifiedHistories.forEach((history) {
+      CalendarPillSheetModifiedHistoryListModel? model;
+
+      models.forEach((m) {
+        if (isSameMonth(m.dateTimeOfMonth, history.createdAt)) {
+          model = m;
+          return;
+        }
+      });
+
+      final m = model;
+      if (m != null) {
+        models.add(m);
+      } else {
+        models.add(CalendarPillSheetModifiedHistoryListModel(
+            dateTimeOfMonth: history.createdAt,
+            pillSheetModifiedHistories: pillSheetModifiedHistories));
+      }
+    });
+    return models;
+  }
 
   @override
   Widget build(BuildContext context) {
