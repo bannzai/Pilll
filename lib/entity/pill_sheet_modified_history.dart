a5a41dc3929fc82b32137ca9283067c462c10132
bannzai <kingkong999yhirose@gmail.com>
2021-08-09 09:26:15 +0900

    :memo: adapt timestamp converter

--- lib/entity/pill_sheet_modified_history.dart
+++ lib/entity/pill_sheet_modified_history.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:pilll/entity/firestore_timestamp_converter.dart';
 
