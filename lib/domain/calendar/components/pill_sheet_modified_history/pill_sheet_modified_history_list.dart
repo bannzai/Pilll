b82723c0f5e3f5090c6152d4aec38387dd48d0a2
bannzai <kingkong999yhirose@gmail.com>
2021-08-09 09:11:53 +0900

    :sparkles: Fill Implementation

--- lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list.dart
+++ lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_automatically_recorded_last_taken_date_action.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_changed_pill_number_action.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_deleted_pill_sheet_action.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_ended_pill_sheet_action.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_monthly_header.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_revert_taken_pill_action.dart';
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_taken_pill_action.dart';
 import 'package:pilll/entity/pill_sheet_modified_history.dart';
 import 'package:pilll/util/datetime/date_compare.dart';
@@ -47,37 +53,41 @@ class CalendarPillSheetModifiedHistoryList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: pillSheetModifiedHistories.map((pillSheetModifiedHistory) {
-        final actionType = pillSheetModifiedHistory.enumActionType;
-        if (actionType == null) {
-          return Container();
-        }
-        switch (actionType) {
-          case PillSheetModifiedActionType.createdPillSheet:
-            // TODO: Handle this case.
-            break;
-          case PillSheetModifiedActionType.automaticallyRecordedLastTakenDate:
-            // TODO: Handle this case.
-            break;
-          case PillSheetModifiedActionType.deletedPillSheet:
-            // TODO: Handle this case.
-            break;
-          case PillSheetModifiedActionType.takenPill:
-            // TODO: Handle this case.
-            break;
-          case PillSheetModifiedActionType.revertTakenPill:
-            // TODO: Handle this case.
-            break;
-          case PillSheetModifiedActionType.changedPillNumber:
-            // TODO: Handle this case.
-            break;
-          case PillSheetModifiedActionType.endedPillSheet:
-            // TODO: Handle this case.
-            break;
-        }
-        return Container();
-      })
-          // .expand((element) => element)
+      children: models
+          .map((model) {
+            return [
+              CalendarPillSheetModifiedHistoryMonthlyHeader(
+                dateTimeOfMonth: model.dateTimeOfMonth,
+              ),
+              ...model.pillSheetModifiedHistories
+                  .map((pillSheetModifiedHistory) {
+                final actionType = pillSheetModifiedHistory.enumActionType;
+                if (actionType == null) {
+                  return Container();
+                }
+                switch (actionType) {
+                  case PillSheetModifiedActionType.createdPillSheet:
+                    // TODO: Handle this case.
+                    break;
+                  case PillSheetModifiedActionType
+                      .automaticallyRecordedLastTakenDate:
+                    return PillSheetModifiedHistoryAutomaticallyRecordedLastTakenDateAction();
+                  case PillSheetModifiedActionType.deletedPillSheet:
+                    return PillSheetModifiedHistoryDeletedPillSheetAction();
+                  case PillSheetModifiedActionType.takenPill:
+                    return PillSheetModifiedHistoryRevertTakenPillAction();
+                  case PillSheetModifiedActionType.revertTakenPill:
+                    return PillSheetModifiedHistoryRevertTakenPillAction();
+                  case PillSheetModifiedActionType.changedPillNumber:
+                    return PillSheetModifiedHistoryChangedPillNumberAction();
+                  case PillSheetModifiedActionType.endedPillSheet:
+                    return PillSheetModifiedHistoryEndedPillSheetAction();
+                }
+                return Container();
+              }).toList()
+            ];
+          })
+          .expand((element) => element)
           .toList(),
 //        CalendarPillSheetModifiedHistoryMonthlyHeader(
 //          dateTimeOfMonth: today(),
