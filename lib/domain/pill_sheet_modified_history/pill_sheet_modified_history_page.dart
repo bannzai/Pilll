334ad1999242842f94c3b82a644177fdadbef76d
bannzai <kingkong999yhirose@gmail.com>
2021-08-10 14:59:59 +0900

    :recycle: fix padding position

--- lib/domain/pill_sheet_modified_history/pill_sheet_modified_history_page.dart
+++ lib/domain/pill_sheet_modified_history/pill_sheet_modified_history_page.dart
@@ -41,8 +41,8 @@ class PillSheetModifiedHistoriesPage extends HookWidget {
             return true;
           },
           child: SingleChildScrollView(
+            padding: EdgeInsets.only(left: 32, right: 32, bottom: 100),
             child: Container(
-              padding: EdgeInsets.only(left: 32, right: 32),
               child: CalendarPillSheetModifiedHistoryList(
                 pillSheetModifiedHistories: state.pillSheetModifiedHistories,
               ),
