4ab214cc0f1a0bdd6cd74ac0c401c6c58b1666c0
bannzai <kingkong999yhirose@gmail.com>
2021-08-12 20:25:01 +0900

    :recycle: try blur

--- lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_card.dart
+++ lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_card.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
@@ -7,7 +9,11 @@ import 'package:pilll/domain/calendar/calendar_store.dart';
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_more_button.dart';
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list.dart';
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list_header.dart';
+import 'package:pilll/domain/pill_sheet_modified_history/pill_sheet_modified_history_state.dart';
+import 'package:pilll/entity/pill_sheet.dart';
 import 'package:pilll/entity/pill_sheet_modified_history.dart';
+import 'package:pilll/entity/pill_sheet_modified_history_value.dart';
+import 'package:pilll/entity/pill_sheet_type.dart';
 
 class CalendarPillSheetModifiedHistoryCardState {
   static final pillSheetModifiedHistoriesThreshold = 6;
@@ -24,9 +30,10 @@ class CalendarPillSheetModifiedHistoryCardState {
   });
 
   bool get moreButtonIsShown =>
+      (isPremium || isTrial) &&
       _allPillSheetModifiedHistories.length >
-      CalendarPillSheetModifiedHistoryCardState
-          .pillSheetModifiedHistoriesThreshold;
+          CalendarPillSheetModifiedHistoryCardState
+              .pillSheetModifiedHistoriesThreshold;
   List<PillSheetModifiedHistory> get pillSheetModifiedHistories {
     if (_allPillSheetModifiedHistories.length >
         CalendarPillSheetModifiedHistoryCardState
@@ -78,15 +85,21 @@ class CalendarPillSheetModifiedHistoryCard extends StatelessWidget {
               ],
             ),
             SizedBox(height: 16),
+            Text(
+              "28日連続服用記録中👏",
+              style: TextStyle(
+                color: TextColor.main,
+                fontFamily: FontFamily.japanese,
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(height: 16),
             PillSheetModifiedHisotiryListHeader(),
             SizedBox(height: 4),
             Container(
               padding: const EdgeInsets.only(left: 8, right: 8),
-              child: CalendarPillSheetModifiedHistoryList(
-                padding: null,
-                scrollPhysics: NeverScrollableScrollPhysics(),
-                pillSheetModifiedHistories: state.pillSheetModifiedHistories,
-              ),
+              child: _list(state),
             ),
             if (state.moreButtonIsShown)
               PillSheetModifiedHistoryMoreButton(state: state),
@@ -94,5 +107,53 @@ class CalendarPillSheetModifiedHistoryCard extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _list(CalendarPillSheetModifiedHistoryCardState state) {
+    if (state.isPremium || state.isTrial) {
+      return CalendarPillSheetModifiedHistoryList(
+        padding: null,
+        scrollPhysics: NeverScrollableScrollPhysics(),
+        pillSheetModifiedHistories: state.pillSheetModifiedHistories,
+      );
+    } else {
+      return Container(
+        height: 135,
+        child: Stack(
+          children: [
+            CalendarPillSheetModifiedHistoryList(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              scrollPhysics: NeverScrollableScrollPhysics(),
+              pillSheetModifiedHistories: List.generate(
+                3,
+                (index) => PillSheetModifiedHistory(
+                  id: "1",
+                  actionType: "takenPill",
+                  userID: "1",
+                  value: PillSheetModifiedHistoryValue(
+                    takenPill: TakenPillValue(
+                        afterLastTakenDate: DateTime.now(),
+                        afterLastTakenPillNumber: 10,
+                        beforeLastTakenDate: DateTime.now(),
+                        beforeLastTakenPillNumber: 9),
+                  ),
+                  after: PillSheet.create(PillSheetType.pillsheet_21),
+                  estimatedEventCausingDate: DateTime.now(),
+                ),
+              ),
+            ),
+            Container(
+              height: 135,
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  color: Colors.black.withOpacity(0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
