770f1b8c06c0bc5c3a00962c041f691125e1f04a
bannzai <kingkong999yhirose@gmail.com>
2021-08-13 12:47:34 +0900

    WIP adjustment o.svg

--- lib/domain/calendar/components/pill_sheet_modified_history/components/taken_pill_action_o_list.dart
+++ lib/domain/calendar/components/pill_sheet_modified_history/components/taken_pill_action_o_list.dart
@@ -4,6 +4,9 @@ import 'package:pilll/entity/pill_sheet.dart';
 import 'package:pilll/entity/pill_sheet_type.dart';
 import 'package:pilll/entity/pill_sheet_modified_history_value.dart';
 
+final double _oWidth = 17;
+final double _halfOWidth = 13;
+
 class TakenPillActionOList extends StatelessWidget {
   final TakenPillValue value;
   final PillSheet afterPillSheet;
@@ -16,31 +19,38 @@ class TakenPillActionOList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-        children: List.generate(3, (index) {
-      final inRestDuration = _inRestDuration(
-          afterPillSheet, value.afterLastTakenPillNumber, index);
-      if (index == 0) {
-        return _centerWidget(!inRestDuration
-            ? SvgPicture.asset(
-                "images/dash_o.svg",
-              )
-            : SvgPicture.asset(
-                "images/o.svg",
-              ));
-      } else {
-        return _shiftWidget(
-            !inRestDuration
-                ? SvgPicture.asset("images/dash_half_o.svg")
-                : SvgPicture.asset(
-                    "images/half_o.svg",
-                  ),
-            index);
-      }
-    }).reversed.toList());
+    // final count =
+    // value.afterLastTakenPillNumber - (value.beforeLastTakenPillNumber ?? 1);
+    return Container(
+      decoration: BoxDecoration(border: Border.all()),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: List.generate(3, (index) {
+            final inRestDuration = _inRestDuration(
+                afterPillSheet, value.afterLastTakenPillNumber, index);
+            if (index == 0) {
+              return _centerWidget(!inRestDuration
+                  ? SvgPicture.asset(
+                      "images/dash_o.svg",
+                    )
+                  : SvgPicture.asset(
+                      "images/o.svg",
+                    ));
+            } else {
+              return _shiftWidget(
+                  !inRestDuration
+                      ? SvgPicture.asset("images/dash_half_o.svg")
+                      : SvgPicture.asset(
+                          "images/half_o.svg",
+                        ),
+                  index);
+            }
+          }).toList()),
+    );
   }
 
   Widget _centerWidget(Widget picture) {
+    return picture;
     return Align(
       alignment: Alignment.center,
       child: Container(
@@ -50,6 +60,8 @@ class TakenPillActionOList extends StatelessWidget {
   }
 
   Widget _shiftWidget(Widget picture, int index) {
+    return picture;
+    final double x = (_oWidth / 2) * (_halfOWidth / _oWidth * index);
     return Align(
       alignment: Alignment(0.6 * index, 0),
       child: Container(
