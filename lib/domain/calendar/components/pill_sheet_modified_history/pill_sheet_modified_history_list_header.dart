3517f5f904a62766a93dced1aca97b0a02d71a7e
bannzai <kingkong999yhirose@gmail.com>
2021-08-14 18:16:19 +0900

    :art: adjustment layout

--- lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list_header.dart
+++ lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list_header.dart
@@ -23,6 +23,7 @@ class PillSheetModifiedHisotiryListHeader extends StatelessWidget {
             child: Row(
               children: [
                 Container(
+                  padding: EdgeInsets.only(left: 7),
                   width:
                       PillSheetModifiedHistoryTakenActionLayoutWidths.takenMark,
                   child: Text(
@@ -36,7 +37,7 @@ class PillSheetModifiedHisotiryListHeader extends StatelessWidget {
                     textAlign: TextAlign.start,
                   ),
                 ),
-                SizedBox(width: 24),
+                Spacer(),
                 Container(
                   constraints: BoxConstraints(
                     maxWidth: PillSheetModifiedHistoryTakenActionLayoutWidths
