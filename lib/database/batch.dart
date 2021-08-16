4f72f32886da2e628b65e54c7fc3bc586231b4d1
bannzai <kingkong999yhirose@gmail.com>
2021-08-16 10:10:54 +0900

    :sparkles: using class

--- lib/database/batch.dart
+++ lib/database/batch.dart
@@ -2,11 +2,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pilll/database/database.dart';
 import 'package:riverpod/riverpod.dart';
 
-typedef BatchFactory = WriteBatch Function();
+typedef BatchFactoryFunction = WriteBatch Function();
+
+class BatchFactory {
+  final BatchFactoryFunction _batch;
+
+  BatchFactory(this._batch);
+
+  WriteBatch bach() => _batch();
+}
 
 final batchFactoryProvider = Provider<BatchFactory>((ref) {
   final database = ref.watch(databaseProvider);
-  return () {
+  return BatchFactory(() {
     return database.batch();
-  };
+  });
 });
