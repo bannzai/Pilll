d411f0dbaa4a7ed62e3167f14353ee0a35b9555e
bannzai <kingkong999yhirose@gmail.com>
2021-09-02 21:35:46 +0900

    Restrict change pill sheet type when PillSheetGroup is exists

---
+++ lib/components/page/ok_dialog.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pilll/components/atoms/buttons.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+
+class OKDialog extends StatelessWidget {
+  final String title;
+  final String message;
+
+  const OKDialog({
+    Key? key,
+    required this.title,
+    required this.message,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: SvgPicture.asset("images/alert_24.svg", width: 24, height: 24),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          if (title.isNotEmpty) ...[
+            Text(title, style: FontType.subTitle.merge(TextColorStyle.main)),
+            SizedBox(
+              height: 15,
+            ),
+          ],
+          Text(message, style: FontType.assisting.merge(TextColorStyle.main)),
+        ],
+      ),
+      actions: <Widget>[
+        SecondaryButton(
+          text: "OK",
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
+
+showOKDialog(
+  BuildContext context, {
+  required String title,
+  required String message,
+}) {
+  showDialog(
+    context: context,
+    builder: (context) => OKDialog(
+      title: title,
+      message: message,
+    ),
+  );
+}
