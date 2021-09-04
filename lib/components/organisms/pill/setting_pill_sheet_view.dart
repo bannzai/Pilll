813c5b86df7fabcc4370d4cb52f965c64e7c9e80
bannzai <kingkong999yhirose@gmail.com>
2021-09-04 20:57:28 +0900

    Add generically setting pill sheet

---
+++ lib/components/organisms/pill/setting_pill_sheet_view.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:pilll/analytics.dart';
+import 'package:pilll/components/atoms/color.dart';
+import 'package:pilll/components/organisms/pill/pill_mark.dart';
+import 'package:pilll/components/organisms/pill/pill_mark_line.dart';
+import 'package:pilll/components/organisms/pill/pill_mark_with_number_layout.dart';
+import 'package:pilll/components/organisms/pill/pill_sheet_view_layout.dart';
+import 'package:pilll/entity/pill_mark_type.dart';
+import 'package:pilll/entity/pill_sheet_type.dart';
+import 'package:pilll/entity/weekday.dart';
+
+class SettingPillSheetView extends StatelessWidget {
+  final PillSheetType pillSheetType;
+  final int selectedPillNumber;
+  final Function(int) markSelected;
+
+  const SettingPillSheetView({
+    Key? key,
+    required this.pillSheetType,
+    required this.selectedPillNumber,
+    required this.markSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PillSheetViewLayout(
+      weekdayLines: null,
+      pillMarkLines: List.generate(
+        pillSheetType.numberOfLineInPillSheet,
+        (index) {
+          return PillMarkLine(pillMarks: _pillMarks(context, index));
+        },
+      ),
+    );
+  }
+
+  List<Widget> _pillMarks(BuildContext context, int index) {
+    final lineNumber = index + 1;
+    int countOfPillMarksInLine = Weekday.values.length;
+    if (lineNumber * Weekday.values.length > pillSheetType.totalCount) {
+      int diff = pillSheetType.totalCount - index * Weekday.values.length;
+      countOfPillMarksInLine = diff;
+    }
+    return List.generate(Weekday.values.length, (index) {
+      if (index >= countOfPillMarksInLine) {
+        return Container(width: PillSheetViewLayout.componentWidth);
+      }
+      final number = index + 1;
+      return Container(
+        width: PillSheetViewLayout.componentWidth,
+        child: PillMarkWithNumberLayout(
+          textOfPillNumber:
+              Text("$number", style: TextStyle(color: PilllColors.weekday)),
+          pillMark: PillMark(
+            hasRippleAnimation: false,
+            isDone: false,
+            pillSheetType: selectedPillNumber == number
+                ? PillMarkType.selected
+                : PillMarkType.normal,
+          ),
+          onTap: () {
+            analytics.logEvent(name: "setting_pill_mark_tapped", parameters: {
+              "number": number,
+            });
+            markSelected(number);
+          },
+        ),
+      );
+    });
+  }
+}
