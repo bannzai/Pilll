60987809bdba43077c02a163b152d8eddda8d512
bannzai <kingkong999yhirose@gmail.com>
2021-09-04 21:19:54 +0900

    Move to pill_mark

--- lib/domain/record/components/pill_sheet/record_page_pill_sheet.dart
+++ lib/domain/record/components/pill_sheet/record_page_pill_sheet.dart
@@ -3,7 +3,7 @@ import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/organisms/pill_mark/pill_mark.dart';
 import 'package:pilll/components/organisms/pill_mark/pill_mark_line.dart';
-import 'package:pilll/components/organisms/pill/pill_mark_with_number_layout.dart';
+import 'package:pilll/components/organisms/pill_mark/pill_mark_with_number_layout.dart';
 import 'package:pilll/components/organisms/pill/pill_sheet_view_layout.dart';
 import 'package:pilll/components/organisms/pill/pill_sheet_view_weekday_line.dart';
 import 'package:pilll/domain/record/record_page_state.dart';
