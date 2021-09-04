f7bc8b80d673fb2211ba4a9af6b784711be5c19d
bannzai <kingkong999yhirose@gmail.com>
2021-09-04 21:01:42 +0900

    Remove unnecessary import

--- lib/components/organisms/setting/setting_menstruation_page.dart
+++ lib/components/organisms/setting/setting_menstruation_page.dart
@@ -5,7 +5,6 @@ import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/components/organisms/pill/setting_pill_sheet_view.dart';
-import 'package:pilll/entity/pill_mark_type.dart';
 import 'package:pilll/entity/pill_sheet_type.dart';
 import 'package:pilll/util/toolbar/picker_toolbar.dart';
 import 'package:flutter/cupertino.dart';
