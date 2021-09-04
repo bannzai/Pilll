a40be72c404f23362266c9fae7c2886fae28e625
bannzai <kingkong999yhirose@gmail.com>
2021-09-04 18:01:19 +0900

    :sparkels: extract PillMarkWithNumberLayout

---
+++ lib/components/organisms/pill/pill_mark_with_number_layout.dart
@@ -0,0 +1,52 @@
+import 'package:pilll/components/atoms/color.dart';
+import 'package:pilll/components/organisms/pill/pill_mark.dart';
+import 'package:flutter/material.dart';
+
+class PillMarkWithNumberLayout extends StatelessWidget {
+  final Text textOfPillNumber;
+  final PillMark pillMark;
+  final VoidCallback onTap;
+
+  const PillMarkWithNumberLayout({
+    Key? key,
+    required this.pillMark,
+    required this.textOfPillNumber,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onTap(),
+      child: Column(
+        children: <Widget>[
+          textOfPillNumber,
+          pillMark,
+        ],
+      ),
+    );
+  }
+}
+
+extension PillMarkWithNumberLayoutHelper on PillMarkWithNumberLayout {
+  static TextStyle upperTextColor({
+    required bool isPremium,
+    required int pillNumberForMenstruationBegin,
+    required int menstruationDuration,
+    required int maxPillNumber,
+    required int pillMarkNumber,
+  }) {
+    if (!isPremium) {
+      return TextStyle(color: PilllColors.weekday);
+    }
+    final begin = pillNumberForMenstruationBegin;
+    final duration = menstruationDuration;
+    final menstruationNumbers = List.generate(duration, (index) {
+      final number = (begin + index) % maxPillNumber;
+      return number == 0 ? maxPillNumber : number;
+    });
+    return menstruationNumbers.contains(pillMarkNumber)
+        ? TextStyle(color: PilllColors.primary)
+        : TextStyle(color: PilllColors.weekday);
+  }
+}
