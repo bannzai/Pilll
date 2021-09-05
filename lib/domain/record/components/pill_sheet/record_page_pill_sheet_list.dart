2ca09545ba26ffb5ec9a3729d0696e91d139e42d
bannzai <kingkong999yhirose@gmail.com>
2021-09-05 12:07:30 +0900

    :sparkles: implement dot indicaters

--- lib/domain/record/components/pill_sheet/record_page_pill_sheet_list.dart
+++ lib/domain/record/components/pill_sheet/record_page_pill_sheet_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:pilll/components/molecules/dots_page_indicator.dart';
 import 'package:pilll/components/organisms/pill_sheet/pill_sheet_view_layout.dart';
 import 'package:pilll/domain/record/components/pill_sheet/record_page_pill_sheet.dart';
 import 'package:pilll/domain/record/record_page_state.dart';
@@ -26,33 +27,53 @@ class RecordPagePillSheetList extends HookWidget {
     if (pillSheetGroup == null) {
       return Container();
     }
-    return Container(
-      height: PillSheetViewLayout.calcHeight(
-          pillSheetGroup.pillSheets.first.pillSheetType.numberOfLineInPillSheet,
-          false),
-      child: PageView(
-        clipBehavior: Clip.none,
-        controller: PageController(
-            viewportFraction: (PillSheetViewLayout.width + 20) /
-                MediaQuery.of(context).size.width),
-        scrollDirection: Axis.horizontal,
-        children: pillSheetGroup.pillSheets
-            .map((pillSheet) {
-              return [
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: RecordPagePillSheet(
-                    pillSheet: pillSheet,
-                    store: store,
-                    state: state,
-                    setting: setting,
-                  ),
-                ),
-              ];
-            })
-            .expand((element) => element)
-            .toList(),
-      ),
+
+    final pageController = usePageController(
+        viewportFraction: (PillSheetViewLayout.width + 20) /
+            MediaQuery.of(context).size.width);
+    return Column(
+      children: [
+        Container(
+          height: PillSheetViewLayout.calcHeight(
+              pillSheetGroup
+                  .pillSheets.first.pillSheetType.numberOfLineInPillSheet,
+              false),
+          child: PageView(
+            clipBehavior: Clip.none,
+            controller: pageController,
+            scrollDirection: Axis.horizontal,
+            children: pillSheetGroup.pillSheets
+                .map((pillSheet) {
+                  return [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: RecordPagePillSheet(
+                        pillSheet: pillSheet,
+                        store: store,
+                        state: state,
+                        setting: setting,
+                      ),
+                    ),
+                  ];
+                })
+                .expand((element) => element)
+                .toList(),
+          ),
+        ),
+        if (pillSheetGroup.pillSheets.length > 1) ...[
+          SizedBox(height: 16),
+          DotsIndicator(
+              controller: pageController,
+              itemCount: pillSheetGroup.pillSheets.length,
+              onDotTapped: (page) {
+                pageController.animateToPage(
+                  page,
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                );
+              })
+        ]
+      ],
     );
   }
 }
