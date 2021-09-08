fc6b5b1d10ca1cc36f85a6fab1f84684fdb04057
bannzai <kingkong999yhirose@gmail.com>
2021-09-08 10:50:49 +0900

    :memo: Pass activedPillSheet

--- lib/domain/settings/components/rows/today_pill_number.dart
+++ lib/domain/settings/components/rows/today_pill_number.dart
@@ -5,14 +5,17 @@ import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/domain/settings/modifing_pill_number_page.dart';
 import 'package:pilll/domain/settings/setting_page_store.dart';
+import 'package:pilll/entity/pill_sheet.dart';
 import 'package:pilll/entity/setting.dart';
 
 class TodayPllNumberRow extends HookWidget {
   final Setting setting;
+  final PillSheet activedPillSheet;
 
   const TodayPllNumberRow({
     Key? key,
     required this.setting,
+    required this.activedPillSheet,
   }) : super(key: key);
 
   @override
@@ -20,17 +23,18 @@ class TodayPllNumberRow extends HookWidget {
     final store = useProvider(settingStoreProvider);
     return ListTile(
       title: Text("今日飲むピル番号の変更", style: FontType.listRow),
-      onTap: () => _onTap(context, store, setting),
+      onTap: () => _onTap(context, store, setting, activedPillSheet),
     );
   }
 
-  _onTap(BuildContext context, SettingStateStore store, Setting setting) {
+  _onTap(BuildContext context, SettingStateStore store, Setting setting,
+      PillSheet activedPillSheet) {
     analytics.logEvent(
       name: "did_select_changing_pill_number",
     );
     Navigator.of(context).push(
       ModifingPillNumberPageRoute.route(
-        pillSheetType: setting.legacyPropertyForPillSheetType,
+        activedPillSheet: activedPillSheet,
         markSelected: (number) {
           Navigator.pop(context);
           store.modifyBeginingDate(number);
