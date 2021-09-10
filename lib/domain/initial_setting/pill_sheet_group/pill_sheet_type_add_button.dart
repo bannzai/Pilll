8102be1c3e7efdef101b4e6b77469d733ed0cb4d
bannzai <kingkong999yhirose@gmail.com>
2021-09-10 21:36:13 +0900

    separate widget

---
+++ lib/domain/initial_setting/pill_sheet_group/pill_sheet_type_add_button.dart
@@ -0,0 +1,51 @@
+import 'package:pilll/domain/initial_setting/pill_sheet_group/initial_setting_pill_sheet_group_select_pill_sheet_type_page.dart';
+
+import 'package:pilll/domain/initial_setting/initial_setting_store.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PillSheetTypeAddButton extends StatelessWidget {
+  final InitialSettingStateStore store;
+  const PillSheetTypeAddButton({Key? key, required this.store})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showInitialSettingPillSheetGroupSelectPillSheetTypePage(
+            context: context,
+            pillSheetType: null,
+            onSelect: (pillSheetType) {
+              store.addPillSheetType(pillSheetType);
+            });
+      },
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.add,
+              color: TextColor.noshime,
+              size: 20,
+            ),
+            SizedBox(width: 4),
+            Text(
+              "ピルシートを追加",
+              style: TextStyle(
+                color: TextColor.main,
+                fontFamily: FontFamily.japanese,
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
