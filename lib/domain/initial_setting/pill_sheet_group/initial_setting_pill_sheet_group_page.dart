bd3b20c8d8673952ccbf1f5fc4967741301fba7a
bannzai <kingkong999yhirose@gmail.com>
2021-09-10 19:34:20 +0900

    :art: adjust layout

--- lib/domain/initial_setting/pill_sheet_group/initial_setting_pill_sheet_group_page.dart
+++ lib/domain/initial_setting/pill_sheet_group/initial_setting_pill_sheet_group_page.dart
@@ -32,6 +32,7 @@ class InitialSettingPillSheetGroupPage extends HookWidget {
       ),
       body: SafeArea(
         child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 40),
           child: Stack(
             children: [
               ListView(
@@ -43,68 +44,50 @@ class InitialSettingPillSheetGroupPage extends HookWidget {
                     textAlign: TextAlign.center,
                   ),
                   SizedBox(height: 6),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 26),
-                    child: Column(
+                  ...state.pillSheetTypes
+                      .asMap()
+                      .map((index, pillSheetType) {
+                        return MapEntry(
+                          index,
+                          [
+                            SizedBox(height: 16),
+                            InitialSettingPillSheetGroupPillSheetTypeSelectRow(
+                              index: index,
+                              pillSheetType: pillSheetType,
+                              state: state,
+                              store: store,
+                            ),
+                          ],
+                        );
+                      })
+                      .values
+                      .expand((element) => element)
+                      .toList(),
+                  if (state.pillSheetTypes.length > 1) ...[
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        ...state.pillSheetTypes
-                            .asMap()
-                            .map((index, pillSheetType) {
-                              return MapEntry(
-                                index,
-                                [
-                                  SizedBox(height: 16),
-                                  InitialSettingPillSheetGroupPillSheetTypeSelectRow(
-                                    index: index,
-                                    pillSheetType: pillSheetType,
-                                    state: state,
-                                    store: store,
-                                  ),
-                                ],
-                              );
-                            })
-                            .values
-                            .expand((element) => element)
-                            .toList(),
-                        SizedBox(height: 20),
-                        PillSheetTypeAddButton(store: store),
+                        Text(
+                          "連番表示モード",
+                          style: TextStyle(
+                            color: TextColor.main,
+                            fontSize: 14,
+                            fontFamily: FontFamily.japanese,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Spacer(),
+                        Switch(
+                            value: state.isOnSequenceAppearance,
+                            onChanged: (isOn) {
+                              store.setIsOnSequenceAppearance(isOn);
+                            }),
                       ],
                     ),
-                  ),
-                  Spacer(),
-                  Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "連番表示モード",
-                            style: TextStyle(
-                              color: TextColor.main,
-                              fontSize: 14,
-                              fontFamily: FontFamily.japanese,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          Switch(
-                              value: state.isOnSequenceAppearance,
-                              onChanged: (isOn) {
-                                store.setIsOnSequenceAppearance(isOn);
-                              }),
-                        ],
-                      ),
-                      SizedBox(height: 16),
-                      Text(
-                        "連続服用する方は連番表示をお勧めします\n（ヤーズフレックスやジェミーナなど）",
-                        style: TextStyle(
-                          color: TextColor.main,
-                          fontFamily: FontFamily.japanese,
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
+                  ],
+                  SizedBox(height: 24),
+                  PillSheetTypeAddButton(store: store),
                   SizedBox(height: 80),
                 ],
               ),
@@ -112,13 +95,17 @@ class InitialSettingPillSheetGroupPage extends HookWidget {
                 alignment: Alignment.bottomCenter,
                 child: Padding(
                   padding: const EdgeInsets.only(bottom: 35),
-                  child: PrimaryButton(
-                    text: "次へ",
-                    onPressed: () async {
-                      analytics.logEvent(name: "next_pill_sheet_count");
-                      Navigator.of(context).push(
-                          InitialSettingSelectTodayPillNumberPageRoute.route());
-                    },
+                  child: Container(
+                    color: PilllColors.background,
+                    child: PrimaryButton(
+                      text: "次へ",
+                      onPressed: () async {
+                        analytics.logEvent(name: "next_pill_sheet_count");
+                        Navigator.of(context).push(
+                            InitialSettingSelectTodayPillNumberPageRoute
+                                .route());
+                      },
+                    ),
                   ),
                 ),
               ),
@@ -149,7 +136,7 @@ class PillSheetTypeAddButton extends StatelessWidget {
       },
       child: Container(
         width: double.infinity,
-        padding: EdgeInsets.symmetric(vertical: 16),
+        padding: EdgeInsets.symmetric(vertical: 13),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
