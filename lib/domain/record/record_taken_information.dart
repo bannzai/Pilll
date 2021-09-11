38f41c73003062e2db6212706969c6344dcc147c
bannzai <kingkong999yhirose@gmail.com>
2021-09-11 11:32:11 +0900

    :sparkles: Remake RecordTakenInformation

--- lib/domain/record/record_taken_information.dart
+++ lib/domain/record/record_taken_information.dart
@@ -1,8 +1,7 @@
-import 'package:pilll/domain/record/record_page_state.dart';
-import 'package:pilll/entity/pill_sheet.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
+import 'package:pilll/entity/pill_sheet_group.dart';
 import 'package:pilll/entity/pill_sheet_type.dart';
 import 'package:pilll/util/formatter/date_time_formatter.dart';
 import 'package:flutter/material.dart';
@@ -13,20 +12,17 @@ abstract class RecordTakenInformationConst {
 
 class RecordTakenInformation extends StatelessWidget {
   final DateTime today;
-  final RecordPageState state;
-  PillSheet? get pillSheetModel => state.pillSheetGroup?.activedPillSheet;
+  final PillSheetGroup? pillSheetGroup;
   final VoidCallback onPressed;
   const RecordTakenInformation({
     Key? key,
     required this.today,
-    required this.state,
+    required this.pillSheetGroup,
     required this.onPressed,
   }) : super(key: key);
 
   String _formattedToday() => DateTimeFormatter.monthAndDay(this.today);
-
   String _todayWeekday() => DateTimeFormatter.weekday(this.today);
-  bool get pillSheetIsValid => pillSheetModel != null && !state.pillSheetGroupIsHidden;
 
   @override
   Widget build(BuildContext context) {
@@ -48,62 +44,11 @@ class RecordTakenInformation extends StatelessWidget {
                 ),
               ),
               SizedBox(width: 28),
-              _takenWidget(),
+              TakeToday(pillSheetGroup: pillSheetGroup, onPressed: onPressed),
             ],
           ),
         ],
       ),
-    );
-  }
-
-  Widget _takenWidget() {
-    final pillSheetModel = this.pillSheetModel;
-    return GestureDetector(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "💊 今日飲むピル",
-            style: FontType.assisting.merge(TextColorStyle.noshime),
-          ),
-          if (pillSheetIsValid) SizedBox(height: 4),
-          if (!pillSheetIsValid) SizedBox(height: 12),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.baseline,
-            textBaseline: TextBaseline.ideographic,
-            children: <Widget>[
-              if (pillSheetIsValid) ...[
-                if (pillSheetModel != null &&
-                    !pillSheetModel.inNotTakenDuration) ...[
-                  Text("${pillSheetModel.todayPillNumber}",
-                      style: FontType.xHugeNumber.merge(TextColorStyle.main)),
-                  Text("番",
-                      style:
-                          FontType.assistingBold.merge(TextColorStyle.noshime)),
-                ],
-                if (pillSheetModel != null &&
-                    pillSheetModel.inNotTakenDuration) ...[
-                  Text(
-                    "${pillSheetModel.pillSheetType.notTakenWord}${pillSheetModel.todayPillNumber - pillSheetModel.typeInfo.dosingPeriod}日目",
-                    style: FontType.assistingBold.merge(TextColorStyle.main),
-                  ),
-                ],
-              ],
-              if (!pillSheetIsValid) ...[
-                Text("-",
-                    style: FontType.assisting.merge(TextColorStyle.noshime)),
-              ],
-            ],
-          )
-        ],
-      ),
-      onTap: () {
-        if (!pillSheetIsValid) {
-          return;
-        }
-        this.onPressed();
-      },
     );
   }
 
@@ -113,6 +58,63 @@ class RecordTakenInformation extends StatelessWidget {
         "${_formattedToday()} (${_todayWeekday()})",
         style: FontType.xBigNumber.merge(TextColorStyle.gray),
       ),
+    );
+  }
+}
+
+class TakeToday extends StatelessWidget {
+  final PillSheetGroup? pillSheetGroup;
+  final VoidCallback onPressed;
+
+  const TakeToday({
+    Key? key,
+    required this.pillSheetGroup,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "💊 今日飲むピル",
+            style: FontType.assisting.merge(TextColorStyle.noshime),
+          ),
+          _content(),
+        ],
+      ),
+      onTap: () {
+        if (pillSheetGroup?.activedPillSheet == null) {
+          return;
+        }
+        this.onPressed();
+      },
+    );
+  }
+
+  Widget _content() {
+    final pillSheetGroup = this.pillSheetGroup;
+    final pillSheet = this.pillSheetGroup?.activedPillSheet;
+    if (pillSheetGroup == null || pillSheet == null || pillSheet.isInvalid) {
+      return Text("-", style: FontType.assisting.merge(TextColorStyle.noshime));
+    }
+    if (pillSheet.inNotTakenDuration) {
+      return Text(
+        "${pillSheet.pillSheetType.notTakenWord}${pillSheet.todayPillNumber - pillSheet.typeInfo.dosingPeriod}日目",
+        style: FontType.assistingBold.merge(TextColorStyle.main),
+      );
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.ideographic,
+      children: <Widget>[
+        Text("${pillSheetGroup.serializedTodayPillNumber}",
+            style: FontType.xHugeNumber.merge(TextColorStyle.main)),
+        Text("番", style: FontType.assistingBold.merge(TextColorStyle.noshime)),
+      ],
     );
   }
 }
