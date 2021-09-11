7c81aa0bd7df09b0c825a04a9a9a91c059ccaf07
bannzai <kingkong999yhirose@gmail.com>
2021-09-11 11:41:57 +0900

    :recycle: fix change number

--- lib/domain/record/components/header/record_page_header.dart
+++ lib/domain/record/components/header/record_page_header.dart
@@ -5,7 +5,6 @@ import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/domain/record/components/header/take_today.dart';
 import 'package:pilll/domain/record/record_page_store.dart';
-import 'package:pilll/entity/pill_sheet.dart';
 import 'package:pilll/entity/pill_sheet_group.dart';
 import 'package:pilll/util/formatter/date_time_formatter.dart';
 import 'package:flutter/material.dart';
@@ -57,7 +56,7 @@ class RecordPageHeaderrmation extends StatelessWidget {
                     analytics.logEvent(
                         name: "tapped_record_information_header");
                     if (pillSheet != null) {
-                      _showBeginDatePicker(context, pillSheet, store);
+                      _showBeginDatePicker(context, pillSheetGroup, store);
                     }
                   }),
             ],
@@ -76,19 +75,27 @@ class RecordPageHeaderrmation extends StatelessWidget {
     );
   }
 
-  _showBeginDatePicker(
-      BuildContext context, PillSheet currentPillSheet, RecordPageStore store) {
+  _showBeginDatePicker(BuildContext context, PillSheetGroup? pillSheetGroup,
+      RecordPageStore store) {
+    if (pillSheetGroup == null) {
+      return;
+    }
+    var selectedTodayPillNumber = pillSheetGroup.serializedTodayPillNumber;
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
-        var selectedTodayPillNumber = currentPillSheet.todayPillNumber;
         return Column(
           mainAxisAlignment: MainAxisAlignment.end,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             PickerToolbar(
               done: (() {
-                store.modifyBeginingDate(selectedTodayPillNumber);
+                final _selectedTodayPillNumber = selectedTodayPillNumber;
+                if (_selectedTodayPillNumber == null) {
+                  Navigator.pop(context);
+                  return;
+                }
+                store.modifyBeginingDate(_selectedTodayPillNumber);
                 Navigator.pop(context);
               }),
               cancel: (() {
@@ -103,13 +110,20 @@ class RecordPageHeaderrmation extends StatelessWidget {
                 },
                 child: CupertinoPicker(
                   itemExtent: 40,
-                  children: List.generate(currentPillSheet.typeInfo.totalCount,
+                  children: List.generate(
+                      pillSheetGroup.totalPillCountIntoGroup,
                       (index) => Text("${index + 1}")),
                   onSelectedItemChanged: (index) {
                     selectedTodayPillNumber = index + 1;
                   },
                   scrollController: FixedExtentScrollController(
-                    initialItem: selectedTodayPillNumber - 1,
+                    initialItem: () {
+                      final _selectedTodayPillNumber = selectedTodayPillNumber;
+                      if (_selectedTodayPillNumber == null) {
+                        return 0;
+                      }
+                      return _selectedTodayPillNumber - 1;
+                    }(),
                   ),
                 ),
               ),
