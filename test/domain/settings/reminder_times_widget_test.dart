5fbf8e7409c22893aeb7f4bef76a8a91dd54ca0b
bannzai <kingkong999yhirose@gmail.com>
2021-09-15 17:20:47 +0900

    :bug: missing test case

--- test/domain/settings/reminder_times_widget_test.dart
+++ test/domain/settings/reminder_times_widget_test.dart
@@ -107,7 +107,7 @@ void main() {
       await tester.pumpAndSettle(Duration(milliseconds: 500));
 
       expect(find.text("通知時間の追加"), findsOneWidget);
-      expect(find.byWidgetPredicate((widget) => Widget is Dismissible),
+      expect(find.byWidgetPredicate((widget) => widget is Dismissible),
           findsNothing);
     });
     testWidgets('when setting has maximum count reminder times︎',
