5690492439dcf0abbfa4c173799d159804908b13
bannzai <kingkong999yhirose@gmail.com>
2021-09-15 19:57:19 +0900

    :sparkels: add test case

---
+++ test/domain/initial_setting/initial_setting_state_test.dart
@@ -0,0 +1,67 @@
+import 'package:pilll/domain/initial_setting/initial_setting_state.dart';
+import 'package:pilll/entity/pill_sheet.dart';
+import 'package:pilll/entity/pill_sheet_type.dart';
+import 'package:pilll/service/day.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../helper/mock.mocks.dart';
+
+void main() {
+  setUp(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+  });
+  group("#InitialSettingState.buildPillSheet", () {
+    test("it is builded pillSheet.gropuIndex == todayPillNumber.pageIndex ",
+        () async {
+      final mockTodayRepository = MockTodayService();
+      final today = DateTime.parse("2020-11-23");
+      todayRepository = mockTodayRepository;
+      when(mockTodayRepository.now()).thenReturn(today);
+      when(mockTodayRepository.today()).thenReturn(today);
+
+      final pillSheet = InitialSettingState.buildPillSheet(
+        pageIndex: 0,
+        todayPillNumber: InitialSettingTodayPillNumber(
+            pageIndex: 0, pillNumberIntoPillSheet: 1),
+        pillSheetTypes: [PillSheetType.pillsheet_21],
+      );
+
+      final expected = PillSheet(
+        typeInfo: PillSheetType.pillsheet_21.typeInfo,
+        beginingDate: DateTime.parse("2020-11-23"),
+      );
+
+      expect(expected, pillSheet);
+    });
+    test("it is builded pillSheet.gropuIndex > todayPillNumber.pageIndex ",
+        () async {
+      final mockTodayRepository = MockTodayService();
+      final today = DateTime.parse("2020-11-23");
+      todayRepository = mockTodayRepository;
+      when(mockTodayRepository.now()).thenReturn(today);
+      when(mockTodayRepository.today()).thenReturn(today);
+
+      final pillSheet = InitialSettingState.buildPillSheet(
+        pageIndex: 1,
+        todayPillNumber: InitialSettingTodayPillNumber(
+            pageIndex: 0, pillNumberIntoPillSheet: 1),
+        pillSheetTypes: [
+          PillSheetType.pillsheet_21,
+          PillSheetType.pillsheet_24_0,
+        ],
+      );
+
+      final expected = PillSheet(
+        groupIndex: 1,
+        typeInfo: PillSheetType.pillsheet_24_0.typeInfo,
+        beginingDate: DateTime.parse("2020-12-21"),
+        lastTakenDate: null,
+      );
+
+      expect(expected, pillSheet);
+    });
+  });
+}
