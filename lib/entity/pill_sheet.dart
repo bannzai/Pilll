a636432fa6e4cd310eb8a72841f4555c7071ee3c
bannzai <kingkong999yhirose@gmail.com>
2021-09-16 11:31:26 +0900

    :recycle: remove unnecessary option

--- lib/entity/pill_sheet.dart
+++ lib/entity/pill_sheet.dart
@@ -98,7 +98,7 @@ abstract class PillSheet implements _$PillSheet {
   bool get isDeleted => deletedAt != null;
   bool get inNotTakenDuration => todayPillNumber > typeInfo.dosingPeriod;
   bool get isFill => lastTakenPillNumber >= pillSheetType.totalCount;
-  bool get isInvalid => isDeleted || isReached;
+  bool get isInvalid => isDeleted;
   bool get hasRestDuration => !pillSheetType.isNotExistsNotTakenDuration;
 
   bool get isActive {
