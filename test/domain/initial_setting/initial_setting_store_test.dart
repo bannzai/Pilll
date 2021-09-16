2e286c31510af55b7a0b780cf161314800f9fa17
bannzai <kingkong999yhirose@gmail.com>
2021-09-16 09:27:02 +0900

    :sparekls: add test case

--- test/domain/initial_setting/initial_setting_store_test.dart
+++ test/domain/initial_setting/initial_setting_store_test.dart
@@ -125,4 +125,111 @@ void main() {
               pageIndex: 0, pillNumberIntoPillSheet: 24));
     });
   });
+  group("#addPillSheetType", () {
+    test("add new one", () {
+      final batchFactory = MockBatchFactory();
+      final authService = MockAuthService();
+      when(authService.subscribe())
+          .thenAnswer((realInvocation) => Stream.empty());
+      final settingService = MockSettingService();
+      final pillSheetService = MockPillSheetService();
+      final pillSheetModifiedHistoryService =
+          MockPillSheetModifiedHistoryService();
+      final pillSheetGroupService = MockPillSheetGroupService();
+
+      final container = ProviderContainer(
+        overrides: [
+          batchFactoryProvider.overrideWithValue(batchFactory),
+          authServiceProvider.overrideWithValue(authService),
+          settingServiceProvider.overrideWithValue(settingService),
+          pillSheetServiceProvider.overrideWithValue(pillSheetService),
+          pillSheetModifiedHistoryServiceProvider
+              .overrideWithValue(pillSheetModifiedHistoryService),
+          pillSheetGroupServiceProvider
+              .overrideWithValue(pillSheetGroupService),
+        ],
+      );
+      final store = container.read(initialSettingStoreProvider);
+
+      store.selectedPillSheetType(PillSheetType.pillsheet_21);
+      store.addPillSheetType(PillSheetType.pillsheet_28_0);
+      expect(container.read(initialSettingStateProvider).pillSheetTypes,
+          [PillSheetType.pillsheet_21, PillSheetType.pillsheet_28_0]);
+    });
+    test("overwrite pill sheet type", () {
+      final batchFactory = MockBatchFactory();
+      final authService = MockAuthService();
+      when(authService.subscribe())
+          .thenAnswer((realInvocation) => Stream.empty());
+      final settingService = MockSettingService();
+      final pillSheetService = MockPillSheetService();
+      final pillSheetModifiedHistoryService =
+          MockPillSheetModifiedHistoryService();
+      final pillSheetGroupService = MockPillSheetGroupService();
+
+      final container = ProviderContainer(
+        overrides: [
+          batchFactoryProvider.overrideWithValue(batchFactory),
+          authServiceProvider.overrideWithValue(authService),
+          settingServiceProvider.overrideWithValue(settingService),
+          pillSheetServiceProvider.overrideWithValue(pillSheetService),
+          pillSheetModifiedHistoryServiceProvider
+              .overrideWithValue(pillSheetModifiedHistoryService),
+          pillSheetGroupServiceProvider
+              .overrideWithValue(pillSheetGroupService),
+        ],
+      );
+      final store = container.read(initialSettingStoreProvider);
+
+      store.selectedPillSheetType(PillSheetType.pillsheet_21);
+      expect(container.read(initialSettingStateProvider).pillSheetTypes,
+          [PillSheetType.pillsheet_21]);
+
+      store.selectedPillSheetType(PillSheetType.pillsheet_28_0);
+      expect(container.read(initialSettingStateProvider).pillSheetTypes,
+          [PillSheetType.pillsheet_28_0]);
+    });
+
+    test("reset todayPillNumber", () {
+      final batchFactory = MockBatchFactory();
+      final authService = MockAuthService();
+      when(authService.subscribe())
+          .thenAnswer((realInvocation) => Stream.empty());
+      final settingService = MockSettingService();
+      final pillSheetService = MockPillSheetService();
+      final pillSheetModifiedHistoryService =
+          MockPillSheetModifiedHistoryService();
+      final pillSheetGroupService = MockPillSheetGroupService();
+
+      final container = ProviderContainer(
+        overrides: [
+          batchFactoryProvider.overrideWithValue(batchFactory),
+          authServiceProvider.overrideWithValue(authService),
+          settingServiceProvider.overrideWithValue(settingService),
+          pillSheetServiceProvider.overrideWithValue(pillSheetService),
+          pillSheetModifiedHistoryServiceProvider
+              .overrideWithValue(pillSheetModifiedHistoryService),
+          pillSheetGroupServiceProvider
+              .overrideWithValue(pillSheetGroupService),
+        ],
+      );
+      final store = container.read(initialSettingStoreProvider);
+      // ignore: invalid_use_of_protected_member
+      store.state = store.state.copyWith(
+          todayPillNumber: InitialSettingTodayPillNumber(
+              pageIndex: 0, pillNumberIntoPillSheet: 28));
+      expect(
+          container.read(initialSettingStateProvider).todayPillNumber,
+          InitialSettingTodayPillNumber(
+              pageIndex: 0, pillNumberIntoPillSheet: 28));
+
+      store.selectedPillSheetType(PillSheetType.pillsheet_24_0);
+      expect(container.read(initialSettingStateProvider).pillSheetTypes,
+          [PillSheetType.pillsheet_24_0]);
+      expect(
+          container.read(initialSettingStateProvider).todayPillNumber,
+          InitialSettingTodayPillNumber(
+              pageIndex: 0, pillNumberIntoPillSheet: 24));
+    });
+  });
 }
