1fe2956f2d88efc091b240322cf85339dacf94ec
bannzai <kingkong999yhirose@gmail.com>
2021-09-18 21:47:44 +0900

    :sparkels: add test case for record page store#register

---
+++ test/domain/record/record_page_store_test.dart
@@ -0,0 +1,125 @@
+import 'package:pilll/database/batch.dart';
+import 'package:pilll/domain/initial_setting/initial_setting_state.dart';
+import 'package:pilll/domain/initial_setting/initial_setting_store.dart';
+import 'package:pilll/domain/record/record_page_store.dart';
+import 'package:pilll/entity/pill_sheet.dart';
+import 'package:pilll/entity/pill_sheet_group.dart';
+import 'package:pilll/entity/pill_sheet_type.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pilll/entity/setting.dart';
+import 'package:pilll/entity/user.dart';
+import 'package:pilll/service/auth.dart';
+import 'package:pilll/service/day.dart';
+import 'package:pilll/service/pill_sheet.dart';
+import 'package:pilll/service/pill_sheet_group.dart';
+import 'package:pilll/service/pill_sheet_modified_history.dart';
+import 'package:pilll/service/setting.dart';
+import 'package:pilll/service/user.dart';
+import 'package:pilll/util/datetime/day.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../helper/mock.mocks.dart';
+
+void main() {
+  setUp(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+  });
+  group("#register", () {
+    test("group has two pill sheet", () {
+      var mockTodayRepository = MockTodayService();
+      final _today = DateTime.parse("2020-09-19");
+      todayRepository = mockTodayRepository;
+      when(mockTodayRepository.today()).thenReturn(_today);
+      when(mockTodayRepository.now()).thenReturn(_today);
+
+      final batchFactory = MockBatchFactory();
+      final batch = MockWriteBatch();
+      when(batchFactory.batch()).thenReturn(batch);
+      final authService = MockAuthService();
+      when(authService.subscribe())
+          .thenAnswer((realInvocation) => Stream.empty());
+
+      final pillSheet = PillSheet(
+        typeInfo: PillSheetType.pillsheet_28_0.typeInfo,
+        beginingDate: _today,
+        groupIndex: 0,
+        lastTakenDate: null,
+      );
+      final pillSheet2 = PillSheet(
+        typeInfo: PillSheetType.pillsheet_21.typeInfo,
+        beginingDate: _today.add(Duration(days: 28)),
+        lastTakenDate: null,
+        groupIndex: 1,
+      );
+      final pillSheetService = MockPillSheetService();
+      when(pillSheetService.register(batch, [pillSheet, pillSheet2]))
+          .thenReturn([
+        pillSheet.copyWith(id: "sheet_id"),
+        pillSheet2.copyWith(id: "sheet_id2")
+      ]);
+
+      final pillSheetGroup = PillSheetGroup(
+        pillSheetIDs: ["sheet_id", "sheet_id2"],
+        pillSheets: [
+          pillSheet.copyWith(id: "sheet_id"),
+          pillSheet2.copyWith(id: "sheet_id2"),
+        ],
+        createdAt: now(),
+      );
+      final pillSheetGroupService = MockPillSheetGroupService();
+      when(pillSheetGroupService.register(batch, pillSheetGroup))
+          .thenReturn(pillSheetGroup.copyWith(id: "group_id"));
+
+      final history = PillSheetModifiedHistoryServiceActionFactory
+          .createCreatedPillSheetAction(
+              pillSheetGroupID: "group_id",
+              pillSheetIDs: ["sheet_id", "sheet_id2"]);
+      final pillSheetModifiedHistoryService =
+          MockPillSheetModifiedHistoryService();
+      when(pillSheetModifiedHistoryService.add(batch, history))
+          .thenReturn(null);
+
+      final setting = Setting(
+        pillNumberForFromMenstruation: 22,
+        durationMenstruation: 3,
+        isOnReminder: true,
+        reminderTimes: [
+          ReminderTime(hour: 21, minute: 20),
+          ReminderTime(hour: 22, minute: 0)
+        ],
+        pillSheetTypes: [
+          PillSheetType.pillsheet_28_0,
+          PillSheetType.pillsheet_21
+        ],
+      );
+      final settingService = MockSettingService();
+      when(settingService.fetch())
+          .thenAnswer((realInvocation) async => setting);
+      when(settingService.updateWithBatch(batch, setting)).thenReturn(null);
+
+      final user = User();
+      final userService = MockUserService();
+      when(userService.fetch()).thenAnswer((realInvocation) async => user);
+
+      final container = ProviderContainer(
+        overrides: [
+          batchFactoryProvider.overrideWithValue(batchFactory),
+          authServiceProvider.overrideWithValue(authService),
+          settingServiceProvider.overrideWithValue(settingService),
+          pillSheetServiceProvider.overrideWithValue(pillSheetService),
+          pillSheetModifiedHistoryServiceProvider
+              .overrideWithValue(pillSheetModifiedHistoryService),
+          pillSheetGroupServiceProvider
+              .overrideWithValue(pillSheetGroupService),
+          userServiceProvider.overrideWithValue(userService),
+        ],
+      );
+      final store = container.read(recordPageStoreProvider);
+
+      store.register(setting);
+    });
+  });
+}
