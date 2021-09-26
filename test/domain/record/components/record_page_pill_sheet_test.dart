1183ebcde8f2596eb5ab8ac4be88409d6dca8d55
bannzai <kingkong999yhirose@gmail.com>
2021-09-27 08:25:50 +0900

    :sparkles: add test case

--- test/domain/record/components/record_page_pill_sheet_test.dart
+++ test/domain/record/components/record_page_pill_sheet_test.dart
@@ -17,6 +17,7 @@ void main() {
       final pillSheet = PillSheet(
         typeInfo: PillSheetType.pillsheet_28_0.typeInfo,
         beginingDate: anyDate,
+        groupIndex: 0,
       );
       final pillSheetGroup = PillSheetGroup(
         pillSheetIDs: ["1"],
@@ -39,6 +40,59 @@ void main() {
                 pageIndex: pageIndex,
                 setting: setting),
             22 <= i && i <= 24,
+            reason: "print debug informations pillNumberIntoPillSheet is $i");
+      }
+    });
+    test(
+        "group has three pill sheet and scheduled menstruation begin No.2 pillSheet",
+        () async {
+      final anyDate = DateTime.parse("2020-09-19");
+      final one = PillSheet(
+        typeInfo: PillSheetType.pillsheet_28_0.typeInfo,
+        beginingDate: anyDate,
+        groupIndex: 0,
+      );
+      final two = PillSheet(
+        typeInfo: PillSheetType.pillsheet_28_0.typeInfo,
+        beginingDate: anyDate,
+        groupIndex: 1,
+      );
+      final three = PillSheet(
+        typeInfo: PillSheetType.pillsheet_28_0.typeInfo,
+        beginingDate: anyDate,
+        groupIndex: 2,
+      );
+      final pillSheetGroup = PillSheetGroup(
+        pillSheetIDs: ["1", "2", "3"],
+        pillSheets: [one, two, three],
+        createdAt: anyDate,
+      );
+      final setting = Setting(
+        pillSheetTypes: [
+          PillSheetType.pillsheet_28_0,
+          PillSheetType.pillsheet_28_0,
+          PillSheetType.pillsheet_28_0
+        ],
+        pillNumberForFromMenstruation: 46,
+        durationMenstruation: 3,
+        isOnReminder: true,
+      );
+      final pageIndex = 0;
+
+      final totalCount = [
+        PillSheetType.pillsheet_28_0,
+        PillSheetType.pillsheet_28_0,
+        PillSheetType.pillsheet_28_0
+      ].map((e) => e.totalCount).reduce((value, element) => value + element);
+
+      for (int i = 1; i <= totalCount; i++) {
+        expect(
+            RecordPagePillSheet.isContainedMenstruationDuration(
+                pillNumberIntoPillSheet: i,
+                pillSheetGroup: pillSheetGroup,
+                pageIndex: pageIndex,
+                setting: setting),
+            46 <= i && i <= 48,
             reason: "print debug informations pillNumberIntoPillSheet is $i");
       }
     });
