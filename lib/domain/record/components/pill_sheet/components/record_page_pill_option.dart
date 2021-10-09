a72f1d903d50fbdbbfd1857d5746371f5fe9dcde
bannzai <kingkong999yhirose@gmail.com>
2021-10-10 06:03:27 +0900

    :sparkles: add option widget

---
+++ lib/domain/record/components/pill_sheet/components/record_page_pill_option.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:pilll/components/atoms/buttons.dart';
+import 'package:pilll/domain/record/record_page_store.dart';
+import 'package:pilll/entity/pill_sheet.dart';
+import 'package:pilll/entity/pill_sheet_group.dart';
+
+class RecordPagePillOption extends StatelessWidget {
+  final RecordPageStore store;
+  final PillSheetGroup pillSheetGroup;
+  final PillSheet activedPillSheet;
+
+  const RecordPagePillOption({
+    Key? key,
+    required this.store,
+    required this.pillSheetGroup,
+    required this.activedPillSheet,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Spacer(),
+      PrimaryOutlinedButton(
+          onPressed: () {
+            store.beginResting(
+              pillSheetGroup: pillSheetGroup,
+              activedPillSheet: activedPillSheet,
+            );
+          },
+          text: "休薬する"),
+    ]);
+  }
+}
