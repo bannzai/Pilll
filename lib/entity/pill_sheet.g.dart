f29a4c9a1eb2fd0bde0fda0a82081dbb18e10e91
bannzai <kingkong999yhirose@gmail.com>
2021-10-10 05:24:11 +0900

    :sparkles: Add RestDuration

--- lib/entity/pill_sheet.g.dart
+++ lib/entity/pill_sheet.g.dart
@@ -24,6 +24,23 @@ Map<String, dynamic> _$_$_PillSheetTypeInfoToJson(
       'dosingPeriod': instance.dosingPeriod,
     };
 
+_$_RestDuration _$_$_RestDurationFromJson(Map<String, dynamic> json) {
+  return _$_RestDuration(
+    pillSheetTypeReferencePath: json['pillSheetTypeReferencePath'] as String,
+    name: json['name'] as String,
+    totalCount: json['totalCount'] as int,
+    dosingPeriod: json['dosingPeriod'] as int,
+  );
+}
+
+Map<String, dynamic> _$_$_RestDurationToJson(_$_RestDuration instance) =>
+    <String, dynamic>{
+      'pillSheetTypeReferencePath': instance.pillSheetTypeReferencePath,
+      'name': instance.name,
+      'totalCount': instance.totalCount,
+      'dosingPeriod': instance.dosingPeriod,
+    };
+
 _$_PillSheet _$_$_PillSheetFromJson(Map<String, dynamic> json) {
   return _$_PillSheet(
     id: json['id'] as String?,
@@ -38,6 +55,9 @@ _$_PillSheet _$_$_PillSheetFromJson(Map<String, dynamic> json) {
     deletedAt:
         TimestampConverter.timestampToDateTime(json['deletedAt'] as Timestamp?),
     groupIndex: json['groupIndex'] as int? ?? 0,
+    restDuration: json['restDuration'] == null
+        ? null
+        : RestDuration.fromJson(json['restDuration'] as Map<String, dynamic>),
   );
 }
 
@@ -59,5 +79,6 @@ Map<String, dynamic> _$_$_PillSheetToJson(_$_PillSheet instance) {
   val['createdAt'] = TimestampConverter.dateTimeToTimestamp(instance.createdAt);
   val['deletedAt'] = TimestampConverter.dateTimeToTimestamp(instance.deletedAt);
   val['groupIndex'] = instance.groupIndex;
+  val['restDuration'] = instance.restDuration?.toJson();
   return val;
 }
