e51e4c91957f3f40710a11476cb086cf82dd3421
bannzai <kingkong999yhirose@gmail.com>
2021-10-10 15:18:18 +0900

    :memo: Fill pill sheet modified history kind widgets

---
+++ lib/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_ended_manual_rest_duration.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PillSheetModifiedHistoryEndedManualRestDuration extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
