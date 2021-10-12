d98c08be8fb65ac2dbb4b567bfaa316f4ec47a2f
bannzai <kingkong999yhirose@gmail.com>
2021-10-13 05:28:36 +0900

    :sparkles: add test caes

--- test/util/day_test.dart
+++ test/util/day_test.dart
@@ -3,9 +3,23 @@ import 'package:pilll/util/datetime/day.dart';
 
 void main() {
   group("#daysBetween", () {
-    DateTime date1 = DateTime.parse("2020-01-09 23:00:00.299871");
-    DateTime date2 = DateTime.parse("2020-01-10 00:00:00.299871");
+    test("difference in 1 hour", () {
+      DateTime date1 = DateTime.parse("2020-01-09 23:00:00.299871");
+      DateTime date2 = DateTime.parse("2020-01-10 00:00:00.299871");
 
-    expect(daysBetween(date1, date2), 1);
+      expect(daysBetween(date1, date2), 1);
+    });
+    test("difference in 1 minute", () {
+      DateTime date1 = DateTime.parse("2020-01-09 23:59:59.299871");
+      DateTime date2 = DateTime.parse("2020-01-10 00:00:00.299871");
+
+      expect(daysBetween(date1, date2), 1);
+    });
+    test("it is same day", () {
+      DateTime date1 = DateTime.parse("2020-01-10 00:00:00.299871");
+      DateTime date2 = DateTime.parse("2020-01-10 23:59:59.299871");
+
+      expect(daysBetween(date1, date2), 0);
+    });
   });
 }
