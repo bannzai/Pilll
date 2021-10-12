43514d586ab54ec80f205495ee3e31dd686651c4
bannzai <kingkong999yhirose@gmail.com>
2021-10-13 07:57:56 +0900

    Revert "Revert ":recycle: replace to new difference functions""
    This reverts commit dccd50198110ed38bd1c071ffacb28aaf81ff521.

--- lib/domain/record/components/notification_bar/notification_bar_state.dart
+++ lib/domain/record/components/notification_bar/notification_bar_state.dart
@@ -39,7 +39,7 @@ abstract class NotificationBarState implements _$NotificationBarState {
     if (activedPillSheet.pillSheetType.isNotExistsNotTakenDuration) {
       final restDuration = activedPillSheet.activeRestDuration;
       if (restDuration != null) {
-        final day = today().difference(restDuration.beginDate.date()).inDays;
+        final day = daysBetween(restDuration.beginDate.date(), today());
         return "休薬${activedPillSheet.pillSheetType.notTakenWord}$day日目";
       } else {
         return null;
@@ -78,7 +78,7 @@ abstract class NotificationBarState implements _$NotificationBarState {
       return null;
     }
 
-    final diff = trialDeadlineDate.difference(now()).inDays + 1;
+    final diff = daysBetween(now(), trialDeadlineDate) + 1;
     if (diff < 0) {
       return null;
     }
