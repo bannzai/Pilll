ffa84ccc1e104653c19165a8c8f351958eaa3e1a
bannzai <kingkong999yhirose@gmail.com>
2021-10-16 06:53:52 +0900

    :sparkles: add test case

--- test/domain/service/pill_sheet_modified_service_test.dart
+++ test/domain/service/pill_sheet_modified_service_test.dart
@@ -61,7 +61,7 @@ void main() {
         lastTakenDate: DateTime.parse("2020-09-19"),
       );
       final after = before.copyWith(
-        lastTakenDate: DateTime.parse("2020-09-20"),
+        lastTakenDate: DateTime.parse("2020-09-21"),
       );
 
       final pillSheetGroup = PillSheetGroup(
@@ -83,7 +83,50 @@ void main() {
 
       expect(history.enumActionType, PillSheetModifiedActionType.takenPill);
       expect(history.value.takenPill, isNotNull);
-      expect(history.value.takenPill?.differencePillCount, 1);
+      expect(history.value.takenPill?.differencePillCount, 2);
+      expect(history.value.takenPill?.beforeLastTakenPillNumber, 1);
+      expect(history.value.takenPill?.afterLastTakenPillNumber, 3);
+    });
+    test("group has two pill sheets", () async {
+      final _today = DateTime.parse("2020-09-01");
+
+      final before = PillSheet(
+        id: "sheet_id",
+        typeInfo: PillSheetType.pillsheet_28_0.typeInfo,
+        beginingDate: _today,
+        groupIndex: 0,
+        lastTakenDate: DateTime.parse("2020-09-24"),
+      );
+      final after = PillSheet(
+        id: "sheet_id",
+        typeInfo: PillSheetType.pillsheet_28_0.typeInfo,
+        beginingDate: DateTime.parse("2020-09-29"),
+        groupIndex: 1,
+        lastTakenDate: DateTime.parse("2020-09-30"),
+      );
+
+      final pillSheetGroup = PillSheetGroup(
+        id: "group_id",
+        pillSheetIDs: ["sheet_id"],
+        pillSheets: [
+          after,
+        ],
+        createdAt: now(),
+      );
+
+      final history =
+          PillSheetModifiedHistoryServiceActionFactory.createTakenPillAction(
+        pillSheetGroupID: "group_id",
+        before: before,
+        after: after,
+        pillSheetGroup: pillSheetGroup,
+      );
+
+      expect(history.enumActionType, PillSheetModifiedActionType.takenPill);
+      expect(history.value.takenPill, isNotNull);
+      expect(history.value.takenPill?.differencePillCount, 7);
+      expect(history.value.takenPill?.beforeLastTakenPillNumber, 24);
+      expect(history.value.takenPill?.afterLastTakenPillNumber, 2);
     });
   });
 }
