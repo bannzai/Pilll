680d844058802199e382d576c1db6f718f7dbd82
bannzai <kingkong999yhirose@gmail.com>
2021-10-16 19:58:45 +0900

    :sparkels: call discard dialog

--- lib/domain/record/components/pill_sheet/components/record_page_pill_option.dart
+++ lib/domain/record/components/pill_sheet/components/record_page_pill_option.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pilll/components/atoms/buttons.dart';
 import 'package:pilll/components/organisms/pill_sheet/pill_sheet_view_layout.dart';
+import 'package:pilll/components/page/discard_dialog.dart';
 import 'package:pilll/domain/record/components/pill_sheet/components/record_page_rest_duration_dialog.dart';
 import 'package:pilll/domain/record/record_page_store.dart';
 import 'package:pilll/entity/pill_sheet.dart';
@@ -33,7 +34,22 @@ class RecordPagePillOption extends StatelessWidget {
               text: restDuration == null ? "休薬する" : "休薬終了",
               fontSize: 12,
               onPressed: () async {
-                if (restDuration == null) {
+                if (!activedPillSheet.isAllTaken) {
+                  showDiscardDialog(
+                    context,
+                    title: "未服用のピルがある場合\n休薬できません",
+                    message:
+                        "服用済みにしてから「休薬する」を押してください。前日以前から休薬したい場合は、「今日飲むピル番号」を調整してください。",
+                    actions: [
+                      SecondaryButton(
+                        text: "閉じる",
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                } else if (restDuration == null) {
                   showRecordPageRestDurationDialog(context, () async {
                     Navigator.of(context).pop();
                     await store.beginResting(
