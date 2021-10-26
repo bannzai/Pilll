b5935cc3b3df1c062fd7b459c990523b9a34756b
bannzai <kingkong999yhirose@gmail.com>
2021-10-26 19:57:44 +0900

    :sparkles: add widget of switch appearance

---
+++ lib/domain/record/components/appearance_mode/pill_sheet_appearance_mode_switch.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PillSheetAppearanceModeSwitch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
