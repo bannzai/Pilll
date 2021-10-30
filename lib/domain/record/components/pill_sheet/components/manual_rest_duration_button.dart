582fdc02d6db57f3d68b66dbe340b7cce7f578b7
bannzai <kingkong999yhirose@gmail.com>
2021-10-31 01:45:51 +0900

    :recycle: Separate manual rest duration button

---
+++ lib/domain/record/components/pill_sheet/components/manual_rest_duration_button.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:pilll/components/atoms/buttons.dart';
+import 'package:pilll/components/page/discard_dialog.dart';
+import 'package:pilll/domain/record/components/pill_sheet/components/record_page_rest_duration_dialog.dart';
+import 'package:pilll/domain/record/record_page_store.dart';
+import 'package:pilll/entity/pill_sheet.dart';
+import 'package:pilll/entity/pill_sheet_group.dart';
+
+class ManualRestDurationButton extends StatelessWidget {
+  const ManualRestDurationButton({
+    Key? key,
+    required this.restDuration,
+    required this.activedPillSheet,
+    required this.store,
+    required this.pillSheetGroup,
+  }) : super(key: key);
+
+  final RestDuration? restDuration;
+  final PillSheet activedPillSheet;
+  final RecordPageStore store;
+  final PillSheetGroup pillSheetGroup;
+
+  @override
+  Widget build(BuildContext context) {
+    final restDuration = this.restDuration;
+
+    return SizedBox(
+      width: 80,
+      child: PrimaryOutlinedButton(
+        text: restDuration == null ? "休薬する" : "休薬終了",
+        fontSize: 12,
+        onPressed: () async {
+          if (restDuration == null) {
+            if (activedPillSheet.todayPillNumber - 1 >
+                activedPillSheet.lastTakenPillNumber) {
+              showDiscardDialog(
+                context,
+                title: "未服用のピルがある場合\n休薬できません",
+                message:
+                    "ピル番号をタップして昨日までのピルを服用済みにしてから「休薬する」を押してください。それよりも前から休薬したい場合は、「今日飲むピル番号」を調整してください。",
+                actions: [
+                  SecondaryButton(
+                    text: "閉じる",
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            } else {
+              showRecordPageRestDurationDialog(context, () async {
+                Navigator.of(context).pop();
+                await store.beginResting(
+                  pillSheetGroup: pillSheetGroup,
+                  activedPillSheet: activedPillSheet,
+                );
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    duration: Duration(
+                      seconds: 2,
+                    ),
+                    content: Text("休薬期間が始まりました"),
+                  ),
+                );
+              });
+            }
+          } else {
+            await store.endResting(
+              pillSheetGroup: pillSheetGroup,
+              activedPillSheet: activedPillSheet,
+              restDuration: restDuration,
+            );
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                duration: Duration(
+                  seconds: 2,
+                ),
+                content: Text("休薬期間が終了しました"),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
