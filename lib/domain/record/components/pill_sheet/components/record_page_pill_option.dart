6fbdd05cc448aa685b5c9abca40766eac4595a2f
bannzai <kingkong999yhirose@gmail.com>
2021-11-07 09:08:03 +0900

    :art: put appearance UI

--- lib/domain/record/components/pill_sheet/components/record_page_pill_option.dart
+++ lib/domain/record/components/pill_sheet/components/record_page_pill_option.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/components/organisms/pill_sheet/pill_sheet_view_layout.dart';
 import 'package:pilll/domain/record/components/rest_duration/manual_rest_duration_button.dart';
 import 'package:pilll/domain/record/record_page_store.dart';
@@ -24,6 +27,18 @@ class RecordPagePillOption extends StatelessWidget {
     return Container(
       width: PillSheetViewLayout.width,
       child: Row(children: [
+        Row(children: [
+          SvgPicture.asset("images/switching_appearance_mode.svg"),
+          Text(
+            "表示モード",
+            style: TextStyle(
+              color: TextColor.main,
+              fontSize: 12,
+              fontFamily: FontFamily.japanese,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ]),
         Spacer(),
         if (!pillSheetGroup.hasPillSheetRestDuration)
           ManualRestDurationButton(
