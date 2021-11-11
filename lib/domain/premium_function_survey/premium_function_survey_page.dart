f233a4529b14c81ce2bfc41b91b51bf83a259405
bannzai <kingkong999yhirose@gmail.com>
2021-11-11 12:33:57 +0900

    :construction: add debug code

--- lib/domain/premium_function_survey/premium_function_survey_page.dart
+++ lib/domain/premium_function_survey/premium_function_survey_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/util/environment.dart';
 import 'package:pilll/util/shared_preference/keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -29,6 +30,9 @@ class PremiumFunctionSurveyPage extends StatelessWidget {
 
 showPremiumFunctionSurveyPage(BuildContext context) async {
   final sharedPreferences = await SharedPreferences.getInstance();
+  if (Environment.isDevelopment) {
+    sharedPreferences..setBool(BoolKey.isAlreadyShowPremiumSurvey, false);
+  }
   if (sharedPreferences.getBool(BoolKey.isAlreadyShowPremiumSurvey) ?? false) {
     return;
   }
