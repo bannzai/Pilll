76c563b66cca3a6a015b412bb334fd5fcf5db734
bannzai <kingkong999yhirose@gmail.com>
2021-11-11 19:45:28 +0900

    :recycle: Remove unnecessary import

--- lib/domain/premium_function_survey/premium_function_survey_state.dart
+++ lib/domain/premium_function_survey/premium_function_survey_state.dart
@@ -1,5 +1,4 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:pilll/domain/premium_function_survey/premium_function_survey_element.dart';
 import 'package:pilll/domain/premium_function_survey/premium_function_survey_element_type.dart';
 
 part 'premium_function_survey_state.freezed.dart';
