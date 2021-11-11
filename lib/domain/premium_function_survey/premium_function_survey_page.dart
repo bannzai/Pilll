936be13620da21ffbf8b8db631595a40e43bb387
bannzai <kingkong999yhirose@gmail.com>
2021-11-11 12:55:53 +0900

    :art: WIP layout

--- lib/domain/premium_function_survey/premium_function_survey_page.dart
+++ lib/domain/premium_function_survey/premium_function_survey_page.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:pilll/components/atoms/buttons.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
 
 class PremiumFunctionSurveyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: PilllColors.background,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.close, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        elevation: 0,
+        backgroundColor: PilllColors.background,
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-            child: Column(
-              children: [
-                Text(
-                  "プレミアム体験が終了しました",
-                  style: TextStyle(
+            child: Center(
+              child: Column(
+                children: [
+                  Text(
+                    "プレミアム体験が終了しました",
+                    style: TextStyle(
+                      color: TextColor.main,
                       fontFamily: FontFamily.japanese,
                       fontWeight: FontWeight.w500,
-                      fontSize: 20),
-                ),
-              ],
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "もしよければ、便利だと感じた機能を教えて下さい！ぜひ、あなたのご意見をお聞かせください。",
+                    style: TextStyle(
+                      color: TextColor.main,
+                      fontFamily: FontFamily.japanese,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                  AppBarTextActionButton(
+                      onPressed: () {}, text: "プレミアム機能の詳細はこちら"),
+                ],
+              ),
             ),
           ),
         ),
