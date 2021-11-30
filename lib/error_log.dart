c42e85c37c26dfa0497ae5b48d8f47323860135c
bannzai <kingkong999yhirose@gmail.com>
2021-11-30 13:55:20 +0900

    :sparkles: implement ErrorLogger proxy

--- lib/error_log.dart
+++ lib/error_log.dart
@@ -1,3 +1,14 @@
+import 'dart:async';
+
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-final errorLogger = FirebaseCrashlytics.instance;
+final errorLogger = ErrorLogger();
+
+class ErrorLogger {
+  recordError(
+    dynamic exception,
+    StackTrace? stack,
+  ) {
+    unawaited(FirebaseCrashlytics.instance.recordError(exception, stack));
+  }
+}
