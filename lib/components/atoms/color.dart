3074ddabbbdead3bd23d3032c7d6d762222a6e13
bannzai <kingkong999yhirose@gmail.com>
2021-11-30 14:54:05 +0900

    :recycle: WIP migrate color

--- lib/components/atoms/color.dart
+++ lib/components/atoms/color.dart
@@ -20,46 +20,46 @@ extension HexColor on Color {
 class PilllColors {
   const PilllColors();
 
-  static final Color primary = HexColor.fromHex("E56A45");
-  static final Color secondary = HexColor.fromHex("4E6287");
-  static final Color calendarHeader = HexColor.fromHex("B9D7F1");
-  static final Color accent = Colors.white;
-  static final Color pillSheet = HexColor.fromHex("E8EBED");
-  static final Color mat = HexColor.fromHex("FAFAFA").withAlpha(80);
-  static final Color sunday = HexColor.fromHex("E17F7F");
-  static final Color saturday = HexColor.fromHex("7FB9E1");
-  static final Color weekday = HexColor.fromHex("7E7E7E");
-  static final Color enable = HexColor.fromHex('E56A45');
-  static final Color disable = HexColor.fromHex("EAEAEA");
-  static final Color divider = HexColor.fromHex("9DAFBD");
+  static const Color primary = Color(0xFFE56A45);
+  static const Color secondary = Color(0xFF4E6287);
+  static const Color calendarHeader = Color(0XFFB9D7F1);
+  static const Color accent = Colors.white;
+  static const Color pillSheet = Color(0xFFE8EBED);
+  static final Color mat = Color(0xFFFAFAFA).withAlpha(80);
+  static const Color sunday = Color(0xFFE17F7F);
+  static const Color saturday = HexColor.fromHex("7FB9E1");
+  static const Color weekday = HexColor.fromHex("7E7E7E");
+  static const Color enable = HexColor.fromHex('E56A45');
+  static const Color disable = HexColor.fromHex("EAEAEA");
+  static const Color divider = HexColor.fromHex("9DAFBD");
 
-  static final Color bottomBar = HexColor.fromHex("FAFAFA");
-  static final Color border = HexColor.fromHex("DADADA");
-  static final Color unselect = HexColor.fromHex("DFDFDF");
-  static final Color background = HexColor.fromHex("F6F9FB");
-  static final Color blueBackground = HexColor.fromHex("F1F2F5");
+  static const Color bottomBar = HexColor.fromHex("FAFAFA");
+  static const Color border = HexColor.fromHex("DADADA");
+  static const Color unselect = HexColor.fromHex("DFDFDF");
+  static const Color background = HexColor.fromHex("F6F9FB");
+  static const Color blueBackground = HexColor.fromHex("F1F2F5");
 
-  static final Color blank = Colors.white;
-  static final Color black = HexColor.fromHex("333333");
-  static final Color red = HexColor.fromHex("EB5757");
-  static final Color potti = HexColor.fromHex("7C8E9C");
-  static final Color lightGray = HexColor.fromHex("CDCFD1");
-  static final Color gray = HexColor.fromHex("BEC0C2");
-  static final Color gold = HexColor.fromHex("B29446");
+  static const Color blank = Colors.white;
+  static const Color black = HexColor.fromHex("333333");
+  static const Color red = HexColor.fromHex("EB5757");
+  static const Color potti = HexColor.fromHex("7C8E9C");
+  static const Color lightGray = HexColor.fromHex("CDCFD1");
+  static const Color gray = HexColor.fromHex("BEC0C2");
+  static const Color gold = HexColor.fromHex("B29446");
 
-  static final Color menstruation = HexColor.fromHex("E3BFC2");
-  static final Color duration = HexColor.fromHex("6A7DA5");
-  static final Color overlay = primary.withAlpha(20);
+  static const Color menstruation = HexColor.fromHex("E3BFC2");
+  static const Color duration = HexColor.fromHex("6A7DA5");
+  static const Color overlay = primary.withAlpha(20);
 
-  static final Color modalBackground =
+  static const Color modalBackground =
       HexColor.fromHex("333333").withAlpha((255 * 0.7).round());
-  static final Color white = Colors.white;
+  static const Color white = Colors.white;
 
   static Color get disabledSheet => PilllColors.pillSheet;
-  static final Color thinSecondary = HexColor.fromHex("4E6287").withAlpha(20);
-  static final Color shadow = HexColor.fromHex("212121").withOpacity(0.14);
-  static final Color tinBackground =
+  static const Color thinSecondary = HexColor.fromHex("4E6287").withAlpha(20);
+  static const Color shadow = HexColor.fromHex("212121").withOpacity(0.14);
+  static const Color tinBackground =
       HexColor.fromHex("212121").withOpacity(0.08);
 
-  static final Color appleBlack = HexColor.fromHex("231815");
+  static const Color appleBlack = HexColor.fromHex("231815");
 }
