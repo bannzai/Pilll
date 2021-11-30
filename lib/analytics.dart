f5c3ce99e2ba1ce1f2863560ac603c2b311d0ff6
bannzai <kingkong999yhirose@gmail.com>
2021-11-30 14:01:27 +0900

    :recycle: remove future keyword

--- lib/analytics.dart
+++ lib/analytics.dart
@@ -5,15 +5,14 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 final firebaseAnalytics = FirebaseAnalytics();
 
 class Analytics {
-  Future<void> logEvent(
-      {required String name, Map<String, dynamic>? parameters}) async {
+  logEvent({required String name, Map<String, dynamic>? parameters}) async {
     assert(name.length <= 40,
         "firebase analytics log event name limit length up to 40");
     print("[INFO] logEvent name: $name, parameters: $parameters");
     return firebaseAnalytics.logEvent(name: name, parameters: parameters);
   }
 
-  Future<void> setCurrentScreen(
+  setCurrentScreen(
       {required String screenName,
       String screenClassOverride = 'Flutter'}) async {
     unawaited(firebaseAnalytics.logEvent(name: "screen_$screenName"));
