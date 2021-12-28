5e3cd09d28ce2c0a57a84529de1341f3a2e00e8e
bannzai <kingkong999yhirose@gmail.com>
2021-12-28 22:07:11 +0900

    :recycle: remove unnecessary function

--- lib/auth/hash.dart
+++ lib/auth/hash.dart
@@ -1,15 +1,6 @@
 import 'dart:convert';
-import 'dart:math';
 
 import 'package:crypto/crypto.dart';
-
-String generateNonce([int length = 32]) {
-  final charset =
-      '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
-  final random = Random.secure();
-  return List.generate(length, (_) => charset[random.nextInt(charset.length)])
-      .join();
-}
 
 String sha256ofString(String input) {
   final bytes = utf8.encode(input);
