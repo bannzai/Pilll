86bdc90f010345813566e78c01b5313e1974eea8
bannzai <kingkong999yhirose@gmail.com>
2021-12-29 19:28:34 +0900

    :memo: fix calendar page state managements

--- lib/domain/calendar/calendar_page.dart
+++ lib/domain/calendar/calendar_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pilll/analytics.dart';
@@ -11,14 +12,13 @@ import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/pil
 import 'package:pilll/domain/home/home_page.dart';
 import 'package:pilll/domain/calendar/calendar_page_store.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/error/universal_error_page.dart';
 
 class CalendarPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final store = ref.watch(calendarPageStateStoreProvider);
-    final state = ref.watch(calendarPageStateStoreProvider.notifier);
+    final store = ref.watch(calendarPageStateStoreProvider.notifier);
+    final state = ref.watch(calendarPageStateStoreProvider);
     homeKey.currentState?.diaries = state.diariesForMonth;
 
     final exception = state.exception;
