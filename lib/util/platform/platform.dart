e0d539368c97772c3c96178923538fb238c9d920
bannzai <kingkong999yhirose@gmail.com>
2021-12-30 17:16:12 +0900

    :sparkles: add store link URL

--- lib/util/platform/platform.dart
+++ lib/util/platform/platform.dart
@@ -7,3 +7,9 @@ String get storeName {
 String get accountName {
   return Platform.isIOS ? "Apple ID" : "Google アカウント";
 }
+
+String get storeLink {
+  return Platform.isIOS
+      ? "https://apps.apple.com/jp/app/pilll-%E3%83%94%E3%83%AB%E3%83%AA%E3%83%9E%E3%82%A4%E3%83%B3%E3%83%80%E3%83%BC%E3%81%B4%E3%82%8B%E3%82%8B/id1405931017"
+      : "https://play.google.com/store/apps/details?id=com.mizuki.Ohashi.Pilll";
+}
