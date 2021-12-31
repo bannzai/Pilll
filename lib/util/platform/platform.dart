0f9dae7bd78fa35f537a025eab313d4dd9cb8e86
bannzai <kingkong999yhirose@gmail.com>
2021-12-31 23:07:16 +0900

    :memo: fix url

--- lib/util/platform/platform.dart
+++ lib/util/platform/platform.dart
@@ -10,6 +10,6 @@ String get accountName {
 
 String get storeURL {
   return Platform.isIOS
-      ? "https://apps.apple.com/jp/app/pilll-%E3%83%94%E3%83%AB%E3%83%AA%E3%83%9E%E3%82%A4%E3%83%B3%E3%83%80%E3%83%BC%E3%81%B4%E3%82%8B%E3%82%8B/id1405931017"
+      ? "https://apps.apple.com/jp/app/id1405931017"
       : "https://play.google.com/store/apps/details?id=com.mizuki.Ohashi.Pilll";
 }
