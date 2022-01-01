425450376cb73af6ebc032e5b9b25653abe32ba3
bannzai <kingkong999yhirose@gmail.com>
2022-01-01 16:42:45 +0900

    :recycle: remove unnecessary import

--- lib/domain/settings/setting_account_list/setting_account_cooperation_list_page.dart
+++ lib/domain/settings/setting_account_list/setting_account_cooperation_list_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/auth/apple.dart';
 import 'package:pilll/auth/google.dart';
