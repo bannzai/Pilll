8c625f467b6ae6b48ff18bf4e6fdd13d225f21e1
bannzai <kingkong999yhirose@gmail.com>
2022-01-05 15:14:35 +0900

    :recycle: Remove unnnecessary import

--- lib/domain/settings/components/rows/notification_customize_word.dart
+++ lib/domain/settings/components/rows/notification_customize_word.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/analytics.dart';
-import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/molecules/premium_badge.dart';
 import 'package:pilll/domain/premium_introduction/premium_introduction_sheet.dart';
 import 'package:pilll/domain/premium_trial/premium_trial_complete_modal.dart';
 import 'package:pilll/domain/premium_trial/premium_trial_modal.dart';
-import 'package:pilll/domain/settings/setting_page_store.dart';
 import 'package:pilll/entity/setting.dart';
 
 class NotificationCustomizeWord extends HookConsumerWidget {
