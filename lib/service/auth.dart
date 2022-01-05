c621ee2aa54b44975754c2068a8114c0590e3ee5
bannzai <kingkong999yhirose@gmail.com>
2022-01-05 14:35:35 +0900

    :recycle: remove unnecessary import

--- lib/service/auth.dart
+++ lib/service/auth.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/auth/apple.dart';
 import 'package:pilll/auth/google.dart';
