5eddeef6d2dce575d13cb9d01d0a70674e5eb2a4
bannzai <kingkong999yhirose@gmail.com>
2022-01-06 16:32:19 +0900

    :memo: fix release note

--- lib/domain/modal/release_note.dart
+++ lib/domain/modal/release_note.dart
@@ -42,7 +42,7 @@ class ReleaseNote extends StatelessWidget {
                       child: Container(
                         padding: EdgeInsets.only(top: 40, left: 40, right: 40),
                         child: Text(
-                          "好きなタイミングで\n休薬できるようになりました",
+                          "服用通知を\nカスタマイズできるように",
                           style: FontType.subTitle.merge(TextColorStyle.black),
                           textAlign: TextAlign.center,
                         ),
@@ -57,7 +57,7 @@ class ReleaseNote extends StatelessWidget {
                     children: [
                       Text(
                         '''
-ヤーズフレックスで連続服用している方、生理移動をしたい時等にお使いください。
+Pilllからの通知と悟られたくない方にオススメ！※こちらの機能はプレミアムメンバー限定になります
                         ''',
                         style: FontType.assisting.merge(TextColorStyle.main),
                       ),
@@ -72,7 +72,7 @@ class ReleaseNote extends StatelessWidget {
                         Navigator.of(context).pop();
                         await openReleaseNote();
                       },
-                      text: "詳細を見る"),
+                      text: "設定を見る"),
                 ),
                 SizedBox(height: 20),
               ],
@@ -103,7 +103,7 @@ openReleaseNote() async {
   final ChromeSafariBrowser browser = new ChromeSafariBrowser();
   await browser.open(
       url: Uri.parse(
-          "https://pilll.wraptas.site/cfb7c1d2ef874316a8b385525e8f2ee1"),
+          "https://pilll.wraptas.site/721b7ae08aa2422e901cae27ffbbbf04"),
       options: ChromeSafariBrowserClassOptions(
           android:
               AndroidChromeCustomTabsOptions(addDefaultShareMenuItem: false),
