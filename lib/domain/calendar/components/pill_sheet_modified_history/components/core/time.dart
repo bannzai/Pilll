8b7b4be2965a210548c3bb81ac69573ce051d247
bannzai <kingkong999yhirose@gmail.com>
2022-01-11 23:20:35 +0900

    :memo: implement time

---
+++ lib/domain/calendar/components/pill_sheet_modified_history/components/core/time.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+
+class Time extends StatelessWidget {
+  const Time({
+    Key? key,
+    required this.time,
+  }) : super(key: key);
+
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      time,
+      style: TextStyle(
+        decoration: TextDecoration.underline,
+        letterSpacing: 1.5,
+        color: TextColor.main,
+        fontSize: 15,
+        fontFamily: FontFamily.number,
+        fontWeight: FontWeight.w400,
+      ),
+      textAlign: TextAlign.start,
+    );
+  }
+}
