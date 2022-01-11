ef4172d792db3224f234675d6c5830c4be8cb95c
bannzai <kingkong999yhirose@gmail.com>
2022-01-12 08:07:34 +0900

    :art: fix header layout

--- lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list_header.dart
+++ lib/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list_header.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
-import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_date_component.dart';
-
-typedef HeaderLayout = RowLayout;
 
 class PillSheetModifiedHisotiryListHeader extends StatelessWidget {
   const PillSheetModifiedHisotiryListHeader({
@@ -12,51 +9,77 @@ class PillSheetModifiedHisotiryListHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _HeaderLayout(
+      day: Container(width: 64),
+      effectiveNumbersOrHyphen: Container(),
+      detail: Text(
+        "服用時間",
+        style: TextStyle(
+          color: TextColor.main,
+          fontFamily: FontFamily.japanese,
+          fontSize: 12,
+          fontWeight: FontWeight.w400,
+        ),
+        textAlign: TextAlign.left,
+      ),
+      takenPillActionOList: Text(
+        "服用済み",
+        style: TextStyle(
+          color: TextColor.main,
+          fontFamily: FontFamily.japanese,
+          fontSize: 12,
+          fontWeight: FontWeight.w400,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+
+// Copied from RowLayout
+class _HeaderLayout extends StatelessWidget {
+  final Widget day;
+  final Widget effectiveNumbersOrHyphen;
+  final Widget detail;
+  final Widget takenPillActionOList;
+
+  const _HeaderLayout({
+    Key? key,
+    required this.day,
+    required this.effectiveNumbersOrHyphen,
+    required this.detail,
+    required this.takenPillActionOList,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final takenPillActionOList = this.takenPillActionOList;
     return Container(
-      padding: const EdgeInsets.only(right: 12),
+      padding: EdgeInsets.only(left: 4),
       child: Row(
         children: [
-          SizedBox(
-            width: PillSheetModifiedHistoryTakenActionLayoutWidths.leading,
-          ),
-          Spacer(),
+          day,
+          SizedBox(width: 8),
           Container(
-            width: PillSheetModifiedHistoryTakenActionLayoutWidths.trailing,
-            child: Row(
-              children: [
-                Container(
-                  width: 55,
-                  child: Text(
-                    "服用時間",
-                    style: TextStyle(
-                      color: TextColor.main,
-                      fontFamily: FontFamily.japanese,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Spacer(),
-                Container(
-                  constraints: BoxConstraints(
-                    maxWidth: PillSheetModifiedHistoryTakenActionLayoutWidths
-                        .takenMark,
-                  ),
-                  child: Text(
-                    "服用済み",
-                    style: TextStyle(
-                      color: TextColor.main,
-                      fontFamily: FontFamily.japanese,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ],
-            ),
+            height: 26,
+            width: 0.5,
           ),
+          SizedBox(width: 8),
+          Container(
+            width: 79,
+            child: effectiveNumbersOrHyphen,
+          ),
+          SizedBox(width: 8),
+          Expanded(
+            child: detail,
+          ),
+          if (takenPillActionOList != null) ...[
+            SizedBox(width: 8),
+            Container(
+              width: 57,
+              child: takenPillActionOList,
+            ),
+          ],
         ],
       ),
     );
