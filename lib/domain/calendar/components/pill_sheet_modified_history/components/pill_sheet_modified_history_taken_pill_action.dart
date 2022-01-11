8eaa71cd2b92251dcdfc6210d0902dd4173ff37e
bannzai <kingkong999yhirose@gmail.com>
2022-01-12 07:14:12 +0900

    :recycle: Move to core

--- lib/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_taken_pill_action.dart
+++ lib/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_taken_pill_action.dart
@@ -5,7 +5,7 @@ import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/com
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/core/row_layout.dart';
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/core/time.dart';
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/pill_sheet_modified_history_date_component.dart';
-import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/taken_pill_action_o_list.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/components/core/taken_pill_action_o_list.dart';
 import 'package:pilll/entity/pill_sheet.dart';
 import 'package:pilll/entity/pill_sheet_modified_history_value.dart';
 import 'package:pilll/error/error_alert.dart';
