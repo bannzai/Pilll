4365cda7444908defae6a0f6e7f7c480a9c7d072
bannzai <kingkong999yhirose@gmail.com>
2022-01-18 18:12:17 +0900

    :memo: fix shows pill number

--- lib/domain/record/components/pill_sheet/components/record_page_rest_duration_dialog.dart
+++ lib/domain/record/components/pill_sheet/components/record_page_rest_duration_dialog.dart
@@ -107,11 +107,11 @@ class RecordPageRestDurationDialogTitle extends StatelessWidget {
   String get _number {
     switch (appearanceMode) {
       case PillSheetAppearanceMode.number:
-        return "${activedPillSheet.lastTakenPillNumber}番";
+        return "${activedPillSheet.lastTakenPillNumber - 1}番";
       case PillSheetAppearanceMode.date:
-        return "${activedPillSheet.lastTakenPillNumber}日";
+        return "${activedPillSheet.lastTakenPillNumber - 1}日";
       case PillSheetAppearanceMode.sequential:
-        return "${activedPillSheet.lastTakenPillNumber}番";
+        return "${activedPillSheet.lastTakenPillNumber - 1}番";
     }
   }
 }
