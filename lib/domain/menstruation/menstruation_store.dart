1427acaea6ad9d4891e06e2eb16348f6eceeea5d
bannzai <kingkong999yhirose@gmail.com>
2022-02-05 06:38:03 +0900

    Revert ":construction: test code"
    This reverts commit 0d8dfa8795fa47ccf2efecccc5221cac754986f4.

--- lib/domain/menstruation/menstruation_store.dart
+++ lib/domain/menstruation/menstruation_store.dart
@@ -5,7 +5,6 @@ import 'package:pilll/components/organisms/calendar/utility.dart';
 import 'package:pilll/domain/menstruation/menstruation_card_state.dart';
 import 'package:pilll/domain/menstruation/menstruation_history_card_state.dart';
 import 'package:pilll/entity/menstruation.dart';
-import 'package:pilll/global_method_channel.dart';
 import 'package:pilll/service/diary.dart';
 import 'package:pilll/service/menstruation.dart';
 import 'package:pilll/service/pill_sheet.dart';
@@ -130,10 +129,7 @@ class MenstruationStore extends StateNotifier<MenstruationState> {
         beginDate: begin,
         endDate: begin.add(Duration(days: duration - 1)),
         createdAt: now());
-
-    writeMenstrualFlowHealthKitData(menstruation);
-
-    return Future.value(menstruation);
+    return menstruationService.create(menstruation);
   }
 
   Future<Menstruation> recordFromYesterday() {
