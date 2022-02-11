3af694e4e914e0e0370b701be22f4ae0f427820a
bannzai <kingkong999yhirose@gmail.com>
2022-02-12 06:03:12 +0900

    Add test case of is less than

--- test/util/version_test.dart
+++ test/util/version_test.dart
@@ -10,4 +10,25 @@ void main() {
       expect(Version.parse("1.0.0").version, "1.0.0");
     });
   });
+
+  group("#Version.isLessThan", () {
+    test("lhs: 0.0.0, rhs: 1.0.0", () {
+      expect(Version.parse("0.0.0").isLessThan(Version.parse("1.0.0")), true);
+    });
+    test("lhs: 0.0.1, rhs: 1.0.0", () {
+      expect(Version.parse("0.0.1").isLessThan(Version.parse("1.0.0")), true);
+    });
+    test("lhs: 0.1.0, rhs: 1.0.0", () {
+      expect(Version.parse("0.1.0").isLessThan(Version.parse("1.0.0")), true);
+    });
+    test("lhs: 1.0.0, rhs: 1.0.0", () {
+      expect(Version.parse("1.0.0").isLessThan(Version.parse("1.0.0")), false);
+    });
+    test("lhs: 1.0.1, rhs: 1.0.0", () {
+      expect(Version.parse("1.0.1").isLessThan(Version.parse("1.0.0")), false);
+    });
+    test("lhs: 1.1.0, rhs: 1.0.0", () {
+      expect(Version.parse("1.1.0").isLessThan(Version.parse("1.0.0")), false);
+    });
+  });
 }
