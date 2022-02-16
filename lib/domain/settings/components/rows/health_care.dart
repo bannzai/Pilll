a8621952d349f88224bb1f5f06f9ebfd00dd6474
bannzai <kingkong999yhirose@gmail.com>
2022-02-16 09:56:36 +0900

    :memo: add premium condition to health care

--- lib/domain/settings/components/rows/health_care.dart
+++ lib/domain/settings/components/rows/health_care.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/domain/premium_introduction/premium_introduction_sheet.dart';
+import 'package:pilll/domain/premium_trial/premium_trial_complete_modal.dart';
+import 'package:pilll/domain/premium_trial/premium_trial_modal.dart';
 import 'package:pilll/entity/user_error.dart';
 import 'package:pilll/error/error_alert.dart';
 import 'package:pilll/error/universal_error_page.dart';
@@ -8,6 +11,17 @@ import 'package:pilll/native/health_care.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HealthCareRow extends StatelessWidget {
+  final bool isTrial;
+  final bool isPremium;
+  final DateTime? trialDeadlineDate;
+
+  const HealthCareRow({
+    Key? key,
+    required this.isTrial,
+    required this.isPremium,
+    required this.trialDeadlineDate,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -23,20 +37,30 @@ class HealthCareRow extends StatelessWidget {
           name: "did_select_health_care_row",
         );
 
-        try {
-          if (await isAuthorizedReadAndShareToHealthKitData()) {
-            launch(
-                "https://pilll.wraptas.site/c26580878bb74fdba86f1b71e93c7c02");
-          } else {
-            if (await shouldRequestForAccessToHealthKitData()) {
-              await requestWriteMenstrualFlowHealthKitDataPermission();
+        if (isTrial || isPremium) {
+          try {
+            if (await isAuthorizedReadAndShareToHealthKitData()) {
+              launch(
+                  "https://pilll.wraptas.site/c26580878bb74fdba86f1b71e93c7c02");
+            } else {
+              if (await shouldRequestForAccessToHealthKitData()) {
+                await requestWriteMenstrualFlowHealthKitDataPermission();
+              }
+            }
+          } catch (error) {
+            if (error is UserDisplayedError) {
+              showErrorAlertWithError(context, error);
+            } else {
+              UniversalErrorPage.of(context).showError(error);
             }
           }
-        } catch (error) {
-          if (error is UserDisplayedError) {
-            showErrorAlertWithError(context, error);
+        } else {
+          if (trialDeadlineDate == null) {
+            showPremiumTrialModal(context, () {
+              showPremiumTrialCompleteModalPreDialog(context);
+            });
           } else {
-            UniversalErrorPage.of(context).showError(error);
+            showPremiumIntroductionSheet(context);
           }
         }
       },
