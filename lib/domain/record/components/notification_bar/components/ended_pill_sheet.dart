81821c82e1427e5dafc6fb33e75eb4e41400620f
bannzai <kingkong999yhirose@gmail.com>
2022-02-27 07:10:55 +0900

    add link

--- lib/domain/record/components/notification_bar/components/ended_pill_sheet.dart
+++ lib/domain/record/components/notification_bar/components/ended_pill_sheet.dart
@@ -3,13 +3,40 @@ import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
 
 class EndedPillSheet extends StatelessWidget {
+  final bool isTrial;
+  final bool isPremium;
+
+  const EndedPillSheet({
+    Key? key,
+    required this.isTrial,
+    required this.isPremium,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(top: 8, bottom: 8),
       child: Center(
-        child: Text("ピルシートが終了しました",
-            style: FontType.assistingBold.merge(TextColorStyle.white)),
+        child: Column(
+          children: [
+            Text(
+              "ピルシートが終了しました",
+              style: FontType.assistingBold.merge(
+                TextColorStyle.white,
+              ),
+            ),
+            Text(
+              "前回の服用履歴を確認する",
+              style: TextStyle(
+                color: TextColor.white,
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                fontFamily: FontFamily.japanese,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
