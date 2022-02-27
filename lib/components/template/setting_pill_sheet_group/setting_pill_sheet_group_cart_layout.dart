326e261ab215ffa1b623210948167696581e486b
bannzai <kingkong999yhirose@gmail.com>
2022-02-28 06:36:06 +0900

    WIP add card layout

---
+++ lib/components/template/setting_pill_sheet_group/setting_pill_sheet_group_cart_layout.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:pilll/components/atoms/color.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+
+class SettingPillSheetGroupCardLayout extends StatelessWidget {
+  final String caption;
+  final Widget image;
+  final String name;
+  final String supplement;
+
+  const SettingPillSheetGroupCardLayout({
+    Key? key,
+    required this.caption,
+    required this.image,
+    required this.name,
+    required this.supplement,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 8, left: 12, right: 12, bottom: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: PilllColors.mat,
+            ),
+            child: Text(
+              caption,
+              style: TextStyle(
+                color: TextColor.main,
+                fontSize: 10,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
