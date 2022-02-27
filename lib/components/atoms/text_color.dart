a55f6392b37c053fcecd3d6317e5c2ef25284e23
bannzai <kingkong999yhirose@gmail.com>
2022-02-28 06:36:38 +0900

    :recycle: to cosnt

--- lib/components/atoms/text_color.dart
+++ lib/components/atoms/text_color.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 
 class TextColor {
   static Color get standard => black;
-  static final Color black = Colors.black;
-  static final Color white = Colors.white;
-  static final Color darkGray = const Color(0xFF000000).withAlpha(153);
-  static final Color gray = const Color(0xFF7E7E7E);
-  static final Color lightGray = const Color(0xFFB1B1B1);
-  static final Color lightGray2 = const Color(0xFF666666);
-  static final Color noshime = const Color(0xFF3D4662);
-  static final Color primary = PilllColors.primary;
-  static final Color main = const Color(0xFF29304D);
-  static final Color link = primary;
-  static final Color danger = PilllColors.red;
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+  static const Color darkGray = const Color(0xFF000000).withAlpha(153);
+  static const Color gray = const Color(0xFF7E7E7E);
+  static const Color lightGray = const Color(0xFFB1B1B1);
+  static const Color lightGray2 = const Color(0xFF666666);
+  static const Color noshime = const Color(0xFF3D4662);
+  static const Color primary = PilllColors.primary;
+  static const Color main = const Color(0xFF29304D);
+  static const Color link = primary;
+  static const Color danger = PilllColors.red;
 
   static Color highEmphasis(Color color) => color.withOpacity(0.87);
   static Color mediumEmphasis(Color color) => color.withOpacity(0.6);
@@ -27,15 +27,15 @@ class TextColor {
 
 class TextColorStyle {
   static TextStyle get standard => black;
-  static final TextStyle black = TextStyle(color: TextColor.black);
-  static final TextStyle white = TextStyle(color: TextColor.white);
-  static final TextStyle darkGray = TextStyle(color: TextColor.darkGray);
-  static final TextStyle gray = TextStyle(color: TextColor.gray);
-  static final TextStyle lightGray = TextStyle(color: TextColor.lightGray);
-  static final TextStyle lightGray2 = TextStyle(color: TextColor.lightGray2);
-  static final TextStyle noshime = TextStyle(color: TextColor.noshime);
-  static final TextStyle primary = TextStyle(color: TextColor.primary);
-  static final TextStyle main = TextStyle(color: TextColor.main);
-  static final TextStyle link = TextStyle(color: TextColor.link);
-  static final TextStyle danger = TextStyle(color: TextColor.danger);
+  static const TextStyle black = TextStyle(color: TextColor.black);
+  static const TextStyle white = TextStyle(color: TextColor.white);
+  static const TextStyle darkGray = TextStyle(color: TextColor.darkGray);
+  static const TextStyle gray = TextStyle(color: TextColor.gray);
+  static const TextStyle lightGray = TextStyle(color: TextColor.lightGray);
+  static const TextStyle lightGray2 = TextStyle(color: TextColor.lightGray2);
+  static const TextStyle noshime = TextStyle(color: TextColor.noshime);
+  static const TextStyle primary = TextStyle(color: TextColor.primary);
+  static const TextStyle main = TextStyle(color: TextColor.main);
+  static const TextStyle link = TextStyle(color: TextColor.link);
+  static const TextStyle danger = TextStyle(color: TextColor.danger);
 }
