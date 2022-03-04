080ab3b611742ca709dc7d3c17b44b8fb7d80767
bannzai <kingkong999yhirose@gmail.com>
2022-03-05 06:12:15 +0900

    Remove unnecessary subscribe function

--- lib/domain/root/root.dart
+++ lib/domain/root/root.dart
@@ -19,7 +19,6 @@ import 'package:pilll/service/user.dart';
 import 'package:pilll/util/platform/platform.dart';
 import 'package:pilll/util/shared_preference/keys.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:pilll/util/version/version.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -79,35 +78,16 @@ class RootState extends State<Root> {
     return UniversalErrorPage(
       error: _error,
       reload: () => reload(),
-      child: Consumer(builder: (context, ref, child) {
-        return ref.watch(authStateStreamProvider).when(data: (snapshot) {
-          switch (screenType) {
-            case ScreenType.home:
-              return HomePage(key: homeKey);
-            case ScreenType.initialSetting:
-              return InitialSettingPillSheetCountPageRoute.screen();
-            default:
-              return ScaffoldIndicator();
-          }
-        }, loading: () {
-          return ScaffoldIndicator();
-        }, error: (error, stacktrace) {
-          print(error);
-          print(stacktrace);
-          errorLogger.recordError(error, stacktrace);
-          setState(() {
-            final displayError = ErrorMessages.connection +
-                "\n" +
-                "errorType: ${error.runtimeType.toString()}\n" +
-                error.toString() +
-                "error: ${error.toString()}\n" +
-                stacktrace.toString();
-            _error = displayError;
-          });
-
-          return const Indicator();
-        });
-      }),
+      child: () {
+        switch (screenType) {
+          case ScreenType.home:
+            return HomePage(key: homeKey);
+          case ScreenType.initialSetting:
+            return InitialSettingPillSheetCountPageRoute.screen();
+          default:
+            return ScaffoldIndicator();
+        }
+      }(),
     );
   }
 
