8a2ac713fb72b3a39d37c0c33db1b5ae08d65dd0
bannzai <kingkong999yhirose@gmail.com>
2022-03-07 22:24:16 +0900

    Remove unnecessary code

--- lib/domain/initial_setting/pill_type/initial_setting_pill_type_page.dart
+++ lib/domain/initial_setting/pill_type/initial_setting_pill_type_page.dart
@@ -1,15 +1,9 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/page/hud.dart';
-import 'package:pilll/components/template/setting_pill_sheet_group/pill_sheet_group_select_pill_sheet_type_page.dart';
-import 'package:pilll/components/template/setting_pill_sheet_group/setting_pill_sheet_group.dart';
-import 'package:pilll/domain/initial_setting/initial_setting_state.dart';
 import 'package:pilll/domain/initial_setting/pill_type/card_layout.dart';
-import 'package:pilll/domain/initial_setting/today_pill_number/initial_setting_select_today_pill_number_page.dart';
 import 'package:pilll/domain/initial_setting/initial_setting_store.dart';
-import 'package:pilll/components/atoms/buttons.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/text_color.dart';
 import 'package:flutter/material.dart';
@@ -17,9 +11,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/entity/pill_type.dart';
 import 'package:pilll/router/router.dart';
 import 'package:pilll/service/auth.dart';
-import 'package:pilll/signin/signin_sheet.dart';
-import 'package:pilll/signin/signin_sheet_state.dart';
-import 'package:pilll/entity/pill_sheet_type.dart';
 import 'package:pilll/entity/link_account_type.dart';
 
 class InitialSettingPillTypePage extends HookConsumerWidget {
