5b6457a644ab767b42f6156a359337f8ac872e85
bannzai <kingkong999yhirose@gmail.com>
2022-03-09 07:37:21 +0900

    Fix pill type

--- lib/entity/pill_type.dart
+++ lib/entity/pill_type.dart
@@ -1,8 +1,14 @@
 enum PillType {
+  // ヤーズフレックス 連続
   pill_type_yaz_flex,
+  // 77日+7日休薬
   pill_type_jemina,
+  // 21錠＋7日休薬
   pill_type_21_rest_7,
-  pill_type_28,
+  // 24錠＋4偽薬
+  pill_type_24_fake_4,
+  // 24錠＋4休薬
   pill_type_24_rest_4,
+  // 21錠＋7偽薬
   pill_type_21_fake_7,
 }
