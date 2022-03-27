cb751647d048e8f18bd1c9cbe72fc842c46ab775
bannzai <kingkong999yhirose@gmail.com>
2022-03-27 18:54:52 +0900

    Escape error

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -31,6 +31,8 @@ for codegen_file in $CODEGEN_FILES; do
     codegen_file_without_file_extension=$(echo $codegen_file | awk -F'\.' '{print $1}')
     echo $codegen_file_without_file_extension | xargs -I '{}' mv {}.dart {}.codegen.dart
     echo $codegen_file_without_file_extension | xargs -I '{}' mv {}.freezed.dart {}.codegen.freezed.dart
+    set +e
     echo $codegen_file_without_file_extension | xargs -I '{}' mv {}.g.dart {}.codegen.g.dart
+    set -e
   fi
 done
