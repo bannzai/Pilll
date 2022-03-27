5e39cdecb3f62597bdbaa2fed0bebb0b2d6b6336
bannzai <kingkong999yhirose@gmail.com>
2022-03-27 16:56:02 +0900

    for loop included files

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -10,7 +10,9 @@ for codegen_file in $CODEGEN_FILES; do
     echo $codegen_file_without_file_extension | xargs -I '{}' mv {}.dart {}.codegen.dart
 
     codegen_file_without_lib_package_and_file_extension=$(echo $codegen_file_without_file_extension | sed 's|lib/||')
-    include_codegen_file=$(git grep $codegen_file_without_lib_package_and_file_extension | awk -F':' '{print $1}')
-    sed -i '' -e "s|$codegen_file_without_lib_package_and_file_extension.dart|$codegen_file_without_lib_package_and_file_extension.codegen.dart|" $include_codegen_file
+    include_codegen_files=$(git grep $codegen_file_without_lib_package_and_file_extension | awk -F':' '{print $1}')
+    for include_codegen_file in $include_codegen_files; do
+      sed -i '' -e "s|$codegen_file_without_lib_package_and_file_extension.dart|$codegen_file_without_lib_package_and_file_extension.codegen.dart|" $include_codegen_file
+    done
   fi
 done
