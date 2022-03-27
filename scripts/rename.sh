c66ec2f7893e25d8589e294a6a988aa42fedff47
bannzai <kingkong999yhirose@gmail.com>
2022-03-27 16:46:48 +0900

    without e

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -14,6 +14,6 @@ for codegen_file in $CODEGEN_FILES; do
   echo "codegen_file_without_lib_package_and_file_extension:$codegen_file_without_lib_package_and_file_extension"
   include_codegen_file=$(git grep $codegen_file_without_lib_package_and_file_extension | awk -F':' '{print $1}')
   echo "include_codegen_file:$include_codegen_file"
-  sed -i -e "s|$codegen_file_without_lib_package_and_file_extension.dart|$codegen_file_without_lib_package_and_file_extension.codegen.dart|" $include_codegen_file
+  sed -i '' -e "s|$codegen_file_without_lib_package_and_file_extension.dart|$codegen_file_without_lib_package_and_file_extension.codegen.dart|" $include_codegen_file
 
 done
