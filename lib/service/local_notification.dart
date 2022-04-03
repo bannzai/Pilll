fe80d1afc51b66585f607de67477f95bbe52fdc9
bannzai <kingkong999yhirose@gmail.com>
2022-04-04 08:52:55 +0900

    Add service for local notification

---
+++ lib/service/local_notification.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class LocalNotification {
+  final plugin = FlutterLocalNotificationsPlugin();
+
+  Future<void> scheduleRemiderNotification({
+    required int hour,
+    required int minute,
+    required DateTime from,
+    required DateTime to,
+  }) async {
+    print("[DEBUG] ${tz.local}");
+    await plugin.zonedSchedule(
+      0,
+      'scheduled title',
+      'scheduled body',
+      tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+      const NotificationDetails(
+          android: AndroidNotificationDetails(
+              'your channel id', 'your channel name',
+              channelDescription: 'your channel description')),
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time,
+    );
+  }
+}
+
+final localNotification = LocalNotification();
