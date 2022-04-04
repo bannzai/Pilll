1396982773c9d13816c8898960d0d75abc69bb29
bannzai <kingkong999yhirose@gmail.com>
2022-04-04 10:19:31 +0900

    Remove unnecessary import

--- test/domain/initial_setting/initial_setting_store_test.dart
+++ test/domain/initial_setting/initial_setting_store_test.dart
@@ -1,5 +1,4 @@
 import 'package:pilll/database/batch.dart';
-import 'package:pilll/domain/initial_setting/initial_setting_state.codegen.dart';
 import 'package:pilll/domain/initial_setting/initial_setting_store.dart';
 import 'package:pilll/entity/pill_sheet.codegen.dart';
 import 'package:pilll/entity/pill_sheet_group.codegen.dart';
