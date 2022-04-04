c131ec5dbd51d74055fe1db12e390bdcf6a6a90d
bannzai <kingkong999yhirose@gmail.com>
2022-04-05 08:08:56 +0900

    Fix scheduleReminderNotification

--- lib/service/local_notification.dart
+++ lib/service/local_notification.dart
@@ -1,7 +1,15 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:pilll/util/datetime/day.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+
+// NOTE: Unixtime is 13 digits
+const millisecondsMaxUnixtime = 9999999999999;
+const maxUnixtimePlusOne = millisecondsMaxUnixtime + 1;
+
+// Concrete identifier offsets
+const reminderNotificationIdentifierOffset = 1 * maxUnixtimePlusOne;
 
 class LocalNotification {
   final plugin = FlutterLocalNotificationsPlugin();
@@ -15,28 +23,38 @@ class LocalNotification {
   Future<void> scheduleRemiderNotification({
     required int hour,
     required int minute,
-    required DateTime from,
-    required DateTime to,
+    required int totalPillNumberOfPillSheetGroup,
+    required bool isTrialOrPremium,
+    required tz.TZDateTime tzFrom,
   }) async {
-    print("[DEBUG] ${tz.local}");
-    for (var i = 1; i <= 20; i++) {
-      await plugin.zonedSchedule(
-        i,
+    final processes = List.generate(totalPillNumberOfPillSheetGroup, (index) {
+      final date = tzFrom.add(Duration(days: index)).tzDate();
+      final reminderDate =
+          date.add(Duration(hours: hour)).add(Duration(minutes: minute));
+
+      return plugin.zonedSchedule(
+        reminderNotificationIdentifierOffset +
+            reminderDate.millisecondsSinceEpoch,
         'scheduled title',
         'scheduled body',
-        tz.TZDateTime.now(tz.local).add(Duration(seconds: i * 5)),
+        reminderDate,
         const NotificationDetails(
           android: AndroidNotificationDetails(
             'your channel id',
             'your channel name',
             channelDescription: 'your channel description',
           ),
+          iOS: DarwinNotificationDetails(
+            categoryIdentifier: "PILL_REMINDER",
+          ),
         ),
         androidAllowWhileIdle: true,
         uiLocalNotificationDateInterpretation:
             UILocalNotificationDateInterpretation.absoluteTime,
       );
-    }
+    });
+
+    await Future.wait(processes);
   }
 }
 
