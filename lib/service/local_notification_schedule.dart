75258496732f05188b88ff5e7bcfd5c82cc6488c
bannzai <kingkong999yhirose@gmail.com>
2022-04-09 08:24:59 +0900

    Defined stream

--- lib/service/local_notification_schedule.dart
+++ lib/service/local_notification_schedule.dart
@@ -5,11 +5,20 @@ import 'package:pilll/entity/local_notification_schedule.codegen.dart';
 import 'package:riverpod/riverpod.dart';
 
 final localNotificationScheduleCollectionServiceProvider = Provider((ref) =>
-    LocalNotificationScheduleCollectionService(ref.watch(databaseProvider)));
+    LocalNotificationScheduleCollectionService(
+        ref.watch(databaseProvider),
+        ref.watch(
+            localNotificationScheduleCollectionServiceStreamProvider.stream)));
+
+final localNotificationScheduleCollectionServiceStreamProvider =
+    StreamProvider<List<LocalNotificationScheduleCollection>>((ref) => ref
+        .watch(databaseProvider)
+        .localNotificationScheduleCollectionStream());
 
 class LocalNotificationScheduleCollectionService {
   final DatabaseConnection _database;
-  LocalNotificationScheduleCollectionService(this._database);
+  final Stream<List<LocalNotificationScheduleCollection>> _stream;
+  LocalNotificationScheduleCollectionService(this._database, this._stream);
 
   Future<LocalNotificationScheduleCollection?> fetchReminderNotification() {
     return _database
@@ -17,5 +26,11 @@ class LocalNotificationScheduleCollectionService {
             LocalNotificationScheduleKind.reminderNotification)
         .get()
         .then((e) => e.data());
+  }
+
+  Stream<List<LocalNotificationScheduleCollection>> stream(
+      LocalNotificationScheduleKind kind) {
+    return _stream.map(
+        (event) => event.where((element) => element.kind == kind).toList());
   }
 }
