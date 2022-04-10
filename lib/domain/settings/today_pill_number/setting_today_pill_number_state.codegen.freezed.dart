7e49a15889344c3893e6cb32d2d6211b0d7496a0
bannzai <kingkong999yhirose@gmail.com>
2022-04-10 21:28:42 +0900

    Fix parameter and state

--- lib/domain/settings/today_pill_number/setting_today_pill_number_state.codegen.freezed.dart
+++ lib/domain/settings/today_pill_number/setting_today_pill_number_state.codegen.freezed.dart
@@ -19,13 +19,19 @@ class _$SettingTodayPillNumberStateTearOff {
   const _$SettingTodayPillNumberStateTearOff();
 
   _SettingTodayPillNumberState call(
-      {int selectedPillSheetPageIndex = 0,
+      {required Setting? setting,
+      int selectedPillSheetPageIndex = 0,
       int selectedPillMarkNumberIntoPillSheet = 0,
-      required PillSheetAppearanceMode appearanceMode}) {
+      required PillSheetAppearanceMode appearanceMode,
+      required bool isTrial,
+      required bool isPremium}) {
     return _SettingTodayPillNumberState(
+      setting: setting,
       selectedPillSheetPageIndex: selectedPillSheetPageIndex,
       selectedPillMarkNumberIntoPillSheet: selectedPillMarkNumberIntoPillSheet,
       appearanceMode: appearanceMode,
+      isTrial: isTrial,
+      isPremium: isPremium,
     );
   }
 }
@@ -35,11 +41,14 @@ const $SettingTodayPillNumberState = _$SettingTodayPillNumberStateTearOff();
 
 /// @nodoc
 mixin _$SettingTodayPillNumberState {
+  Setting? get setting => throw _privateConstructorUsedError;
   int get selectedPillSheetPageIndex => throw _privateConstructorUsedError;
   int get selectedPillMarkNumberIntoPillSheet =>
       throw _privateConstructorUsedError;
   PillSheetAppearanceMode get appearanceMode =>
       throw _privateConstructorUsedError;
+  bool get isTrial => throw _privateConstructorUsedError;
+  bool get isPremium => throw _privateConstructorUsedError;
 
   @JsonKey(ignore: true)
   $SettingTodayPillNumberStateCopyWith<SettingTodayPillNumberState>
@@ -53,9 +62,14 @@ abstract class $SettingTodayPillNumberStateCopyWith<$Res> {
           $Res Function(SettingTodayPillNumberState) then) =
       _$SettingTodayPillNumberStateCopyWithImpl<$Res>;
   $Res call(
-      {int selectedPillSheetPageIndex,
+      {Setting? setting,
+      int selectedPillSheetPageIndex,
       int selectedPillMarkNumberIntoPillSheet,
-      PillSheetAppearanceMode appearanceMode});
+      PillSheetAppearanceMode appearanceMode,
+      bool isTrial,
+      bool isPremium});
+
+  $SettingCopyWith<$Res>? get setting;
 }
 
 /// @nodoc
@@ -69,11 +83,18 @@ class _$SettingTodayPillNumberStateCopyWithImpl<$Res>
 
   @override
   $Res call({
+    Object? setting = freezed,
     Object? selectedPillSheetPageIndex = freezed,
     Object? selectedPillMarkNumberIntoPillSheet = freezed,
     Object? appearanceMode = freezed,
+    Object? isTrial = freezed,
+    Object? isPremium = freezed,
   }) {
     return _then(_value.copyWith(
+      setting: setting == freezed
+          ? _value.setting
+          : setting // ignore: cast_nullable_to_non_nullable
+              as Setting?,
       selectedPillSheetPageIndex: selectedPillSheetPageIndex == freezed
           ? _value.selectedPillSheetPageIndex
           : selectedPillSheetPageIndex // ignore: cast_nullable_to_non_nullable
@@ -87,7 +108,26 @@ class _$SettingTodayPillNumberStateCopyWithImpl<$Res>
           ? _value.appearanceMode
           : appearanceMode // ignore: cast_nullable_to_non_nullable
               as PillSheetAppearanceMode,
+      isTrial: isTrial == freezed
+          ? _value.isTrial
+          : isTrial // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isPremium: isPremium == freezed
+          ? _value.isPremium
+          : isPremium // ignore: cast_nullable_to_non_nullable
+              as bool,
     ));
+  }
+
+  @override
+  $SettingCopyWith<$Res>? get setting {
+    if (_value.setting == null) {
+      return null;
+    }
+
+    return $SettingCopyWith<$Res>(_value.setting!, (value) {
+      return _then(_value.copyWith(setting: value));
+    });
   }
 }
 
@@ -100,9 +140,15 @@ abstract class _$SettingTodayPillNumberStateCopyWith<$Res>
       __$SettingTodayPillNumberStateCopyWithImpl<$Res>;
   @override
   $Res call(
-      {int selectedPillSheetPageIndex,
+      {Setting? setting,
+      int selectedPillSheetPageIndex,
       int selectedPillMarkNumberIntoPillSheet,
-      PillSheetAppearanceMode appearanceMode});
+      PillSheetAppearanceMode appearanceMode,
+      bool isTrial,
+      bool isPremium});
+
+  @override
+  $SettingCopyWith<$Res>? get setting;
 }
 
 /// @nodoc
@@ -120,11 +166,18 @@ class __$SettingTodayPillNumberStateCopyWithImpl<$Res>
 
   @override
   $Res call({
+    Object? setting = freezed,
     Object? selectedPillSheetPageIndex = freezed,
     Object? selectedPillMarkNumberIntoPillSheet = freezed,
     Object? appearanceMode = freezed,
+    Object? isTrial = freezed,
+    Object? isPremium = freezed,
   }) {
     return _then(_SettingTodayPillNumberState(
+      setting: setting == freezed
+          ? _value.setting
+          : setting // ignore: cast_nullable_to_non_nullable
+              as Setting?,
       selectedPillSheetPageIndex: selectedPillSheetPageIndex == freezed
           ? _value.selectedPillSheetPageIndex
           : selectedPillSheetPageIndex // ignore: cast_nullable_to_non_nullable
@@ -138,6 +191,14 @@ class __$SettingTodayPillNumberStateCopyWithImpl<$Res>
           ? _value.appearanceMode
           : appearanceMode // ignore: cast_nullable_to_non_nullable
               as PillSheetAppearanceMode,
+      isTrial: isTrial == freezed
+          ? _value.isTrial
+          : isTrial // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isPremium: isPremium == freezed
+          ? _value.isPremium
+          : isPremium // ignore: cast_nullable_to_non_nullable
+              as bool,
     ));
   }
 }
@@ -146,11 +207,16 @@ class __$SettingTodayPillNumberStateCopyWithImpl<$Res>
 
 class _$_SettingTodayPillNumberState extends _SettingTodayPillNumberState {
   const _$_SettingTodayPillNumberState(
-      {this.selectedPillSheetPageIndex = 0,
+      {required this.setting,
+      this.selectedPillSheetPageIndex = 0,
       this.selectedPillMarkNumberIntoPillSheet = 0,
-      required this.appearanceMode})
+      required this.appearanceMode,
+      required this.isTrial,
+      required this.isPremium})
       : super._();
 
+  @override
+  final Setting? setting;
   @JsonKey()
   @override
   final int selectedPillSheetPageIndex;
@@ -159,10 +225,14 @@ class _$_SettingTodayPillNumberState extends _SettingTodayPillNumberState {
   final int selectedPillMarkNumberIntoPillSheet;
   @override
   final PillSheetAppearanceMode appearanceMode;
+  @override
+  final bool isTrial;
+  @override
+  final bool isPremium;
 
   @override
   String toString() {
-    return 'SettingTodayPillNumberState(selectedPillSheetPageIndex: $selectedPillSheetPageIndex, selectedPillMarkNumberIntoPillSheet: $selectedPillMarkNumberIntoPillSheet, appearanceMode: $appearanceMode)';
+    return 'SettingTodayPillNumberState(setting: $setting, selectedPillSheetPageIndex: $selectedPillSheetPageIndex, selectedPillMarkNumberIntoPillSheet: $selectedPillMarkNumberIntoPillSheet, appearanceMode: $appearanceMode, isTrial: $isTrial, isPremium: $isPremium)';
   }
 
   @override
@@ -170,21 +240,27 @@ class _$_SettingTodayPillNumberState extends _SettingTodayPillNumberState {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _SettingTodayPillNumberState &&
+            const DeepCollectionEquality().equals(other.setting, setting) &&
             const DeepCollectionEquality().equals(
                 other.selectedPillSheetPageIndex, selectedPillSheetPageIndex) &&
             const DeepCollectionEquality().equals(
                 other.selectedPillMarkNumberIntoPillSheet,
                 selectedPillMarkNumberIntoPillSheet) &&
             const DeepCollectionEquality()
-                .equals(other.appearanceMode, appearanceMode));
+                .equals(other.appearanceMode, appearanceMode) &&
+            const DeepCollectionEquality().equals(other.isTrial, isTrial) &&
+            const DeepCollectionEquality().equals(other.isPremium, isPremium));
   }
 
   @override
   int get hashCode => Object.hash(
       runtimeType,
+      const DeepCollectionEquality().hash(setting),
       const DeepCollectionEquality().hash(selectedPillSheetPageIndex),
       const DeepCollectionEquality().hash(selectedPillMarkNumberIntoPillSheet),
-      const DeepCollectionEquality().hash(appearanceMode));
+      const DeepCollectionEquality().hash(appearanceMode),
+      const DeepCollectionEquality().hash(isTrial),
+      const DeepCollectionEquality().hash(isPremium));
 
   @JsonKey(ignore: true)
   @override
@@ -196,18 +272,26 @@ class _$_SettingTodayPillNumberState extends _SettingTodayPillNumberState {
 abstract class _SettingTodayPillNumberState
     extends SettingTodayPillNumberState {
   const factory _SettingTodayPillNumberState(
-          {int selectedPillSheetPageIndex,
-          int selectedPillMarkNumberIntoPillSheet,
-          required PillSheetAppearanceMode appearanceMode}) =
-      _$_SettingTodayPillNumberState;
+      {required Setting? setting,
+      int selectedPillSheetPageIndex,
+      int selectedPillMarkNumberIntoPillSheet,
+      required PillSheetAppearanceMode appearanceMode,
+      required bool isTrial,
+      required bool isPremium}) = _$_SettingTodayPillNumberState;
   const _SettingTodayPillNumberState._() : super._();
 
+  @override
+  Setting? get setting;
   @override
   int get selectedPillSheetPageIndex;
   @override
   int get selectedPillMarkNumberIntoPillSheet;
   @override
   PillSheetAppearanceMode get appearanceMode;
+  @override
+  bool get isTrial;
+  @override
+  bool get isPremium;
   @override
   @JsonKey(ignore: true)
   _$SettingTodayPillNumberStateCopyWith<_SettingTodayPillNumberState>
--- lib/domain/settings/today_pill_number/setting_today_pill_number_store_parameter.codegen.dart
+++ lib/domain/settings/today_pill_number/setting_today_pill_number_store_parameter.codegen.dart
@@ -9,8 +9,12 @@ part 'setting_today_pill_number_store_parameter.codegen.freezed.dart';
 class SettingTodayPillNumberStoreParameter
     with _$SettingTodayPillNumberStoreParameter {
   const factory SettingTodayPillNumberStoreParameter({
+    // TODO: Temporary optional for pass compile for test
+    required Setting? setting,
     required PillSheetGroup pillSheetGroup,
     required PillSheetAppearanceMode appearanceMode,
     required PillSheet activedPillSheet,
+    required bool isTrial,
+    required bool isPremium,
   }) = _SettingTodayPillNumberStoreParameter;
 }
