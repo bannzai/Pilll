e801f9cf446acc2fb789a14c7e4993c70da14e70
bannzai <kingkong999yhirose@gmail.com>
2022-04-13 11:00:05 +0900

    Add service of messaging

---
+++ lib/service/messaging.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class Messaging {
+  // Only Apple Platfroms
+  Future<void> requestPermission() async {
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+  }
+}
