4e74fb0fead699c0a9de578d07535f724e88ee6a
bannzai <kingkong999yhirose@gmail.com>
2022-04-13 12:16:23 +0900

    Fix notification settings

--- android/app/src/main/kotlin/com/example/Pilll/PilllFirebaseMessagingService.kt
+++ android/app/src/main/kotlin/com/example/Pilll/PilllFirebaseMessagingService.kt
@@ -26,7 +26,7 @@ public class PilllFirebaseMessagingService: FirebaseMessagingService() {
         val mainActivityIntent = Intent(this, MainActivity::class.java).also {
             it.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
-        val openAppPendingIntent = PendingIntent.getActivity( this,0, mainActivityIntent, 0)
+        val openAppPendingIntent = PendingIntent.getActivity( this,1, mainActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT)
         val title = data["title"]
         val body = data["body"]
         val builder = NotificationCompat.Builder(this, "PILL_REMINDER")
@@ -44,7 +44,7 @@ public class PilllFirebaseMessagingService: FirebaseMessagingService() {
                 action = "PILL_REMINDER"
             }
             val pendingIntent: PendingIntent =
-                    PendingIntent.getBroadcast(this, 0, intent, 0)
+                    PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
             builder.addAction(0, "飲んだ", pendingIntent)
         }
