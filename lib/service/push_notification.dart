82541ebfd9611fa10e76ed2a47948409a6beb6cc
bannzai <kingkong999yhirose@gmail.com>
2022-04-16 20:19:52 +0900

    Remove unnecessary code

--- lib/service/push_notification.dart
+++ lib/service/push_notification.dart
@@ -40,63 +40,8 @@ Future<void> requestNotificationPermissions() async {
 }
 
 void listenNotificationEvents() {
-  FirebaseMessaging.onMessage.listen(handleMessage);
-  FirebaseMessaging.onBackgroundMessage(handleMessage);
   FirebaseMessaging.onMessageOpenedApp.listen((event) {
     analytics.logEvent(name: "opened_from_notification_on_background");
     print("onMessageOpenedApp: $event");
   });
-}
-
-Future<void> handleMessage(RemoteMessage message) async {
-  debugPrint("handleMessage: $message");
-
-  final title = message.data["title"];
-  final body = message.data["body"];
-  final notificationType = message.data["notificationType"];
-  if (title == null || body == null || notificationType == null) {
-    return;
-  }
-
-  switch (notificationType) {
-    case "CREATE_NEW_PILL_SHEET":
-      if (Firebase.apps.isEmpty) {
-        await Firebase.initializeApp();
-      }
-      final firebaseUser = FirebaseAuth.instance.currentUser;
-      if (firebaseUser == null) {
-        return;
-      }
-
-      final database = DatabaseConnection(firebaseUser.uid);
-      final userService = UserService(database);
-      final pillSheetGroupService = PillSheetGroupService(database);
-
-      final user = await userService.fetch();
-      final setting = user.setting;
-      final pillSheetGroup = await pillSheetGroupService.fetchLatest();
-      final activedPillSheet = pillSheetGroup?.activedPillSheet;
-      if (pillSheetGroup == null ||
-          activedPillSheet == null ||
-          setting == null) {
-        return;
-      }
-
-      final isTrialOrPremium = user.isTrial || user.isPremium;
-      await localNotification.scheduleRemiderNotification(
-        pillSheetGroup: pillSheetGroup,
-        activedPillSheet: activedPillSheet,
-        isTrialOrPremium: isTrialOrPremium,
-        setting: setting,
-        tzFrom: now().tzDate(),
-      );
-
-      await localNotification.fireCreateNewPillSheetNotification(
-        title: title,
-        body: body,
-      );
-      break;
-    default:
-      return;
-  }
 }
