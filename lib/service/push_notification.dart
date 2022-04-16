fc3250a1ef3d4015bdcd10f043198ecd37d8bf17
bannzai <kingkong999yhirose@gmail.com>
2022-04-16 20:20:11 +0900

    Remove unnecessary code

--- lib/service/push_notification.dart
+++ lib/service/push_notification.dart
@@ -2,15 +2,11 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/database/database.dart';
-import 'package:pilll/service/local_notification.dart';
-import 'package:pilll/service/pill_sheet_group.dart';
 import 'package:pilll/service/user.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:pilll/util/datetime/day.dart';
 
 Future<void> requestNotificationPermissions() async {
   final firebaseUser = FirebaseAuth.instance.currentUser;
