82fc68aee41ed36cd9344f72beb24894e873f567
bannzai <kingkong999yhirose@gmail.com>
2022-04-17 21:38:30 +0900

    add state and store

--- lib/domain/record/components/add_pill_sheet_group/add_pill_sheet_group_state.dart
+++ lib/domain/record/components/add_pill_sheet_group/add_pill_sheet_group_state.dart
@@ -3,14 +3,14 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'add_pill_sheet_group_state.codegen.freezed.dart';
 
 @freezed
-class AddPillSheetGroupStateState with _$AddPillSheetGroupStateState {
-  factory AddPillSheetGroupStateState({
+class AddPillSheetGroupState with _$AddPillSheetGroupState {
+  factory AddPillSheetGroupState({
     DateTime? beginTrialDate,
     @Default(false) bool isTrial,
     @Default(false) bool isPremium,
     @Default(false) bool isLoading,
     @Default(false) bool isFirstLoadEnded,
     Object? exception,
-  }) = _AddPillSheetGroupStateState;
-  AddPillSheetGroupStateState._();
+  }) = _$AddPillSheetGroupState;
+  AddPillSheetGroupState._();
 }
--- lib/domain/record/components/add_pill_sheet_group/add_pill_sheet_group_store.dart
+++ lib/domain/record/components/add_pill_sheet_group/add_pill_sheet_group_store.dart
@@ -1,1 +1,53 @@
 
+
+import 'dart:async';
+
+import 'package:pilll/domain/~/ghq/github.com/bannzai/pilll/lib/domain/record/components/add_pill_sheet_group/state.dart';
+import 'package:pilll/service/user.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final addPillSheetGroupStateStoreProvider = StateNotifierProvider<AddPillSheetGroupStateStore, AddPillSheetGroupState>(
+(ref) => AddPillSheetGroupStateStore(
+ref.watch(userServiceProvider),
+),
+);
+
+class AddPillSheetGroupStateStore extends StateNotifier<AddPillSheetGroupState> {
+final UserService _userService;
+AddPillSheetGroupStateStore(
+this._userService,
+) : super(AddPillSheetGroupState()) {
+setup();
+}
+
+void setup() {
+state = state.copyWith(isLoading: true);
+Future(() async {
+final storage = await SharedPreferences.getInstance();
+final user = await _userService.fetch();
+this.state = AddPillSheetGroupState(
+isPremium: user.isPremium,
+isTrial: user.isTrial,
+beginTrialDate: user.beginTrialDate,
+isLoading: false,
+isFirstLoadEnded: true,
+);
+_subscribe();
+});
+}
+
+StreamSubscription? _userSubscribeCanceller;
+void _subscribe() {
+_userSubscribeCanceller?.cancel();
+_userSubscribeCanceller = _userService.subscribe().listen((event) {
+state = state.copyWith(isPremium: event.isPremium, isTrial: event.isTrial, beginTrialDate: event.beginTrialDate);
+});
+}
+
+@override
+void dispose() {
+_userSubscribeCanceller?.cancel();
+super.dispose();
+}
+}
