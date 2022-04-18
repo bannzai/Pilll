c72d58d751c8001181f5d8bdf0d410c1c6e48bb1
bannzai <kingkong999yhirose@gmail.com>
2022-04-18 09:02:01 +0900

    Payload beforePillSheetGroup

--- lib/domain/record/components/supports/components/display_number_setting/display_number_setting_sheet.dart
+++ lib/domain/record/components/supports/components/display_number_setting/display_number_setting_sheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/color.dart';
@@ -8,139 +10,178 @@ import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/domain/record/record_page_store.dart';
 import 'package:pilll/entity/pill_sheet_group.codegen.dart';
 
-const _defaultBegin = 3;
-const _defaultEnd = 120;
-
 class DisplayNumberSettingSheet extends HookConsumerWidget {
+  final PillSheetGroup? beforePillSheetGroup;
   final PillSheetGroup pillSheetGroup;
   final RecordPageStore store;
 
   DisplayNumberSettingSheet({
+    required this.beforePillSheetGroup,
     required this.pillSheetGroup,
     required this.store,
   });
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final begin = useState(
-        pillSheetGroup.displayNumberSetting?.beginPillNumber ?? _defaultBegin);
-    final end = useState(
-        pillSheetGroup.displayNumberSetting?.endPillNumber ?? _defaultEnd);
+    final begin =
+        useState(pillSheetGroup.displayNumberSetting?.beginPillNumber ?? 1);
+    final end = useState(pillSheetGroup.displayNumberSetting?.endPillNumber ??
+        pillSheetGroup.estimatedEndPillNumber);
+
     final beginTextFieldController =
         useTextEditingController(text: "${begin.value}");
     final endTextFieldController =
         useTextEditingController(text: "${end.value}");
 
-    return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-        ),
-        color: PilllColors.white,
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text(
-            "表示する番号を調整できます",
-            style: TextStyle(
-              color: TextColor.main,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+    final beforePillSheetGroup = this.beforePillSheetGroup;
+
+    return DraggableScrollableSheet(
+      initialChildSize: 0.7,
+      maxChildSize: 0.7,
+      builder: (context, scrollController) {
+        return Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
             ),
-            textAlign: TextAlign.center,
+            color: PilllColors.white,
           ),
-          const SizedBox(height: 16),
-          Column(
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
+              Row(children: [
+                const Text(
+                  "シートの服用日数を変更",
+                  style: TextStyle(
+                    color: TextColor.main,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const Spacer(),
+                const Text(
+                  "変更",
+                  style: TextStyle(
+                    color: TextColor.primary,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: FontFamily.japanese,
+                  ),
+                )
+              ]),
+              const SizedBox(height: 24),
+              Column(
                 children: [
-                  TextField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      constraints: const BoxConstraints(maxWidth: 60),
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(color: PilllColors.primary),
-                      ),
-                      counter: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
                         children: [
-                          Text(
-                            "1番→${begin.value}番",
+                          SvgPicture.asset(
+                              "images/begin_display_number_setting.svg"),
+                          const SizedBox(width: 4),
+                          const Text(
+                            "服用日数の始まり",
                             style: TextStyle(
                               fontFamily: FontFamily.japanese,
-                              fontSize: 10,
-                              fontWeight: FontWeight.w400,
-                              color: TextColor.darkGray,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                              color: TextColor.main,
                             ),
-                            textAlign: TextAlign.center,
                           ),
                         ],
                       ),
-                    ),
-                    onChanged: (_begin) {
-                      try {
-                        begin.value = int.parse(_begin);
-                        store.setDisplayNumberSettingBegin(begin.value);
-                      } catch (_) {}
-                    },
-                    controller: beginTextFieldController,
-                    keyboardType: TextInputType.number,
-                  ),
-                  const SizedBox(width: 10),
-                  const Text("から始める"),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  TextField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      constraints: const BoxConstraints(maxWidth: 60),
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(color: PilllColors.primary),
-                      ),
-                      counter: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                      const SizedBox(height: 16),
+                      Row(
                         children: [
-                          Text(
-                            "${end.value + 1}番→1番",
+                          const Text(
+                            "服用",
                             style: TextStyle(
                               fontFamily: FontFamily.japanese,
-                              fontSize: 10,
+                              fontSize: 14,
                               fontWeight: FontWeight.w400,
-                              color: TextColor.darkGray,
+                              color: TextColor.main,
                             ),
-                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(width: 5),
+                          SizedBox(
+                            width: 42,
+                            height: 40,
+                            child: TextField(
+                              style: TextStyle(
+                                color: TextColor.darkGray,
+                                fontSize: 15,
+                                fontFamily: FontFamily.number,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              textAlign: TextAlign.center,
+                              controller: beginTextFieldController,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                              decoration: const InputDecoration(
+                                fillColor: PilllColors.mat,
+                                filled: true,
+                                border: UnderlineInputBorder(
+                                  borderSide: BorderSide(width: 1),
+                                ),
+                                contentPadding: EdgeInsets.only(bottom: 8),
+                              ),
+                              onChanged: (text) {
+                                try {
+                                  begin.value = int.parse(text);
+                                  store.setBeginDisplayPillNumber(begin.value);
+                                } catch (_) {}
+                              },
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                          const Text(
+                            "日目からスタート",
+                            style: TextStyle(
+                              fontFamily: FontFamily.japanese,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: TextColor.main,
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                    onChanged: (_end) {
-                      try {
-                        end.value = int.parse(_end);
-                        store.setDisplayNumberSettingEnd(end.value);
-                      } catch (_) {}
-                    },
-                    controller: endTextFieldController,
-                    keyboardType: TextInputType.number,
+                      if (beforePillSheetGroup != null)
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "前回のシートの最後：${beforePillSheetGroup.estimatedEndPillNumber}",
+                              style: const TextStyle(
+                                fontFamily: FontFamily.japanese,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: TextColor.main,
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
                   ),
-                  const SizedBox(width: 10),
-                  const Text("を番号の最後にする"),
                 ],
               ),
             ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
 
 void showDisplayNumberSettingSheet(
   BuildContext context, {
+  required PillSheetGroup? beforePillSheetGroup,
   required PillSheetGroup pillSheetGroup,
   required RecordPageStore store,
 }) {
@@ -148,9 +189,11 @@ void showDisplayNumberSettingSheet(
   showModalBottomSheet(
     context: context,
     builder: (context) => DisplayNumberSettingSheet(
+      beforePillSheetGroup: beforePillSheetGroup,
       pillSheetGroup: pillSheetGroup,
       store: store,
     ),
     backgroundColor: Colors.transparent,
+    isScrollControlled: true,
   );
 }
