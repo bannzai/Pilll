6b45fd9fb970bc849d0fece452a81b70efd82b3f
bannzai <kingkong999yhirose@gmail.com>
2022-04-20 08:05:09 +0900

    Add modal

--- lib/domain/record/components/supports/components/rest_duration/end_manual_rest_duration_button.dart
+++ lib/domain/record/components/supports/components/rest_duration/end_manual_rest_duration_button.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/buttons.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/domain/record/record_page_store.dart';
 import 'package:pilll/entity/pill_sheet.codegen.dart';
 import 'package:pilll/entity/pill_sheet_group.codegen.dart';
@@ -45,6 +47,66 @@ class EndManualRestDurationButton extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+class EndRestDurationModal extends StatelessWidget {
+  final PillSheetGroup pillSheetGroup;
+
+  const EndRestDurationModal({
+    Key? key,
+    required this.pillSheetGroup,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: const Text(
+          "休薬期間終了",
+          style: TextStyle(
+            color: TextColor.white,
+            fontSize: 12,
+            fontFamily: FontFamily.japanese,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const Text(
+            "服用1日目から再開しますか？",
+            style: TextStyle(
+              color: TextColor.main,
+              fontSize: 16,
+              fontFamily: FontFamily.japanese,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(height: 24),
+          Text(
+            "服用${pillSheetGroup.estimatedEndPillNumber}日目→1日目",
+            style: const TextStyle(
+              color: TextColor.main,
+              fontSize: 14,
+              fontFamily: FontFamily.japanese,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        AppOutlinedButton(onPressed: () async {}, text: "はい"),
+        AppOutlinedButton(
+            onPressed: () async {
+              Navigator.of(context).pop();
+            },
+            text: "いいえ"),
+      ],
     );
   }
 }
