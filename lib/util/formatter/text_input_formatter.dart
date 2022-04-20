fa5761132939e8c8c00f8cc2981173f6ee82e4ef
bannzai <kingkong999yhirose@gmail.com>
2022-04-20 14:28:49 +0900

    Add early return

--- lib/util/formatter/text_input_formatter.dart
+++ lib/util/formatter/text_input_formatter.dart
@@ -3,6 +3,9 @@ import 'package:flutter/services.dart';
 abstract class AppTextFieldFormatter {
   static final greaterThanZero =
       TextInputFormatter.withFunction((oldValue, newValue) {
+    if (newValue.text.isEmpty) {
+      return newValue;
+    }
     try {
       final n = int.parse(newValue.text);
       if (n > 0) {
