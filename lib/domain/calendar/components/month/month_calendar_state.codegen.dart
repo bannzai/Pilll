0837d39e37eefb3bda3fbbad55704c047902ee07
bannzai <kingkong999yhirose@gmail.com>
2022-04-29 13:04:29 +0900

    Add month calendar state

---
+++ lib/domain/calendar/components/month/month_calendar_state.codegen.dart
@@ -0,0 +1,89 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:pilll/components/organisms/calendar/weekly/weekly_calendar_state.dart';
+import 'package:pilll/database/diary.dart';
+import 'package:pilll/domain/calendar/date_range.dart';
+import 'package:pilll/domain/calendar/weekly_calendar_state.dart';
+import 'package:pilll/entity/diary.codegen.dart';
+import 'package:pilll/entity/weekday.dart';
+import 'package:riverpod/riverpod.dart';
+
+part 'month_calendar_state.codegen.freezed.dart';
+
+final monthCalendarStateProvider =
+    Provider.family<AsyncValue<MonthCalendarState>, DateTime>(
+        (ref, DateTime dateForMonth) {
+  final diaries = ref.watch(diariesStreamForMonthProvider(dateForMonth));
+
+  if (diaries is AsyncLoading) {
+    return const AsyncValue.loading();
+  }
+
+  try {
+    return AsyncValue.data(
+      MonthCalendarState(
+        dateForMonth: dateForMonth,
+        diaries: diaries.value!,
+      ),
+    );
+  } catch (error, _) {
+    return AsyncValue.error(error);
+  }
+});
+
+@freezed
+class MonthCalendarState with _$MonthCalendarState {
+  factory MonthCalendarState({
+    required DateTime dateForMonth,
+    required List<Diary> diaries,
+  }) = _MonthCalendarState;
+  MonthCalendarState._();
+
+  List<CalendarTabWeeklyCalendarState> get weekCalendarStatuses =>
+      List.generate(6, (index) => index + 1)
+          .map(
+            (line) => CalendarTabWeeklyCalendarState(
+              dateRange: _dateRangeOfLine(1),
+              diariesForMonth: diaries,
+              // TODO:
+              allBandModels: [],
+              targetDateOfMonth: dateForMonth,
+            ),
+          )
+          .toList();
+
+  DateRange _dateRangeOfLine(int line) {
+    if (line == 1) {
+      return DateRange(
+        DateTime(dateForMonth.year, dateForMonth.month,
+            1 - _previousMonthDayCount()),
+        DateTime(dateForMonth.year, dateForMonth.month,
+            Weekday.values.length - _previousMonthDayCount()),
+      );
+    }
+    if (line == weeklineCount()) {
+      return DateRange(
+        DateTime(dateForMonth.year, dateForMonth.month,
+            Weekday.values.length * (line - 1) + 1 - _previousMonthDayCount()),
+        DateTime(dateForMonth.year, dateForMonth.month, _lastDay()),
+      );
+    }
+    var beginDay =
+        Weekday.values.length * (line - 1) - _previousMonthDayCount() + 1;
+    var endDay = Weekday.values.length * line - _previousMonthDayCount();
+    return DateRange(
+      DateTime(dateForMonth.year, dateForMonth.month, beginDay),
+      DateTime(dateForMonth.year, dateForMonth.month, endDay),
+    );
+  }
+
+  int _lastDay() => DateTime(dateForMonth.year, dateForMonth.month + 1, 0).day;
+  int _weekdayOffset() =>
+      WeekdayFunctions.weekdayFromDate(_firstDayOfMonth(dateForMonth)).index;
+  int _previousMonthDayCount() => _weekdayOffset();
+  int _tileCount() => _previousMonthDayCount() + _lastDay();
+  int weeklineCount() => (_tileCount() / Weekday.values.length).ceil();
+
+  DateTime _firstDayOfMonth(DateTime date) {
+    return DateTime(date.year, date.month, 1);
+  }
+}
---
+++ lib/domain/calendar/components/month/month_calendar_state.codegen.freezed.dart
@@ -0,0 +1,169 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'month_calendar_state.codegen.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$MonthCalendarStateTearOff {
+  const _$MonthCalendarStateTearOff();
+
+  _MonthCalendarState call(
+      {required DateTime dateForMonth, required List<Diary> diaries}) {
+    return _MonthCalendarState(
+      dateForMonth: dateForMonth,
+      diaries: diaries,
+    );
+  }
+}
+
+/// @nodoc
+const $MonthCalendarState = _$MonthCalendarStateTearOff();
+
+/// @nodoc
+mixin _$MonthCalendarState {
+  DateTime get dateForMonth => throw _privateConstructorUsedError;
+  List<Diary> get diaries => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $MonthCalendarStateCopyWith<MonthCalendarState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MonthCalendarStateCopyWith<$Res> {
+  factory $MonthCalendarStateCopyWith(
+          MonthCalendarState value, $Res Function(MonthCalendarState) then) =
+      _$MonthCalendarStateCopyWithImpl<$Res>;
+  $Res call({DateTime dateForMonth, List<Diary> diaries});
+}
+
+/// @nodoc
+class _$MonthCalendarStateCopyWithImpl<$Res>
+    implements $MonthCalendarStateCopyWith<$Res> {
+  _$MonthCalendarStateCopyWithImpl(this._value, this._then);
+
+  final MonthCalendarState _value;
+  // ignore: unused_field
+  final $Res Function(MonthCalendarState) _then;
+
+  @override
+  $Res call({
+    Object? dateForMonth = freezed,
+    Object? diaries = freezed,
+  }) {
+    return _then(_value.copyWith(
+      dateForMonth: dateForMonth == freezed
+          ? _value.dateForMonth
+          : dateForMonth // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      diaries: diaries == freezed
+          ? _value.diaries
+          : diaries // ignore: cast_nullable_to_non_nullable
+              as List<Diary>,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$MonthCalendarStateCopyWith<$Res>
+    implements $MonthCalendarStateCopyWith<$Res> {
+  factory _$MonthCalendarStateCopyWith(
+          _MonthCalendarState value, $Res Function(_MonthCalendarState) then) =
+      __$MonthCalendarStateCopyWithImpl<$Res>;
+  @override
+  $Res call({DateTime dateForMonth, List<Diary> diaries});
+}
+
+/// @nodoc
+class __$MonthCalendarStateCopyWithImpl<$Res>
+    extends _$MonthCalendarStateCopyWithImpl<$Res>
+    implements _$MonthCalendarStateCopyWith<$Res> {
+  __$MonthCalendarStateCopyWithImpl(
+      _MonthCalendarState _value, $Res Function(_MonthCalendarState) _then)
+      : super(_value, (v) => _then(v as _MonthCalendarState));
+
+  @override
+  _MonthCalendarState get _value => super._value as _MonthCalendarState;
+
+  @override
+  $Res call({
+    Object? dateForMonth = freezed,
+    Object? diaries = freezed,
+  }) {
+    return _then(_MonthCalendarState(
+      dateForMonth: dateForMonth == freezed
+          ? _value.dateForMonth
+          : dateForMonth // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      diaries: diaries == freezed
+          ? _value.diaries
+          : diaries // ignore: cast_nullable_to_non_nullable
+              as List<Diary>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_MonthCalendarState extends _MonthCalendarState {
+  _$_MonthCalendarState({required this.dateForMonth, required this.diaries})
+      : super._();
+
+  @override
+  final DateTime dateForMonth;
+  @override
+  final List<Diary> diaries;
+
+  @override
+  String toString() {
+    return 'MonthCalendarState(dateForMonth: $dateForMonth, diaries: $diaries)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _MonthCalendarState &&
+            const DeepCollectionEquality()
+                .equals(other.dateForMonth, dateForMonth) &&
+            const DeepCollectionEquality().equals(other.diaries, diaries));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(dateForMonth),
+      const DeepCollectionEquality().hash(diaries));
+
+  @JsonKey(ignore: true)
+  @override
+  _$MonthCalendarStateCopyWith<_MonthCalendarState> get copyWith =>
+      __$MonthCalendarStateCopyWithImpl<_MonthCalendarState>(this, _$identity);
+}
+
+abstract class _MonthCalendarState extends MonthCalendarState {
+  factory _MonthCalendarState(
+      {required DateTime dateForMonth,
+      required List<Diary> diaries}) = _$_MonthCalendarState;
+  _MonthCalendarState._() : super._();
+
+  @override
+  DateTime get dateForMonth;
+  @override
+  List<Diary> get diaries;
+  @override
+  @JsonKey(ignore: true)
+  _$MonthCalendarStateCopyWith<_MonthCalendarState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
