f9f07b3a92c19af9b788225ea6c3a4879f19ce67
bannzai <kingkong999yhirose@gmail.com>
2022-04-29 14:11:13 +0900

    Remove unnecessary file

--- lib/domain/calendar/calendar_card_state.dart
+++
@@ -1,6 +0,0 @@
-import 'package:pilll/components/organisms/calendar/monthly/calendar_state.dart';
-
-class CalendarCardState extends MonthlyCalendarState {
-  final DateTime dateForMonth;
-  CalendarCardState(this.dateForMonth);
-}
