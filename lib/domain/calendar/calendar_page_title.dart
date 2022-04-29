2eb4f6844ce9170e80ab9cd55defd220f301eb41
bannzai <kingkong999yhirose@gmail.com>
2022-04-30 08:17:27 +0900

    Fix index

--- lib/domain/calendar/calendar_page_title.dart
+++ lib/domain/calendar/calendar_page_title.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/domain/calendar/calendar_page_state.codegen.dart';
 import 'package:pilll/components/atoms/font.dart';
@@ -6,7 +7,7 @@ import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/domain/calendar/calendar_page_store.dart';
 import 'package:flutter/material.dart';
 
-class CalendarPageTitle extends StatelessWidget {
+class CalendarPageTitle extends HookConsumerWidget {
   const CalendarPageTitle({
     Key? key,
     required this.state,
@@ -19,7 +20,7 @@ class CalendarPageTitle extends StatelessWidget {
   final CalendarPageStateStore store;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -27,12 +28,19 @@ class CalendarPageTitle extends StatelessWidget {
           icon: SvgPicture.asset("images/arrow_left.svg"),
           onPressed: () {
             final previousMonthIndex = state.currentCalendarIndex - 1;
-            pageController.jumpToPage(previousMonthIndex);
-            store.updateCurrentCalendarIndex(previousMonthIndex);
+
             analytics.logEvent(name: "pressed_previous_month", parameters: {
               "current_index": state.currentCalendarIndex,
               "previous_index": previousMonthIndex
             });
+
+            pageController.jumpToPage(previousMonthIndex);
+            final currentCalendarPageIndex =
+                ref.read(currentCalendarPageIndexProvider.notifier).state;
+            if (currentCalendarPageIndex != previousMonthIndex) {
+              ref.read(currentCalendarPageIndexProvider.notifier).state =
+                  previousMonthIndex;
+            }
           },
         ),
         Text(
@@ -43,12 +51,19 @@ class CalendarPageTitle extends StatelessWidget {
           icon: SvgPicture.asset("images/arrow_right.svg"),
           onPressed: () {
             final nextMonthIndex = state.currentCalendarIndex + 1;
-            pageController.jumpToPage(nextMonthIndex);
-            store.updateCurrentCalendarIndex(nextMonthIndex);
+
             analytics.logEvent(name: "pressed_next_month", parameters: {
               "current_index": state.currentCalendarIndex,
               "next_index": nextMonthIndex
             });
+
+            pageController.jumpToPage(nextMonthIndex);
+            final currentCalendarPageIndex =
+                ref.read(currentCalendarPageIndexProvider.notifier).state;
+            if (currentCalendarPageIndex != nextMonthIndex) {
+              ref.read(currentCalendarPageIndexProvider.notifier).state =
+                  nextMonthIndex;
+            }
           },
         ),
       ],
