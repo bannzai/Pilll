760a047e28bea586696ac6045ef044aa2edbd74f
bannzai <kingkong999yhirose@gmail.com>
2022-04-29 14:32:48 +0900

    Fix import

--- lib/components/organisms/calendar/band/calendar_band.dart
+++ lib/components/organisms/calendar/band/calendar_band.dart
@@ -1,10 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:pilll/components/atoms/font.dart';
-import 'package:pilll/components/atoms/text_color.dart';
-import 'package:pilll/components/molecules/diagonal_striped_line.dart';
-
-import 'calendar_band_model.dart';
-
 abstract class CalendarBandConst {
   static const double height = 13;
 }
