3ffb23e196b3c8d77ede5755ea5822504394ff5d
bannzai <kingkong999yhirose@gmail.com>
2022-04-29 16:42:53 +0900

    Fix calendar store

---
+++ lib/domain/calendar/calendar_page_async_action.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_card.dart';
+import 'package:pilll/entity/pill_sheet_modified_history.codegen.dart';
+import 'package:pilll/entity/pill_sheet_modified_history_value.codegen.dart';
+import 'package:pilll/database/diary.dart';
+import 'package:pilll/database/menstruation.dart';
+import 'package:pilll/database/pill_sheet_modified_history.dart';
+
+final calendarPageAsyncActionProvider = Provider(
+  (ref) => CalendarPageAsyncAction(
+    ref.watch(pillSheetModifiedHistoryDatastoreProvider),
+  ),
+);
+
+class CalendarPageAsyncAction {
+  final PillSheetModifiedHistoryDatastore _pillSheetModifiedHistoryDatastore;
+
+  CalendarPageAsyncAction(this._pillSheetModifiedHistoryDatastore);
+
+  Future<void> editTakenValue(
+    DateTime actualTakenDate,
+    PillSheetModifiedHistory history,
+    PillSheetModifiedHistoryValue value,
+    TakenPillValue takenPillValue,
+  ) {
+    return updateForEditTakenValue(
+      service: _pillSheetModifiedHistoryDatastore,
+      actualTakenDate: actualTakenDate,
+      history: history,
+      value: value,
+      takenPillValue: takenPillValue,
+    );
+  }
+}
--- lib/domain/calendar/calendar_page_store.dart
+++ lib/domain/calendar/calendar_page_store.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:pilll/domain/calendar/calendar_card_state.dart';
+import 'package:pilll/domain/calendar/calendar_page_async_action.dart';
 import 'package:pilll/domain/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_card.dart';
 import 'package:pilll/entity/pill_sheet_modified_history.codegen.dart';
 import 'package:pilll/entity/pill_sheet_modified_history_value.codegen.dart';
@@ -14,156 +14,41 @@ import 'package:pilll/domain/calendar/calendar_page_state.codegen.dart';
 import 'package:pilll/database/user.dart';
 import 'package:pilll/util/datetime/date_compare.dart';
 
-final calendarPageStateStoreProvider =
-    StateNotifierProvider<CalendarPageStateStore, CalendarPageState>(
+final calendarPageStateStoreProvider = StateNotifierProvider<
+    CalendarPageStateStore, AsyncValue<CalendarPageState>>(
   (ref) => CalendarPageStateStore(
-    ref.watch(menstruationDatastoreProvider),
-    ref.watch(settingDatastoreProvider),
-    ref.watch(diaryDatastoreProvider),
-    ref.watch(pillSheetModifiedHistoryDatastoreProvider),
-    ref.watch(userDatastoreProvider),
-    ref.watch(pillSheetGroupDatastoreProvider),
+    asyncAction: ref.read(calendarPageAsyncActionProvider),
+    initialState: ref.watch(calendarPageStateProvider),
   ),
 );
 
-class CalendarPageStateStore extends StateNotifier<CalendarPageState> {
-  final MenstruationDatastore _menstruationDatastore;
-  final SettingDatastore _settingDatastore;
-  final DiaryDatastore _diaryDatastore;
-  final PillSheetModifiedHistoryDatastore _pillSheetModifiedHistoryDatastore;
-  final UserDatastore _userDatastore;
-  final PillSheetGroupDatastore _pillSheetGroupDatastore;
+class CalendarPageStateStore
+    extends StateNotifier<AsyncValue<CalendarPageState>> {
+  final CalendarPageAsyncAction asyncAction;
 
-  CalendarPageStateStore(
-    this._menstruationDatastore,
-    this._settingDatastore,
-    this._diaryDatastore,
-    this._pillSheetModifiedHistoryDatastore,
-    this._userDatastore,
-    this._pillSheetGroupDatastore,
-  ) : super(CalendarPageState(menstruations: [])) {
-    reset();
-  }
-
-  void reset() async {
-    state = state.copyWith(exception: null);
-    state = state.copyWith(currentCalendarIndex: state.todayCalendarIndex);
-    try {
-      await Future(() async {
-        final menstruations = await _menstruationDatastore.fetchAll();
-        final setting = await _settingDatastore.fetch();
-        final latestPillSheetGroup =
-            await _pillSheetGroupDatastore.fetchLatest();
-        final diaries = await _diaryDatastore.fetchListForMonth(
-            state.calendarDataSource[state.todayCalendarIndex]);
-        final pillSheetModifiedHistories =
-            await _pillSheetModifiedHistoryDatastore.fetchList(
-                null,
-                CalendarPillSheetModifiedHistoryCardState
-                        .pillSheetModifiedHistoriesThreshold +
-                    1);
-        final user = await _userDatastore.fetch();
-        state = state.copyWith(
-          menstruations: menstruations,
-          setting: setting,
-          latestPillSheetGroup: latestPillSheetGroup,
-          diariesForMonth: diaries,
-          allPillSheetModifiedHistories: pillSheetModifiedHistories,
-          isNotYetLoaded: false,
-          isPremium: user.isPremium,
-          isTrial: user.isTrial,
-          trialDeadlineDate: user.trialDeadlineDate,
-        );
-        _subscribe();
-      });
-    } catch (exception) {
-      state = state.copyWith(exception: exception);
-    }
-  }
-
-  StreamSubscription? _menstruationCanceller;
-  StreamSubscription? _settingCanceller;
-  StreamSubscription? _latestPillSheetGroupCanceller;
-  StreamSubscription? _diariesCanceller;
-  StreamSubscription? _pillSheetModifiedHistoryCanceller;
-  StreamSubscription? _userCanceller;
-  void _subscribe() {
-    _menstruationCanceller?.cancel();
-    _menstruationCanceller =
-        _menstruationDatastore.streamAll().listen((entities) {
-      state = state.copyWith(menstruations: entities);
-    });
-    _settingCanceller?.cancel();
-    _settingCanceller = _settingDatastore.stream().listen((entity) {
-      state = state.copyWith(setting: entity);
-    });
-    _latestPillSheetGroupCanceller?.cancel();
-    _latestPillSheetGroupCanceller =
-        _pillSheetGroupDatastore.latestPillSheetGroupStream().listen((entity) {
-      state = state.copyWith(latestPillSheetGroup: entity);
-    });
-    _diariesCanceller?.cancel();
-    _diariesCanceller = _diaryDatastore.stream().listen((entities) {
-      state = state.copyWith(diariesForMonth: entities);
-    });
-    _pillSheetModifiedHistoryCanceller?.cancel();
-    _pillSheetModifiedHistoryCanceller = _pillSheetModifiedHistoryDatastore
-        .stream(CalendarPillSheetModifiedHistoryCardState
-                .pillSheetModifiedHistoriesThreshold +
-            1)
-        .listen((event) {
-      state = state.copyWith(allPillSheetModifiedHistories: event);
-    });
-    _userCanceller?.cancel();
-    _userCanceller = _userDatastore.stream().listen((event) {
-      state = state.copyWith(
-        isPremium: event.isPremium,
-        isTrial: event.isTrial,
-        trialDeadlineDate: event.trialDeadlineDate,
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    _menstruationCanceller?.cancel();
-    _settingCanceller?.cancel();
-    _latestPillSheetGroupCanceller?.cancel();
-    _diariesCanceller?.cancel();
-    _pillSheetModifiedHistoryCanceller?.cancel();
-    _userCanceller?.cancel();
-    super.dispose();
-  }
+  CalendarPageStateStore({
+    required this.asyncAction,
+    required AsyncValue<CalendarPageState> initialState,
+  }) : super(initialState);
 
   void updateCurrentCalendarIndex(int index) {
-    if (state.currentCalendarIndex == index) {
+    final value = state.asData?.value;
+    if (value == null) {
       return;
     }
-    state = state.copyWith(currentCalendarIndex: index);
-    final date = state.calendarDataSource[state.currentCalendarIndex];
-    _diaryDatastore.fetchListForMonth(date).then((diaries) {
-      final ignoredSameMonth = state.diariesForMonth
-          .where((element) => !isSameMonth(element.date, date))
-          .toList();
-      final updated = ignoredSameMonth..addAll(diaries);
-      state = state.copyWith(diariesForMonth: updated);
-    });
-  }
+    if (value.currentCalendarIndex == index) {
+      return;
+    }
+    state = AsyncValue.data(value.copyWith(currentCalendarIndex: index));
 
-  CalendarCardState cardState(DateTime date) => CalendarCardState(date);
-
-  Future<void> editTakenValue(
-    DateTime actualTakenDate,
-    PillSheetModifiedHistory history,
-    PillSheetModifiedHistoryValue value,
-    TakenPillValue takenPillValue,
-  ) {
-    return updateForEditTakenValue(
-      service: _pillSheetModifiedHistoryDatastore,
-      actualTakenDate: actualTakenDate,
-      history: history,
-      value: value,
-      takenPillValue: takenPillValue,
-    );
+    // TODO: Remove or move to asyncAction
+    //   final date = state.calendarDataSource[state.currentCalendarIndex];
+    //   _diaryDatastore.fetchListForMonth(date).then((diaries) {
+    //     final ignoredSameMonth = state.diariesForMonth
+    //         .where((element) => !isSameMonth(element.date, date))
+    //         .toList();
+    //     final updated = ignoredSameMonth..addAll(diaries);
+    //     state = state.copyWith(diariesForMonth: updated);
+    //   });
   }
 }
