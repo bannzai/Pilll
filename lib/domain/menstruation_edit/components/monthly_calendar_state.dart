64b7e55f8db881f58365316a791c0c0ceffe214b
bannzai <kingkong999yhirose@gmail.com>
2022-04-29 22:03:51 +0900

    Remove unnecessary state

--- lib/domain/menstruation_edit/components/monthly_calendar_state.dart
+++
@@ -1,8 +0,0 @@
-import 'package:pilll/components/organisms/calendar/monthly/calendar_state.dart';
-import 'package:pilll/entity/menstruation.codegen.dart';
-
-class MenstruationEditCalendarState extends MonthlyCalendarState {
-  final DateTime dateForMonth;
-  final Menstruation? menstruation;
-  MenstruationEditCalendarState(this.dateForMonth, this.menstruation);
-}
