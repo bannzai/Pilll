d6ebaf5ff209b60533133cd6b6f6dcabfb31e4dc
bannzai <kingkong999yhirose@gmail.com>
2022-04-29 14:52:27 +0900

    Add providers

---
+++ lib/components/organisms/calendar/band/calendar_band_provider.dart
@@ -0,0 +1,75 @@
+import 'package:pilll/components/organisms/calendar/band/calendar_band_function.dart';
+import 'package:pilll/components/organisms/calendar/band/calendar_band_model.dart';
+import 'package:pilll/components/organisms/calendar/band/calendar_menstruation_band.dart';
+import 'package:pilll/database/menstruation.dart';
+import 'package:pilll/database/pill_sheet_group.dart';
+import 'package:pilll/database/setting.dart';
+import 'package:riverpod/riverpod.dart';
+
+final calendarMenstruationBandListProvider =
+    Provider<AsyncValue<List<CalendarMenstruationBandModel>>>((ref) {
+  final allMenstruations = ref.watch(allMenstruationStreamProvider);
+
+  if (allMenstruations is AsyncLoading) {
+    return const AsyncValue.loading();
+  }
+
+  try {
+    return AsyncValue.data(
+      allMenstruations.value!
+          .map((menstruation) => CalendarMenstruationBandModel(menstruation))
+          .toList(),
+    );
+  } catch (error, _) {
+    return AsyncValue.error(error);
+  }
+});
+
+final calendarScheduledMenstruationBandListProvider =
+    Provider<AsyncValue<List<CalendarScheduledMenstruationBandModel>>>((ref) {
+  final allMenstruations = ref.watch(allMenstruationStreamProvider);
+  final pillSheetGroup = ref.watch(latestPillSheetGroupStreamProvider);
+  final setting = ref.watch(settingStreamProvider);
+
+  if (allMenstruations is AsyncLoading ||
+      pillSheetGroup is AsyncLoading ||
+      setting is AsyncLoading) {
+    return const AsyncValue.loading();
+  }
+
+  try {
+    return AsyncValue.data(
+      scheduledOrInTheMiddleMenstruationDateRanges(
+        pillSheetGroup.value!,
+        setting.value!,
+        allMenstruations.value!,
+        15,
+      )
+          .map((dateRange) => CalendarScheduledMenstruationBandModel(
+              dateRange.begin, dateRange.end))
+          .toList(),
+    );
+  } catch (error, _) {
+    return AsyncValue.error(error);
+  }
+});
+
+final calendarNextPillSheetBandListProvider =
+    Provider<AsyncValue<List<CalendarScheduledMenstruationBandModel>>>((ref) {
+  final pillSheetGroup = ref.watch(latestPillSheetGroupStreamProvider);
+
+  if (pillSheetGroup is AsyncLoading) {
+    return const AsyncValue.loading();
+  }
+
+  try {
+    return AsyncValue.data(
+      nextPillSheetDateRanges(pillSheetGroup.value!, 15)
+          .map((dateRange) => CalendarScheduledMenstruationBandModel(
+              dateRange.begin, dateRange.end))
+          .toList(),
+    );
+  } catch (error, _) {
+    return AsyncValue.error(error);
+  }
+});
