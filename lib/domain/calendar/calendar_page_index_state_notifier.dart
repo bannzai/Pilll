600c81c66f2a7ed46d3ff4967f552a39be88a0eb
bannzai <kingkong999yhirose@gmail.com>
2022-04-30 16:37:29 +0900

    Prefetch

--- lib/domain/calendar/calendar_page_index_state_notifier.dart
+++ lib/domain/calendar/calendar_page_index_state_notifier.dart
@@ -1,20 +1,29 @@
+import 'package:pilll/database/diary.dart';
 import 'package:pilll/domain/calendar/calendar_page_state.codegen.dart';
 
 import 'package:riverpod/riverpod.dart';
 
 final calendarPageIndexStateNotifierProvider =
     StateNotifierProvider<CalendarPageIndexStateNotifier, int>(
-  (ref) => CalendarPageIndexStateNotifier(todayCalendarPageIndex),
+  (ref) => CalendarPageIndexStateNotifier(
+      todayCalendarPageIndex, ref.watch(diaryDatastoreProvider)),
 );
 
 class CalendarPageIndexStateNotifier extends StateNotifier<int> {
+  final DiaryDatastore _diaryDatastore;
   CalendarPageIndexStateNotifier(
     int initialState,
+    this._diaryDatastore,
   ) : super(initialState);
 
   void set(int index) {
-    if (state != index) {
-      state = index;
+    if (state == index) {
+      return;
     }
+
+    // Prefetch diary data
+    _diaryDatastore.fetchListForMonth(calendarDataSource[index]);
+
+    state = index;
   }
 }
