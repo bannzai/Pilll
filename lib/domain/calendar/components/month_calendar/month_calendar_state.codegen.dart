bc494f47289342a64cc3aa8ac0404e9d5e780a3c
bannzai <kingkong999yhirose@gmail.com>
2022-04-30 16:51:51 +0900

    Remove unnecessary import

--- lib/domain/calendar/components/month_calendar/month_calendar_state.codegen.dart
+++ lib/domain/calendar/components/month_calendar/month_calendar_state.codegen.dart
@@ -2,11 +2,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:pilll/components/organisms/calendar/weekly/utility.dart';
 import 'package:pilll/database/diary.dart';
 import 'package:pilll/database/menstruation.dart';
-import 'package:pilll/domain/calendar/date_range.dart';
 import 'package:pilll/domain/calendar/week_calendar_state.dart';
 import 'package:pilll/entity/diary.codegen.dart';
 import 'package:pilll/entity/menstruation.codegen.dart';
-import 'package:pilll/entity/weekday.dart';
 import 'package:riverpod/riverpod.dart';
 
 part 'month_calendar_state.codegen.freezed.dart';
