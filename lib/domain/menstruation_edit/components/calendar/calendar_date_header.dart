affd2064f860bf441eda24d17d207a8a664b7d53
bannzai <kingkong999yhirose@gmail.com>
2022-04-30 16:20:42 +0900

    Revert "Remove unnecessary file"
    This reverts commit a4bfb966f65f485338e06e7c3dc4f0d3d6e1b024.

---
+++ lib/domain/menstruation_edit/components/calendar/calendar_date_header.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+import 'package:pilll/util/formatter/date_time_formatter.dart';
+
+abstract class CalendarDateHeaderConst {
+  static double headerHeight = 64;
+}
+
+class CalendarDateHeader extends StatelessWidget {
+  final DateTime date;
+  const CalendarDateHeader({
+    Key? key,
+    required this.date,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints:
+          BoxConstraints.expand(height: CalendarDateHeaderConst.headerHeight),
+      child: Row(
+        children: [
+          const SizedBox(width: 16),
+          Text(
+            DateTimeFormatter.yearAndMonth(date),
+            textAlign: TextAlign.left,
+            style: FontType.cardHeader.merge(TextColorStyle.noshime),
+          ),
+          const SizedBox(width: 16),
+        ],
+      ),
+    );
+  }
+}
