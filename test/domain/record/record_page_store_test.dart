70dfaf8877cbc46fa584ea8389546d6ef68b3be2
bannzai <kingkong999yhirose@gmail.com>
2022-05-01 16:57:40 +0900

    Remove unnecessary code

--- test/domain/record/record_page_store_test.dart
+++ test/domain/record/record_page_store_test.dart
@@ -8,7 +8,6 @@ import 'package:pilll/entity/pill_sheet_type.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:pilll/entity/setting.codegen.dart';
-import 'package:pilll/provider/premium_and_trial.codegen.dart';
 import 'package:pilll/service/day.dart';
 import 'package:pilll/database/pill_sheet.dart';
 import 'package:pilll/database/pill_sheet_group.dart';
