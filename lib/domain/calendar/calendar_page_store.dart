4715b1060c2c4a7d68a1b0ec9733fd05eb27d94e
bannzai <kingkong999yhirose@gmail.com>
2022-05-01 16:10:11 +0900

    Remove unnecessary method

--- lib/domain/calendar/calendar_page_store.dart
+++ lib/domain/calendar/calendar_page_store.dart
@@ -18,26 +18,4 @@ class CalendarPageStateStore
     required this.asyncAction,
     required AsyncValue<CalendarPageState> initialState,
   }) : super(initialState);
-
-  // TODO: Remove
-//  void updateCurrentCalendarIndex(int index) {
-//    final value = state.asData?.value;
-//    if (value == null) {
-//      return;
-//    }
-//    if (value.currentCalendarIndex == index) {
-//      return;
-//    }
-//    state = AsyncValue.data(value.copyWith(currentCalendarIndex: index));
-//
-//    // TODO: Remove or move to asyncAction
-//    //   final date = state.calendarDataSource[state.currentCalendarIndex];
-//    //   _diaryDatastore.fetchListForMonth(date).then((diaries) {
-//    //     final ignoredSameMonth = state.diariesForMonth
-//    //         .where((element) => !isSameMonth(element.date, date))
-//    //         .toList();
-//    //     final updated = ignoredSameMonth..addAll(diaries);
-//    //     state = state.copyWith(diariesForMonth: updated);
-//    //   });
-//  }
 }
--- lib/domain/record/components/supports/components/appearance_mode/switching_appearance_mode.dart
+++ lib/domain/record/components/supports/components/appearance_mode/switching_appearance_mode.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:pilll/analytics.dart';
 import 'package:pilll/components/atoms/font.dart';
