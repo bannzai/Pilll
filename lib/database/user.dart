93aabc05aab5d2fd569fe981ef8f27b119ea2552
bannzai <kingkong999yhirose@gmail.com>
2022-06-22 21:29:16 +0900

    Fill useTimeZoneOffset after initial setting

--- lib/database/user.dart
+++ lib/database/user.dart
@@ -163,6 +163,7 @@ class UserDatastore {
           now().add(const Duration(days: 30)),
       UserFirestoreFieldKeys.settings: settingForTrial.toJson(),
       UserFirestoreFieldKeys.hasDiscountEntitlement: true,
+      UserFirestoreFieldKeys.useTimeZoneOffset: true,
     }, SetOptions(merge: true));
   }
 
