8b6508b1f7dcdb28830fe126c1e30ecd450636c2
bannzai <kingkong999yhirose@gmail.com>
2022-07-10 20:09:01 +0900

    Fix delaytime

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -174,9 +174,9 @@ extension AppDelegate {
                 // application(_:didFinishLaunchingWithOptions:)が終了してからFlutterのmainの開始は非同期的でFlutterのmainの完了までラグがある
                 // 特にアプリのプロセスがKillされている状態では、先にuserNotificationCenter(_:didReceive:withCompletionHandler:)の処理が走り
                 // Flutter側でのMethodChannelが確立される前にQuickRecordの呼び出しをおこなってしまう。この場合次にChanelが確立するまでFlutter側の処理の実行は遅延される。これは次のアプリの起動時まで遅延されるとほぼ同義になる
-                // よって対処療法的ではあるが、5秒待つことでほぼ間違いなくmain(の中でもMethodChanelの確立までは)の処理はすべて終えているとしてここではdelayを設けている。
+                // よって対処療法的ではあるが、3秒待つことでほぼ間違いなくmain(の中でもMethodChanelの確立までは)の処理はすべて終えているとしてここではdelayを設けている。
                 // ちなみに通常は1秒前後あれば十分であるが念のためくらいの間を持たせている
-                DispatchQueue.main.asyncAfter(deadline: .now() + 5) { [self] in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [self] in
                     channel?.invokeMethod("recordPill", arguments: nil, result: { result in
                         end()
                     })
