d1caaf1a5667513e5c71527f5bb68a1fb9b91b60
bannzai <kingkong999yhirose@gmail.com>
2022-07-12 07:38:14 +0900

    Avoid warnings

--- lib/entity/pill_sheet_type.dart
+++ lib/entity/pill_sheet_type.dart
@@ -16,6 +16,7 @@ enum PillSheetType {
   // "21錠タイプ(すべて実薬)";
   pillsheet_21_0,
   // "24錠+4日休薬";
+  // ignore: constant_identifier_names
   pillsheet_24_rest_4,
 }
 
--- lib/entity/weekday.dart
+++ lib/entity/weekday.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/widgets.dart';
 import 'package:pilll/components/atoms/color.dart';
 
