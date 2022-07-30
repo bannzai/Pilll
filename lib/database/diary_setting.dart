dc5617afd471f987bb6ffdca87ae917c9f10aca4
bannzai <kingkong999yhirose@gmail.com>
2022-07-30 20:42:18 +0900

    to nullable

--- lib/database/diary_setting.dart
+++ lib/database/diary_setting.dart
@@ -3,4 +3,4 @@ import 'package:pilll/database/database.dart';
 import 'package:pilll/entity/diary_setting.codegen.dart';
 
 final diarySettingStreamProvider =
-    StreamProvider<DiarySetting>((ref) => ref.watch(databaseProvider).diarySettingReference().snapshots().);
+    StreamProvider<DiarySetting?>((ref) => ref.watch(databaseProvider).diarySettingReference().snapshots().map((event) => event.data()));
