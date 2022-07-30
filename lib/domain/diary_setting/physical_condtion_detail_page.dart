d3e14cbe36ec7b3338a0fcb7eafbec6a8a7d3afb
bannzai <kingkong999yhirose@gmail.com>
2022-07-30 20:18:21 +0900

    Add diary_setting dir

---
+++ lib/domain/diary_setting/physical_condtion_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class DiarySettingPhysicalConditionDetailPage extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
