6e24043b7b0a8092a3cc67852ee73847e559190d
bannzai <kingkong999yhirose@gmail.com>
2022-08-14 12:12:15 +0900

    add isMigratedSharedKeychaim method

--- lib/native/ios_keychain_migration.dart
+++ lib/native/ios_keychain_migration.dart
@@ -1,5 +1,10 @@
 import 'package:pilll/native/channel.dart';
 
+Future<bool> isMigratedSharedKeychain() async {
+  final result = await methodChannel.invokeMethod("isMigratedSharedKeychain");
+  return result["isMigratedSharedKeychain"] == true;
+}
+
 Future<bool> iOSKeychainMigrateToSharedKeychain() async {
   final result = await methodChannel.invokeMethod("iOSKeychainMigrateToSharedKeychain");
   return result["iOSKeychainMigrateToSharedKeychain"] == true;
