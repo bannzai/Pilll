3627629331f4ea32f631263b49abb4c957dab42b
bannzai <kingkong999yhirose@gmail.com>
2022-08-20 17:08:46 +0900

    Remove unnecessary dependencies

--- ios/Widget/WidgetBundle.swift
+++ ios/Widget/WidgetBundle.swift
@@ -1,19 +1,8 @@
-import FirebaseAuth
-import FirebaseCore
 import SwiftUI
 import WidgetKit
 
 @main
 struct Widgets: WidgetBundle {
-
-    init() {
-        FirebaseApp.configure()
-
-        let teamID = "TQPN82UBBY"
-        let keychainAccessGroup: String = "\(teamID).\(Bundle.main.bundleIdentifier!).keychain"
-        try! Auth.auth().useUserAccessGroup(keychainAccessGroup)
-    }
-
     var body: some Widget {
         SmallWidget()
         MediumWidget()
