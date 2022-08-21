ac93890471fd2d9a070f108c617bb73b059d9ca7
bannzai <kingkong999yhirose@gmail.com>
2022-08-21 21:10:10 +0900

    Add no premium user pattern

--- ios/Widget/Widget.swift
+++ ios/Widget/Widget.swift
@@ -63,87 +63,107 @@ struct WidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            HStack(alignment: .top) {
-                VStack(spacing: 0) {
-                    Text(weekday)
-                        .foregroundColor(.mainText)
-                        .font(.system(size: 12))
-                        .fontWeight(.medium)
+        if UserDefaults(suiteName: Const.appGroupKey)?.bool(forKey: Const.userIsPremiumOrTrial) == true {
+            VStack {
+                HStack(alignment: .top) {
+                    VStack(spacing: 0) {
+                        Text(weekday)
+                            .foregroundColor(.mainText)
+                            .font(.system(size: 12))
+                            .fontWeight(.medium)
 
-                    Text("\(day)")
-                        .foregroundColor(.black)
-                        .font(.system(size: 36))
-                        .fontWeight(.regular)
+                        Text("\(day)")
+                            .foregroundColor(.black)
+                            .font(.system(size: 36))
+                            .fontWeight(.regular)
+                    }
+
+                    Spacer()
+
+                    Image("pilll-widget-icon")
+                        .frame(width: 11, height: 16)
                 }
 
                 Spacer()
 
-                Image("pilll-widget-icon")
-                    .frame(width: 11, height: 16)
-            }
+                if let lastTakenPillNumber = entry.lastTakenPillNumber, let todayPillNumber = entry.todayPillNumber {
+                    let alreadyTaken = lastTakenPillNumber == todayPillNumber
+                    HStack {
+                        HStack(spacing: 6) {
+                            Divider()
+                                .frame(width: 4)
+                                .overlay(alreadyTaken ? Color.primary : Color.orange)
+                                .cornerRadius(2)
 
-            Spacer()
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text(entry.pilllNumberDisplayMode == "date" ? "\(todayPillNumber)日目" : "\(todayPillNumber)番")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 15))
+                                    .fontWeight(.semibold)
 
-            if let lastTakenPillNumber = entry.lastTakenPillNumber, let todayPillNumber = entry.todayPillNumber {
-                let alreadyTaken = lastTakenPillNumber == todayPillNumber
-                HStack {
-                    HStack(spacing: 6) {
-                        Divider()
-                            .frame(width: 4)
-                            .overlay(alreadyTaken ? Color.primary : Color.orange)
-                            .cornerRadius(2)
-
-                        VStack(alignment: .leading, spacing: 2) {
-                            Text(entry.pilllNumberDisplayMode == "date" ? "\(todayPillNumber)日目" : "\(todayPillNumber)番")
-                                .foregroundColor(.black)
-                                .font(.system(size: 15))
-                                .fontWeight(.semibold)
-
-                            Text(alreadyTaken ? "服用済み" : "未服用")
-                                .foregroundColor(.mainText)
-                                .font(.system(size: 12))
-                                .fontWeight(.medium)
+                                Text(alreadyTaken ? "服用済み" : "未服用")
+                                    .foregroundColor(.mainText)
+                                    .font(.system(size: 12))
+                                    .fontWeight(.medium)
+                            }
                         }
+
+                        Spacer()
+
+                        Image(alreadyTaken ? "check-icon-on.svg" : "check-icon-off.svg")
+                            .frame(width: 32, height: 32)
+                            .shadow(color: Color(red: 78 / 255, green: 98 / 255, blue: 135 / 255, opacity: 0.4), radius: 5, x: 0, y: 2)
                     }
+                    .frame(maxHeight: 40)
+                } else {
+                    HStack {
+                        HStack(spacing: 6) {
+                            Divider()
+                                .frame(width: 4)
+                                .overlay(Color(red: 190 / 255, green: 192 / 255, blue: 194 / 255))
+                                .cornerRadius(2)
 
-                    Spacer()
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text(entry.pilllNumberDisplayMode == "date" ? "- 日目" : "- 番")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 15))
+                                    .fontWeight(.semibold)
 
-                    Image(alreadyTaken ? "check-icon-on.svg" : "check-icon-off.svg")
-                        .frame(width: 32, height: 32)
-                        .shadow(color: Color(red: 78 / 255, green: 98 / 255, blue: 135 / 255, opacity: 0.4), radius: 5, x: 0, y: 2)
-                }
-                .frame(maxHeight: 40)
-            } else {
-                HStack {
-                    HStack(spacing: 6) {
-                        Divider()
-                            .frame(width: 4)
-                            .overlay(Color(red: 190 / 255, green: 192 / 255, blue: 194 / 255))
-                            .cornerRadius(2)
-
-                        VStack(alignment: .leading, spacing: 2) {
-                            Text(entry.pilllNumberDisplayMode == "date" ? "- 日目" : "- 番")
-                                .foregroundColor(.black)
-                                .font(.system(size: 15))
-                                .fontWeight(.semibold)
-
-                            Text("シートがありません")
-                                .foregroundColor(.orange)
-                                .font(.system(size: 12))
-                                .fontWeight(.medium)
-                                .background(Color(red: 230 / 255, green: 94 / 255, blue: 90 / 255, opacity: 0.1))
+                                Text("シートがありません")
+                                    .foregroundColor(.orange)
+                                    .font(.system(size: 12))
+                                    .fontWeight(.medium)
+                                    .background(Color(red: 230 / 255, green: 94 / 255, blue: 90 / 255, opacity: 0.1))
+                            }
                         }
-                    }
 
-                    Spacer()
+                        Spacer()
+                    }
+                    .frame(maxHeight: 40)
                 }
-                .frame(maxHeight: 40)
             }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 15)
+            .background(Color.white)
+        } else {
+            VStack {
+                HStack {
+                    Spacer()
+                    Image("pilll-widget-icon")
+                        .frame(width: 11, height: 16, alignment: .topTrailing)
+                        .padding(.horizontal, 8)
+                }
+                Spacer()
+                Text("プレミアムユーザーのみご利用できます")
+                    .foregroundColor(.gray)
+
+                Spacer()
+            }
+            .padding(.horizontal, 8)
+            .padding(.vertical, 15)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
         }
-        .padding(.horizontal, 16)
-        .padding(.vertical, 15)
-        .background(Color.white)
     }
 
     private var weekday: String {
@@ -164,18 +184,15 @@ extension Color {
 
 @main
 struct Entrypoint: Widget {
-    let kind: String = "Widget"
+    let kind: String = "com.mizuki.Ohashi.Pilll.widget"
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             WidgetEntryView(entry: entry)
         }
-        .supportedFamilies(supportedFamilies)
+        .supportedFamilies([.systemSmall])
+        .configurationDisplayName("今日飲むピルが一目でわかる")
         .description("This is an Pilll widget")
-    }
-
-    private var supportedFamilies: [WidgetFamily] {
-        UserDefaults(suiteName: Const.appGroupKey)?.bool(forKey: Const.userIsPremiumOrTrial) == true ? [.systemSmall] : []
     }
 }
 
