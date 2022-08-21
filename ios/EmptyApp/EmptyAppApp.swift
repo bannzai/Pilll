dae413665a35d9e69b35d853d88db4ebcd41f32e
bannzai <kingkong999yhirose@gmail.com>
2022-08-21 17:19:15 +0900

    Add empty widget

---
+++ ios/EmptyApp/EmptyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyAppApp.swift
+//  EmptyApp
+//
+//  Created by 廣瀬雄大 on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct EmptyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
