b2a37233b7708d743efa44fa439d0ec428ce6cd1
bannzai <kingkong999yhirose@gmail.com>
2022-09-02 07:17:36 +0900

    Remove unnecessary state

--- ios/Widget/Widget.swift
+++ ios/Widget/Widget.swift
@@ -93,50 +93,28 @@ struct WidgetEntryView : View {
 
         Spacer()
 
-        if let alreadyTaken = alreadyTaken {
-          HStack {
-            HStack(spacing: 6) {
-              Divider()
-                .frame(width: 4)
-                .overlay(alreadyTaken ? Color.primary : Color.orange)
-                .cornerRadius(2)
+        HStack {
+          HStack(spacing: 6) {
+            Divider()
+              .frame(width: 4)
+              .overlay(alreadyTaken ? Color.primary : Color.orange)
+              .cornerRadius(2)
 
-              VStack(alignment: .leading, spacing: 2) {
-                Text(alreadyTaken ? "服用済み" : "未服用")
-                  .foregroundColor(.mainText)
-                  .font(.system(size: 12))
-                  .fontWeight(.medium)
-              }
+            VStack(alignment: .leading, spacing: 2) {
+              Text(alreadyTaken ? "服用済み" : "未服用")
+                .foregroundColor(.mainText)
+                .font(.system(size: 12))
+                .fontWeight(.medium)
             }
-
-            Spacer()
-
-            Image(alreadyTaken ? "check-icon-on" : "check-icon-off")
-              .frame(width: 32, height: 32)
-              .shadow(color: Color(red: 78 / 255, green: 98 / 255, blue: 135 / 255, opacity: 0.4), radius: 5, x: 0, y: 2)
           }
-          .frame(maxHeight: 40)
-        } else {
-          HStack {
-            HStack(spacing: 6) {
-              Divider()
-                .frame(width: 4)
-                .overlay(Color(red: 190 / 255, green: 192 / 255, blue: 194 / 255))
-                .cornerRadius(2)
 
-              VStack(alignment: .leading, spacing: 2) {
-                Text("シートがありません")
-                  .foregroundColor(.orange)
-                  .font(.system(size: 12))
-                  .fontWeight(.medium)
-                  .background(Color(red: 230 / 255, green: 94 / 255, blue: 90 / 255, opacity: 0.1))
-              }
-            }
+          Spacer()
 
-            Spacer()
-          }
-          .frame(maxHeight: 40)
+          Image(alreadyTaken ? "check-icon-on" : "check-icon-off")
+            .frame(width: 32, height: 32)
+            .shadow(color: Color(red: 78 / 255, green: 98 / 255, blue: 135 / 255, opacity: 0.4), radius: 5, x: 0, y: 2)
         }
+        .frame(maxHeight: 40)
       }
       .padding(.horizontal, 16)
       .padding(.vertical, 15)
@@ -170,9 +148,9 @@ struct WidgetEntryView : View {
     calendar.component(.day, from: entry.date)
   }
 
-  private var alreadyTaken: Bool? {
+  private var alreadyTaken: Bool {
     guard let pillSheetLastTakenDate = entry.pillSheetLastTakenDate else {
-      return nil
+      return false
     }
     return calendar.isDate(entry.date, inSameDayAs: pillSheetLastTakenDate)
   }
