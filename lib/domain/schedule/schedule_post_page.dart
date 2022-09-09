8a6aafd7892da184bd79a2c1d460affbe0780049
bannzai <kingkong999yhirose@gmail.com>
2022-09-09 20:57:46 +0900

    add page

---
+++ lib/domain/schedule/schedule_post_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SchedulePostPage extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
