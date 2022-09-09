0a5e090d913d2eea647eeea9047f1c42485fa570
bannzai <kingkong999yhirose@gmail.com>
2022-09-09 22:26:10 +0900

    Add state

---
+++ lib/domain/schedule_post/state.codegen.dart
@@ -0,0 +1,37 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pilll/database/user.dart';
+import 'package:pilll/provider/premium_and_trial.codegen.dart';
+import 'package:pilll/provider/shared_preference.dart';
+
+part 'state.codegen.freezed.dart';
+
+final schedulePostAsyncStateProvider = Provider.autoDispose<AsyncValue<SchedulePostState>>((ref) {
+  final user = ref.watch(userStreamProvider);
+  final premiumAndTrial = ref.watch(premiumAndTrialProvider);
+  final sharedPreferencesAsyncValue = ref.watch(sharedPreferenceProvider);
+
+  if (user is AsyncLoading || premiumAndTrial is AsyncLoading || sharedPreferencesAsyncValue is AsyncLoading) {
+    return const AsyncValue.loading();
+  }
+
+  try {
+    final sharedPreferences = sharedPreferencesAsyncValue.value!;
+
+    return AsyncValue.data(SchedulePostState(
+      user: user,
+      premiumAndTrial: premiumAndTrial,
+    ));
+  } catch (error, stackTrace) {
+    return AsyncValue.error(error, stackTrace: stackTrace);
+  }
+});
+
+@freezed
+class SchedulePostState with _$SchedulePostState {
+  factory SchedulePostState({
+    required User user,
+    required PremiumAndTrial premiumAndTrial,
+  }) = _SchedulePostState;
+  SchedulePostState._();
+}
