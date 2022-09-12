39727df834aa3346aecfcbd26f14a92412cf6679
bannzai <kingkong999yhirose@gmail.com>
2022-09-13 06:07:55 +0900

    Remove unnecessary method

--- lib/database/diary.dart
+++ lib/database/diary.dart
@@ -6,8 +6,6 @@ import 'package:pilll/util/datetime/day.dart';
 
 final diaryDatastoreProvider = Provider<DiaryDatastore>((ref) => DiaryDatastore(ref.watch(databaseProvider)));
 
-final diariesStreamProvider = StreamProvider((ref) => ref.watch(diaryDatastoreProvider).stream());
-
 final diariesStreamForMonthProvider = StreamProvider.family((ref, DateTime dateForMonth) {
   final range = dateForMonth.dateTimeRange();
   return ref
@@ -23,7 +21,17 @@ final diariesStreamForMonthProvider = StreamProvider.family((ref, DateTime dateF
       .map((diaries) => sortedDiaries(diaries));
 });
 
-final diariesStreamAround90Days = StreamProvider.family((ref, DateTime base) => ref.watch(diaryDatastoreProvider).streamForAround90Days(base: base));
+final diariesStreamAround90Days = StreamProvider.family((ref, DateTime base) {
+  return ref
+      .watch(databaseProvider)
+      .diariesReference()
+      .where(DiaryFirestoreKey.date,
+          isLessThanOrEqualTo: DateTime(base.year, base.month, 90), isGreaterThanOrEqualTo: DateTime(base.year, base.month, -90))
+      .orderBy(DiaryFirestoreKey.date)
+      .snapshots()
+      .map((event) => event.docs.map((e) => e.data()).toList())
+      .map((diaries) => sortedDiaries(diaries));
+});
 
 int sortDiary(Diary a, Diary b) => a.date.compareTo(b.date);
 List<Diary> sortedDiaries(List<Diary> diaries) {
@@ -36,50 +44,21 @@ class DiaryDatastore {
 
   DiaryDatastore(this._database);
 
-  Future<List<Diary>> fetchListAround90Days(DateTime base) {
-    return _database
-        .diariesReference()
-        .where(DiaryFirestoreKey.date,
-            isLessThanOrEqualTo: DateTime(base.year, base.month, 90), isGreaterThanOrEqualTo: DateTime(base.year, base.month, -90))
-        .orderBy(DiaryFirestoreKey.date)
-        .get()
-        .then((event) => event.docs.map((e) => e.data()).toList());
-  }
-
-  Future<List<Diary>> fetchListForMonth(DateTime dateTimeOfMonth) {
-    return _database
-        .diariesReference()
-        .where(DiaryFirestoreKey.date,
-            isLessThanOrEqualTo: DateTime(dateTimeOfMonth.year, dateTimeOfMonth.month + 1, 0),
-            isGreaterThanOrEqualTo: DateTime(dateTimeOfMonth.year, dateTimeOfMonth.month, 1))
-        .orderBy(DiaryFirestoreKey.date)
-        .get()
-        .then((event) => event.docs.map((e) => e.data()).toList());
-  }
-
+  // TODO: Remove
   Future<Diary> register(Diary diary) {
     return _database.diaryReference(diary).set(diary, SetOptions(merge: true)).then((_) => diary);
   }
 
+  // TODO: Remove
   Future<Diary> update(Diary diary) {
     return _database.diaryReference(diary).set(diary, SetOptions(merge: true)).then((_) => diary);
   }
 
+  // TODO: Remove
   Future<Diary> delete(Diary diary) {
     return _database.diaryReference(diary).delete().then((_) => diary);
   }
 
   Stream<List<Diary>> stream() =>
       _database.diariesReference().snapshots().map((event) => event.docs.map((e) => e.data()).toList()).map((diaries) => sortedDiaries(diaries));
-
-  Stream<List<Diary>> streamForAround90Days({required DateTime base}) {
-    return _database
-        .diariesReference()
-        .where(DiaryFirestoreKey.date,
-            isLessThanOrEqualTo: DateTime(base.year, base.month, 90), isGreaterThanOrEqualTo: DateTime(base.year, base.month, -90))
-        .orderBy(DiaryFirestoreKey.date)
-        .snapshots()
-        .map((event) => event.docs.map((e) => e.data()).toList())
-        .map((diaries) => sortedDiaries(diaries));
-  }
 }
