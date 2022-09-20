2ca07198f5d8fd16d9e8aa871ac01722849668ff
bannzai <kingkong999yhirose@gmail.com>
2022-09-20 15:24:55 +0900

    Fix notification bar state

--- lib/domain/record/components/notification_bar/state_notifier.dart
+++ lib/domain/record/components/notification_bar/state_notifier.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 
-import 'package:pilll/database/pill_sheet_group.dart';
 import 'package:pilll/domain/record/components/notification_bar/notification_bar_state.codegen.dart';
 import 'package:pilll/domain/record/record_page_state_notifier.dart';
-import 'package:pilll/provider/premium_and_trial.codegen.dart';
 import 'package:pilll/provider/shared_preference.dart';
 import 'package:pilll/service/auth.dart';
 import 'package:pilll/util/shared_preference/keys.dart';
@@ -16,34 +14,24 @@ final notificationBarStateNotifierProvider = StateNotifierProvider.autoDispose<N
   ),
 );
 final notificationBarStateProvider = Provider.autoDispose((ref) {
-  final latestPillSheetGroup = ref.watch(latestPillSheetGroupStreamProvider);
-  final premiumAndTrial = ref.watch(premiumAndTrialProvider);
+  final recordPageState = ref.watch(recordPageStateNotifierProvider).value!;
+  final sharedPreferences = ref.watch(sharedPreferenceProvider).value!;
 
-  final sharedPreferencesAsyncValue = ref.watch(sharedPreferenceProvider);
-  if (latestPillSheetGroup is AsyncLoading || premiumAndTrial is AsyncLoading || sharedPreferencesAsyncValue is AsyncLoading) {
-    return const AsyncValue.loading();
-  }
-
-  try {
-    final sharedPreferences = sharedPreferencesAsyncValue.value!;
-    return NotificationBarState(
-      latestPillSheetGroup: latestPillSheetGroup.value,
-      totalCountOfActionForTakenPill: sharedPreferences.getInt(IntKey.totalCountOfActionForTakenPill) ?? 0,
-      premiumAndTrial: premiumAndTrial.value!,
-      isLinkedLoginProvider: ref.watch(isLinkedProvider),
-      recommendedSignupNotificationIsAlreadyShow: sharedPreferences.getBool(BoolKey.recommendedSignupNotificationIsAlreadyShow) ?? false,
-      premiumTrialBeginAnouncementIsClosed: sharedPreferences.getBool(BoolKey.premiumTrialBeginAnouncementIsClosed) ?? false,
-      premiumUserIsClosedAdsMederiPill: sharedPreferences.getBool(BoolKey.premiumUserIsClosedAdsMederiPill) ?? false,
-      userAnsweredSurvey: sharedPreferences.getBool(BoolKey.userAnsweredSurvey) ?? false,
-      userClosedSurvey: sharedPreferences.getBool(BoolKey.userClosedSurvey) ?? false,
-    );
-  } catch (error, stackTrace) {
-    return AsyncValue.error(error, stackTrace: stackTrace);
-  }
+  return NotificationBarState(
+    latestPillSheetGroup: recordPageState.pillSheetGroup,
+    totalCountOfActionForTakenPill: sharedPreferences.getInt(IntKey.totalCountOfActionForTakenPill) ?? 0,
+    premiumAndTrial: recordPageState.premiumAndTrial,
+    isLinkedLoginProvider: ref.watch(isLinkedProvider),
+    recommendedSignupNotificationIsAlreadyShow: sharedPreferences.getBool(BoolKey.recommendedSignupNotificationIsAlreadyShow) ?? false,
+    premiumTrialBeginAnouncementIsClosed: sharedPreferences.getBool(BoolKey.premiumTrialBeginAnouncementIsClosed) ?? false,
+    premiumUserIsClosedAdsMederiPill: sharedPreferences.getBool(BoolKey.premiumUserIsClosedAdsMederiPill) ?? false,
+    userAnsweredSurvey: sharedPreferences.getBool(BoolKey.userAnsweredSurvey) ?? false,
+    userClosedSurvey: sharedPreferences.getBool(BoolKey.userClosedSurvey) ?? false,
+  );
 });
 
-class NotificationBarStateNotifier extends StateNotifier<AsyncValue<NotificationBarState>> {
-  NotificationBarStateNotifier(AsyncValue<NotificationBarState> state) : super(state);
+class NotificationBarStateNotifier extends StateNotifier<NotificationBarState> {
+  NotificationBarStateNotifier(NotificationBarState state) : super(state);
 
   Future<void> closeRecommendedSignupNotification() async {
     final sharedPreferences = await SharedPreferences.getInstance();
