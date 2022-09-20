0990b3204c208cf8006a0a5258d1de0f18a5686b
bannzai <kingkong999yhirose@gmail.com>
2022-09-20 15:50:51 +0900

    :art: Fix

--- lib/domain/record/components/notification_bar/components/user_survey.dart
+++ lib/domain/record/components/notification_bar/components/user_survey.dart
@@ -19,7 +19,7 @@ class UserSurvey extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       color: PilllColors.secondary,
       child: GestureDetector(
         onTap: () async {
