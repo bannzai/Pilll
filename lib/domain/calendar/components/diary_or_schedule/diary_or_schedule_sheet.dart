e23281f007a34babc1e77f125ec698ca8d254625
bannzai <kingkong999yhirose@gmail.com>
2022-09-21 20:33:53 +0900

    add diary or schedule sheet

---
+++ lib/domain/calendar/components/diary_or_schedule/diary_or_schedule_sheet.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+
+const double _tileHeight = 48;
+
+class DiaryOrScheduleSheet extends StatelessWidget {
+  final VoidCallback showDiary;
+  final VoidCallback showSchedule;
+
+  const DiaryOrScheduleSheet({Key? key, required this.showDiary, required this.showSchedule}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 24),
+            _tile(title: "日記を記録", onTap: () => showDiary()),
+            _tile(title: "予定を記入", onTap: () => showSchedule()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _tile({required String title, required VoidCallback onTap}) {
+    return SizedBox(
+      height: _tileHeight,
+      child: ListTile(
+        title: Text(
+          title,
+          style: FontType.assisting.merge(TextColorStyle.main),
+        ),
+        onTap: () => onTap(),
+      ),
+    );
+  }
+}
