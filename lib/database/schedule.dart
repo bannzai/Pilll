d7494c9ca7c74ae492605e0595deebe087cf2ed7
bannzai <kingkong999yhirose@gmail.com>
2022-09-22 15:09:41 +0900

    To stream provider

--- lib/database/schedule.dart
+++ lib/database/schedule.dart
@@ -3,9 +3,9 @@ import 'package:pilll/entity/schedule.codegen.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:pilll/util/datetime/day.dart';
 
-final schedulesForDateProvider = FutureProvider.family((ref, DateTime date) async {
+final schedulesForDateProvider = StreamProvider.family((ref, DateTime date) {
   final range = date.dateTimeRange();
-  return await ref
+  return ref
       .watch(databaseProvider)
       .schedulesReference()
       .where(
@@ -13,13 +13,13 @@ final schedulesForDateProvider = FutureProvider.family((ref, DateTime date) asyn
         isGreaterThanOrEqualTo: range.start,
         isLessThanOrEqualTo: range.end,
       )
-      .get()
-      .then((value) => value.docs.map((e) => e.data()).toList());
+      .snapshots()
+      .map((event) => event.docs.map((e) => e.data()).toList());
 });
 
-final schedulesForMonthProvider = FutureProvider.family((ref, DateTime dateForMonth) async {
+final schedulesForMonthProvider = StreamProvider.family((ref, DateTime dateForMonth) {
   final range = MonthDateTimeRange.monthRange(dateForMonth: dateForMonth);
-  return await ref
+  return ref
       .watch(databaseProvider)
       .schedulesReference()
       .where(
@@ -27,8 +27,8 @@ final schedulesForMonthProvider = FutureProvider.family((ref, DateTime dateForMo
         isGreaterThanOrEqualTo: range.start,
         isLessThanOrEqualTo: range.end,
       )
-      .get()
-      .then((value) => value.docs.map((e) => e.data()).toList());
+      .snapshots()
+      .map((event) => event.docs.map((e) => e.data()).toList());
 });
 
 final schedulesAround90Days = StreamProvider.family((ref, DateTime base) {
