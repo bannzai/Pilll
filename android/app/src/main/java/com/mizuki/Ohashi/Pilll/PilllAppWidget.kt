11f1f8853ef15083686412d48370f3e0c116cee9
bannzai <kingkong999yhirose@gmail.com>
2022-10-12 21:51:16 +0900

    WIP fix update views method

--- android/app/src/main/java/com/mizuki/Ohashi/Pilll/PilllAppWidget.kt
+++ android/app/src/main/java/com/mizuki/Ohashi/Pilll/PilllAppWidget.kt
@@ -3,17 +3,17 @@ package com.mizuki.Ohashi.Pilll
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.view.View
 import android.widget.RemoteViews
+import org.threeten.bp.LocalDate
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 /**
  * Implementation of App Widget functionality.
  */
 class PilllAppWidget : AppWidgetProvider() {
-    override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
-    ) {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -29,15 +29,23 @@ class PilllAppWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
+internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
     val views = RemoteViews(context.packageName, R.layout.pilll_app_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    val now = LocalDate.now()
+
+    val format = SimpleDateFormat("EEEE");
+    val dayOfWeekName = format.format(now)
+    views.setTextViewText(R.id.widget_weekday, dayOfWeekName)
+
+    val day = now.dayOfMonth
+    views.setTextViewText(R.id.widget_day, day.toString())
+
+    // TODO:
+    views.setViewVisibility(R.id.widget_check_on, View.INVISIBLE)
+
+    // TODO:
+    views.setTextViewText(R.id.widget_todayPillNumber, "10番")
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
