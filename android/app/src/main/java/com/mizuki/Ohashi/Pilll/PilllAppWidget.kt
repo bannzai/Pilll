04565074e5b3fb1938c97293f4f55123aee83500
bannzai <kingkong999yhirose@gmail.com>
2022-10-15 14:41:23 +0900

    Extract to Const

--- android/app/src/main/java/com/mizuki/Ohashi/Pilll/PilllAppWidget.kt
+++ android/app/src/main/java/com/mizuki/Ohashi/Pilll/PilllAppWidget.kt
@@ -13,20 +13,6 @@ import java.text.SimpleDateFormat
  * Implementation of App Widget functionality.
  */
 class PilllAppWidget : AppWidgetProvider() {
-    companion object {
-        const val userIsPremiumOrTrial = "userIsPremiumOrTrial"
-
-        const val pillSheetGroupTodayPillNumber = "pillSheetGroupTodayPillNumber"
-        const val pillSheetTodayPillNumber = "pillSheetTodayPillNumber"
-        const val pillSheetEndDisplayPillNumber = "pillSheetEndDisplayPillNumber"
-        // Epoch milli second
-        const val pillSheetLastTakenDate = "pillSheetLastTakenDate"
-        // Epoch milli second
-        const val pillSheetValueLastUpdateDateTime = "pillSheetValueLastUpdateDateTime"
-
-        const val settingPillSheetAppearanceMode = "settingPillSheetAppearanceMode" // number or date or sequential
-
-    }
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
---
+++ android/app/src/main/kotlin/com/example/Pilll/Const.kt
@@ -0,0 +1,14 @@
+package com.example.Pilll
+
+const val userIsPremiumOrTrial = "userIsPremiumOrTrial"
+
+const val pillSheetGroupTodayPillNumber = "pillSheetGroupTodayPillNumber"
+const val pillSheetTodayPillNumber = "pillSheetTodayPillNumber"
+const val pillSheetEndDisplayPillNumber = "pillSheetEndDisplayPillNumber"
+// Epoch milli second
+const val pillSheetLastTakenDate = "pillSheetLastTakenDate"
+// Epoch milli second
+const val pillSheetValueLastUpdateDateTime = "pillSheetValueLastUpdateDateTime"
+
+const val settingPillSheetAppearanceMode = "settingPillSheetAppearanceMode" // number or date or sequential
+
