cc7b3df792ccedf13fce792e784e174e932e4665
bannzai <kingkong999yhirose@gmail.com>
2022-10-23 17:05:54 +0900

    Remove unnecessary code

--- lib/database/pilll_ads.dart
+++ lib/database/pilll_ads.dart
@@ -1,5 +1,4 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/database/database.dart';
-import 'package:pilll/util/stream.dart';
 
 final pilllAdsProvider = StreamProvider((ref) => ref.watch(databaseProvider).pilllAds().snapshots().map((event) => event.data()));
