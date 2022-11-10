177a01d868232752b77696758ecac26ea822424a
bannzai <kingkong999yhirose@gmail.com>
2022-11-10 22:09:56 +0900

    Add string provider and to nullable

--- lib/provider/shared_preferences.dart
+++ lib/provider/shared_preferences.dart
@@ -3,44 +3,62 @@ import 'package:pilll/provider/shared_preference.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-final boolSharedPreferencesProvider = AsyncNotifierProvider.family<BoolSharedPreferences, bool, String>(() => BoolSharedPreferences());
+final boolSharedPreferencesProvider = AsyncNotifierProvider.family<BoolSharedPreferences, bool?, String>(() => BoolSharedPreferences());
 
-class BoolSharedPreferences extends FamilyAsyncNotifier<bool, String> {
+class BoolSharedPreferences extends FamilyAsyncNotifier<bool?, String> {
   late String key;
   late SharedPreferences sharedPreferences;
 
-  bool get value => state.asData?.value ?? false;
-  set value(bool value) {
+  void set(bool value) {
     sharedPreferences.setBool(key, value);
     // NOTE: Allow recreate AsyncNotifier everytime. Not call update((_) => value), Keep SSoT with fetching data via shared_preferences
     ref.invalidateSelf();
   }
 
   @override
-  FutureOr<bool> build(String arg) async {
+  FutureOr<bool?> build(String arg) async {
     key = arg;
     sharedPreferences = await ref.watch(sharedPreferenceProvider.future);
-    return sharedPreferences.getBool(key) ?? false;
+    return sharedPreferences.getBool(key);
   }
 }
 
-final intSharedPreferencesProvider = AsyncNotifierProvider.family<BoolSharedPreferences, int, String>(() => IntSharedPreferences());
+final intSharedPreferencesProvider = AsyncNotifierProvider.family<IntSharedPreferences, int, String>(() => IntSharedPreferences());
 
-class IntSharedPreferences extends FamilyAsyncNotifier<int, String> {
+class IntSharedPreferences extends FamilyAsyncNotifier<int?, String> {
   late String key;
   late SharedPreferences sharedPreferences;
 
-  int get value => state.asData?.value ?? 0;
-  set value(int value) {
+  void set(int value) {
     sharedPreferences.setInt(key, value);
     // NOTE: Allow recreate AsyncNotifier everytime. Not call update((_) => value), Keep SSoT with fetching data via shared_preferences
     ref.invalidateSelf();
   }
 
   @override
-  FutureOr<int> build(String arg) async {
+  FutureOr<int?> build(String arg) async {
     key = arg;
     sharedPreferences = await ref.watch(sharedPreferenceProvider.future);
-    return sharedPreferences.getInt(key) ?? 0;
+    return sharedPreferences.getInt(key);
+  }
+}
+
+final stringSharedPreferencesProvider = AsyncNotifierProvider.family<StringSharedPreferences, String?, String>(() => StringSharedPreferences());
+
+class StringSharedPreferences extends FamilyAsyncNotifier<String?, String> {
+  late String key;
+  late SharedPreferences sharedPreferences;
+
+  void set(String value) {
+    sharedPreferences.setString(key, value);
+    // NOTE: Allow recreate AsyncNotifier everytime. Not call update((_) => value), Keep SSoT with fetching data via shared_preferences
+    ref.invalidateSelf();
+  }
+
+  @override
+  FutureOr<String?> build(String arg) async {
+    key = arg;
+    sharedPreferences = await ref.watch(sharedPreferenceProvider.future);
+    return sharedPreferences.getString(key);
   }
 }
