16fb4d957db881bf30029e5f0a612e369733e6aa
bannzai <kingkong999yhirose@gmail.com>
2022-11-10 21:57:15 +0900

    add int provider

--- lib/provider/shared_preferences.dart
+++ lib/provider/shared_preferences.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-
-import 'package:flutter/material.dart';
 import 'package:pilll/provider/shared_preference.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -23,5 +21,26 @@ class BoolSharedPreferences extends FamilyAsyncNotifier<bool, String> {
     key = arg;
     sharedPreferences = await ref.watch(sharedPreferenceProvider.future);
     return sharedPreferences.getBool(key) ?? false;
+  }
+}
+
+final intSharedPreferencesProvider = AsyncNotifierProvider.family<BoolSharedPreferences, int, String>(() => IntSharedPreferences());
+
+class IntSharedPreferences extends FamilyAsyncNotifier<int, String> {
+  late String key;
+  late SharedPreferences sharedPreferences;
+
+  int get value => state.asData?.value ?? 0;
+  set value(int value) {
+    sharedPreferences.setInt(key, value);
+    // NOTE: Allow recreate AsyncNotifier everytime. Not call update((_) => value), Keep SSoT with fetching data via shared_preferences
+    ref.invalidateSelf();
+  }
+
+  @override
+  FutureOr<int> build(String arg) async {
+    key = arg;
+    sharedPreferences = await ref.watch(sharedPreferenceProvider.future);
+    return sharedPreferences.getInt(key) ?? 0;
   }
 }
