70286253fe4047baf1efe0edab1ea649d731a47c
bannzai <kingkong999yhirose@gmail.com>
2022-11-14 22:04:42 +0900

    Delete SettingMenstrautionState

--- lib/domain/settings/menstruation/setting_menstruation_state.codegen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'setting_menstruation_state.codegen.freezed.dart';
-
-@freezed
-class SettingMenstruationState with _$SettingMenstruationState {
-  const SettingMenstruationState._();
-  const factory SettingMenstruationState() = _SettingMenstruationState;
-}
