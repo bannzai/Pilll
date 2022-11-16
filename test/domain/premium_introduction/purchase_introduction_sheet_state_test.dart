5d7b254582137446c0556a2070d38f6c5a38aba9
bannzai <kingkong999yhirose@gmail.com>
2022-11-16 20:48:46 +0900

    Fix test case

--- test/domain/premium_introduction/purchase_introduction_sheet_state_test.dart
+++ test/domain/premium_introduction/purchase_introduction_sheet_state_test.dart
@@ -1,11 +1,28 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/analytics.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pilll/domain/premium_introduction/util/discount_deadline.dart';
+import 'package:pilll/provider/premium_and_trial.codegen.dart';
+import 'package:pilll/service/purchase.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../helper/mock.mocks.dart';
 
 class _FakeOfferings extends Fake implements Offerings {}
+
+class _FakePremiumAndTrial extends Fake implements PremiumAndTrial {
+  _FakePremiumAndTrial({
+    required this.fakeHasDiscountEntitlement,
+  });
+  final bool fakeHasDiscountEntitlement;
+
+  @override
+  bool get hasDiscountEntitlement => fakeHasDiscountEntitlement;
+
+  @override
+  DateTime? get discountEntitlementDeadlineDate => null; // any value
+}
 
 void main() {
   setUp(() async {
@@ -15,28 +32,43 @@ void main() {
   });
   group("#offeringType", () {
     test("when hasDiscountEntitlement = false should return premium", () async {
-      final state = PremiumIntroductionState(
-        offerings: _FakeOfferings(),
-        hasDiscountEntitlement: false,
-        isOverDiscountDeadline: false,
+      final premiumAndTrial = _FakePremiumAndTrial(
+        fakeHasDiscountEntitlement: false,
       );
-      expect(state.currentOfferingType, equals(OfferingType.premium));
+      final container = ProviderContainer(
+        overrides: [
+          isOverDiscountDeadlineProvider.overrideWith((ref, arg) => false),
+          purchaseOfferingsProvider.overrideWith((ref) => _FakeOfferings()),
+        ],
+      );
+      final currentOfferingType = container.read(currentOfferingTypeProvider(premiumAndTrial));
+      expect(currentOfferingType, equals(OfferingType.premium));
     });
     test("when isOverDiscountDeadline = true should return premium", () async {
-      final state = PremiumIntroductionState(
-        offerings: _FakeOfferings(),
-        hasDiscountEntitlement: true,
-        isOverDiscountDeadline: true,
+      final premiumAndTrial = _FakePremiumAndTrial(
+        fakeHasDiscountEntitlement: false,
       );
-      expect(state.currentOfferingType, equals(OfferingType.premium));
+      final container = ProviderContainer(
+        overrides: [
+          isOverDiscountDeadlineProvider.overrideWith((ref, arg) => true),
+          purchaseOfferingsProvider.overrideWith((ref) => _FakeOfferings()),
+        ],
+      );
+      final currentOfferingType = container.read(currentOfferingTypeProvider(premiumAndTrial));
+      expect(currentOfferingType, equals(OfferingType.premium));
     });
     test("should return limited", () async {
-      final state = PremiumIntroductionState(
-        offerings: _FakeOfferings(),
-        hasDiscountEntitlement: true,
-        isOverDiscountDeadline: false,
+      final premiumAndTrial = _FakePremiumAndTrial(
+        fakeHasDiscountEntitlement: true,
       );
-      expect(state.currentOfferingType, equals(OfferingType.limited));
+      final container = ProviderContainer(
+        overrides: [
+          isOverDiscountDeadlineProvider.overrideWith((ref, arg) => false),
+          purchaseOfferingsProvider.overrideWith((ref) => _FakeOfferings()),
+        ],
+      );
+      final currentOfferingType = container.read(currentOfferingTypeProvider(premiumAndTrial));
+      expect(currentOfferingType, equals(OfferingType.limited));
     });
   });
 }
