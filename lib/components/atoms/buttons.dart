de8b629a573ad5c1136785e17e72c4493fd58b0b
bannzai <kingkong999yhirose@gmail.com>
2022-11-20 09:15:47 +0900

    Fix

--- lib/components/atoms/buttons.dart
+++ lib/components/atoms/buttons.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:pilll/components/atoms/button.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
