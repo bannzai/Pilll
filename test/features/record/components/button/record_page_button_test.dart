66f04e8fd3a47d951e2f914d15431075acb551af
bannzai <kingkong999yhirose@gmail.com>
2022-11-20 13:44:15 +0900

    Fix

--- test/features/record/components/button/record_page_button_test.dart
+++ test/features/record/components/button/record_page_button_test.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:pilll/features/record/components/button/cancel_button.dart';
 import 'package:pilll/features/record/components/button/record_page_button.dart';
 import 'package:pilll/features/record/components/button/rest_duration_button.dart';
@@ -7,40 +5,18 @@ import 'package:pilll/features/record/components/button/taken_button.dart';
 import 'package:pilll/provider/revert_take_pill.dart';
 import 'package:pilll/provider/take_pill.dart';
 import 'package:pilll/utils/analytics.dart';
-import 'package:pilll/provider/pill_sheet_group.dart';
-import 'package:pilll/provider/pilll_ads.dart';
-import 'package:pilll/features/premium_introduction/util/discount_deadline.dart';
-import 'package:pilll/features/record/components/announcement_bar/components/discount_price_deadline.dart';
-import 'package:pilll/features/record/components/announcement_bar/components/ended_pill_sheet.dart';
-import 'package:pilll/features/record/components/announcement_bar/components/pilll_ads.dart';
-import 'package:pilll/features/record/components/announcement_bar/notification_bar.dart';
-import 'package:pilll/features/record/components/announcement_bar/components/premium_trial_limit.dart';
-import 'package:pilll/features/record/components/announcement_bar/components/recommend_signup.dart';
-import 'package:pilll/features/record/components/announcement_bar/components/recommend_signup_premium.dart';
-import 'package:pilll/features/record/components/announcement_bar/components/rest_duration.dart';
-import 'package:pilll/entity/pill_sheet.codegen.dart';
-import 'package:pilll/entity/pill_sheet_group.codegen.dart';
-import 'package:pilll/entity/pill_sheet_type.dart';
-import 'package:pilll/entity/pilll_ads.codegen.dart';
-import 'package:pilll/provider/locale.dart';
-import 'package:pilll/provider/premium_and_trial.codegen.dart';
-import 'package:pilll/provider/auth.dart';
 import 'package:pilll/utils/datetime/day.dart';
-import 'package:pilll/utils/emoji/emoji.dart';
 import 'package:pilll/utils/environment.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mockito/mockito.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:pilll/utils/shared_preference/keys.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../helper/factory.dart';
 import '../../../../helper/mock.mocks.dart';
 
 void main() {
-  const totalCountOfActionForTakenPillForLongTimeUser = 14;
   setUp(() {
     TestWidgetsFlutterBinding.ensureInitialized();
     initializeDateFormatting('ja_JP');
