0c86d67550578e5c088980ff66755f35ee6b4f53
bannzai <kingkong999yhirose@gmail.com>
2022-11-20 10:48:10 +0900

    Delete TextColorStyle

--- lib/components/atoms/text_color.dart
+++ lib/components/atoms/text_color.dart
@@ -19,18 +19,3 @@ abstract class TextColor {
   static Color mediumEmphasis(Color color) => color.withOpacity(0.6);
   static Color disabled(Color color) => color.withOpacity(0.37);
 }
-
-class TextColorStyle {
-  static TextStyle get standard => black;
-  static const TextStyle black = TextStyle(color: TextColor.black);
-  static const TextStyle white = TextStyle(color: TextColor.white);
-  static const TextStyle darkGray = TextStyle(color: TextColor.darkGray);
-  static const TextStyle gray = TextStyle(color: TextColor.gray);
-  static const TextStyle lightGray = TextStyle(color: TextColor.lightGray);
-  static const TextStyle lightGray2 = TextStyle(color: TextColor.lightGray2);
-  static const TextStyle noshime = TextStyle(color: TextColor.noshime);
-  static const TextStyle primary = TextStyle(color: TextColor.primary);
-  static const TextStyle main = TextStyle(color: TextColor.main);
-  static const TextStyle link = TextStyle(color: TextColor.link);
-  static const TextStyle danger = TextStyle(color: TextColor.danger);
-}
