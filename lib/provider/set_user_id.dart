78ff431f8afdf815f00d9fed92bb0aac49c904d0
bannzai <kingkong999yhirose@gmail.com>
2022-11-20 14:24:06 +0900

    add set user id provider

---
+++ lib/provider/set_user_id.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:pilll/provider/purchase.dart';
+import 'package:pilll/utils/analytics.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+import 'package:riverpod/riverpod.dart';
+
+final serUserIDProvider = Provider((_) => SetUserID());
+
+class SetUserID {
+  Future<void> call({required String userID}) async {
+    unawaited(FirebaseCrashlytics.instance.setUserIdentifier(userID));
+    unawaited(firebaseAnalytics.setUserId(id: userID));
+
+    // Keep call initialPurchase before logIn.
+    await initializePurchase(userID);
+    unawaited(Purchases.logIn(userID));
+  }
+}
