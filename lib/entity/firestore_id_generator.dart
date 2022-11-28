f5f8cf06de7fd297feb7933ef2184058cffe0751
bannzai <kingkong999yhirose@gmail.com>
2022-11-28 21:23:47 +0900

    Revert "Revert "Add Firestore ID Generator""
    This reverts commit 5dfaf39948e70833a98d7a72281b7e86e507dcfb.

---
+++ lib/entity/firestore_id_generator.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uuid/uuid.dart';
+
+class FirestoreIDGenerator {
+  String call() => const Uuid().v4();
+}
+
+FirestoreIDGenerator firestoreIDGenerator = FirestoreIDGenerator();
+
+class IDDefault implements Default {
+  const IDDefault();
+
+  @override
+  String get defaultValue => firestoreIDGenerator();
+}
