b7ebd4c1a0c940ebe5c89303971f7031137a573e
bannzai <kingkong999yhirose@gmail.com>
2022-11-29 22:14:29 +0900

    Fix

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pilll/provider/shared_preferences.dart';
 import 'package:pilll/utils/analytics.dart';
 import 'package:pilll/utils/shared_preference/keys.dart';
 import 'package:flutter/material.dart';
@@ -8,22 +9,10 @@ class AppRouter {
 // NOTE: This method call after user end all initialSetting
 // OR user signed with 3rd party provider
 // So, Don't forget when this function is edited. Both test necessary .
-  static void endInitialSetting(BuildContext context) {
+  static void endInitialSetting(BuildContext context, BoolSharedPreferences didEndInitialSettingNotifier) {
     analytics.logEvent(name: "end_initial_setting");
-    SharedPreferences.getInstance().then((storage) {
-      storage.setBool(BoolKey.didEndInitialSetting, true);
-      Navigator.popUntil(context, (router) => router.isFirst);
-// TODO: Remove
-//      requestNotificationPermissions().then((value) {
-//        listenNotificationEvents();
-//        // rootKey.currentState?.showHome();
-//      });
-    });
-  }
-
-  static void signInAccount(BuildContext context) {
-    analytics.logEvent(name: "initial_setting_signin_account", parameters: {"uid": FirebaseAuth.instance.currentUser?.uid});
-    return endInitialSetting(context);
+    didEndInitialSettingNotifier.set(true);
+    Navigator.popUntil(context, (router) => router.isFirst);
   }
 
   static Future<void> routeToInitialSetting(BuildContext context) async {
