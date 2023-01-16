f3799cbded3e9268dfe5fb024eaba4c06b71c036
bannzai <kingkong999yhirose@gmail.com>
2023-01-17 08:56:01 +0900

    set didEndInitialSetting false for debug menu

--- lib/main.dev.dart
+++ lib/main.dev.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:pilll/entrypoint.dart';
 import 'package:pilll/utils/environment.dart';
+import 'package:pilll/utils/shared_preference/keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   Environment.flavor = Flavor.DEVELOP;
@@ -8,12 +10,14 @@ Future<void> main() async {
     if (!Environment.isDevelopment) {
       throw AssertionError("This method should not call out of development");
     }
+    (await SharedPreferences.getInstance()).setBool(BoolKey.didEndInitialSetting, false);
     await FirebaseAuth.instance.currentUser?.delete();
   };
   Environment.signOutUser = () async {
     if (!Environment.isDevelopment) {
       throw AssertionError("This method should not call out of development");
     }
+    (await SharedPreferences.getInstance()).setBool(BoolKey.didEndInitialSetting, false);
     await FirebaseAuth.instance.signOut();
   };
   await entrypoint();
