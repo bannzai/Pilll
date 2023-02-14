302c3047ec7aaf85328a2d9e0359290fa3ad764a
bannzai <kingkong999yhirose@gmail.com>
2023-02-15 07:55:55 +0900

    Fix

--- lib/features/initial_setting/initial_setting_state.codegen.freezed.dart
+++ lib/features/initial_setting/initial_setting_state.codegen.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'initial_setting_state.codegen.dart';
 
@@ -372,6 +372,7 @@ class _$_InitialSettingState extends _InitialSettingState {
   @override
   @JsonKey()
   List<PillSheetType> get pillSheetTypes {
+    if (_pillSheetTypes is EqualUnmodifiableListView) return _pillSheetTypes;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_pillSheetTypes);
   }
@@ -381,6 +382,7 @@ class _$_InitialSettingState extends _InitialSettingState {
   final List<ReminderTime> _reminderTimes;
   @override
   List<ReminderTime> get reminderTimes {
+    if (_reminderTimes is EqualUnmodifiableListView) return _reminderTimes;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_reminderTimes);
   }
