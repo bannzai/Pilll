15dc3d2e35d1e3a8f38b20b237d071e73e469244
bannzai <kingkong999yhirose@gmail.com>
2023-04-09 13:58:53 +0900

    Add month validation

--- lib/features/record/components/announcement_bar/announcement_bar.dart
+++ lib/features/record/components/announcement_bar/announcement_bar.dart
@@ -97,17 +97,19 @@ class AnnouncementBar extends HookConsumerWidget {
       }
 
       // TODO: 値上げと同時に消す
-      if (!priceUpAnnouncementIsAlreadyShow) {
-        return PriceUpAnnouncementBar(
-          onTap: () {
-            analytics.logEvent(name: "tapped_price_up_bar");
-            launchUrl(Uri.parse("https://pilll.wraptas.site/bfce3a94f6bf44258e134c2aa69dbb6d"), mode: LaunchMode.inAppWebView);
-          },
-          onClose: () {
-            analytics.logEvent(name: "close_price_up_bar");
-            priceUpAnnouncementIsAlreadyShowNotifier.set(true);
-          },
-        );
+      if (DateTime.now().month < 6) {
+        if (!priceUpAnnouncementIsAlreadyShow) {
+          return PriceUpAnnouncementBar(
+            onTap: () {
+              analytics.logEvent(name: "tapped_price_up_bar");
+              launchUrl(Uri.parse("https://pilll.wraptas.site/bfce3a94f6bf44258e134c2aa69dbb6d"), mode: LaunchMode.inAppWebView);
+            },
+            onClose: () {
+              analytics.logEvent(name: "close_price_up_bar");
+              priceUpAnnouncementIsAlreadyShowNotifier.set(true);
+            },
+          );
+        }
       }
 
       final premiumTrialLimit = PremiumTrialLimitAnnouncementBar.retrievePremiumTrialLimit(premiumAndTrial);
