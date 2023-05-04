70efef57f8bec149959984d056062d5918bce76b
bannzai <kingkong999yhirose@gmail.com>
2023-05-04 21:09:50 +0900

    Fix force update URL

--- lib/features/root/root_page.dart
+++ lib/features/root/root_page.dart
@@ -93,7 +93,7 @@ class RootPage extends HookConsumerWidget {
         await showOKDialog(context, title: "アプリをアップデートしてください", message: "お使いのアプリのバージョンのアップデートをお願いしております。$storeNameから最新バージョンにアップデートしてください",
             ok: () async {
           await launchUrl(
-            Uri.parse(storeURL),
+            Uri.parse(forceUpdateStoreURL),
             mode: LaunchMode.externalApplication,
           );
         });
