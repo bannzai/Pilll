450edfa16ab917b1933d0d0af3ea14f3fff179c2
bannzai <kingkong999yhirose@gmail.com>
2023-06-07 15:58:54 +0900

    Revert "Fix"
    This reverts commit bc523adfb4117b9194d9cfdda767dfe539d21382.

--- test/features/record/record_page_button_state_test.dart
+++ test/features/record/record_page_button_state_test.dart
@@ -31,10 +31,13 @@ void main() {
   group('appearance taken button type', () {
     testWidgets('today pill not taken', (WidgetTester tester) async {
       final yesterday = today().subtract(const Duration(days: 1));
-      final pillSheet = PillSheet.create(
-        PillSheetType.pillsheet_21,
-        beginDate: yesterday,
+      final pillSheet = PillSheet(
+        id: firestoreIDGenerator(),
+        typeInfo: PillSheetType.pillsheet_21.typeInfo,
+        beginingDate: yesterday,
         lastTakenDate: yesterday,
+        createdAt: now(),
+        pills: Pill.generateAndFillTo(pillSheetType: PillSheetType.pillsheet_21, toDate: yesterday),
       );
 
       await tester.pumpWidget(
@@ -56,10 +59,13 @@ void main() {
     });
   });
   testWidgets('today pill is already taken', (WidgetTester tester) async {
-    final pillSheet = PillSheet.create(
-      PillSheetType.pillsheet_21,
-      beginDate: today(),
+    final pillSheet = PillSheet(
+      id: firestoreIDGenerator(),
+      typeInfo: PillSheetType.pillsheet_21.typeInfo,
+      beginingDate: today(),
       lastTakenDate: today(),
+      createdAt: now(),
+      pills: Pill.generateAndFillTo(pillSheetType: PillSheetType.pillsheet_21, toDate: today()),
     );
 
     expect(true, pillSheet.todayPillsAreAlreadyTaken);
