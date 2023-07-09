d2107de6a21cb7f6df237b66908d74f4e446fee7
bannzai <kingkong999yhirose@gmail.com>
2023-07-09 10:20:28 +0900

    Add component

--- lib/components/template/setting_pill_sheet_group/setting_pill_sheet_group.dart
+++ lib/components/template/setting_pill_sheet_group/setting_pill_sheet_group.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/components/template/setting_pill_sheet_group/pill_sheet_type_add_button.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pilll/components/template/setting_pill_sheet_group/setting_pill_sheet_group_pill_sheet_type_select_row.dart';
 import 'package:pilll/entity/pill_sheet_type.dart';
 
-class SettingPillSheetGroup extends HookConsumerWidget {
+class SettingPillSheetGroup extends StatelessWidget {
   const SettingPillSheetGroup({
     Key? key,
     required this.pillSheetTypes,
@@ -16,13 +15,12 @@ class SettingPillSheetGroup extends HookConsumerWidget {
   }) : super(key: key);
 
   final List<PillSheetType> pillSheetTypes;
-  final Function(PillSheetType, bool) onAdd;
-  final Function(int, PillSheetType, bool) onChange;
+  final Function(PillSheetType) onAdd;
+  final Function(int, PillSheetType) onChange;
   final Function(int) onDelete;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final pillSheetIsTwoTaken = useState(false);
+  Widget build(BuildContext context) {
     return Column(
       children: [
         for (var i = 0; i < pillSheetTypes.length; i++) ...[
@@ -30,7 +28,7 @@ class SettingPillSheetGroup extends HookConsumerWidget {
           SettingPillSheetGroupPillSheetTypeSelectRow(
             index: i,
             pillSheetType: pillSheetTypes[i],
-            onSelect: (index, pillSheetType) => onChange(index, pillSheetType, pillSheetIsTwoTaken.value),
+            onSelect: onChange,
             onDelete: onDelete,
           ),
         ],
@@ -38,16 +36,26 @@ class SettingPillSheetGroup extends HookConsumerWidget {
           const SizedBox(height: 24),
           PillSheetTypeAddButton(
             pillSheetTypes: pillSheetTypes,
-            onAdd: (pillSheetType) => onAdd(pillSheetType, pillSheetIsTwoTaken.value),
+            onAdd: (pillSheetType) => onAdd(pillSheetType),
           ),
         ],
-        Row(children: [
-          const Text("1日に2回服用する"),
-          const Spacer(),
-          Switch(value: pillSheetIsTwoTaken.value, onChanged: (value) => pillSheetIsTwoTaken.value = value)
-        ]),
         const SizedBox(height: 80),
       ],
     );
+  }
+}
+
+class SettingPillSheetIsTwoTakenToggle extends HookConsumerWidget {
+  final bool initialValue;
+  final Function(bool) onChanged;
+  const SettingPillSheetIsTwoTakenToggle({Key? key, required this.initialValue, required this.onChanged}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Row(children: [
+      const Text("1日に2回服用する"),
+      const Spacer(),
+      Switch(value: initialValue, onChanged: onChanged),
+    ]);
   }
 }
