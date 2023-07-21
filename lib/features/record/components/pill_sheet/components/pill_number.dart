f4fbdac5aae76b40fa42f92f4d7029036df681c6
bannzai <kingkong999yhirose@gmail.com>
2023-07-21 15:57:26 +0900

    Fix import

--- lib/features/record/components/pill_sheet/components/pill_number.dart
+++ lib/features/record/components/pill_sheet/components/pill_number.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/font.dart';
-import 'package:pilll/entity/pill_sheet_group.codegen.dart';
-import 'package:pilll/utils/formatter/date_time_formatter.dart';
 
 class PlainPillNumber extends StatelessWidget {
   final String text;
