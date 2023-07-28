53c69651b392a31a43684f0b4d56a28ee355f4fd
bannzai <kingkong999yhirose@gmail.com>
2023-07-29 07:26:52 +0900

    fix test/features/menstruation/menstruation_store_test.dart

--- test/features/menstruation/menstruation_store_test.dart
+++ test/features/menstruation/menstruation_store_test.dart
@@ -3,7 +3,6 @@ import 'package:mockito/mockito.dart';
 import 'package:pilll/components/organisms/calendar/band/calendar_band_function.dart';
 import 'package:pilll/components/organisms/calendar/band/calendar_band_model.dart';
 import 'package:pilll/entity/firestore_id_generator.dart';
-import 'package:pilll/entity/pill.codegen.dart';
 import 'package:pilll/features/menstruation/components/menstruation_card_list.dart';
 import 'package:pilll/features/menstruation/menstruation_card_state.codegen.dart';
 import 'package:pilll/entity/menstruation.codegen.dart';
@@ -125,8 +124,6 @@ void main() {
               beginingDate: DateTime(2021, 04, 22),
               lastTakenDate: null,
               createdAt: now(),
-              pills: Pill.testGenerateAndIterateTo(
-                  pillSheetType: PillSheetType.pillsheet_21, fromDate: DateTime(2021, 04, 22), lastTakenDate: null, pillTakenCount: 1),
             ),
           ],
           createdAt: now(),
@@ -169,8 +166,6 @@ void main() {
               beginingDate: DateTime(2021, 04, 07),
               lastTakenDate: null,
               createdAt: now(),
-              pills: Pill.testGenerateAndIterateTo(
-                  pillSheetType: PillSheetType.pillsheet_21, fromDate: DateTime(2021, 04, 07), lastTakenDate: null, pillTakenCount: 1),
             ),
           ],
           createdAt: now(),
