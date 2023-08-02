d2dd4fb5f2097c4d8eefbeff71918fb19516b323
bannzai <kingkong999yhirose@gmail.com>
2023-08-03 08:56:25 +0900

    fix about churn

--- lib/features/settings/components/rows/about_churn.dart
+++ lib/features/settings/components/rows/about_churn.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/utils/analytics.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class AboutChurn extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return ListTile(
+      title: const Text("解約について",
+          style: TextStyle(
+            fontFamily: FontFamily.roboto,
+            fontWeight: FontWeight.w300,
+            fontSize: 16,
+          )),
+      onTap: () {
+        analytics.logEvent(
+          name: "did_select_about_churn",
+        );
+
+        launchUrl(Uri.parse("https://pilll.wraptas.site/b10fd76f1d2246d286ad5cff03f22940"), mode: LaunchMode.inAppWebView);
+      },
+    );
   }
 }
