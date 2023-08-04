80583b0ea91eaa016652df54eee921921f77ecfc
bannzai <kingkong999yhirose@gmail.com>
2023-08-04 21:22:25 +0900

    Fix warn

--- lib/features/settings/components/rows/about_churn.dart
+++ lib/features/settings/components/rows/about_churn.dart
@@ -5,6 +5,8 @@ import 'package:pilll/utils/analytics.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AboutChurn extends HookConsumerWidget {
+  const AboutChurn({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ListTile(
