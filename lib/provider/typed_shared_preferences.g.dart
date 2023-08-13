4e2977be6b3527f19422628df486344710023193
bannzai <kingkong999yhirose@gmail.com>
2023-08-13 13:37:13 +0900

    replace to typed_shared_preferences

---
+++ lib/provider/typed_shared_preferences.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'typed_shared_preferences.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$boolSharedPreferencesHash() =>
+    r'3f14410227a78c2ac734268cde68561d7da49d3c';
+
+/// See also [BoolSharedPreferences].
+@ProviderFor(BoolSharedPreferences)
+final boolSharedPreferencesProvider =
+    AutoDisposeNotifierProvider<BoolSharedPreferences, bool?>.internal(
+  BoolSharedPreferences.new,
+  name: r'boolSharedPreferencesProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$boolSharedPreferencesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BoolSharedPreferences = AutoDisposeNotifier<bool?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
