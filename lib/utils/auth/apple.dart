6a2273c13f7285a5eb6f19463dae5b4491ea616d
bannzai <kingkong999yhirose@gmail.com>
2023-08-20 08:32:52 +0900

    Fix sign in apple

--- lib/utils/auth/apple.dart
+++ lib/utils/auth/apple.dart
@@ -1,45 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:pilll/utils/auth/hash.dart';
 import 'package:pilll/utils/auth/link_value_container.dart';
 import 'package:pilll/provider/auth.dart';
-import 'package:pilll/utils/environment.dart';
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
-
-const appleProviderID = "apple.com";
 
 enum SignInWithAppleState { determined, cancel }
 
 Future<LinkValueContainer?> linkWithApple(User user) async {
   try {
-    final rawNonce = generateNonce();
-    final state = generateNonce();
-    final appleCredential = await SignInWithApple.getAppleIDCredential(
-      scopes: [AppleIDAuthorizationScopes.email],
-      webAuthenticationOptions: WebAuthenticationOptions(
-        clientId: Environment.siwaServiceIdentifier,
-        redirectUri: Uri.parse(Environment.androidSiwaRedirectURL),
-      ),
-      nonce: sha256ofString(rawNonce).toString(),
-      state: state,
-    );
-    debugPrint("appleCredential: $appleCredential");
-    if (state != appleCredential.state) {
-      throw AssertionError('state not matched!');
-    }
-    final email = appleCredential.email;
-    final credential = OAuthProvider(appleProviderID).credential(
-      idToken: appleCredential.identityToken,
-      accessToken: appleCredential.authorizationCode,
-      rawNonce: rawNonce,
-    );
-    final linkedCredential = await user.linkWithCredential(credential);
-    return Future.value(LinkValueContainer(linkedCredential, email));
-  } on SignInWithAppleAuthorizationException catch (e) {
-    if (e.code == AuthorizationErrorCode.canceled) {
-      return Future.value(null);
-    }
+    final provider = AppleAuthProvider()..addScope('email');
+    final linkedCredential = await user.linkWithProvider(provider);
+    return Future.value(LinkValueContainer(linkedCredential, linkedCredential.user?.email));
+  } on FirebaseAuthException catch (e) {
     rethrow;
   }
 }
@@ -49,35 +20,8 @@ Future<UserCredential?> signInWithApple() async {
   if (user == null) {
     throw const FormatException("Anonymous User not found");
   }
-  try {
-    final rawNonce = generateNonce();
-    final state = generateNonce();
-    final appleCredential = await SignInWithApple.getAppleIDCredential(
-      scopes: [AppleIDAuthorizationScopes.email],
-      webAuthenticationOptions: WebAuthenticationOptions(
-        clientId: Environment.siwaServiceIdentifier,
-        redirectUri: Uri.parse(Environment.androidSiwaRedirectURL),
-      ),
-      nonce: sha256ofString(rawNonce).toString(),
-      state: state,
-    );
-    debugPrint("appleCredential: $appleCredential");
-    if (state != appleCredential.state) {
-      throw AssertionError('state not matched!');
-    }
-    final credential = OAuthProvider(appleProviderID).credential(
-      idToken: appleCredential.identityToken,
-      accessToken: appleCredential.authorizationCode,
-      rawNonce: rawNonce,
-    );
-
-    return await FirebaseAuth.instance.signInWithCredential(credential);
-  } on SignInWithAppleAuthorizationException catch (e) {
-    if (e.code == AuthorizationErrorCode.canceled) {
-      return Future.value(null);
-    }
-    rethrow;
-  }
+  final provider = AppleAuthProvider()..addScope('email');
+  return await FirebaseAuth.instance.signInWithProvider(provider);
 }
 
 final isAppleLinkedProvider = Provider((ref) {
@@ -88,40 +32,13 @@ final isAppleLinkedProvider = Provider((ref) {
   }
   return isLinkedAppleFor(userValue);
 });
-// bool isLinkedApple() {
-//   final user = FirebaseAuth.instance.currentUser;
-//   if (user == null) {
-//     return false;
-//   }
-//   return isLinkedAppleFor(user);
-// }
 
 bool isLinkedAppleFor(User user) {
-  return user.providerData.where((element) => element.providerId == appleProviderID).isNotEmpty;
+  return user.providerData.where((element) => element.providerId == AppleAuthProvider.PROVIDER_ID).isNotEmpty;
 }
 
 Future<bool> appleReauthentification() async {
-  final rawNonce = generateNonce();
-  final state = generateNonce();
-  final appleCredential = await SignInWithApple.getAppleIDCredential(
-    scopes: [AppleIDAuthorizationScopes.email],
-    webAuthenticationOptions: WebAuthenticationOptions(
-      clientId: Environment.siwaServiceIdentifier,
-      redirectUri: Uri.parse(Environment.androidSiwaRedirectURL),
-    ),
-    nonce: sha256ofString(rawNonce).toString(),
-    state: state,
-  );
-  debugPrint("appleCredential: $appleCredential");
-  if (state != appleCredential.state) {
-    throw AssertionError('state not matched!');
-  }
-  final credential = OAuthProvider(appleProviderID).credential(
-    idToken: appleCredential.identityToken,
-    accessToken: appleCredential.authorizationCode,
-    rawNonce: rawNonce,
-  );
-
-  await FirebaseAuth.instance.currentUser?.reauthenticateWithCredential(credential);
+  final provider = AppleAuthProvider();
+  await FirebaseAuth.instance.currentUser?.reauthenticateWithProvider(provider);
   return true;
 }
