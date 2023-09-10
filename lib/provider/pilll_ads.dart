8da29ab88aca70beed4dfc7ca31dbe638065c11d
bannzai <kingkong999yhirose@gmail.com>
2023-09-10 20:59:57 +0900

    Remove unnecessary import

--- lib/provider/pilll_ads.dart
+++ lib/provider/pilll_ads.dart
@@ -1,6 +1,4 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/provider/database.dart';
-import 'package:pilll/provider/package_info.dart';
-import 'package:pilll/utils/version/version.dart';
 
 final pilllAdsProvider = StreamProvider((ref) => ref.watch(databaseProvider).pilllAds().snapshots().map((event) => event.data()));
