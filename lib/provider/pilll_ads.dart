8fb764aba2834f153121248779c1c5b6935a175a
bannzai <kingkong999yhirose@gmail.com>
2023-09-10 21:01:10 +0900

    Add TODO provider

--- lib/provider/pilll_ads.dart
+++ lib/provider/pilll_ads.dart
@@ -2,3 +2,5 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/provider/database.dart';
 
 final pilllAdsProvider = StreamProvider((ref) => ref.watch(databaseProvider).pilllAds().snapshots().map((event) => event.data()));
+// TODO:
+// final affiliateProvier 
