69eaf4f61b92cdb74dcfb71b8d615bb699085915
bannzai <kingkong999yhirose@gmail.com>
2023-09-18 06:23:05 +0900

    Add admob native advance identifier to dart secret

--- scripts/secret.sh
+++ scripts/secret.sh
@@ -4,5 +4,7 @@ set -o pipefail
 
 cat lib/secret/secret.dart.sample | sed \
   -e "s|\[REVENUE_CAT_PUBLIC_API_KEY\]|$REVENUE_CAT_PUBLIC_API_KEY|g" \
+  -e "s|\[IOS_ADMOB_NATIVE_ADVANCE_IDENTIFIER\]|$IOS_ADMOB_NATIVE_ADVANCE_IDENTIFIER|g" \
+  -e "s|\[ANDROID_ADMOB_NATIVE_ADVANCE_IDENTIFIER\]|$ANDROID_ADMOB_NATIVE_ADVANCE_IDENTIFIER|g" \
 > lib/secret/secret.dart
 
