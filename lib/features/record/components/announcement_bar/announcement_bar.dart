ccd809b0eba92a8801b629b96866ea7d7accab3b
bannzai <kingkong999yhirose@gmail.com>
2023-09-19 10:10:47 +0900

    Fix widget tree

--- lib/features/record/components/announcement_bar/announcement_bar.dart
+++ lib/features/record/components/announcement_bar/announcement_bar.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:pilll/features/record/components/announcement_bar/components/admob_banner.dart';
-import 'package:pilll/features/record/components/announcement_bar/components/admob_native_advanced.dart';
 import 'package:pilll/utils/analytics.dart';
 import 'package:pilll/provider/pill_sheet_group.dart';
 import 'package:pilll/provider/pilll_ads.dart';
@@ -122,17 +120,7 @@ class AnnouncementBar extends HookConsumerWidget {
       if (!isAdsDisabled && pilllAds != null) {
         return PilllAdsAnnouncementBar(pilllAds: pilllAds, onClose: () => showPremiumIntroductionSheet(context));
       }
-
-      // iPhone mini, iPhone SEサイズだとNativeAdvanceの広告の高さだと画面占領が目立つので高さで広告を分岐する
-      // SEもminiも将来的にサポート対象外(どちらも生産終了)しているので、この分岐は将来的には不要になる
-      // iPhone SEはiOS15までサポートされているので、2024-01以降にiOSのバージョンを16に上げて回避するでも良さそう
-      // ただ、一時的にサポートするコードを書いてしまっているので、iOSのバージョンを上げるのは必須じゃない。2024-01以降で他に困ることがあればiOSのバージョンを上げる
-      // mini: 780, SE: 667
-      if (MediaQuery.of(context).size.height <= 780) {
-        return const AdMobBanner();
-      } else {
-        return const AdMobNativeAdvance();
-      }
+    return AdMobh
     } else {
       final shownRecommendSignupNotificationForPremium = () {
         if (isLinkedLoginProvider) {
