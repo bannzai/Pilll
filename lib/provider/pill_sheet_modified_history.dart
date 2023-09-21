14a3478db144f6804779953b7cd3ce93c79c06f4
bannzai <kingkong999yhirose@gmail.com>
2023-09-21 20:46:40 +0900

    Remove unnecessary integrate stream

--- lib/provider/pill_sheet_modified_history.dart
+++ lib/provider/pill_sheet_modified_history.dart
@@ -4,11 +4,11 @@ import 'package:pilll/provider/database.dart';
 import 'package:pilll/entity/pill_sheet_modified_history.codegen.dart';
 import 'package:pilll/utils/datetime/day.dart';
 
-final pillSheetModifiedHistoriesReferenceProvider = Provider((ref) => ref.watch(databaseProvider).pillSheetModifiedHistoriesReference());
 final pillSheetModifiedHistoriesProvider = StreamProvider.family.autoDispose((ref, DateTime? afterCursor) {
   if (afterCursor != null) {
     return ref
-        .watch(pillSheetModifiedHistoriesReferenceProvider)
+        .watch(databaseProvider)
+        .pillSheetModifiedHistoriesReference()
         .where(
           PillSheetModifiedHistoryFirestoreKeys.estimatedEventCausingDate,
           isLessThanOrEqualTo: today().add(const Duration(days: 1)),
@@ -23,7 +23,8 @@ final pillSheetModifiedHistoriesProvider = StreamProvider.family.autoDispose((re
         .map((docs) => docs.map((doc) => doc.data()).toList());
   } else {
     return ref
-        .watch(pillSheetModifiedHistoriesReferenceProvider)
+        .watch(databaseProvider)
+        .pillSheetModifiedHistoriesReference()
         .where(
           PillSheetModifiedHistoryFirestoreKeys.estimatedEventCausingDate,
           isLessThanOrEqualTo: today().add(const Duration(days: 1)),
@@ -39,7 +40,8 @@ final pillSheetModifiedHistoriesProvider = StreamProvider.family.autoDispose((re
 });
 final pillSheetModifiedHistoriesWithLimitProvider = StreamProvider.family.autoDispose((ref, int limit) {
   return ref
-      .watch(pillSheetModifiedHistoriesReferenceProvider)
+      .watch(databaseProvider)
+      .pillSheetModifiedHistoriesReference()
       .where(
         PillSheetModifiedHistoryFirestoreKeys.estimatedEventCausingDate,
         isLessThanOrEqualTo: today().add(const Duration(days: 1)),
