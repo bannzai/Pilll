f283b55a79edf8cb1491236abc0e89fff6f16098
bannzai <kingkong999yhirose@gmail.com>
2023-09-28 15:35:37 +0900

    fix import

--- lib/features/release_note/release_note.dart
+++ lib/features/release_note/release_note.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:pilll/utils/shared_preference/keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 class ReleaseNote extends StatelessWidget {
   const ReleaseNote({Key? key}) : super(key: key);
