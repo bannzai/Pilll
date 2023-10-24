96a9d04ce2f0e0be6e6958e1e64d2dc2715ca470
bannzai <kingkong999yhirose@gmail.com>
2023-10-24 17:25:57 +0900

    Remove NOTE comment

--- lib/features/root/root_page.dart
+++ lib/features/root/root_page.dart
@@ -2,7 +2,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
 
 class RootPage extends HookConsumerWidget {
-  // NOTE: テスト時にRootPageより下のWidgetのProviderを用意するのは現実的では無いので、builderからメインストリームのWidgetを決定する。
   final Widget Function(BuildContext) builder;
   const RootPage({Key? key, required this.builder}) : super(key: key);
 
