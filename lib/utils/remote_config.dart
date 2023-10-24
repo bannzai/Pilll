f0319b73f5451fd804b8865a9dfe5b354b72f85d
bannzai <kingkong999yhirose@gmail.com>
2023-10-24 15:56:31 +0900

    Add remote config utils

---
+++ lib/utils/remote_config.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:pilll/entity/remote_config_parameter.codegen.dart';
+
+final remoteConfig = FirebaseRemoteConfig.instance;
+
+Future<void> setupRemoteConfig() async {
+  await (
+    remoteConfig.setConfigSettings(RemoteConfigSettings(
+      fetchTimeout: const Duration(minutes: 1),
+      minimumFetchInterval: const Duration(hours: 1),
+    )),
+    remoteConfig.setDefaults({
+      RemoteConfigKeys.isPaywallFirst: RemoteConfigParameterDefaultValues.isPaywallFirst,
+      RemoteConfigKeys.isUnnecessaryOnBoarding: RemoteConfigParameterDefaultValues.isUnnecessaryOnBoarding,
+      RemoteConfigKeys.trialDeadlineDateOffsetDay: RemoteConfigParameterDefaultValues.trialDeadlineDateOffsetDay,
+      RemoteConfigKeys.discountEntitlementOffsetDay: RemoteConfigParameterDefaultValues.discountEntitlementOffsetDay,
+      RemoteConfigKeys.discountCountdownBoundaryHour: RemoteConfigParameterDefaultValues.discountCountdownBoundaryHour,
+    }),
+    remoteConfig.fetchAndActivate()
+  ).wait;
+
+  remoteConfig.onConfigUpdated.listen((event) {
+    remoteConfig.activate();
+  });
+}
