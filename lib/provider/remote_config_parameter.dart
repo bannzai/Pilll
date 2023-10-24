5dee616d71bdd1b35c19934f37e4dba12bb752f5
bannzai <kingkong999yhirose@gmail.com>
2023-10-24 15:58:29 +0900

    Remove unnecessary import

--- lib/provider/remote_config_parameter.dart
+++ lib/provider/remote_config_parameter.dart
@@ -1,9 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:pilll/entity/remote_config_parameter.codegen.dart';
-import 'package:pilll/provider/database.dart';
-import 'package:pilll/entity/pill_sheet_modified_history.codegen.dart';
-import 'package:pilll/utils/datetime/day.dart';
 import 'package:pilll/utils/remote_config.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
