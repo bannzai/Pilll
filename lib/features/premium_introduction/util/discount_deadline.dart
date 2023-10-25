980f3aa3bef4a8fcc06fe144352fb72deb910d21
bannzai <kingkong999yhirose@gmail.com>
2023-10-25 10:13:47 +0900

    Revert "Replace to use riverpodgen"
    This reverts commit ff5f7950002941b7d1201d6be253bd5706ca25ce.

--- lib/features/premium_introduction/util/discount_deadline.dart
+++ lib/features/premium_introduction/util/discount_deadline.dart
@@ -1,34 +1,29 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/provider/tick.dart';
 import 'package:pilll/utils/formatter/date_time_formatter.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'discount_deadline.g.dart';
-
-@Riverpod()
-bool isOverDiscountDeadline(IsOverDiscountDeadlineRef ref, {required DateTime? discountEntitlementDeadlineDate}) {
+final isOverDiscountDeadlineProvider = Provider.family.autoDispose((ref, DateTime? discountEntitlementDeadlineDate) {
   if (discountEntitlementDeadlineDate == null) {
     // NOTE: discountEntitlementDeadlineDate が存在しない時はbackendの方でまだ期限を決めていないのでfalse状態で扱う
     return false;
   }
   final timer = ref.watch(tickProvider);
   return timer.isAfter(discountEntitlementDeadlineDate);
-}
+});
 
-@Riverpod()
-bool hiddenCountdownDiscountDeadline(HiddenCountdownDiscountDeadlineRef ref, {required DateTime? discountEntitlementDeadlineDate}) {
+final hiddenCountdownDiscountDeadlineProvider = Provider.family.autoDispose((ref, DateTime? discountEntitlementDeadlineDate) {
   if (discountEntitlementDeadlineDate == null) {
     // NOTE: discountEntitlementDeadlineDate が存在しない時はbackendの方でまだ期限を決めていないのでfalse状態で扱う
     return false;
   }
   final timer = ref.watch(tickProvider);
   return !(timer.isBefore(discountEntitlementDeadlineDate) && discountEntitlementDeadlineDate.difference(timer).inMinutes <= 48 * 60);
-}
+});
 
-@Riverpod()
-Duration durationToDiscountPriceDeadline(DurationToDiscountPriceDeadlineRef ref, {required DateTime discountEntitlementDeadlineDate}) {
+final durationToDiscountPriceDeadline = Provider.family.autoDispose((ref, DateTime discountEntitlementDeadlineDate) {
   final timerDate = ref.watch(tickProvider);
   return discountEntitlementDeadlineDate.difference(timerDate);
-}
+});
 
 String discountPriceDeadlineCountdownString(Duration diff) {
   final hour = diff.inHours;
