ff5f7950002941b7d1201d6be253bd5706ca25ce
bannzai <kingkong999yhirose@gmail.com>
2023-10-25 09:57:23 +0900

    Replace to use riverpodgen

--- lib/features/premium_introduction/util/discount_deadline.dart
+++ lib/features/premium_introduction/util/discount_deadline.dart
@@ -1,29 +1,34 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/provider/tick.dart';
 import 'package:pilll/utils/formatter/date_time_formatter.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final isOverDiscountDeadlineProvider = Provider.family.autoDispose((ref, DateTime? discountEntitlementDeadlineDate) {
+part 'discount_deadline.g.dart';
+
+@Riverpod()
+bool isOverDiscountDeadline(IsOverDiscountDeadlineRef ref, {required DateTime? discountEntitlementDeadlineDate}) {
   if (discountEntitlementDeadlineDate == null) {
     // NOTE: discountEntitlementDeadlineDate が存在しない時はbackendの方でまだ期限を決めていないのでfalse状態で扱う
     return false;
   }
   final timer = ref.watch(tickProvider);
   return timer.isAfter(discountEntitlementDeadlineDate);
-});
+}
 
-final hiddenCountdownDiscountDeadlineProvider = Provider.family.autoDispose((ref, DateTime? discountEntitlementDeadlineDate) {
+@Riverpod()
+bool hiddenCountdownDiscountDeadline(HiddenCountdownDiscountDeadlineRef ref, {required DateTime? discountEntitlementDeadlineDate}) {
   if (discountEntitlementDeadlineDate == null) {
     // NOTE: discountEntitlementDeadlineDate が存在しない時はbackendの方でまだ期限を決めていないのでfalse状態で扱う
     return false;
   }
   final timer = ref.watch(tickProvider);
   return !(timer.isBefore(discountEntitlementDeadlineDate) && discountEntitlementDeadlineDate.difference(timer).inMinutes <= 48 * 60);
-});
+}
 
-final durationToDiscountPriceDeadline = Provider.family.autoDispose((ref, DateTime discountEntitlementDeadlineDate) {
+@Riverpod()
+Duration durationToDiscountPriceDeadline(DurationToDiscountPriceDeadlineRef ref, {required DateTime discountEntitlementDeadlineDate}) {
   final timerDate = ref.watch(tickProvider);
   return discountEntitlementDeadlineDate.difference(timerDate);
-});
+}
 
 String discountPriceDeadlineCountdownString(Duration diff) {
   final hour = diff.inHours;
