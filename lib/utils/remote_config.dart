c4872275d29f9f1d5c8292e69d8cc72eb950d0c0
bannzai <kingkong999yhirose@gmail.com>
2023-11-05 12:10:08 +0900

    Ignore error

--- lib/utils/remote_config.dart
+++ lib/utils/remote_config.dart
@@ -5,26 +5,31 @@ import 'package:pilll/entity/remote_config_parameter.codegen.dart';
 final remoteConfig = FirebaseRemoteConfig.instance;
 
 Future<void> setupRemoteConfig() async {
-  await (
-    remoteConfig.setConfigSettings(RemoteConfigSettings(
-      fetchTimeout: const Duration(minutes: 1),
-      minimumFetchInterval: const Duration(hours: 1),
-    )),
-    remoteConfig.setDefaults({
-      RemoteConfigKeys.isPaywallFirst: RemoteConfigParameterDefaultValues.isPaywallFirst,
-      RemoteConfigKeys.skipInitialSetting: RemoteConfigParameterDefaultValues.skipInitialSetting,
-      RemoteConfigKeys.trialDeadlineDateOffsetDay: RemoteConfigParameterDefaultValues.trialDeadlineDateOffsetDay,
-      RemoteConfigKeys.discountEntitlementOffsetDay: RemoteConfigParameterDefaultValues.discountEntitlementOffsetDay,
-      RemoteConfigKeys.discountCountdownBoundaryHour: RemoteConfigParameterDefaultValues.discountCountdownBoundaryHour,
-    }),
-    remoteConfig.fetchAndActivate()
-  ).wait;
+  try {
+    await (
+      remoteConfig.setConfigSettings(RemoteConfigSettings(
+        fetchTimeout: const Duration(minutes: 1),
+        minimumFetchInterval: const Duration(hours: 1),
+      )),
+      remoteConfig.setDefaults({
+        RemoteConfigKeys.isPaywallFirst: RemoteConfigParameterDefaultValues.isPaywallFirst,
+        RemoteConfigKeys.skipInitialSetting: RemoteConfigParameterDefaultValues.skipInitialSetting,
+        RemoteConfigKeys.trialDeadlineDateOffsetDay: RemoteConfigParameterDefaultValues.trialDeadlineDateOffsetDay,
+        RemoteConfigKeys.discountEntitlementOffsetDay: RemoteConfigParameterDefaultValues.discountEntitlementOffsetDay,
+        RemoteConfigKeys.discountCountdownBoundaryHour: RemoteConfigParameterDefaultValues.discountCountdownBoundaryHour,
+      }),
+      remoteConfig.fetchAndActivate()
+    ).wait;
 
-  debugPrintRemoteConfig();
+    debugPrintRemoteConfig();
 
-  remoteConfig.onConfigUpdated.listen((event) {
-    remoteConfig.activate();
-  });
+    remoteConfig.onConfigUpdated.listen((event) {
+      remoteConfig.activate();
+    });
+  } catch (error) {
+    // ignore error
+    debugPrint(error.toString());
+  }
 }
 
 void debugPrintRemoteConfig() {
