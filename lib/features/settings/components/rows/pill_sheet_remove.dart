3e8765c553e2ad57a41a69fd22cb00340dc636e0
bannzai <kingkong999yhirose@gmail.com>
2023-11-15 13:22:39 +0900

    Fix import

--- lib/features/settings/components/rows/pill_sheet_remove.dart
+++ lib/features/settings/components/rows/pill_sheet_remove.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:pilll/entrypoint.dart';
+import 'package:pilll/app.dart';
 import 'package:pilll/utils/analytics.dart';
 import 'package:pilll/components/atoms/button.dart';
 import 'package:pilll/components/atoms/font.dart';
