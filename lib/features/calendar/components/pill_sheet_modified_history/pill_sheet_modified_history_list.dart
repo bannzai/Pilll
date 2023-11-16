3ab5820f9277e0bea7baecc041c29c3cdb7ed7e9
bannzai <kingkong999yhirose@gmail.com>
2023-11-16 19:06:04 +0900

    Add extension of find first difference pill sheet

--- lib/features/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list.dart
+++ lib/features/calendar/components/pill_sheet_modified_history/pill_sheet_modified_history_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pilll/entity/pill_sheet.codegen.dart';
 import 'package:pilll/features/calendar/components/pill_sheet_modified_history/components/rows/pill_sheet_modified_history_automatically_recorded_last_taken_date_action.dart';
 import 'package:pilll/features/calendar/components/pill_sheet_modified_history/components/rows/pill_sheet_modified_history_began_rest_duration.dart';
 import 'package:pilll/features/calendar/components/pill_sheet_modified_history/components/rows/pill_sheet_modified_history_begin_display_number_action.dart';
@@ -226,5 +227,22 @@ class PillSheetModifiedHistoryList extends HookConsumerWidget {
       }
     }
     return models;
+  }
+}
+
+extension on List<PillSheet> {
+  PillSheet? findFirstDifferencePillSheet(List<PillSheet> otherPillSheets) {
+    for (final (index, pillSheet) in indexed) {
+      if (otherPillSheets.length <= index) {
+        continue;
+      }
+
+      final otherPillSheet = otherPillSheets[index];
+      if (pillSheet != otherPillSheet) {
+        // NOTE: 変化後のPillSheetは_afterActivePillSheetで良い
+        return pillSheet;
+      }
+    }
+    return null;
   }
 }
