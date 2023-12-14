4a0b71852bdbed53d7e56efdaee6f734029c3027
bannzai <kingkong999yhirose@gmail.com>
2023-12-15 06:06:29 +0900

    add debug code

--- lib/features/root/resolver/show_paywall_on_app_launch.dart
+++ lib/features/root/resolver/show_paywall_on_app_launch.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +20,10 @@ class ShowPaywallOnAppLaunch extends HookConsumerWidget {
     final shownPaywallWhenAppFirstLaunch = ref.watch(boolSharedPreferencesProvider(BoolKey.shownPaywallWhenAppFirstLaunch));
     final shownPaywallWhenAppFirstLaunchNotifier = ref.watch(boolSharedPreferencesProvider(BoolKey.shownPaywallWhenAppFirstLaunch).notifier);
     if (!remoteConfigParameter.isPaywallFirst) {
+      return builder(context);
+    }
+    // Androidの場合revenueCatから情報を取得する処理でエラーになるので早期リターンして回避
+    if (kDebugMode && Platform.isAndroid) {
       return builder(context);
     }
 
