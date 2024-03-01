3ae8981cc8730ce510399f80de6033826d86d7e5
bannzai <kingkong999yhirose@gmail.com>
2024-03-01 19:53:23 +0900

    Add updateUseLocalNotification mock

--- test/helper/mock.mocks.dart
+++ test/helper/mock.mocks.dart
@@ -1516,3 +1516,38 @@ class MockLocalNotificationService extends _i1.Mock
                     <_i14.PendingNotificationRequest>[]),
           ) as _i9.Future<List<_i14.PendingNotificationRequest>>);
 }
+
+/// A class which mocks [UpdateUseLocalNotification].
+///
+/// See the documentation for Mockito's code generation for more information.
+class MockUpdateUseLocalNotification extends _i1.Mock
+    implements _i26.UpdateUseLocalNotification {
+  MockUpdateUseLocalNotification() {
+    _i1.throwOnMissingStub(this);
+  }
+
+  @override
+  _i2.DatabaseConnection get databaseConnection => (super.noSuchMethod(
+        Invocation.getter(#databaseConnection),
+        returnValue: _FakeDatabaseConnection_1(
+          this,
+          Invocation.getter(#databaseConnection),
+        ),
+      ) as _i2.DatabaseConnection);
+  @override
+  _i9.Future<void> call(
+    _i4.User? user,
+    bool? value,
+  ) =>
+      (super.noSuchMethod(
+        Invocation.method(
+          #call,
+          [
+            user,
+            value,
+          ],
+        ),
+        returnValue: _i9.Future<void>.value(),
+        returnValueForMissingStub: _i9.Future<void>.value(),
+      ) as _i9.Future<void>);
+}
