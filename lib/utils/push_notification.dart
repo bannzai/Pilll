efc4b31698f6e14627816892516c6466bfa538b1
bannzai <kingkong999yhirose@gmail.com>
2024-03-06 12:00:54 +0900

    Fix

--- lib/utils/push_notification.dart
+++ lib/utils/push_notification.dart
@@ -7,10 +7,10 @@ import 'package:pilll/provider/user.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> requestNotificationPermissions(RegisterRemotePushNotificationToken registerRemotePushNotificationToken) async {
-  await FirebaseMessaging.instance.requestPermission();
   if (Platform.isIOS) {
     await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(alert: true, badge: true, sound: true);
   }
+  await FirebaseMessaging.instance.requestPermission(alert: true, badge: true, sound: true);
   listenNotificationEvents();
   registerRemotePushNotificationToken(await FirebaseMessaging.instance.getToken());
 }
