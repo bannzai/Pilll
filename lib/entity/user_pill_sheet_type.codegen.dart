da2f64bb5dc0868fbe758994a2054fd75ef07165
bannzai <kingkong999yhirose@gmail.com>
2024-03-07 12:55:10 +0900

    Fix entity

--- lib/entity/user_pill_sheet_type.codegen.dart
+++ lib/entity/user_pill_sheet_type.codegen.dart
@@ -1,8 +1,6 @@
 import 'package:pilll/entity/firestore_timestamp_converter.dart';
-import 'package:pilll/entity/pill_sheet_type.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:pilll/entity/reminder_notification_customization.codegen.dart';
 
 part 'user_pill_sheet_type.codegen.g.dart';
 part 'user_pill_sheet_type.codegen.freezed.dart';
