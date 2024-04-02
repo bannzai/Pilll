1f75208c7cd194d9c0fcd1b2895680cf05767757
bannzai <kingkong999yhirose@gmail.com>
2024-04-02 11:00:28 +0900

    Remove useLocalNotificationForReminder

--- lib/entity/user.codegen.dart
+++ lib/entity/user.codegen.dart
@@ -79,9 +79,6 @@ class User with _$User {
     @Default([]) List<String> firebaseCurrentUserIDSets,
     @Default(false) bool isPremium,
     @Default(false) bool shouldAskCancelReason,
-// TODO: [UseLocalNotification-Beta] 2024-04-01 に削除する。
-// バックエンドの方で使っていないかの確認を忘れずに
-    @Default(true) bool useLocalNotificationForReminder,
     @Default(false) bool analyticsDebugIsEnabled,
     @JsonKey(
       fromJson: TimestampConverter.timestampToDateTime,
