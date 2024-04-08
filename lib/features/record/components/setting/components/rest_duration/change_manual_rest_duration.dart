15fac4f4ff1a9526c167a0a7ae190c09a82c8464
bannzai <kingkong999yhirose@gmail.com>
2024-04-08 11:25:47 +0900

    Fix format

--- lib/features/record/components/setting/components/rest_duration/change_manual_rest_duration.dart
+++ lib/features/record/components/setting/components/rest_duration/change_manual_rest_duration.dart
@@ -24,8 +24,13 @@ class ChangeManualRestDuration extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final changeRestDurationBeginDate = ref.watch(changeRestDurationBeginDateProvider);
     final changeRestDuration = ref.watch(changeRestDurationProvider);
-    final begin = DateTimeFormatter.monthAndDay(restDuration.beginDate);
-    final end = restDuration.endDate != null ? DateTimeFormatter.monthAndDay(restDuration.endDate!) : null;
+
+    String format(DateTime dateTime) {
+      return "${DateTimeFormatter.monthAndDay(dateTime)}(${DateTimeFormatter.weekday(dateTime)})";
+    }
+
+    final begin = format(restDuration.beginDate);
+    final end = restDuration.endDate != null ? format(restDuration.endDate!) : null;
 
     void onChanged() {
       ScaffoldMessenger.of(context).showSnackBar(
