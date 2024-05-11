15dae57469722e80f6a1afb72a4969a1f39fedb3
bannzai <kingkong999yhirose@gmail.com>
2024-05-11 11:36:44 +0900

    Replace to firebase auth resolver

--- lib/features/root/root_page.dart
+++ lib/features/root/root_page.dart
@@ -2,13 +2,13 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:pilll/features/home/home_page.dart';
 import 'package:pilll/features/initial_setting/pill_sheet_group/initial_setting_pill_sheet_group_page.dart';
+import 'package:pilll/features/root/resolver/firebase_auth_resolver.dart';
 import 'package:pilll/features/root/resolver/force_update.dart';
 import 'package:pilll/features/root/resolver/initial_setting_or_app_page.dart';
 import 'package:pilll/features/root/resolver/show_paywall_on_app_launch.dart';
 import 'package:pilll/features/root/resolver/skip_initial_setting.dart';
 import 'package:pilll/features/root/resolver/sync_data.dart';
 import 'package:pilll/features/root/resolver/user_setup.dart';
-import 'package:pilll/features/root/resolver/user_sign_in.dart';
 import 'package:pilll/features/root/resolver/user_stream.dart';
 import 'package:pilll/utils/analytics.dart';
 
@@ -18,9 +18,9 @@ class RootPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ForceUpdate(
-      builder: (_) => UserSignIn(
-        builder: (_, userID) => UserSetup(
-          userID: userID,
+      builder: (_) => FirebaseAuthResolver(
+        builder: (_, user) => UserSetup(
+          userID: user.uid,
           builder: (_) => Stack(
             children: [
               UserStreamResolver(stream: (user) => analyticsDebugIsEnabled = user.analyticsDebugIsEnabled),
