53e1660db72e2611ecfc5837617ae77beb9f7106
bannzai <kingkong999yhirose@gmail.com>
2024-05-13 11:43:34 +0900

    Fix compile error

--- ios/Runner/AppTrackingTransparency.swift
+++ ios/Runner/AppTrackingTransparency.swift
@@ -1,25 +1,25 @@
 import Foundation
 import AppTrackingTransparency
 
-static func requestAppTrackingTransparency(completion: @escaping [String: Any] -> Void) {
+func requestAppTrackingTransparency(completion: @escaping ([String: Any]) -> Void) {
   switch ATTrackingManager.trackingAuthorizationStatus {
-  case .notDetermined
+  case .notDetermined:
     ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
-      completionHandler([
+      completion([
         "result": "success",
         "status": status.rawValue
       ])
     })
   case .authorized, .denied, .restricted:
     ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
-      completionHandler([
+      completion([
         "result": "success",
         "status": ATTrackingManager.trackingAuthorizationStatus.rawValue
       ])
     })
   @unknown default:
     ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
-      completionHandler([
+      completion([
         "result": "failure",
         "reason": "unknown status: \(ATTrackingManager.trackingAuthorizationStatus.rawValue)"
       ])
