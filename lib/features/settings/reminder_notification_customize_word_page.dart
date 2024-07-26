51381d05c79a4b4738217519d3f3f2a62230e0bb
bannzai <kingkong999yhirose@gmail.com>
2024-07-26 20:30:11 +0900

    WIP extract to component

--- lib/features/settings/reminder_notification_customize_word_page.dart
+++ lib/features/settings/reminder_notification_customize_word_page.dart
@@ -63,49 +63,13 @@ class ReminderNotificationCustomizeWordPage extends HookConsumerWidget {
                     isInvisibleDescription: isInVisibleDescription.value,
                   ),
                   const SizedBox(height: 20),
-                  TextField(
-                    decoration: InputDecoration(
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(color: PilllColors.secondary),
-                      ),
-                      counter: Row(children: [
-                        const Text(
-                          "通知の先頭部分の変更ができます",
-                          style: TextStyle(fontFamily: FontFamily.japanese, fontSize: 12, fontWeight: FontWeight.w400, color: TextColor.darkGray),
-                        ),
-                        const Spacer(),
-                        if (word.value.characters.isNotEmpty)
-                          Text(
-                            "${word.value.characters.length}/8",
-                            style: const TextStyle(
-                                fontFamily: FontFamily.japanese, fontSize: 12, fontWeight: FontWeight.w400, color: TextColor.darkGray),
-                          ),
-                        if (word.value.characters.isEmpty)
-                          const Text(
-                            "0文字以上入力してください",
-                            style: TextStyle(fontFamily: FontFamily.japanese, fontSize: 12, fontWeight: FontWeight.w400, color: TextColor.danger),
-                          ),
-                      ]),
-                    ),
-                    autofocus: true,
-                    onChanged: (value) {
-                      word.value = value;
-                    },
-                    onSubmitted: (word) async {
-                      analytics.logEvent(name: "submit_reminder_notification_customize");
-                      try {
-                        await _reminderNotificationWordSubmit(
-                          word: word,
-                          setting: setting,
-                          setSetting: setSetting,
-                          registerReminderLocalNotification: registerReminderLocalNotification,
-                        );
-                      } catch (error) {
-                        if (context.mounted) showErrorAlert(context, error);
-                      }
-                    },
-                    controller: wordTextFieldController,
-                    maxLength: 8,
+                  _WordTextField(
+                    word,
+                    setting,
+                    setSetting,
+                    registerReminderLocalNotification,
+                    context,
+                    wordTextFieldController,
                   ),
                   const SizedBox(height: 20),
                   Column(
@@ -213,19 +177,6 @@ class ReminderNotificationCustomizeWordPage extends HookConsumerWidget {
     );
   }
 
-  Future<void> _reminderNotificationWordSubmit({
-    required String word,
-    required Setting setting,
-    required SetSetting setSetting,
-    required RegisterReminderLocalNotification registerReminderLocalNotification,
-  }) async {
-    var reminderNotificationCustomization = setting.reminderNotificationCustomization;
-    reminderNotificationCustomization = reminderNotificationCustomization.copyWith(word: word);
-
-    setSetting(setting.copyWith(reminderNotificationCustomization: reminderNotificationCustomization));
-    registerReminderLocalNotification();
-  }
-
   Future<void> _setIsInVisibleReminderDate({
     required bool isInVisibleReminderDate,
     required Setting setting,
@@ -272,6 +223,84 @@ extension ReminderNotificationCustomizeWordPageRoutes on ReminderNotificationCus
       settings: const RouteSettings(name: "ReminderNotificationCustomizeWordPage"),
       builder: (_) => const ReminderNotificationCustomizeWordPage(),
     );
+  }
+}
+
+class _WordTextField extends StatelessWidget {
+  final ValueNotifier<String> word;
+  final Setting setting;
+  final SetSetting setSetting;
+  final RegisterReminderLocalNotification registerReminderLocalNotification;
+  final BuildContext context;
+  final TextEditingController wordTextFieldController;
+
+  const _WordTextField({
+    required this.word,
+    required this.setting,
+    required this.setSetting,
+    required this.registerReminderLocalNotification,
+    required this.context,
+    required this.wordTextFieldController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: PilllColors.secondary),
+        ),
+        counter: Row(children: [
+          const Text(
+            "通知の先頭部分の変更ができます",
+            style: TextStyle(fontFamily: FontFamily.japanese, fontSize: 12, fontWeight: FontWeight.w400, color: TextColor.darkGray),
+          ),
+          const Spacer(),
+          if (word.value.characters.isNotEmpty)
+            Text(
+              "${word.value.characters.length}/8",
+              style: const TextStyle(fontFamily: FontFamily.japanese, fontSize: 12, fontWeight: FontWeight.w400, color: TextColor.darkGray),
+            ),
+          if (word.value.characters.isEmpty)
+            const Text(
+              "0文字以上入力してください",
+              style: TextStyle(fontFamily: FontFamily.japanese, fontSize: 12, fontWeight: FontWeight.w400, color: TextColor.danger),
+            ),
+        ]),
+      ),
+      autofocus: true,
+      onChanged: (value) {
+        word.value = value;
+      },
+      onSubmitted: (word) async {
+        analytics.logEvent(name: "submit_reminder_notification_customize");
+        try {
+          await _reminderNotificationWordSubmit(
+            word: word,
+            setting: setting,
+            setSetting: setSetting,
+            registerReminderLocalNotification: registerReminderLocalNotification,
+          );
+        } catch (error) {
+          if (context.mounted) showErrorAlert(context, error);
+        }
+      },
+      controller: wordTextFieldController,
+      maxLength: 8,
+    );
+  }
+
+  Future<void> _reminderNotificationWordSubmit({
+    required String word,
+    required Setting setting,
+    required SetSetting setSetting,
+    required RegisterReminderLocalNotification registerReminderLocalNotification,
+  }) async {
+    var reminderNotificationCustomization = setting.reminderNotificationCustomization;
+    reminderNotificationCustomization = reminderNotificationCustomization.copyWith(word: word);
+
+    setSetting(setting.copyWith(reminderNotificationCustomization: reminderNotificationCustomization));
+    registerReminderLocalNotification();
   }
 }
 
