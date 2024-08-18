f7560bfff096566f93ae47c986ac49339f0996fe
bannzai <kingkong999yhirose@gmail.com>
2024-08-18 17:45:46 +0900

    Fix init

--- lib/entity/number_range.codegen.dart
+++ lib/entity/number_range.codegen.dart
@@ -3,13 +3,9 @@ part 'number_range.codegen.freezed.dart';
 
 @freezed
 class PillNumberRange with _$PillNumberRange {
-  final int groupIndex;
-  final int begin;
-  final int end;
-
   const factory PillNumberRange({
-    required this.groupIndex,
-    required this.begin,
-    required this.end,
+    required int groupIndex,
+    required int begin,
+    required int end,
   }) = _PillNumberRange;
 }
