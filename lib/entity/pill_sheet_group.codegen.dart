3358340381d6c8ad155e2a4dcef9ee5c9ed528ce
bannzai <kingkong999yhirose@gmail.com>
2024-08-20 16:53:22 +0900

    Remove required

--- lib/entity/pill_sheet_group.codegen.dart
+++ lib/entity/pill_sheet_group.codegen.dart
@@ -37,7 +37,7 @@ class PillSheetGroup with _$PillSheetGroup {
     )
     DateTime? deletedAt,
     PillSheetGroupDisplayNumberSetting? displayNumberSetting,
-    @Default(PillSheetAppearanceMode.number) required PillSheetAppearanceMode pillSheetAppearanceMode,
+    @Default(PillSheetAppearanceMode.number) PillSheetAppearanceMode pillSheetAppearanceMode,
   }) = _PillSheetGroup;
   PillSheetGroup._();
 
