35be13217884bb7659c5d0c79903395b42e9420d
bannzai <kingkong999yhirose@gmail.com>
2024-09-24 05:27:27 +0900

    Add b pattern

---
+++ lib/features/premium_introduction/ab_test/b/components/premium_introduction_header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PremiumIntroductionHeader extends StatelessWidget {
+  const PremiumIntroductionHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 111,
+      width: MediaQuery.of(context).size.width,
+      child: Center(child: SvgPicture.asset("images/pillll_premium_logo.svg")),
+    );
+  }
+}
