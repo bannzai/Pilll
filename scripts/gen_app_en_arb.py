a8d4a3b68c3b956d19c2ae5b05f1069a81c9ddac
bannzai <kingkong999yhirose@gmail.com>
2024-11-20 20:44:00 +0900

    Fix scripts

--- scripts/gen_app_en_arb.py
+++ scripts/gen_app_en_arb.py
@@ -10,51 +10,6 @@ os.chdir("../")
 
 
 # Function to translate text using OpenAI's API with Function Calling
-def translate_key(text):
-    print(f"Translating: {text}")
-    response = openai.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {
-                "role": "system",
-                "content": """
-            Flutterで作られたピル管理アプリ・Pilllの翻訳作業を手伝ってください
-            渡された日本語に対して適切な .arb のkeyを生成してください。keyは英訳したものをUpper Camel Case したものにして欲しいです
-             """,
-            },
-            {
-                "role": "user",
-                "content": f"Translate the following text to .arb file key: '{text}'",
-            },
-        ],
-        functions=[
-            {
-                "name": "translate",
-                "description": """
-                Flutterで作られたピル管理アプリ・Pilllの翻訳作業を手伝ってください
-                渡された日本語に対して適切な .arb のkeyを生成してください。keyは英訳したものをUpper Camel Case したものにして欲しいです
-                """,
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "translated": {
-                            "type": "string",
-                            "description": "The translated text.",
-                        }
-                    },
-                    "required": ["translated"],
-                },
-            }
-        ],
-        function_call={"name": "translate"},
-    )
-    translated = json.loads(response.choices[0].message.function_call.arguments)[
-        "translated"
-    ]
-    print(f"Translated: {translated}")
-    return translated
-
-
 def translate_english_value(text):
     print(f"Translating: {text}")
     response = openai.chat.completions.create(
@@ -63,31 +18,19 @@ def translate_english_value(text):
             {
                 "role": "system",
                 "content": """
-                Please assist with the translation work for the pill management app, Pilll, 
-                created with Flutter. Provide an appropriate English translation for the given Japanese text. 
-                This translation will be used as a value in the .arb file, 
-                so please keep variable placeholders such as \{name\} intact
-
-                This means the following rules apply. Remember that this will be used as a value of .arb file in the Flutter project source code:
-                - Only use the underscore (_) symbol; replace any other symbols, such as periods (.), with underscores. Allow space.
+                    Flutterで作られたピル管理アプリ・Pilllの翻訳作業を手伝ってください。次に渡すアプリ上で使われている文言を適切に英訳してください
             """,
             },
             {
                 "role": "user",
-                "content": f"Translate the following text into English for .arb value: '{text}'",
+                "content": f"Translate the following text into English from ja .arb value: '{text}'",
             },
         ],
         functions=[
             {
                 "name": "translate",
                 "description": """
-                    Please assist with the translation work for the pill management app, Pilll, 
-                    created with Flutter. Provide an appropriate English translation for the given Japanese text. 
-                    This translation will be used as a value in the .arb file, 
-                    so please keep variable placeholders such as \{name\} intact
-
-                    This means the following rules apply. Remember that this will be used as a value of .arb file in the Flutter project source code:
-                    - Only use the underscore (_) symbol; replace any other symbols, such as periods (.), with underscores. Allow space.
+                    Flutterで作られたピル管理アプリ・Pilllの翻訳作業を手伝ってください。次に渡すアプリ上で使われている文言を適切に英訳してください
                 """,
                 "parameters": {
                     "type": "object",
@@ -122,52 +65,33 @@ def save_arb(filepath, data):
         json.dump(data, file, ensure_ascii=False, indent=2)
 
 
-# Replace variable patterns (e.g., $VAL or ${VAL}) with {VAL}
-def replace_variable_patterns(text) -> str:
-    original = text
-    text = re.sub(r"\$(\w+)", r"{\1}", text)  # Replace $VAL with {VAL}
-    text = re.sub(r"\${(\w+)}", r"{\1}", text)  # Replace ${VAL} with {VAL}
-    print(f"Replaced variable patterns: {original} -> {text}")
-    return text
-
-
 # Main function to convert JSON to .arb
 ja_arb = {}
 en_arb = {}
 
 
 def convert_json_to_arb(input_path):
-    print("Converting JSON to .arb...")
-    json_data = load_json(input_path)
+    ja_arb_json_data = load_json(input_path)
     # i = 0
-    for key in json_data:
-        # i += 1
-        # if i > 30:
-        #     return
-        # arb の value (en_value)については Dart $ → {} の変換で良い
-        # key にはそれ以外の制約もあるので後から value から変換する
-        # Replace variable patterns
-        replaced_key = replace_variable_patterns(key)
+    for ja_key in ja_arb_json_data:
+        if ja_key == "" or ja_key[0] == "@":
+            continue
 
-        # Translate the key using OpenAI's API
-        arb_key = translate_key(replaced_key)
+        ja_value = ja_arb_json_data[ja_key]
+
         # Map to number prefix to with `L_`.
-        arb_key = f"l_{arb_key}" if arb_key[0].isdigit() else arb_key
-        english_value = translate_english_value(replaced_key)
+        english_value = translate_english_value(ja_value)
 
         # Add to the .arb data
-        ja_arb[arb_key] = replaced_key
-        en_arb[arb_key] = english_value
+        en_arb[ja_key] = english_value
 
 
 # Paths to input and output files
-input_json_path = "lib/l10n/app_localizations.json"
-en_arb_path = "lib/l10n/app_en.arb"
 ja_arb_path = "lib/l10n/app_ja.arb"
+en_arb_path = "lib/l10n/app_en.arb"
 
 # Execute the conversion
 if __name__ == "__main__":
-    convert_json_to_arb(input_json_path)
+    convert_json_to_arb(ja_arb_path)
     # Save to .arb file
     save_arb(en_arb_path, en_arb)
-    save_arb(ja_arb_path, ja_arb)
