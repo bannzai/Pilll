6f74226de40a947ea4df5ec4878cc10a5964f5b6
bannzai <kingkong999yhirose@gmail.com>
2024-11-20 20:57:17 +0900

    Add AppLocalizationResolver

--- lib/features/root/root_page.dart
+++ lib/features/root/root_page.dart
@@ -2,6 +2,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:pilll/features/home/home_page.dart';
 import 'package:pilll/features/initial_setting/pill_sheet_group/initial_setting_pill_sheet_group_page.dart';
+import 'package:pilll/features/root/resolver/app_localizations.dart';
 import 'package:pilll/features/root/resolver/firebase_auth_resolver.dart';
 import 'package:pilll/features/root/resolver/force_update.dart';
 import 'package:pilll/features/root/resolver/initial_setting_or_app_page.dart';
@@ -18,25 +19,27 @@ class RootPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ForceUpdate(
-      builder: (_) => FirebaseAuthResolver(
-        builder: (_, user) => UserSetup(
-          userID: user.uid,
-          builder: (_) => Stack(
-            children: [
-              UserStreamResolver(stream: (user) => analyticsDebugIsEnabled = user.analyticsDebugIsEnabled),
-              const SyncDataResolver(),
-              const Migration20240819(),
-              InitialSettingOrAppPage(
-                initialSettingPageBuilder: (_) => ShowPaywallOnAppLaunch(
-                  builder: (_) => SkipInitialSetting(
-                    initialSettingPageBuilder: (context) => InitialSettingPillSheetGroupPageRoute.screen(),
-                    homePageBuilder: (_) => const HomePage(),
+    return AppLocalizationResolver(
+      child: ForceUpdate(
+        builder: (_) => FirebaseAuthResolver(
+          builder: (_, user) => UserSetup(
+            userID: user.uid,
+            builder: (_) => Stack(
+              children: [
+                UserStreamResolver(stream: (user) => analyticsDebugIsEnabled = user.analyticsDebugIsEnabled),
+                const SyncDataResolver(),
+                const Migration20240819(),
+                InitialSettingOrAppPage(
+                  initialSettingPageBuilder: (_) => ShowPaywallOnAppLaunch(
+                    builder: (_) => SkipInitialSetting(
+                      initialSettingPageBuilder: (context) => InitialSettingPillSheetGroupPageRoute.screen(),
+                      homePageBuilder: (_) => const HomePage(),
+                    ),
                   ),
+                  homePageBuilder: (_) => const HomePage(),
                 ),
-                homePageBuilder: (_) => const HomePage(),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
