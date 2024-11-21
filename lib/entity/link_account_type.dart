2c4a0034c50bfe2f80b22f03c9283f0c71c40507
bannzai <kingkong999yhirose@gmail.com>
2024-11-21 12:06:07 +0900

    Fix

--- lib/entity/link_account_type.dart
+++ lib/entity/link_account_type.dart
@@ -1,21 +1,23 @@
+import 'package:pilll/features/root/localization/l.dart';  // Lクラスをインポート
+
 enum LinkAccountType { apple, google }
 
 extension LinkAccountTypeExtension on LinkAccountType {
   String get loginContentName {
     switch (this) {
       case LinkAccountType.apple:
-        return 'Apple';
+        return L.signInWithApple;  // Appleを翻訳
       case LinkAccountType.google:
-        return 'Google';
+        return L.signInWithGoogle;  // Googleを翻訳
     }
   }
 
   String get providerName {
     switch (this) {
       case LinkAccountType.apple:
-        return 'Apple';
+        return 'Apple';  // プロバイダー名は翻訳しない
       case LinkAccountType.google:
-        return 'Google';
+        return 'Google';  // プロバイダー名は翻訳しない
     }
   }
 }
