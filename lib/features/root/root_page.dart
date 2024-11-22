1d8e05e53ec0110ae8c6266ad655d4e5e5717750
bannzai <kingkong999yhirose@gmail.com>
2024-11-22 09:29:06 +0900

    Embed AppLocalizationsResolver

--- lib/features/root/root_page.dart
+++ lib/features/root/root_page.dart
@@ -2,6 +2,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:pilll/features/home/home_page.dart';
 import 'package:pilll/features/initial_setting/pill_sheet_group/initial_setting_pill_sheet_group_page.dart';
+import 'package:pilll/features/localizations/resolver.dart';
 import 'package:pilll/features/root/resolver/firebase_auth_resolver.dart';
 import 'package:pilll/features/root/resolver/force_update.dart';
 import 'package:pilll/features/root/resolver/initial_setting_or_app_page.dart';
@@ -19,32 +20,34 @@ class RootPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ForceUpdate(
-      builder: (_) => FirebaseAuthResolver(
-        builder: (_, user) => UserSetup(
-          userID: user.uid,
-          builder: (_) => Stack(
-            children: [
-              UserStreamResolver(stream: (user) => analyticsDebugIsEnabled = user.analyticsDebugIsEnabled),
-              const SyncDataResolver(),
-              const Migration20240819(),
-              InitialSettingOrAppPage(
-                initialSettingPageBuilder: (_) => ShowPaywallOnAppLaunch(
-                  builder: (_) => SkipInitialSetting(
-                    initialSettingPageBuilder: (context) => InitialSettingPillSheetGroupPageRoute.screen(),
-                    homePageBuilder: (_) => PillSheetAppearanceModeMigrationResolver(
-                      builder: (_) => const HomePage(),
+    return AppLocalizationResolver(builder: (context) {
+      return ForceUpdate(
+        builder: (_) => FirebaseAuthResolver(
+          builder: (_, user) => UserSetup(
+            userID: user.uid,
+            builder: (_) => Stack(
+              children: [
+                UserStreamResolver(stream: (user) => analyticsDebugIsEnabled = user.analyticsDebugIsEnabled),
+                const SyncDataResolver(),
+                const Migration20240819(),
+                InitialSettingOrAppPage(
+                  initialSettingPageBuilder: (_) => ShowPaywallOnAppLaunch(
+                    builder: (_) => SkipInitialSetting(
+                      initialSettingPageBuilder: (context) => InitialSettingPillSheetGroupPageRoute.screen(),
+                      homePageBuilder: (_) => PillSheetAppearanceModeMigrationResolver(
+                        builder: (_) => const HomePage(),
+                      ),
                     ),
                   ),
+                  homePageBuilder: (_) => PillSheetAppearanceModeMigrationResolver(
+                    builder: (_) => const HomePage(),
+                  ),
                 ),
-                homePageBuilder: (_) => PillSheetAppearanceModeMigrationResolver(
-                  builder: (_) => const HomePage(),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
