cfaa7c73876eea20c0facf12ee85315279289e78
bannzai <kingkong999yhirose@gmail.com>
2024-12-11 10:39:08 +0900

    Fix load next

--- lib/features/pill_sheet_modified_history/pill_sheet_modified_history_page.dart
+++ lib/features/pill_sheet_modified_history/pill_sheet_modified_history_page.dart
@@ -17,7 +17,6 @@ class PillSheetModifiedHistoriesPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final loadingNext = useState(false);
     final limit = useState(20);
     final historiesAsync = ref.watch(pillSheetModifiedHistoriesWithLimitProvider(limit: limit.value));
     final histories = historiesAsync.asData?.value ?? [];
@@ -47,8 +46,10 @@ class PillSheetModifiedHistoriesPage extends HookConsumerWidget {
               body: SafeArea(
                 child: NotificationListener<ScrollNotification>(
                   onNotification: (notification) {
-                    if (!loadingNext.value && notification.metrics.pixels >= notification.metrics.maxScrollExtent && histories.isNotEmpty) {
-                      loadingNext.value = true;
+                    if (histories.isNotEmpty &&
+                        histories.length == limit.value &&
+                        notification.metrics.pixels >= notification.metrics.maxScrollExtent) {
+                      print('[DEBUG] LoadNext: limit.value: ${limit.value}');
                       limit.value += 20;
                     }
                     return true;
