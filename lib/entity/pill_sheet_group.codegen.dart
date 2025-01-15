e2a71dfb55763d620de4b9a13e8a0ebc90664228
bannzai <kingkong999yhirose@gmail.com>
2025-01-15 21:55:08 +0900

    Fix

--- lib/entity/pill_sheet_group.codegen.dart
+++ lib/entity/pill_sheet_group.codegen.dart
@@ -350,29 +350,11 @@ extension PillSheetGroupPillNumberDomain on PillSheetGroup {
           final loopOffset = (pillMarkIndex ~/ pillCount);
           debugPrint('loopOffset: $loopOffset');
 
-          // if (pillMark.number <= endPillNumber) {
-          //   debugPrint(
-          //       'continue: index: $pillMarkIndex, pillMark.number <= endPillNumber, pillMark.number: ${pillMark.number}, date: ${pillMark.date}');
-          //   continue;
-          // }
-
-          // final base = pillMarkIndex % pillCount;
-          // debugPrint('base: $base');
-          // if (base == 0) {
-          //   pillMarks[pillMarkIndex] = pillMark.copyWith(number: endPillNumber);
-          // } else {
-          if (loopOffset == 1) {
-            final number = pillMark.number - pillCount;
-            debugPrint('loopOffset == 1: index: $pillMarkIndex, pillMark.number: ${pillMark.number},  number: $number, date: ${pillMark.date}');
-            pillMarks[pillMarkIndex] = pillMark.copyWith(number: number);
-          } else {
-            final countOffset = loopOffset * pillCount;
-            final number = pillMark.number - countOffset;
-            debugPrint(
-                'index: $pillMarkIndex, pillMark.number: ${pillMark.number}, loopOffset: $loopOffset, countOffset: $countOffset, number: $number, date: ${pillMark.date}');
-            pillMarks[pillMarkIndex] = pillMark.copyWith(number: number);
-            // }
-          }
+          final countOffset = loopOffset * pillCount;
+          final number = pillMark.number - countOffset;
+          debugPrint(
+              'index: $pillMarkIndex, pillMark.number: ${pillMark.number}, loopOffset: $loopOffset, countOffset: $countOffset, number: $number, date: ${pillMark.date}');
+          pillMarks[pillMarkIndex] = pillMark.copyWith(number: number);
         }
       }
     }
@@ -427,29 +409,11 @@ extension PillSheetGroupPillNumberDomain on PillSheetGroup {
           final loopOffset = (pillMarkIndex ~/ pillCount);
           debugPrint('loopOffset: $loopOffset');
 
-          // if (pillMark.number <= endPillNumber) {
-          //   debugPrint(
-          //       'continue: index: $pillMarkIndex, pillMark.number <= endPillNumber, pillMark.number: ${pillMark.number}, date: ${pillMark.date}');
-          //   continue;
-          // }
-
-          // final base = pillMarkIndex % pillCount;
-          // debugPrint('base: $base');
-          // if (base == 0) {
-          //   pillMarks[pillMarkIndex] = pillMark.copyWith(number: endPillNumber);
-          // } else {
-          if (loopOffset == 1) {
-            final number = pillMark.number - pillCount;
-            debugPrint('loopOffset == 1: index: $pillMarkIndex, pillMark.number: ${pillMark.number},  number: $number, date: ${pillMark.date}');
-            pillMarks[pillMarkIndex] = pillMark.copyWith(number: number);
-          } else {
-            final countOffset = loopOffset * pillCount;
-            final number = pillMark.number - countOffset;
-            debugPrint(
-                'index: $pillMarkIndex, pillMark.number: ${pillMark.number}, loopOffset: $loopOffset, countOffset: $countOffset, number: $number, date: ${pillMark.date}');
-            pillMarks[pillMarkIndex] = pillMark.copyWith(number: number);
-            // }
-          }
+          final countOffset = loopOffset * pillCount;
+          final number = pillMark.number - countOffset;
+          debugPrint(
+              'index: $pillMarkIndex, pillMark.number: ${pillMark.number}, loopOffset: $loopOffset, countOffset: $countOffset, number: $number, date: ${pillMark.date}');
+          pillMarks[pillMarkIndex] = pillMark.copyWith(number: number);
         }
       }
     }
