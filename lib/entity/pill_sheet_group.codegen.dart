89708ac11f96ad69f3d9001be38b952504061358
bannzai <kingkong999yhirose@gmail.com>
2025-02-11 17:33:18 +0900

    Extract PillSheetAppearanceMode to other file

--- lib/entity/pill_sheet_group.codegen.dart
+++ lib/entity/pill_sheet_group.codegen.dart
@@ -4,6 +4,7 @@ import 'package:pilll/entity/firestore_timestamp_converter.dart';
 import 'package:pilll/entity/pill_sheet.codegen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:pilll/entity/pill_sheet_appearance_mode.codegen.dart';
 import 'package:pilll/entity/pill_sheet_type.dart';
 import 'package:pilll/entity/setting.codegen.dart';
 import 'package:pilll/features/localizations/l.dart';
