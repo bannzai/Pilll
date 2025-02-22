fada044b14e118dcb9a338f4f08d4b0e5a78dd8c
bannzai <kingkong999yhirose@gmail.com>
2025-02-22 12:53:00 +0900

    Commentout debugPrint

--- lib/entity/pill_sheet_group.codegen.dart
+++ lib/entity/pill_sheet_group.codegen.dart
@@ -323,23 +323,23 @@ extension PillSheetGroupPillNumberDomain on PillSheetGroup {
       final endPillNumber = displayNumberSetting.endPillNumber;
       if (endPillNumber != null && endPillNumber > 0) {
         final pillCount = endPillNumber - (beginPillNumber ?? 1) + 1;
-        debugPrint('endPillNumber: $endPillNumber, beginPillNumber: $beginPillNumber, pillCount: $pillCount');
+        // debugPrint('endPillNumber: $endPillNumber, beginPillNumber: $beginPillNumber, pillCount: $pillCount');
         for (final (pillMarkIndex, pillMark) in pillMarks.indexed) {
-          debugPrint('--------------------');
+          // debugPrint('--------------------');
           final loopOffset = (pillMarkIndex ~/ pillCount);
-          debugPrint('loopOffset: $loopOffset');
+          // debugPrint('loopOffset: $loopOffset');
 
           final countOffset = loopOffset * pillCount;
           final number = pillMark.number - countOffset;
-          debugPrint(
-              'index: $pillMarkIndex, pillMark.number: ${pillMark.number}, loopOffset: $loopOffset, countOffset: $countOffset, number: $number, date: ${pillMark.date}');
+          // debugPrint(
+          //     'index: $pillMarkIndex, pillMark.number: ${pillMark.number}, loopOffset: $loopOffset, countOffset: $countOffset, number: $number, date: ${pillMark.date}');
           pillMarks[pillMarkIndex] = pillMark.copyWith(number: number);
         }
       }
     }
 
-    debugPrint('_pillNumbersForSequential:pillMarks');
-    debugPrint(pillMarks.map((e) => '${e.date} ${e.number}').join('\n'));
+    // debugPrint('_pillNumbersForSequential:pillMarks');
+    // debugPrint(pillMarks.map((e) => '${e.date} ${e.number}').join('\n'));
 
     return pillMarks;
   }
