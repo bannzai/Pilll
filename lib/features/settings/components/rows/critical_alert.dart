3451d984988b003b775a4b689cbcdc6d2a49dff3
bannzai <kingkong999yhirose@gmail.com>
2025-04-15 18:05:17 +0900

    Fix

--- lib/features/settings/components/rows/critical_alert.dart
+++ lib/features/settings/components/rows/critical_alert.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/entity/setting.codegen.dart';
 import 'package:pilll/features/localizations/l.dart';
 import 'package:pilll/features/settings/critical_alert/page.dart';
 import 'package:pilll/utils/analytics.dart';
 
 class CriticalAlert extends HookConsumerWidget {
-  const CriticalAlert({super.key});
+  final Setting setting;
+  const CriticalAlert({super.key, required this.setting});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ListTile(
       title: Text(
-        '緊急アラート',
+        L.enableNotificationInSilentMode,
         style: const TextStyle(
-          fontFamily: FontFamily.roboto,
+          fontFamily: FontFamily.japanese,
           fontWeight: FontWeight.w300,
           fontSize: 16,
+        ),
+      ),
+      subtitle: Text(
+        L.silentModeNotificationDescription,
+        style: const TextStyle(
+          fontFamily: FontFamily.japanese,
+          fontWeight: FontWeight.w300,
+          fontSize: 14,
         ),
       ),
       trailing: const Icon(Icons.chevron_right),
       contentPadding: const EdgeInsets.fromLTRB(14, 4, 6, 0),
       onTap: () {
         analytics.logEvent(
-          name: 'did_select_emergency_alert_notification',
+          name: 'did_select_critical_alert_notification',
         );
-        Navigator.of(context).push(CriticalAlertPage.route());
+        Navigator.of(context).push(CriticalAlertPageRoutes.route(setting: setting));
       },
     );
   }
