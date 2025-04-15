3108efd8e160513fcfcdce42467eb83f2397d036
bannzai <kingkong999yhirose@gmail.com>
2025-04-15 16:42:36 +0900

    Add emeregency alert notification component

---
+++ lib/features/settings/components/rows/emergency_alert_notification.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class EmergencyAlertNotification extends HookConsumerWidget {
+  const EmergencyAlertNotification({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const SizedBox();
+  }
+}
