bb2e634f1db244f678efd3b0a9efb551cb1c4bd6
bannzai <kingkong999yhirose@gmail.com>
2025-05-26 11:59:21 +0900

    Fix logout row

--- lib/features/settings/setting_account_link/components/logout.dart
+++ lib/features/settings/setting_account_link/components/logout.dart
@@ -5,12 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/features/localizations/l.dart';
+import 'package:pilll/provider/auth.dart';
 import 'package:pilll/utils/analytics.dart';
 import 'package:pilll/utils/auth/apple.dart';
 import 'package:pilll/utils/auth/google.dart';
 import 'package:pilll/components/atoms/button.dart';
 import 'package:pilll/components/atoms/font.dart';
-import 'package:pilll/components/atoms/text_color.dart';
 import 'package:pilll/components/page/discard_dialog.dart';
 import 'package:pilll/features/error/error_alert.dart';
 import 'package:pilll/utils/error_log.dart';
@@ -24,6 +24,17 @@ class Logout extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final cancelReminderLocalNotification = ref.watch(cancelReminderLocalNotificationProvider);
+    final isAppleLinked = ref.watch(isAppleLinkedProvider);
+    final isGoogleLinked = ref.watch(isGoogleLinkedProvider);
+    final firebaseUser = ref.watch(firebaseUserStateProvider);
+    final firebaseUserValue = firebaseUser.valueOrNull;
+    if (firebaseUserValue == null) {
+      return const SizedBox.shrink();
+    }
+    if (firebaseUserValue.isAnonymous) {
+      return const SizedBox.shrink();
+    }
+
     return ListTile(
       title: Text(
         L.logout,
@@ -55,8 +66,10 @@ class Logout extends HookConsumerWidget {
                 await (
                   _logout(
                     context,
+                    isAppleLinked: isAppleLinked,
+                    isGoogleLinked: isGoogleLinked,
                   ),
-                  cancelReminderLocalNotification()
+                  cancelReminderLocalNotification(),
                 ).wait;
               },
             ),
@@ -66,9 +79,55 @@ class Logout extends HookConsumerWidget {
     );
   }
 
-  Future<void> _logout(BuildContext context) async {
-    (await SharedPreferences.getInstance()).setBool(BoolKey.didEndInitialSetting, false);
-    await CancelReminderLocalNotification().call();
-    await FirebaseAuth.instance.signOut();
+  Future<void> _logout(BuildContext context, {required bool isAppleLinked, required bool isGoogleLinked}) async {
+    try {
+      (await SharedPreferences.getInstance()).setBool(BoolKey.didEndInitialSetting, false);
+      await CancelReminderLocalNotification().call();
+      await FirebaseAuth.instance.signOut();
+    } on FirebaseAuthException catch (e, stackTrace) {
+      if (e.code == 'requires-recent-login') {
+        showDiscardDialog(
+          // ignore: use_build_context_synchronously
+          context,
+          title: L.doReLogin,
+          message: L.reLoginMessage,
+          actions: [
+            AlertButton(
+              text: L.cancel,
+              onPressed: () async {
+                Navigator.of(context).pop();
+              },
+            ),
+            AlertButton(
+              text: L.reLogin,
+              onPressed: () async {
+                final navigator = Navigator.of(context);
+                if (isAppleLinked) {
+                  await appleReauthentification();
+                } else if (isGoogleLinked) {
+                  await googleReauthentification();
+                } else {
+                  errorLogger.log('it is not cooperate account');
+                  exit(1);
+                }
+                navigator.pop();
+                await _logout(
+                  // ignore: use_build_context_synchronously
+                  context,
+                  isAppleLinked: isAppleLinked,
+                  isGoogleLinked: isGoogleLinked,
+                );
+              },
+            ),
+          ],
+        );
+      } else {
+        errorLogger.recordError(e, stackTrace);
+        if (context.mounted) showErrorAlert(context, e);
+      }
+    } catch (e, stackTrace) {
+      errorLogger.recordError(e, stackTrace);
+      if (context.mounted) showErrorAlert(context, e);
+    }
   }
 }
