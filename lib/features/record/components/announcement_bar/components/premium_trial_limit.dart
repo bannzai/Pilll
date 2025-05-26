307d1c71e72459270663eb1ccf8b3c35967960a0
bannzai <kingkong999yhirose@gmail.com>
2025-05-27 05:30:10 +0900

    Remove unnecessary code

--- lib/features/record/components/announcement_bar/components/premium_trial_limit.dart
+++ lib/features/record/components/announcement_bar/components/premium_trial_limit.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pilll/entity/user.codegen.dart';
 import 'package:pilll/features/localizations/l.dart';
