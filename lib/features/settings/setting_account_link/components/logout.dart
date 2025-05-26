601725d77be2d3c9ab1e7f9ea786d7eeb274f23c
bannzai <kingkong999yhirose@gmail.com>
2025-05-26 11:55:07 +0900

    WIP add logout row

---
+++ lib/features/settings/setting_account_link/components/logout.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pilll/components/atoms/color.dart';
+import 'package:pilll/features/localizations/l.dart';
+import 'package:pilll/utils/analytics.dart';
+import 'package:pilll/utils/auth/apple.dart';
+import 'package:pilll/utils/auth/google.dart';
+import 'package:pilll/components/atoms/button.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/components/atoms/text_color.dart';
+import 'package:pilll/components/page/discard_dialog.dart';
+import 'package:pilll/features/error/error_alert.dart';
+import 'package:pilll/utils/error_log.dart';
+import 'package:pilll/utils/local_notification.dart';
+import 'package:pilll/utils/shared_preference/keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Logout extends HookConsumerWidget {
+  const Logout({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cancelReminderLocalNotification = ref.watch(cancelReminderLocalNotificationProvider);
+    return ListTile(
+      title: Text(
+        L.logout,
+        style: const TextStyle(
+          color: AppColors.danger,
+          fontSize: 16,
+          fontFamily: FontFamily.japanese,
+        ),
+      ),
+      trailing: const Icon(Icons.delete, color: AppColors.danger),
+      horizontalTitleGap: 4,
+      onTap: () async {
+        showDiscardDialog(
+          context,
+          title: L.logout,
+          message: L.goToInitialSettingAndReloginIfNeededMessage,
+          actions: [
+            AlertButton(
+              text: L.cancel,
+              onPressed: () async {
+                analytics.logEvent(name: 'cancel_delete_user');
+                Navigator.of(context).pop();
+              },
+            ),
+            AlertButton(
+              text: L.withdraw,
+              onPressed: () async {
+                analytics.logEvent(name: 'pressed_delete_user_button');
+                await (
+                  _logout(
+                    context,
+                  ),
+                  cancelReminderLocalNotification()
+                ).wait;
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _logout(BuildContext context) async {
+    (await SharedPreferences.getInstance()).setBool(BoolKey.didEndInitialSetting, false);
+    await CancelReminderLocalNotification().call();
+    await FirebaseAuth.instance.signOut();
+  }
+}
