d9ac3bea554b79e5128319aaff6446561f31a041
bannzai <kingkong999yhirose@gmail.com>
2025-06-14 10:13:02 +0900

    Remove unnecessary import

--- lib/features/premium_introduction/components/lifetime_purchase_button.dart
+++ lib/features/premium_introduction/components/lifetime_purchase_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:pilll/components/atoms/color.dart';
 import 'package:pilll/components/atoms/font.dart';
 import 'package:pilll/components/atoms/text_color.dart';
