9a6aa135fbed42b01085723b9a763529f3b46e43
bannzai <star.kojiki@gmail.com>
2025-09-18 04:16:12 +0900

    Remove unnecessary import

--- lib/utils/push_notification.dart
+++ lib/utils/push_notification.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:pilll/provider/user.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
