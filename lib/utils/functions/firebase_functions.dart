5277f2ddb1a95db1a3372d1039f87be32b15f6e8
bannzai <star.kojiki@gmail.com>
2025-12-19 05:15:08 +0900

    feat: Firebase Functions経由でお問い合わせを送信するcreateInquiryメソッドを追加

---
+++ lib/utils/functions/firebase_functions.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:pilll/entity/inquiry.codegen.dart';
+
+final functions = FirebaseFunctions.instanceFor(region: 'asia-northeast1');
+
+extension FirebaseFunctionsExt on FirebaseFunctions {
+  Future<String> createInquiry({
+    required InquiryType inquiryType,
+    String? otherTypeText,
+    required String email,
+    required String content,
+    required String debugInfo,
+  }) async {
+    final result = await httpsCallable('inquiry').call({
+      'inquiryType': inquiryType.name,
+      if (otherTypeText != null) 'otherTypeText': otherTypeText,
+      'email': email,
+      'content': content,
+      'debugInfo': debugInfo,
+    });
+    final response = mapToJSON(result.data);
+    return response['inquiryId'] as String;
+  }
+
+  Future<bool> startPromotion({required int promotionDayCount}) async {
+    final result = await httpsCallable('startPromotion').call(
+      {
+        'promotionDayCount': promotionDayCount,
+      },
+    );
+    final response = mapToJSON(result.data);
+    if (response['result'] != 'OK') {
+      throw Exception(response['error']['message']);
+    }
+    return response['data']['isAlreadyExist'] as bool;
+  }
+}
+
+// Map<String, dynamic>.fromだけだとネストした子要素が_Map<Object? Object?>のままになる
+// 以下のエラーを回避する _TypeError (type '_Map<Object?, Object?>' is not a subtype of type 'Map<String, dynamic>' in type cast)
+Map<String, dynamic> mapToJSON(Map<dynamic, dynamic> map) {
+  for (var key in map.keys) {
+    if (map[key] is Map) {
+      map[key] = mapToJSON(map[key]);
+    } else if (map[key] is List) {
+      map[key] = map[key].map((e) {
+        if (e is Map) {
+          return mapToJSON(e);
+        }
+        return e;
+      }).toList();
+    }
+  }
+  return Map<String, dynamic>.from(map);
+}
