7d4f0326f6466d93576833ed5e2a1031539e2280
bannzai <star.kojiki@gmail.com>
2026-01-07 12:18:44 +0900

    feat(announcement_bar): Add LifetimeSubscriptionWarningAnnouncementBar component
    買い切りプラン購入者向けサブスク解約促進バーを追加
    - 閉じるボタンで永続的に非表示
    - タップでOS別のサブスクリプション管理ページへ遷移
      - iOS: apps.apple.com/account/subscriptions
      - Android: play.google.com/store/account/subscriptions

---
+++ lib/features/record/components/announcement_bar/components/lifetime_subscription_warning.dart
@@ -0,0 +1,85 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pilll/components/atoms/color.dart';
+import 'package:pilll/components/atoms/font.dart';
+import 'package:pilll/features/localizations/l.dart';
+import 'package:pilll/utils/analytics.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LifetimeSubscriptionWarningAnnouncementBar extends HookConsumerWidget {
+  final ValueNotifier<bool> isClosed;
+
+  const LifetimeSubscriptionWarningAnnouncementBar({
+    super.key,
+    required this.isClosed,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      analytics.logEvent(name: 'lifetime_subscription_warning_viewed');
+      return null;
+    }, []);
+
+    return Container(
+      color: AppColors.secondary,
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      child: GestureDetector(
+        onTap: () {
+          analytics.logEvent(name: 'lifetime_subscription_warning_tapped');
+          _openSubscriptionManagementPage();
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              child: const Icon(
+                Icons.close,
+                color: Colors.white,
+                size: 24,
+              ),
+              onTap: () {
+                analytics.logEvent(name: 'lifetime_subscription_warning_closed');
+                isClosed.value = true;
+              },
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Text(
+                L.lifetimeSubscriptionWarning,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 11,
+                  fontFamily: FontFamily.japanese,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+                maxLines: 3,
+              ),
+            ),
+            const SizedBox(width: 10),
+            SvgPicture.asset(
+              'images/arrow_right.svg',
+              colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+              height: 20,
+              width: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _openSubscriptionManagementPage() {
+    if (Platform.isIOS) {
+      launchUrl(Uri.parse('https://apps.apple.com/account/subscriptions'));
+    } else if (Platform.isAndroid) {
+      launchUrl(Uri.parse('https://play.google.com/store/account/subscriptions'));
+    }
+  }
+}
