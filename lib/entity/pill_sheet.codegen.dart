42691cc247383867541d324266fdf279274d7fee
bannzai <star.kojiki@gmail.com>
2026-01-11 09:51:35 +0900

    fix: isPillDisabledを明日以降のピルのみ無効にするよう変更
    前日のピルが未完了かどうかのチェックを削除し、
    明日以降（todayPillNumberより後）のピルのみをdisabledにするように変更。
    これによりv2でもタップした日付まで一括で服用記録できるようになる。

--- lib/entity/pill_sheet.codegen.dart
+++ lib/entity/pill_sheet.codegen.dart
@@ -501,24 +501,11 @@ extension PillSheetV2Extension on PillSheetV2 {
     return pills[todayPillIndex].pillTakens.isNotEmpty;
   }
 
-  /// 指定したピル番号のピルが、前日のピルが未完了のため押せない状態かどうか
-  /// 前日のピルが未完了（0回でも）の場合はtrue
+  /// 指定したピル番号のピルがタップ不可かどうか
+  /// 明日以降（未来）のピルはタップ不可
   bool isPillDisabled({required int pillNumberInPillSheet}) {
-    final pillIndex = pillNumberInPillSheet - 1;
-    // 1番目のピルは前日がないのでdisabledにならない
-    if (pillIndex <= 0) {
-      return false;
-    }
-    // 既に完了済みのピルはdisabledにならない
-    if (pillIndex < pills.length && pills[pillIndex].isCompleted) {
-      return false;
-    }
-    // 前日のピルが未完了ならdisabled
-    final previousPillIndex = pillIndex - 1;
-    if (previousPillIndex >= 0 && previousPillIndex < pills.length) {
-      return !pills[previousPillIndex].isCompleted;
-    }
-    return false;
+    // 明日以降のピルはdisabled
+    return pillNumberInPillSheet > todayPillNumber;
   }
 
   /// pillsリストの一部を置き換えたリストを返す
