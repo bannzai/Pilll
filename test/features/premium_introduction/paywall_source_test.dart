a88a5edb265d6ebb990721e29c1fa6f722b1a31e
bannzai <star.kojiki@gmail.com>
2026-05-01 08:01:31 +0900

    test: PaywallSource の value ユニーク性 / snake_case テストを削除
    value は switch 文で明示列挙されているため、enum の使い手から見ればユニーク性も snake_case 形式も自明。テストとしての価値が低いので削除。

--- test/features/premium_introduction/paywall_source_test.dart
+++ test/features/premium_introduction/paywall_source_test.dart
@@ -3,18 +3,6 @@ import 'package:pilll/features/premium_introduction/paywall_source.dart';
 
 void main() {
   group('#PaywallSourceFunction', () {
-    test('全 PaywallSource の value がユニーク', () {
-      final values = PaywallSource.values.map((e) => e.value).toList();
-      expect(values.toSet().length, values.length, reason: '重複: $values');
-    });
-
-    test('全 PaywallSource の value が snake_case', () {
-      final pattern = RegExp(r'^[a-z][a-z0-9_]*$');
-      for (final source in PaywallSource.values) {
-        expect(pattern.hasMatch(source.value), isTrue, reason: source.value);
-      }
-    });
-
     test('全 PaywallSource の value が Firebase Analytics の40字制限内', () {
       for (final source in PaywallSource.values) {
         expect(source.value.length, lessThanOrEqualTo(40), reason: source.value);
